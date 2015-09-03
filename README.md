a3d8d3ce58b11c376028d137ea55c319b9b55cd5
r-alex-hall <artdev@inboxalias.com>
2015-09-02 20:15:13 -0600

    Upgraded from repository of font development scripts to a repository of /bin tools and my scripts usable for a wide variety of projects. Renamed project from fontDevTools to _devbin.
    Former-commit-id: b77143f8919de3f172f1a1c6028148c31bed6dbe

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-fontDevTools
-============
+I believe that all of the utilities in this folder and subfolder are at least freely available and open source. If you are the copyright owner of anything in this folder and wish for it to be removed, please contact me and I will do so.
 
-Tools (e.g. console scripts to run tools) for font development.
+http://earthbound.io/contact
+09/02/2015 07:29:00 PM -RAH
 
-The tools these rely on are not provided in this repository, but you may find them freely available on the world wide web. 08/13/2014 06:32:41 PM -RAH
+INSTALLATION AND USAGE:
+
+Note that these are all Windows utilities.
+
+Make a junction link to this directory, pointed to e.g. from one C:\devtools\_devbin, and put that same path in the system %PATH% (along with any other utility or batch folders in this repository that you will use).
+
+To create a junction link, see:http://schinagl.priv.at/nt/hardlinkshellext/linkshellextension.html
+
+As these tools and scripts etc. can be in flux (meaning I edit and develop them as I use them, and/or I freely add or remove utilities from this archive.
+
+This includes zeranoe's build of ffmpeg, AutoHotkey, and gnu core utilities for windows 32-bit.
---
+++ bat/CPP__text-to-cout.bat
@@ -0,0 +1,41 @@
+ECHO OFF
+REM THIS WINDOWS CONSOLE BATCH assists in transforming a source text into a c/c++ string. It converts "unescaped" characters into escape sequences, surrounds the string on each line of a text with double-quotes, broken into a legible multi-line "string" << "string" operator format. It does not do this for the start of the text or end--you must do those manually.
+
+REM TO USE THIS BATCH, 1) Copy this batch file to the same directory as sfk.exe (Swiss File Army Knife--a freely available tool), and rename it with the .bat extension. 2) Create or open a plain-text file in the same directory, named white_space_fixup_scratchpad.txt, and copy and paste into that file (replacing all contents in it) from any source you may want to "fix up" -- e.g. from a .txt, or .cpp file--anything in plain-text format. 3) Execute this batch file. It will alter the contents of said text file per preferences. 4) Copy/paste the fixed-up text to wherever else you want to use it. 4B) NOTE: You may want to first backup whatever source you're fixing up, in case this tool turns out undesireable results.
+
+REM The mentioned (brilliant!) Swiss File Army Knife (sfk.exe) tool may be freely obtained from: http://sourceforge.net/projects/swissfileknife -- note that it is multi-platform, so this batch might easily be adapted for e.g. GNU/Linux or Mac.
+
+IF [%1] NEQ [] SET FILENAME=%1%
+IF [%1]==[] SET FILENAME=scratchpad.txt
+
+ECHO Will attempt to operate on FILENAME = %FILENAME% . . .
+
+IF NOT EXIST %FILENAME% (
+ECHO FILENAME %FILENAME% was not found.
+EXIT /B
+)
+
+REM Re: http://en.cppreference.com/w/cpp/language/escape
+REM Check it out. I have to use escape sequences for sfk.exe in this batch--some for sfk.exe, and some for the windows consoel--to convert characters to a different escape sequence encoding. Meta!
+
+REM TIMEOUT /T 1
+
+REM GUESS WHAT?! If we don't escape all the backslashes first, it will escape all the escape characters that were escaped before the backslash. Error! Avoid!
+REM backslash 	byte 0x5c		\	:
+sfk.exe rep %FILENAME% -yes				/\/\\/
+
+REM single quote 	byte 0x27	'	:
+sfk.exe rep %FILENAME% -yes				/'/\'/
+
+REM double quote 	byte 0x22	"	:
+sfk.exe rep %FILENAME% -yes				/\"/\\\"/
+REM question mark 	byte 0x3f	?	:
+sfk.exe rep %FILENAME% -yes				/?/\?/
+REM horizontal tab 	byte 0x09	\t	:
+sfk.exe rep %FILENAME% -yes -spat		/\t/\\t/
+REM Break lines (hard returns) apart into the format:
+REM 	<< "string\n"
+REM 	<< "string\n"
+sfk.exe rep %FILENAME% -spat -yes "/\r/\\n\"\r\t^<^< \"/"
+
+REM TODO: See also sfk filter -wrap; to split a line of text into the console width!
