1c9ad835537138ccb2f6ce0c2fe1fba9f2428bbc
Alex Hall <artdev@inboxalias.com>
2016-04-02 15:04:38 -0600

    reduced imgs number in FilterForgeBatchWrapper/scrImg, additions to python Backus-Naur poetry generator
    Former-commit-id: 7e4427730aed40a29df42742dc89ba443bf7cd98

--- scripts/imgAndVideo/EXIFdataBatch/prepImageMetaData.sh
+++ scripts/imgAndVideo/EXIFdataBatch/prepImageMetaData.sh
@@ -7,7 +7,10 @@
 # GIBBERISH OR OTHER GENERATED CAPTION insertion path option:
 # NOTE that the two below differ in giving a slash at the end.
 # Between the following double quotes, paste the Windows path to your caption source files:
-captionSearchPath=`cygpath -u "D:\Alex\gibberish\IAE_generatedGibberish\pyMarkovGibbGen\edited"`
+captionSearchPathOne=`cygpath -u "D:\Alex\gibberish\IAE_generatedGibberish\pyMarkovGibbGen\edited"`
+# TO DO: PENDING: Also (for another path):
+# captionSearchPathTwo=`cygpath -u "D:\Alex\gibberish\IAE_generatedGibberish\pyMarkovGibbGen\edited"`
+
 # Custom pre-prepared template metadata to combine with gibberish/caption source metadata; path and file name;
 # Between the following double quotes, paste the Windows path to your custom image metadata template file:
 metaDataTemplatePath=`cygpath -u "D:\Alex\Programming\_devtools\scripts\imgAndVideo\EXIFdataBatch"`
@@ -44,20 +47,20 @@ do
 		# ========== BEGIN GIBBERISH CAPTION CREATE ==========
 			# search a gibbersource path--copy and paste into the following double quote marks to set (will convert to cygwin path):
 				# DEPRECATED development code; moved earlier in this script after testing:
-				# captionSearchPath=`cygpath -u "D:\Alex\gibberish\IAE_generatedGibberish\pyMarkovGibbGen\edited"`
+				# captionSearchPathOne=`cygpath -u "D:\Alex\gibberish\IAE_generatedGibberish\pyMarkovGibbGen\edited"`
 			# List all files in caption search path into temp.txt, then prune it down to only .txt files:
-		ls $captionSearchPath/*.txt > temp.txt
+		ls $captionSearchPathOne/*.txt > temp.txt
 		sed -i '/\(\.txt\)/!d' temp.txt
 			# Put that into an array:
 		mapfile -t gibberishTextFilenamesArray < temp.txt
 			# BUG found: if you use a move command that writes the moved file into a folder that did not exist before the move command was issued, cygwin apparently cannot see the newly created folder. If instead you issue a mkdir command beforehand, cygwin will see the new folder and move the file into it properly. Ergo the necessity of the following command block (if else because mkdir throws an error if the folder already exists) :
-		if [ -a $captionSearchPath/used ]
+		if [ -a $captionSearchPathOne/used ]
 			then
 				# echo A used directory already exists so I will not create it.
 				# Do nothing.
 				derp=dur
 			else
-				mkdir $captionSearchPath/used
+				mkdir $captionSearchPathOne/used
 				echo Created used subdirectory because it did not exist.
 		fi
 		if [ -a ${gibberishTextFilenamesArray[0]} ]
@@ -80,11 +83,11 @@ do
 				cat header.txt ${gibberishTextFilenamesArray[0]} > catHeaderGibText.txt
 				rm header.txt
 				mv catHeaderGibText.txt "${gibberishTextFilenamesArray[0]}"
-				mv "${gibberishTextFilenamesArray[0]}" $captionSearchPath/used/$imageFileNameNoExt-caption.txt
+				mv "${gibberishTextFilenamesArray[0]}" $captionSearchPathOne/used/$imageFileNameNoExt-caption.txt
 				# metaDataAdditionsTextFile
 # TO DO: prepend to the gibberish source text file a note about which file the EXIF data was inserted into.
 			else
-				echo no text file found in $captionSearchPath. Aborting script.
+				echo no text file found in $captionSearchPathOne. Aborting script.
 				exit
 		fi				# This block done, 03/19/2016 01:12:10 AM -RAH
 		# ========== END GIBBERISH CAPTION CREATE ==========
