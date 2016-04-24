e0807ac572807305aa14ee0f97f4c0649da596a8
artdev@inboxalias.com <TPug7ZI*23ARaRYEcXD>
2016-04-24 09:52:14 -0600

    prepImageMetaData.sh dev, test imgs reduced.
    Former-commit-id: 4ef2a055ac7f7973d0b1f7c80370720d711da1fe

--- scripts/imgAndVideo/EXIFdataBatch/prepImageMetaData.sh
+++ scripts/imgAndVideo/EXIFdataBatch/prepImageMetaData.sh
@@ -1,16 +1,8 @@
-# IF NOT EXIST __metadataAdditions MKDIR __metadataAdditions
+# IN DEVELOPMENT
+# MAY NOT bE PRODUCTION READY
 
-# TO DO: make this adaptable for situations without label tags; e.g. no "abstraction" word in the file name--via switches?
-
-# NOTE: This will fail to create an __metadataAdditions subfolder in the root folder from which this script is run (if that would be expected), but in my workflow that should never be expected.
-
-
-# Custom pre-prepared template metadata to combine with gibberish/caption source metadata; path and file name;
-# Between the following double quotes, paste the Windows path to your custom image metadata template file:
 metaDataTemplatePath=`cygpath -u "C:\_devtools\scripts\imgAndVideo\EXIFdataBatch"`
-# For the following variable, give the file name of your custom image metadata template file:
 metaDataTemplateFile=customImageMetadataTemplate.txt
-# metaDataTemplate=$metaDataTemplatePath/$metaDataTemplateFile
 metaDataTemplate=$metaDataTemplatePath/$metaDataTemplateFile
 
 find . -iname \*_FINAL*.tif > imagesMetadataPrepList.txt
@@ -20,9 +12,11 @@ sort imagesMetadataPrepList.txt > temp1.txt
 uniq temp1.txt > temp2.txt
 rm imagesMetadataPrepList.txt temp1.txt
 mv temp2.txt imagesMetadataPrepList.txt
+# exit
 	# dev reference; for to do the preceding line but with many differtent filea tyeawps:
 	# find . -iname \*.tif -o -iname \*.tiff -o -iname \*.png -o -iname \*.psd -o -iname \*.ora -o -iname \*.rif -o -iname \*.riff -o -iname \*.jpg -o -iname \*.jpeg -o -iname \*.gif -o -iname \*.bmp -o -iname \*.cr2 -o -iname \*.raw  -o -iname \*.crw -o -iname \*.pdf > imageFilesList.txt
-
+echo start . . .
+currdir=`pwd`
 mapfile -t imagesMetadataPrepArray < imagesMetadataPrepList.txt
 for element in "${imagesMetadataPrepArray[@]}"
 do
@@ -32,20 +26,27 @@ do
 				# if [ -a $imagePath/_metadataAdditions ]
 	imagePath=`expr match "$element" '\(.*\)\/.*'`
 	imageFileNameNoExt=`expr match "$element" '.*\/\(.*\)\..*'`
-	metaDataAdditionsTextFile=$imagePath/$imageFileNameNoExt.MD_ADDS.txt
+	metaDataAdditionsTextFile=$imagePath/$imageFileNameNoExt\_MD_ADDS.txt
 	if [ -a $imagePath/$metaDataAdditionsTextFile ]
 	then
-			# DO NOT OVERWRITE OR APPEND TO AN EXISTING FILE--and I'd think that for this control block, by removing the -a and moving else to then it would work the same way, but no. Also, this will break unless there is any actual statement to execute after that then clause (then, there, yet).
-		echo $imagePath/$metaDataAdditionsTextFile already exists so I will not alter it. If you mean to recreate that file then delete it and run this script again.
-	else
-				# DEPRECATED, for same reasons noted in earlier similar comment:
-				# mkdir $imagePath/_metadataAdditions
+		echo CREATE METADATA PREP FILE $currdir\/$metaDataAdditionsTextFile\, as it does not already exist.
+# CONTINUE DEBUGGING HERE
 			# Create stub metadata file using filename for title and metadata template:
-		echo Title=\"$imageFileNameNoExt\" > temp.txt
+echo Title=\"$imageFileNameNoExt\" > temp.txt
 		# BUT for flikr, TITLE field will be filled by IPTC "object name--" does it seriously ignore EXIF Title field?! ;
-		cat temp.txt $metaDataTemplate > $metaDataAdditionsTextFile
+cat temp.txt $metaDataTemplate > $metaDataAdditionsTextFile
+rm temp.txt
 		# Else the intended tag for a next line gets munged into the same line:
-		printf "\n" >> $metaDataAdditionsTextFile
+printf "\n" >> $metaDataAdditionsTextFile
+		# To open the metadata prep file and corresponding image file in the default programs, to make any necessary metadata prep. changes:
+# cygstart $element
+echo "$currdir/$metaDataAdditionsTextFile"
+		# read -n 1 "Opening metadata prep text file and corresponding image for reference. Edit and save the prep text file as necessary, then press any key to continue."
+	else
+		# DO NOT OVERWRITE THE EXISTING FILE--and I'd think that for this control block, by removing the -a and moving else to then it would work the same way, but no. Also, this will break unless there is any actual statement to execute after that then clause (then, there, yet).
+		der=duh
+		echo METADATA PREP FILE $imagePath/$metaDataAdditionsTextFile ALREADY EXISTS\, so I will not alter it. If you mean to recreate that file then delete it and run this script again.
+		# continue
 	fi
 done
 
