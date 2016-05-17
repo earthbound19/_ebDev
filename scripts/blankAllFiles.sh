584cb6a18cad9066c877653d6f36e07232a941cc
Alex Hall <artdev@inboxalias.com>
2016-05-17 12:31:51 -0600

    blankAllFiles.sh fix; imgAndVideo scripts dev snapshot
    Former-commit-id: 4593f2f80a7640ae35d0d881e88dc32a2d4d5452

--- scripts/blankAllFiles.sh
+++ scripts/blankAllFiles.sh
@@ -6,16 +6,19 @@
 	# List all files matching a specific date file name pattern to a file:to a text file
 
 # !!===============DANGER ZONE===============!!
-thisDir=`pwd`
-find $thisDir/ -type f > allFiles.txt
-while read p
-do
-	rm "$p"
-	dd count=0 if=/dev/zero of="$p"
-done < allFiles.txt
-rm allFiles.txt
+
+# thisDir=`pwd`
+# find "$thisDir/" -type f > ../allFiles.txt
+# while read p
+# do
+	# rm "$p"
+	# dd count=0 if=/dev/zero of="$p"
+# done < ../allFiles.txt
+# rm allFiles.txt
+
 # !!===============DANGER ZONE===============!!
 
 # Version history:
 # Wrote this script, which it turns out has a bug [unknown when].
 # Updated with a much simpler command to empty files (it didn't always work before now; now it does). 02/21/2016 08:20:22 PM PM -RAH
+# Bugfix: would't work with paths that have space characters. Also, temp list file is in parent dir to avoid clobbering it. ALSO, the dangerous block was not commented out by default. 05/17/2016 04:03:29 AM -RAH
