6e754ab336d4e83e912466cf22b0b80bd8ac93d3
Alex Hall <artdev@inboxalias.com>
2016-05-31 07:12:41 -0600

    png/bmp/svg conversion scripts, switch to magick.exe instead of renamed imagemagick_convert, other, minor changes
    Former-commit-id: 337050008d36706f82409370896d6397e1c11cec

---
+++ scripts/imgAndVideo/cropAllPNG2BMP.sh
@@ -0,0 +1,36 @@
+# DESCRIPTION: Creates cropped .bmp images from all .png images with white borders and a black interior (I think) in a directory tree. Useful e.g. for prepping art for conversion to a vector format without wasted border space.  NOTE: this relies on one of Fred's imagemagick scripts, which are not freely redistributable; you'll have to download it from the source yourself at: http://www.fmwconcepts.com/imagemagick/innercrop/index.php -- and for the newest imagemagick, you may need to search/replace all instances of convert with magick in that script. AND make sure imagemagick is in your PATH.
+
+# USAGE: call this script from a directory tree full of .png images.
+
+# TO DO: upgrade listing to all possible image types.
+
+find . -iname \*.png > crop_imgs.txt
+
+i=0
+mapfile -t imgs < crop_imgs.txt
+for element in "${imgs[@]}"
+do
+	imgFileNoExt=`echo $element | sed 's/\(.*\)\..\{1,4\}/\1/g'`
+	if [ -a $imgFileNoExt.bmp ]
+	then
+		der=duh
+	else
+		echo processing $imgFileNoExt
+		echo command is\:
+		echo innerCrop.sh -o black $element $imgFileNoExt.bmp
+		innerCrop.sh -o black $element $imgFileNoExt.bmp
+# UNCOMMENT the next line to pause between conversions and thereby reduce CPU usage/heat:
+echo pausing for a bit to cool the processor . . . && sleep 4
+# ! --------
+# OPTIONAL--COMMENT OUT THE NEXT LINE IF YOU DON'T WANT THE ORIGINAL IMAGE DELETED! :
+rm $element
+# ! --------
+	i=$[ $i+1 ]
+	fi
+done
+
+echo Cropped $i images. Done.
+
+rm crop_imgs.txt
+
+# ex. command: innerCrop.sh -o black der.png out.bmp
