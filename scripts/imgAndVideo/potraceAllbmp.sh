1698c451451e7ee698e099fb6840b7d8e38a35ad
Alex Hall <artdev@inboxalias.com>
2016-06-29 16:33:02 -0600

    sundry changes/dev; notable changes: add readRandomNbytes, mv allBWbmp2svg.sh potraceAllbmp.sh, setVideoEXIF.bat functionality added to prepImageMetaData.sh, imgTagAndDist.sh changes. Negligible metadata testing scraps. Upgrade ffmpeg etc.
    Former-commit-id: a6b5ee66c155bf920110dbc531aa1897042c484f

---
+++ scripts/imgAndVideo/potraceAllbmp.sh
@@ -0,0 +1,100 @@
+# DESCRIPTION: invokes the potrace utility to convert all black and white .bmp images (in the directory in which this script is invoked) to convert them to .svg vector images.
+
+# DEPENDENCIES: potrace.
+
+# USAGE: ensure that this script and potrace are both in your $PATH, and open a terminal in the directory of .bmp images. Invoke this script by name from the terminal. WARNING: depending on whether the relevant code line is commented out, this will delete the original .bmp images! NOTES: It will not trace a bmp image if the target svg file already exists. To retrace, delete the target svg file, and invoke this script again. Also, the script considers white a background color and black the line/trace area color.
+
+find . -iname \*.bmp > bmp_imgs.txt
+mapfile -t imgs < bmp_imgs.txt
+i=0
+for element in "${imgs[@]}"
+do
+	imgFileNoExt=`echo $element | sed 's/\(.*\)\..\{1,4\}/\1/g'`
+	if [ -a $imgFileNoExt.svg ]
+	then
+		der=duh
+	else
+	echo tracing $element . . .
+	# original command:
+	# potrace -n -s --group -r 24 -C \#000000 --fillcolor \#ffffff $element
+		# experimental commands:
+		# potrace -n -s --group -r 72 -u 5 -t 4 -a 10 -O 10 -C \#000000 --fillcolor \#ffffff $element
+		potrace -s -t 4 -a 0.73 -r 150 -O 0.71 -C \#000000 --fillcolor \#ffffff $element
+	i=$[ $i+1 ]
+	fi
+# ! --------
+# OPTIONAL--COMMENT OUT IF YOU DON'T WANT THE ORIGINAL IMAGE DELETED! :
+# rm $element
+# ! --------
+done
+
+echo Traced $i bitmaps. Done.
+
+rm bmp_imgs.txt
+
+
+# Abridged potrace reference; for details, refer to: http://potrace.sourceforge.net/potrace.1.html
+# Usage: potrace [options] [filename...]
+  # <filename>                 - an input file
+ # -o, --output <filename>    - write all output to this file
+ # --                         - end of options; 0 or more input filenames follow
+# Backend selection:
+ # -b, --backend <name>       - select backend by name
+ # -e, --eps                  - EPS backend (encapsulated PostScript) (default)
+ # -p, --postscript           - PostScript backend
+ # -s, --svg                  - SVG backend (scalable vector graphics)
+ # -g, --pgm                  - PGM backend (portable greymap)
+ # -b pdf                     - PDF backend (portable document format)
+ # -b pdfpage                 - fixed page-size PDF backend
+ # -b dxf                     - DXF backend (drawing interchange format)
+ # -b geojson                 - GeoJSON backend
+ # -b gimppath                - Gimppath backend (GNU Gimp)
+ # -b xfig                    - XFig backend
+# Algorithm options:
+ # -z, --turnpolicy <policy>  - how to resolve ambiguities in path decomposition
+ # -t, --turdsize <n>         - suppress speckles of up to this size (default 2)
+ # -a, --alphamax <n>         - corner threshold parameter (default 1)
+ # -n, --longcurve            - turn off curve optimization
+ # -O, --opttolerance <n>     - curve optimization tolerance (default 0.2)
+ # -u, --unit <n>             - quantize output to 1/unit pixels (default 10)
+ # -d, --debug <n>            - produce debugging output of type n (n=1,2,3)
+# Scaling and placement options:
+ # -P, --pagesize <format>    - page size (default is letter)
+ # -W, --width <dim>          - width of output image
+ # -H, --height <dim>         - height of output image
+ # -r, --resolution <n>[x<n>] - resolution (in dpi) (dimension-based backends)
+ # -x, --scale <n>[x<n>]      - scaling factor (pixel-based backends)
+ # -S, --stretch <n>          - yresolution/xresolution
+ # -A, --rotate <angle>       - rotate counterclockwise by angle
+ # -M, --margin <dim>         - margin
+ # -L, --leftmargin <dim>     - left margin
+ # -R, --rightmargin <dim>    - right margin
+ # -T, --topmargin <dim>      - top margin
+ # -B, --bottommargin <dim>   - bottom margin
+ # --tight                    - remove whitespace around the input image
+# Color options, supported by some backends:
+ # -C, --color #rrggbb        - set foreground color (default black)
+ # --fillcolor #rrggbb        - set fill color (default transparent)
+ # --opaque                   - make white shapes opaque
+# SVG options:
+ # --group                    - group related paths together
+ # --flat                     - whole image as a single path
+# Postscript/EPS/PDF options:
+ # -c, --cleartext            - do not compress the output
+ # -2, --level2               - use postscript level 2 compression (default)
+ # -3, --level3               - use postscript level 3 compression
+ # -q, --longcoding           - do not optimize for file size
+# PGM options:
+ # -G, --gamma <n>            - gamma value for anti-aliasing (default 2.2)
+# Frontend options:
+ # -k, --blacklevel <n>       - black/white cutoff in input file (default 0.5)
+ # -i, --invert               - invert bitmap
+# Progress bar options:
+ # --progress                 - show progress bar
+ # --tty <mode>               - progress bar rendering: vt100 or dumb
+
+# Dimensions can have optional units, e.g. 6.5in, 15cm, 100pt.
+# Default is inches (or pixels for pgm, dxf, and gimppath backends).
+# Possible input file formats are: pnm (pbm, pgm, ppm), bmp.
+# Backends are: eps, postscript, ps, pdf, pdfpage, svg, dxf, geojson, pgm, 
+# gimppath, xfig.
---
+++ scripts/openHardwareMonitorOnSecureDesktop.bat
@@ -0,0 +1,4 @@
+REM NOTE: It's possible you may need to manually switch to session 0's desktop (I think there's a way to do that with paexec) and run OHW and configure it with a manual run of it first. Or copy the .ini to some system folder?
+
+ECHO This doesn't work at the moment; hack it:
+REM paexec -w "C:\Program Files (x86)\Open Hardware Monitor" -d -s -i -x "C:\Program Files (x86)\Open Hardware Monitor\OpenHardWareMonitor.exe"
