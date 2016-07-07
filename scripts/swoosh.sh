f137a3a88109b65e762bb92cd8d0ce2fd1c2f354
artdev@inboxalias.com <TPug7ZI*23ARaRYEcXD>
2016-07-06 23:27:59 -0600

    installConfig.sh: install given UI etc. preferences for Cygwin mintty.
    newlines in complexPassword.sh and randomString.sh output.
    
    swoosh.sh: failed attempt to dedup adjacent duplicates in bash history.
    
    moved config install scripts.
    
    
    Former-commit-id: 820430ed7b9ecc0ae899ca50368421439c24a1e6

---
+++ scripts/swoosh.sh
@@ -0,0 +1,46 @@
+# DESCRIPTION
+# clears adjacent duplicates in shell history via uniq and stuff. Assumes .bash_history is at /home/$username/.
+
+# USAGE
+# With this here in your path, type:
+# swoosh.sh <enter>
+
+# FAILED attempt:
+# username=`whoami`
+# uniq /home/$username/.bash_history > temp.txt
+# cat temp.txt > /home/$username/.bash_history
+# rm temp.txt
+
+# NO, also FAIL:
+# RE: http://stackoverflow.com/a/7449399/1397555
+# remove duplicates while preserving input order
+# function dedup {
+   # awk '! x[$0]++' $@
+# }
+
+# removes $HISTIGNORE commands from input
+# function remove_histignore {
+   # if [ -n "$HISTIGNORE" ]; then
+      # replace : with |, then * with .*
+      # local IGNORE_PAT=`echo "$HISTIGNORE" | sed s/\:/\|/g | sed s/\*/\.\*/g`
+      # negated grep removes matches
+      # grep -vx "$IGNORE_PAT" $@
+   # else
+      # cat $@
+   # fi
+# }
+
+# clean up the history file by remove duplicates and commands matching
+# $HISTIGNORE entries
+# function history_cleanup {
+   # local HISTFILE_SRC=~/.bash_history
+   # local HISTFILE_DST=/tmp/.$USER.bash_history.clean
+   # if [ -f $HISTFILE_SRC ]; then
+      # \cp $HISTFILE_SRC $HISTFILE_SRC.backup
+      # dedup $HISTFILE_SRC | remove_histignore >| $HISTFILE_DST
+      # \mv $HISTFILE_DST $HISTFILE_SRC
+      # chmod go-r $HISTFILE_SRC
+      # history -c
+      # history -r
+   # fi
+# }
