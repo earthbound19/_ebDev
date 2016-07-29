701d424a3d6553c65845748b8bbd27f642d2bc5a
Alex Hall <artdev@inboxalias.com>
2016-07-28 21:44:39 -0600

    readNrandomBytes.sh: read n random bytes from an input file (or, if you hack the script, a device), and write them to randomBytes.bin. getopts-test.sh and getopts-test2.sh: testing getopts scripts found on stackexchange.
    Former-commit-id: 7cf85280872a965c7663997258744467d8cb7a12

---
+++ scripts/readNrandomBytes.sh
@@ -0,0 +1,27 @@
+# DESCRIPTION
+# Reads n random bytes from file y, where n is paramater $2 (number of bytes) to be passed to script, and y is paramater $1 (a file name) to be passed to script. Why? Um. Nothing practical probably. Actually, it can be hacked to randomly destroy bits on a device (drive) for whatever purposes you would want to do that for which are not malicious (I actually have such purposes--simulate hard drive corruption in a virtual machine). Something I did to thaw out after a hard work day. Writes the result to an output file randomBytes.bin
+
+# USAGE
+# readNrandomBytes.sh ./in.flam3 15
+# This script assumes two parameters, being $1 a file name to read and $2 how many random bytes to read from file $1 from a random position in the file.
+# ALSO: THIS SCRIPT MAY BE VERY SLUGGISH with files more than several thousand kilobytes in size.
+# To quickly make a random source file to test this with:
+# dd count=200 bs=1 obs=1 if=/dev/urandom of=random2.bin
+
+NOTE:
+
+howManyBytesToRead=$2
+	echo howManyBytesToRead val is\:
+	echo $howManyBytesToRead
+inputFileByteSize=`stat --printf="%s" $1`
+			# Because we want the "size" of the file to only be itself minus how many bytes to read (to avoid reading anywhere from the end of the file backward to $howManyBytesToRead but getting the same output, which would be a statistical anomoly . . . which only obsessed coders or cryptographers care about) :
+inputFileByteSize=$(($inputFileByteSize - $howManyBytesToRead))
+	echo inputFileByteSize val is\:
+	echo $inputFileByteSize
+skipToByte=`seq 0 $inputFileByteSize | sort -R | head -n 1`
+	echo skipToByte val is\:
+	echo $skipToByte
+
+dd count=$howManyBytesToRead bs=1 obs=1 if=$1 of=randomBytes.bin skip=$skipToByte
+	# DANGEROUS HACK to make the above line randomly corrupt bytes on a drive:
+	# dd count=$howManyBytesToRead bs=1 obs=1 if=/dev/urandom of=/dev/hda0 skip=$skipToByte
