2e82f37825a5c78506f49f3b105c4b769397e2bf
Alex Hall <artdev@inboxalias.com>
2016-08-07 23:54:48 -0600

    renumberFiles.sh: Renames all files of a given extension (via parameter) in the path from which this script is called--renames them to zero-padded numbers matching the number of digit columns of the count of all said files. WARNING: use this only in directories where you actually want _all_ files of the given extension renamed by numbers.
    gifEnc.sh: encodes movies to high-quality (excellent color) .gif images.
    
    Both of these: prior attempted commit, not sure they made it, repository cleanup nuked them it seems! My Notepad++ auto-backups still had them as they most recently were, and I could re-save them. Ditto, gifEnc.sh

---
+++ scripts/imgAndVideo/gifEnc.sh
@@ -0,0 +1,11 @@
+# RE: http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
+
+# USAGE:
+# ./gifenc.sh input.mkv output.gif
+
+palette="./palette.png"
+
+filters="fps=18,scale=-1:-1:flags=lanczos"
+
+ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y -r 10 $2
