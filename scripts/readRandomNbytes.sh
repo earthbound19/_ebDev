544c929c7b9297ed6010f0407d24e4f616800c53
Alex Hall <artdev@inboxalias.com>
2016-08-17 02:20:48 -0600

    ~metamorphose .cfg files: load nonexistent directory to speed loading.
    RoboWipe.bat: trivial comment.
    
    readNrandomBytes.sh: copy and paste into comment redundant note from duplicate work (now deleted) readRandomNbytes.sh.

--- scripts/readRandomNbytes.sh
+++
@@ -1,9 +0,0 @@
-# DESCRIPTION: IN DEVELOPMENT. Read n bytes from file name parameter $1 (parameter $2 specificies how many bytes). EXCEPT this doesn't do that, yet. Right now, it generates a random number in the range of how many bytes long the input file is.
-
-__ln=( $( ls -Lon "$1" ) )
-__size=${__ln[3]}
-	# echo "Size is: $__size bytes"
-echo file size in bytes is $__size. getting random number in that range:
-
-#re: http://stackoverflow.com/a/2556282
-shuf -i 1-$__size -n 1
