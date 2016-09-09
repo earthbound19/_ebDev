333261b714d3c211c9123d3a65a8de3ee4db3a3f
artdev@inboxalias.com <TPug7ZI*23ARaRYEcXD>
2016-09-09 04:45:55 -0600

    Sundry minor changes / moves.

---
+++ _dev/testScriptParameters.sh
@@ -0,0 +1,12 @@
+# DEMO of empty variable test.
+# If this script is passed no parameters, both messages will print. If it is passed 1 param., only the second parameter will print. If passed two parameters, no msg. will print. To invert that situation, add a spaced negation operator ! in the if checks right before the -z.
+
+if [ -z ${1+x} ]
+	then
+	echo msg 1
+fi
+	
+if [ -z ${2+x} ]
+	then
+	echo msg 2
+fi
