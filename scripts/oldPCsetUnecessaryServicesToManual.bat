c8823e95504900ab73cd69278d6b2daca2fa3ab9
artdev@inboxalias.com <TPug7ZI*23ARaRYEcXD>
2016-09-09 04:30:13 -0600

    Changes based on newfound unfriendlies

--- scripts/oldPCsetUnecessaryServicesToManual.bat
+++ scripts/oldPCsetUnecessaryServicesToManual.bat
@@ -1,48 +1,48 @@
-SC STOP "DPS"
-SC STOP "FontCache"
-SC STOP "WPCSvc"
-SC STOP "ehRecvr"
-SC STOP "ehSched"
-SC STOP "WMPNetworkSvc"
-SC STOP "HomeGroupProvider"
-SC STOP "HomeGroupListener"
-SC STOP "TabletInputService"
-SC STOP "wscsvc"
-SC STOP "WinDefend"
-SC STOP "WMPNetworkSvc"
-SC STOP "WinRM"
-SC STOP "Mcx2Svc"
-SC STOP "CscService"
-SC STOP "AeLookupSvc"
-SC STOP "Themes"
-SC STOP "PcaSvc"
-SC STOP "WSearch"
-SC STOP "wuauserv"
-SC STOP "TabletInputService"
-SC STOP "Fax"
-SC CONFIG "DPS" start= DEMAND
-SC CONFIG "FontCache" start= DEMAND
-SC CONFIG "WPCSvc" start= DEMAND
-SC CONFIG "ehRecvr" start= DEMAND
-SC CONFIG "ehSched" start= DEMAND
-SC CONFIG "WMPNetworkSvc" start= DEMAND
-SC CONFIG "HomeGroupProvider" start= DEMAND
-SC CONFIG "HomeGroupListener" start= DEMAND
-SC CONFIG "TabletInputService" start= DEMAND
-SC CONFIG "wscsvc" start= DEMAND
-SC CONFIG "WinDefend" start= DEMAND
-SC CONFIG "WMPNetworkSvc" start= DEMAND
-SC CONFIG "WinRM" start= DEMAND
-SC CONFIG "Mcx2Svc" start= DEMAND
-SC CONFIG "CscService" start= DEMAND
-SC CONFIG "AeLookupSvc" start= DEMAND
-SC CONFIG "Themes" start= DISABLED
-SC CONFIG "PcaSvc" start= DEMAND
-SC CONFIG "WSearch" start= DISABLED
-SC CONFIG "wuauserv" start= DISABLED
-SC CONFIG "TabletInputService" start= DISABLED
-SC CONFIG "Fax" start= DEMAND
-SC STOP gusvc
-SC DELETE gusvc
-SC STOP gupdate
-SC DELETE gupdate
+sc stop "DPS"
+sc stop "FontCache"
+sc stop "WPCSvc"
+sc stop "ehRecvr"
+sc stop "ehSched"
+sc stop "WMPNetworkSvc"
+sc stop "HomeGroupProvider"
+sc stop "HomeGroupListener"
+sc stop "TabletInputService"
+sc stop "wscsvc"
+sc stop "WinDefend"
+sc stop "WMPNetworkSvc"
+sc stop "WinRM"
+sc stop "Mcx2Svc"
+sc stop "CscService"
+sc stop "AeLookupSvc"
+sc stop "Themes"
+sc stop "PcaSvc"
+sc stop "WSearch"
+sc stop "wuauserv"
+sc stop "TabletInputService"
+sc stop "Fax"
+sc config "DPS" start= demand
+sc config "FontCache" start= demand
+sc config "WPCSvc" start= demand
+sc config "ehRecvr" start= demand
+sc config "ehSched" start= demand
+sc config "WMPNetworkSvc" start= demand
+sc config "HomeGroupProvider" start= demand
+sc config "HomeGroupListener" start= demand
+sc config "TabletInputService" start= demand
+sc config "wscsvc" start= demand
+sc config "WinDefend" start= demand
+sc config "WMPNetworkSvc" start= demand
+sc config "WinRM" start= demand
+sc config "Mcx2Svc" start= demand
+sc config "CscService" start= demand
+sc config "AeLookupSvc" start= demand
+sc config "Themes" start= disabled
+sc config "PcaSvc" start= demand
+sc config "WSearch" start= disabled
+sc config "wuauserv" start= disabled
+sc config "TabletInputService" start= disabled
+sc config "Fax" start= demand
+sc stop gusvc
+sc delete gusvc
+sc stop gupdate
+sc delete gupdate
