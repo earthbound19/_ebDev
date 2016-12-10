b7e302a2998f8014c0aaa86c0d75709bd2f78a47
Alex Hall <ansible1@earthbound.io>
2016-12-09 20:14:33 -0700

    ADDED 256terminalColorsHEX.txt: color scheme emulating common terminal colors vid2vid.sh: convert all files of one video format to another via ffmpeg.
    CHANGED
    utVideoNoSound_all.sh: add default format for no parameter

---
+++ scripts/imgAndVideo/vid2vid.sh
@@ -0,0 +1,35 @@
+# DESCRIPTION
+# Converts all video files of a given type ($1) to another type ($2) with default crf (constant rate factor or quality) 13 (quite high quality).
+
+# USAGE
+# Invoke this script with two paramaters, the first being the source format and the second being the target, e.g.:
+# thisScript.sh mov mp4
+
+srcIMGformat=$1
+destIMGformat=$2
+
+if [ ! -a $5 ]
+then
+	param3="-background none"
+fi
+
+
+CygwinFind . -iname \*.$srcIMGformat > IMGconvertList.txt
+sed -i 's/^\.\/\(.*\)/\1/g' IMGconvertList.txt
+mapfile -t IMGconvertList < IMGconvertList.txt
+for element in "${IMGconvertList[@]}"
+do
+		IMGfilenameNoExt=`echo $element | sed 's/\(.*\)\.[^\.]\{1,5\}/\1/g'`
+	if [ -a $IMGfilenameNoExt.$destIMGformat ]
+	then
+		echo conversion target candidate is $IMGfilenameNoExt.$destIMGformat
+		echo target already exists\; will not render.
+		echo . . .
+	else
+		echo converting $element . . .
+		# magick $param3 -scale $img_size $element $IMGfilenameNoExt.$destIMGformat
+		ffmpeg -i $IMGfilenameNoExt.$srcIMGformat -crf 13 $IMGfilenameNoExt.$destIMGformat
+	fi
+done
+
+rm IMGconvertList.txt
