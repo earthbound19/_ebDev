99b0d69579d626f732439e3eff1da107ff15ac7e
Alex Hall <ansible1@earthbound.io>
2017-03-16 21:37:28 -0600

    MOVE/RENAME cr2dng.ahk, crddng.bat into /scripts/imgAndVideo RAW2dngAHK.ahk, RAW2dng.bat ADD allRAW2dng.sh

---
+++ scripts/imgAndVideo/allRAW2dng.sh
@@ -0,0 +1,16 @@
+# DESCRIPTION
+# Converts all images of a given type (CR2, RAW etc.) to DNG type files via AdobeDNGConverter.exe CLI, which must be in your path and the executable so renamed.
+
+# USAGE
+# Invoke with one parameter $1, being a file type to operate on, e.g.:
+# ./thisScript.sh CR2
+
+CygwinFind . -iname \*.$1 > all_wut.txt
+mapfile -t all_wut < all_wut.txt
+for element in "${all_wut[@]}"
+do
+	echo executing command\: AdobeDNGConverter.exe -c -p1 -fl $element . . .
+	AdobeDNGConverter.exe -c -p1 -fl $element
+done
+
+rm all_wut.txt all_wut.txt
