0abdc7078657adecd00bdd4fabcb98a962d39996
Alex Hall <artdev@inboxalias.com>
2017-04-12 17:07:05 -0600

    refactored for better cross-platform functionality

--- getDevEnv.sh
+++ getDevEnv.sh
@@ -1,31 +1,25 @@
 # DESCRIPTION
-# Sets an environment path containing all used /bin etc. folders for the current shell. To do this, cd to the dir containing this script, and execute the command <source ./setTempEnv.sh>.
+# Sets an environment path containing all used /bin etc. folders for the current shell.
 
 # USAGE
-# To conveniently invoke this script from the home dir of your terminal, copy e.g. this code to a file callGetDevEnv.sh in your home dir:
+# CD to the dir containing this script, and execute the command <source ./setTempEnv.sh>. To conveniently invoke this script from the home dir of your terminal, copy e.g. this code to a file callGetDevEnv.sh in your home dir:
 # cd "$USERPROFILE\Documents\scrap\_devtools-master"
 # source ./getDevEnv.sh
 # -- and then when you open your terminal (at the home dir), type:
 # source ./gde.sh
 # -- and you will now have a prompt with all paths in BINPATHSrelativeBash.txt.
 
-mapfile -t paths < BINPATHSrelativeBash.txt
-
-for element in ${paths[@]}
+while read -r element
 do
-		# echo $element
-	_cygDir_=`cygpath -u $element`
-		# echo _cygDir_ val is\:
-		# echo $_cygDir_
 	_pwd_=$(pwd)
-		# echo ~~~~~
-		# echo val of _pwd_ is\:
-		# echo $_pwd_
-	_cygDir_="$_pwd_"/"$_cygDir_"
-		# echo appended to pwd is\:
-		# echo $_cygDir_
-	export PATH="$PATH":"$_cygDir_"
-done
+	addPathToDir="$_pwd_"/"$element"
+	export PATH="$PATH":"$addPathToDir"
+done < ./BINPATHSrelativeBash.txt
 
 echo ----------------
 echo TEMPORARY ENVIRONMENT PATHS SET DONE.
+
+# HISTORY
+# BEFORE NOW
+# Things.
+# 2017 apr 12 refactored for better cross-platform compatibility.
