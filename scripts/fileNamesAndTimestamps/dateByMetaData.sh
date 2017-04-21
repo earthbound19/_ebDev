67afc463d329e8a8784c03b8330b8c6743e86b9d
Alex Hall <ansible1@earthbound.io>
2017-04-20 20:24:55 -0600

    metadata scripts etc. tweaks

--- scripts/fileNamesAndTimestamps/dateByMetaData.sh
+++ scripts/fileNamesAndTimestamps/dateByMetaData.sh
@@ -6,7 +6,7 @@ echo BEGINNING correcting of timestamps to match any EXIF data . . .
 
 # TO DO: remove file extensions from the following list which will never contain metadata.
 # Another (easier to read and change?) way to do the following: find . -type f -iregex '\.\/.*.\(tif\|tiff\|png\|.psd\|ora\|kra\|rif\|riff\|jpg\|jpeg\|gif\|bmp\|cr2\|crw\|pdf\|ptg\)' -printf '%TY %Tm %Td %TH %TM %TS %p\n' | sort -g > _batchNumbering/fileNamesWithNumberTags.txt
-cygwinFind . -iname \*.tif -o -iname \*.tiff -o -iname \*.psd -o -iname \*.mov -o -iname \*.mp4 -o -iname \*.m4a > dateByImageInfoFilesListTemp.txt
+cygwinFind . -iname \*.tif -o -iname \*.tiff -o -iname \*.psd -o -iname \*.mov -o -iname \*.mp4 -o -iname \*.m4a -o -iname \*.jpg > dateByImageInfoFilesListTemp.txt
 mapfile -t imageFiles < ./dateByImageInfoFilesListTemp.txt
 for filename in ${imageFiles[@]}
 do
