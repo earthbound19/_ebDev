6dcbea1839aa8973da7a623aff424543a210aa10
Alex Hall <ansible1@earthbound.io>
2017-04-23 00:59:44 -0600

    begg. of script to montage image with its extracted and rendered palette

---
+++ _dev/montageImageWithPalette.sh
@@ -0,0 +1,4 @@
+# IN DEVELOPMENT. example command; re: https://unix.stackexchange.com/a/4067/110338
+montage -tile 1x2 -geometry +0+50 -background gray splat.jpg splat_colors.png oot.png
+
+# MANY REFERENCE HERE: https://unix.stackexchange.com/questions/4046/remove-extra-tilespace-from-a-montage-imagemagick-composite-image
