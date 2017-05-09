a6b2a752a06832275ef4e152400661e76d401be1
Alex Hall <ansible1@earthbound.io>
2017-05-08 21:36:35 -0600

    move in-development files from all over master branch into this dev branch (in a dedicated folder)

--- _dev/addThisPathTemp.sh
+++
@@ -1,6 +0,0 @@
-# ADDS THE CURRENT path to your terminal profile temporarily (you will lose the path on terminal restart). NOTE that after this has been done for the path which contains this script, since this script is then in the path, the same can be done by calling this script from any other path--that "any other path" will then also be temporarily in your path.
-
-thisDir=`pwd`
-# echo thisDir value is\: $thisDir
-
-PATH=$thisDir:\$PATH
