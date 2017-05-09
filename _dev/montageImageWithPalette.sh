a6b00e62a62e531ae49480a362ea04b3cfe4f89f
Alex Hall <ansible1@earthbound.io>
2017-05-08 21:29:26 -0600

    clear in-development scripts from this branch which I'm moving to dev branch

--- _dev/montageImageWithPalette.sh
+++
@@ -1,8 +0,0 @@
-# IN DEVELOPMENT. example command; re: https://unix.stackexchange.com/a/4067/110338
-montage -tile 1x2 -geometry +0+50 -background gray splat.jpg splat_colors.png oot.png
-
-# MANY REFERENCE HERE: https://unix.stackexchange.com/questions/4046/remove-extra-tilespace-from-a-montage-imagemagick-composite-image
-
-
-# other maybe not needed reference; produces a magenta square image:
-# gm convert -size 250x250 xc:\#ff00ff oot.png
--- scripts/_dev/test414.sh
+++
@@ -1,5 +0,0 @@
-magick.exe -size 2400x1200 xc:White ^
-  -gravity Center ^
-  -weight 700 -pointsize 200 ^
-  -annotate 0 "OIL\nFOUND\nIN CENTRAL PARK" ^
-  oil.png
