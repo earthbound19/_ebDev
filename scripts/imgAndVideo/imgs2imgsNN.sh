cb12db822b2e3a5f62853b53268f697a62049193
Alex Hall <ansible1@earthbound.io>
2017-05-09 21:03:43 -0600

    new feature dev img2imgNN.sh: scale to target pix X (calculating Y) keeping aspect; rm duplicate .txt file

--- scripts/imgAndVideo/imgs2imgsNN.sh
+++ scripts/imgAndVideo/imgs2imgsNN.sh
@@ -6,15 +6,11 @@
 # $1 source format (without .)
 # $2 destination format
 # $3 scale by nearest neighbor method to pixels X
-# $4 scale by nearest neighbor method to pixels Y
-# e.g. imgs2imgsnn.sh ppm png 640 480
+# $4 scale by nearest neighbor method to pixels Y. IF OMITTED, scales to $3 (X pix) by nearest neighbor preserving aspect ratio, e.g.:
+# imgs2imgsnn.sh ppm png 640 480
+# OR:
+# imgs2imgsnn.sh ppm png 640
 
-# TO DO
-		# Make it do no resizing if no params 3 and 4 given; e.g. adapt this:
-		# if [ ! -z ${5+x} ]
-		# then
-			# rescaleParams="-vf scale=$5:flags=neighbor"
-		# fi
 
 	# DEPRECATED command for unexpected behavior; it may be that the following command somehow caused nconvert to iterate over every source file format by wildcard? Removing the . from the command, it iterates over the list; whereas with the . it did so twice:
 	# find . *.$1 > all_$1.txt
@@ -25,6 +21,20 @@ rm all_$1.txt
 
 for img in ${all_imgs[@]}
 do
+
+			# thing $4 not $5 dev. commands:
+			if [ -z ${4+x} ]
+			then
+			derp=`gm identify $img`
+			pixX=`echo $derp | sed 's/.* \([0-9]\{1,\}\)x[0-9]\{1,\}+[0-9]\{1,\}+[0-9]\{1,\} .*/\1/g'`
+			pixY=`echo $derp | sed 's/.* [0-9]\{1,\}x\([0-9]\{1,\}\)+[0-9]\{1,\}+[0-9]\{1,\} .*/\1/g'`
+			echo pixX val is $pixX
+			echo pixY val is $pixY
+			# echo pixY val is $pixY
+			5x6 img upscaled to 850px X maintaining aspect = 850 x 1020
+			fi
+			exit
+
 			# echo img is $img
 	imgFileNoExt=`echo $img | sed 's/\(.*\)\..\{1,4\}/\1/g'`
 			# echo imgFileNoExt val is\:
