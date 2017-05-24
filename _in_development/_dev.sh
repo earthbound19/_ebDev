8c5aad089565281fdf67cb6603724b154bdc39e2
Alex Hall <ansible1@earthbound.io>
2017-05-23 20:43:00 -0600

    moving dev folder back here after shuffle out and merge master into dev

---
+++ _in_development/_dev.sh
@@ -0,0 +1,44 @@
+# DESCRIPTION
+# Creates an mp4 video (AVC) from a series of numbered input images. Automatically detects the number of digits in the input frames. Expects *only* digits in the input filenames. Creates the animation at _out.mp4.
+
+# WARNING: AUTOMATICALLY overwrites _out.mp4 if it already exists.
+
+# USAGE
+# From the directory with the image animation source images, invoke this script with these parameters:
+# $1 input "frame rate" (how to interpret the speed of input images in fps)
+# $2 desired output framerate
+# $3 desired constant quality (crf)
+# $4 the file extension of the input images.
+# Optional: $5 rescale target resolution expressed as nnnnXnnnn. Source images will be rescaled by nearest-neighbor (keep hard edges) option to this target resolution.
+
+# NOTE: You can hack this script to produce an animated .gif image simply by changing the extension at the end of the applicable command line (line 32).
+
+# TO DO: adjustable input file list "frame rate."
+# TO DO? : make it name the output file after the ../.. parent folder name?
+
+if [ ! -z ${5+x} ]
+then
+	rescaleParams="-vf scale=$5:flags=neighbor"
+		# echo rescaleParams val is\:
+		# echo $rescaleParams
+fi
+
+# horked from renumberFiles.sh:
+CygwinFind *.$4 > allFiles.txt
+arraySize=$(wc -l < allFiles.txt)
+numDigitsOf_arraySize=${#arraySize}
+rm allFiles.txt
+
+ffmpeg -y -f image2 -r $1 -i %0"$numDigitsOf_arraySize"d.$4 $rescaleParams -r $2 _out.avi
+
+
+
+
+# DEV NOTES
+# ex. command:
+# ffmpeg -y -r 24 -f image2 -i doctoredFrames\mb-DGYTMSWA-fr_%07d.png -vf "format=yuv420p" -vcodec rawvideo -r 29.97 _DGYTMSWAsourceDoctoredUncompressed.avi
+
+# to enable simple interpolation to up the framerate to 30, use this tag in the codec/output flags section:
+# -vf fps=30
+
+# ex. OLD command: ffmpeg -y -r 18 -f image2 -i %05d.png -crf 13 -vf fps=30 out.mp4
---
+++ _in_development/test1.sh
@@ -0,0 +1,1 @@
+gm -size 200x30 -fill black -pointsize 12 -draw "text 5,15 'this is just a test'" -write "./test.png"
