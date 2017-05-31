c5fdcd3f2abddf82b65ee699ebd03d64e8512bd9
Alex Hall <ansible1@earthbound.io>
2017-05-31 10:06:39 -0600

    typo and pref. fix

--- scripts/imgAndVideo/BWsvgRandomColorFill.sh
+++ scripts/imgAndVideo/BWsvgRandomColorFill.sh
@@ -5,20 +5,20 @@
 # Pass this script three parameters (the third is optional), being:
 # $1 an .svg file name and
 # $2 how many random color fill variations of that file to create, and
-# $3 OPTIONAL; will produce random colors if absent: a flat text file list of hexadecimal RGB color codes, one per line, from which to choose random colors for this fill. NOTE: each hex color must be preceded by #. This script makes a copy of the .svg with a name being a time stamp. NOTE: This expects an svg colored via hexadecimal color code fills where areas to color are designated in the svg as #ffffff. If your svg is not thus, potrace the original black bitmap using potraceAllBMPs.sh, or use the SVGOMG service (convert your SVG file online) at: https://jakearchibald.github.io/svgomg/ -- or use SVGO re https://github.com/svg/svgo and https://web-design-weekly.com/2014/10/22/optimizing-svg-web/ -- It converts rgb values to hex by default. BUT NOTE: DO NOT use the "minify colors" option. 
+# $3 OPTIONAL; will produce random colors if absent: a flat text file list of hexadecimal RGB color codes, one per line, from which to choose random colors for this fill. NOTE: each hex color must be preceded by #. This script makes a copy of the .svg with a name being a time stamp. NOTE: This expects an svg colored via hexadecimal color code fills where areas to color are designated in the svg as #ffffff. If your svg is not thus, potrace the original black bitmap using potraceAllBMPs.sh, or use the SVGOMG service (convert your SVG file online) at: https://jakearchibald.github.io/svgomg/ -- or use SVGO re https://github.com/svg/svgo and https://web-design-weekly.com/2014/10/22/optimizing-svg-web/ -- It converts rgb values to hex by default. BUT NOTE: for our purposes, DO NOT use the "minify colors" option. 
 
 # TO DO: 
 # - make it use an optional global hex color schemes dir tree (search path), otherwise search in path script is invoked from.
 # - make it name the target file after the color scheme.
 # ? - implement an optional buffer memory of the last three colors used, and if the current picked color is among them, pick another color until it is not among them.
-# ? - replace all this functionality with a script that works with a nodejs svg library, if possible? It cold be run from a CLI on any local nodejs (node) install.
+# ? - replace all this functionality with a script that works with a nodejs svg library, if possible? It could be run from a CLI on any local nodejs (node) install.
 
 # CODE
-# If no $3 parameter passed to script, create an array of 10 random hex RGB color values. Otherwise, create the array from the list in the filename specified in $3.
+# If no $3 parameter passed to script, create an array of 18 random hex RGB color values. Otherwise, create the array from the list in the filename specified in $3.
 if [ -z ${3+x} ]
 	then
 		echo Generating random hex colors array . . .
-		for i in $( seq 10 );
+		for i in $( seq 18 );
 		do
 # TO DO: make this work faster with one pre-generated string in memory that you bite six bytes off in increments.
 		randomHexString=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 6`
