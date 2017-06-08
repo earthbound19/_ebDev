3a98acb942000855cad324710daf9016cc3ba8f0
Alex Hall <ansible1@earthbound.io>
2017-06-08 17:49:58 -0600

    trivial code tweak and remove useless comments

--- scripts/imgAndVideo/svgo_optimize_all.sh
+++ scripts/imgAndVideo/svgo_optimize_all.sh
@@ -1,16 +1,10 @@
 # DESCRIPTION
 # Invokes svgo_optimize.sh repeatedly. See comments in that script.
 
-CLIopts="--disable=mergePaths --enable=removeRasterImages --disable=convertShapeToPath"
-# OTHER ADDITIONAL OPTIONS; comment out if you don't want them:
-# moreCLIopts="--enable=removeDimensions --enable=removeRasterImages --enable=removeUnknownsAndDefaults"
-# UNUSED option(s):
-# --enable=removeViewBox
-
 find *.svg > allSVGs.txt
-mapfile -t allSVGs < allSVGs.txt
-rm allSVGs.txt
-for element in "${allSVGs[@]}"
+while read line
 do
-	svgo_optimize.sh "$element"
-done
+	svgo_optimize.sh "$line"
+done < allSVGs.txt
+
+rm ./allSVGs.txt
