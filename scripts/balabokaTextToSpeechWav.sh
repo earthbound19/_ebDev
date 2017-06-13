daaae9e09c0f59df7e2cb9af004f010fe4939d8c
Alex Hall <ansible1@earthbound.io>
2017-06-13 11:19:06 -0600

    Wrapper that renders any text within quote marks to a randomly named audio file.

---
+++ scripts/balabokaTextToSpeechWav.sh
@@ -0,0 +1,18 @@
+# DESCRIPTION
+# Wrapper that renders any text within quote marks to a randomly named audio file.
+
+# USAGE
+# Invoke this script with one paramater, being any text to render to a .wav file (text encased in "quote marks"); e.g.:
+# thisScript.sh "blarpnoi hoi hoi"
+
+# DEPENDENCIES
+# Balaboka.exe / (windows text-to-speech engine tool) and Cygwin or other 'nix environment
+
+textSay=$1
+fileName=$1
+		# OPTIONAL alternate for file names and random text; if you uncomment the next three lines, comment out the previous two:
+		# export LC_CTYPE=C
+		# textSay=`cat /dev/urandom | tr -dc 'a-z ' | head -c 18`
+		# fileName=$textSay
+./balcon -t "$textSay" -w "$fileName"".wav"
+cygstart "./""$fileName"".wav"
