2c16d01c1bfdb6aa69c222a2a02fe2f1216a8201
earthbound19 <ansible1@earthbound.io>
2017-08-01 16:24:24 -0600

    improved

---
+++ scripts/cleanupBashHistory.sh
@@ -0,0 +1,39 @@
+# re: https://stackoverflow.com/a/7449399/1397555
+# SEE ALSO (to do--? implement?) : https://jorge.fbarr.net/2011/03/24/making-your-bash-history-more-efficient/
+
+# remove duplicates while preserving input order
+function dedup {
+   awk '! x[$0]++' $@
+}
+
+# removes $HISTIGNORE commands from input
+function remove_histignore {
+   if [ -n "$HISTIGNORE" ]; then
+      # replace : with |, then * with .*
+      local IGNORE_PAT=`echo "$HISTIGNORE" | sed s/\:/\|/g | sed s/\*/\.\*/g`
+      # negated grep removes matches
+      grep -vx "$IGNORE_PAT" $@
+   else
+      cat $@
+   fi
+}
+
+# clean up the history file by remove duplicates and commands matching
+# $HISTIGNORE entries
+function history_cleanup {
+   local HISTFILE_SRC=~/.bash_history
+   local HISTFILE_DST=/tmp/.$USER.bash_history.clean
+   if [ -f $HISTFILE_SRC ]; then
+      \cp $HISTFILE_SRC $HISTFILE_SRC.backup
+      dedup $HISTFILE_SRC | remove_histignore >| $HISTFILE_DST
+      \mv $HISTFILE_DST $HISTFILE_SRC
+      chmod go-r $HISTFILE_SRC
+      history -c
+      history -r
+   fi
+}
+
+# actually invoke that function which invokes other functions :)
+history_cleanup
+
+echo \.bash_history cleanup done.
