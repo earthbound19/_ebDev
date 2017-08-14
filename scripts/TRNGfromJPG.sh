aa3e739a58ae65c2cc12c7fee03c8df2059b1d5f
Alex Hall <ansible1@earthbound.io>
2017-08-14 06:21:46 -0600

    temp file production reduce, keep pure randomness hex table *.txt src

--- scripts/TRNGfromJPG.sh
+++ scripts/TRNGfromJPG.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# Produce true random data (.dat) file all .jpg files in the directory you run this from. reference: https://crypto.stackexchange.com/a/43121
+# Produce true random data (.dat) file by extracting noise from all .jpg files in the directory you run this from, and hashing all that noise. Reference: https://crypto.stackexchange.com/a/43121
 
 # USAGE
 # thisScript.sh input.jpg
@@ -8,20 +8,18 @@
 # xxd. On Cygwin install it via `apt-cyg install hxd`
 
 # TO DO:
-# use all non-cryptographic hash functions
-# concat hex to binary via dd if possible
+# use all non-cryptographic hash functions available
 
 rehash -none -fnv64 -md2 -md4 -md5 -sha1 -out:raw -out:pad:false -out:nospaces *.jpg > rnd_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt
 # because that results in windows "newlines" (\n\r), change them to 'nix:
 dos2unix rnd_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt
 # strip down that output to only hex prints and newlines:
 sed -i -e 's/.*: \(.*\).*/\1/g' -e 's/<.*>//g' rnd_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt
-tr -d '\n' < rnd_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt > no_newlines_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt
 timestamp=`date +"%Y_%m_%d__%H_%M_%S__%N"`
-xxd -r -p no_newlines_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt __trueRandomData_"$timestamp".dat
-rm rnd_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt no_newlines_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt
+tr -d '\n' < rnd_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt > __trueRandomData_"$timestamp"_HEXsrcTable.txt
+rm rnd_H3pDjjUNgmbsYjGfaYrKQk6mz8yZHNKSqx.txt
+xxd -r -p __trueRandomData_"$timestamp"_HEXsrcTable.txt __trueRandomData_"$timestamp".dat
 
-# rehash -out:text -out:pad:false -out:word:512 -out:nospaces 
 
 # DEV NOTES
 # usage of rehash:
