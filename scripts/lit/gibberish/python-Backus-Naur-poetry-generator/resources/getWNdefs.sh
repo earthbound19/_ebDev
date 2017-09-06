500a3fba6a95b3b277ff656f61d364cfa5ca789f
Alex Hall <ansible1@earthbound.io>
2017-09-06 06:49:25 -0600

    moved gibberish generating scripts to gibGen repo

--- scripts/lit/gibberish/python-Backus-Naur-poetry-generator/resources/getWNdefs.sh
+++
@@ -1,7 +0,0 @@
-mapfile -t wordList < /cygdrive/c/_ebdev/scripts/lit/gibberish/python-Backus-Naur-poetry-generator/resources/ACT_study_words.txt
-
-for word in "${wordList[@]}"
-do
-	"/cygdrive/c/Program Files (x86)/WordNet/2.1/bin/wn.exe" "$word" -over >> ACT_study_words_corrected_definitions.txt
-	echo ---------------- >> ACT_study_words_corrected_definitions.txt
-done
