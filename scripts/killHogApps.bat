4fdf7caa9f5195c767b3abdcd3b589b7382e663a
Alex Hall <ansible1@earthbound.io>
2017-09-26 09:30:13 -0600

    added services that have caused trouble betimes

--- scripts/killHogApps.bat
+++ scripts/killHogApps.bat
@@ -13,6 +13,19 @@ sc stop mi-raysat_3dsmax2013_64
 sc stop LMS
 sc stop jhi_service
 sc stop "Intel(R) Capability Licensing Service Interface"
+REM IT SEEMS that one of the following services, as of 09/25/2017, causes explorer.exe to hang when you attempt to delete a junction; as stopping all the following services eliminates the problem:
+sc stop PSI_SVC_2_x64
+sc stop PSI_SVC_2
+sc stop DbxSvc
+sc stop mi-raysat_3dsmax2013_32
+sc stop mi-raysat_3dsmax2013_32
+sc stop ProtexisLicensing
+sc stop RemoteSolverDispatcher
+sc stop VMAuthdService
+sc stop VMnetDHCP
+sc stop "VMware NAT Service"
+sc stop VMUSBArbService
+sc stop VMwareHostd
 process -k dropbox.exe
 process -k greenshot.exe
 process -k robotaskbaricon.exe
@@ -21,3 +34,4 @@ process -k DisplayFusionHookAppWIN6032.exe
 process -k DisplayFusion.exe
 process -k CCC.exe
 process -k COCIManager.exe
+process -k slack.exe
