57bcb5a2b7712981fe0065c170a930615254ef9a
Alex Hall <ansible1@earthbound.io>
2017-09-27 13:49:53 -0600

    lists of packages I commonly use in various Mac toolchains

---
+++ macCustomize.sh
@@ -0,0 +1,9 @@
+# npm packages I regularly use:
+# PREREQUISITE; npm:
+while read element
+  echo item is $element . .
+done < ./npmPackages.txt
+
+brew packages I regularly use:
+in 
+# ttab && cd "$PWD"
