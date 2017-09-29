65eb245ac8f047bcb2691d8d51bf3295f604a2db
Alex Hall <ansible1@earthbound.io>
2017-09-29 09:54:38 -0600

    dev snapshot -- tested up to filtered pairs with duplicates

--- _in_development/imgsGetSimilar.sh
+++ _in_development/imgsGetSimilar.sh
@@ -25,35 +25,46 @@ else
 	searchRegex=".$1"
 fi
 
-# CODE
-# Delete any wonky file names from prior or interrupted run:
-rm __superShrunkRc6d__*
 
-gfind ./* -type f -iregex ".*\.$1" > allIMGs.txt
+# CODE
+
+	# OPTIONAL wipe of all leftover files from previous run; comment out everything in the following block if you don't want that:
+	rm __superShrunkRc6d__*
+	rm allIMGs.txt compare__superShrunkRc6d__col1.txt compare__superShrunkRc6d__col2.txt tmp_yyYM7wvUZdc3Qg.txt tmp_fx49V6cdmuFp.txt comparisons__superShrunkRc6d__cols.txt __superShrunkRc6d__*
+
+find ./* -type f -iregex ".*\.$1" > allIMGs.txt
+# because gfind produces windows line-endings, convert them to unix:
+dos2unix allIMGs.txt
+sed -i 's/^\(\.\/\)\(.*\)/\2/g' allIMGs.txt
 
 # Create heavily shrunken image copies to run comparison on.
 echo Generating severely shrunken image copies to run comparisons against . . .
-while read element
+allIMGs=( $( < allIMGs.txt) )
+
+for element in "${allIMGs[@]}"
 do
-	echo copying $element to shrunken __superShrunkRc6d__$element to make image comparison much faster . . .
-	gm convert $element -scale 10 __superShrunkRc6d__$element
-done < allIMGs.txt
+	if [ -f "__superShrunkRc6d__""$element" ]
+	then
+		echo COMPARISON SOURCE FILE "__superShrunkRc6d__""$element" already exists\; assuming comparisons against it were already run\; skipping comparison.
+	else
+		echo copying $element to shrunken "__superShrunkRc6d__""$element" to make image comparison much faster . . .
+		gm convert $element -scale 10 __superShrunkRc6d__$element
+	fi
+done
 
 # Prepend everything in allIMGs.txt with that wonky string file name identifier before running comparison via;
 sed -i 's/^\(.*\)/__superShrunkRc6d__\1/g' allIMGs.txt
 
-allIMGs=( $( < allIMGs.txt) )
 
 i_count=0
 j_count=0
 printf "" > compare__superShrunkRc6d__col1.txt
 printf "" > compare__superShrunkRc6d__col2.txt
-# List all possible pairs of file type $1, order is not important, repetition is not allowed ($1 pick 2).
+# List all possible pairs of file type $1, order is not important, repetition is not allowed (math algorithm $1 pick 2).
 for i in "${allIMGs[@]}"
 do
 	i_count=$(( i_count + 1 ))
-	# Remove element i from a copy of the array so that we only iterate through the remaining in the array which have not already been compared;
-	# re: http://unix.stackexchange.com/a/68323
+	# Remove element i from a copy of the array so that we only iterate through the remaining in the array which have not already been compared; re http://unix.stackexchange.com/a/68323 :
 	allIMGs_innerLoop=("${allIMGs[@]:$i_count}")
 			# echo size of arr for inner loop is ${#allIMGs_innerLoop[@]}
 	for j in "${allIMGs_innerLoop[@]}"
@@ -62,22 +73,22 @@ do
 # compare -metric MAE img_11.png img_3.png null: 2>&1
 		echo "comparing images: $i | $j . . . VIA COMMAND: gm compare -metric MAE $i $j null: 2>&1 | grep 'Total'"
 		metricPrint=`gm compare -metric MAE $i $j null: 2>&1 | grep 'Total'`
-		# ODD ERRORS arise from mixed line-ending types, where gm returns windows-style, and printf commands produce unix-style. Solution: write to separate column files, convert all gm-created files to unix via dos2unix, then paste them into one file.
+		# ODD ERRORS arise from mixed line-ending types, where gm returns windows-style, and printf commands produce unix-style. Solution: write to separate column files, later (after these nested loop blocks) convert all gm-created files to unix via dos2unix, then paste them into one file.
 		echo "$metricPrint" >> compare__superShrunkRc6d__col1.txt
 		printf "|$i|$j\n" >> compare__superShrunkRc6d__col2.txt
 	done
 done
 
-# Combine ~col1 and ~col2 files into one file (pasting as columns), after getting the line endings in col1 to match col2 :
+# Re prevous comment in nested loop blocks:
 dos2unix compare__superShrunkRc6d__col1.txt
-paste -d '' compare__superShrunkRc6d__col1.txt compare__superShrunkRc6d__col2.txt > tmp_WzzNtNBw2jYD9A.txt
-# Remove temp file name gobbeldy-gook padding:
-sed -i 's/__superShrunkRc6d__//g' tmp_WzzNtNBw2jYD9A.txt
+paste -d '' compare__superShrunkRc6d__col1.txt compare__superShrunkRc6d__col2.txt > comparisons__superShrunkRc6d__cols.txt
 # Filter out information cruft; NOTE that if the first column isn't preceded by | then the later sort command won't work as intended:
-sed -i 's/.*Total: \([0-9]\{1,11\}\.[0-9]\{1,11\}\).*|\([^|]*\).*|\([^|]*\).*/\1|\2|\3/g' tmp_WzzNtNBw2jYD9A.txt
-# Sort results by reverse rank of keys by priority of columns 2, 1, 3; which is an attempt at most similar pairs adjacent (usually) :
-sort -n -b -t\| -k3R -k1r tmp_WzzNtNBw2jYD9A.txt > tmp_fx49V6cdmuFp.txt
+sed -i 's/.*Total: \([0-9]\{1,11\}\.[0-9]\{1,11\}\).*|\([^|]*\).*|\([^|]*\).*/\1|\2|\3/g' comparisons__superShrunkRc6d__cols.txt
+# Sort results by reverse rank of keys by priority of certain columns in an attempt at most similar pairs adjacent (usually) :
+sort -n -b -t\| -k2r -k1r -k1 comparisons__superShrunkRc6d__cols.txt > tmp_fx49V6cdmuFp.txt
+# Strip the numeric column so we can work up a file list of said ordering for animation:
 sed -i 's/[^|]*|\(.*\)/\1/g' tmp_fx49V6cdmuFp.txt
+exit
 tr '\n' '|' < tmp_fx49V6cdmuFp.txt > tmp_yyYM7wvUZdc3Qg.txt
 
 # Delete all but first occurance of a word e.g. 'pattern' from a line; the way it works is: change the first 1 (in the following command) to a 2 to remove everything but the 2nd occurances of 'pattern', or 4 to remove everything but the 4th occurance of the pattern, or 1 to remove all but the first etc., re; https://unix.stackexchange.com/a/18324/110338 :
@@ -102,7 +113,6 @@ tr '|' '\n' < tmp_yyYM7wvUZdc3Qg.txt > IMGlistByMostSimilar.txt
 # --or, that's ready after one more tweak for file list format ffmpeg demands:
 sed -i "s/^\(.*\)/file '\1'/g" IMGlistByMostSimilar.txt
 
-rm allIMGs.txt compare__superShrunkRc6d__col1.txt compare__superShrunkRc6d__col2.txt tmp_yyYM7wvUZdc3Qg.txt tmp_fx49V6cdmuFp.txt tmp_WzzNtNBw2jYD9A.txt __superShrunkRc6d__*
 
 echo ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 echo FINIS\! You may now use the image list file IMGlistByMostSimilar.txt in conjunction with ffmpegAnimFromFileList.sh \(see comments of that script\) to produce an animation of these images arranged by most similar to nearest neighbor in list \(roughly\, with some randomization in sorting so that most nearly-identical images are not always clumped together with least similar images toward the head or tail of the list\)\.
