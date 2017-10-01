78549b14c553824c729f815a6af65e83267f5f2b
Alex Hall <ansible1@earthbound.io>
2017-10-01 14:08:29 -0600

    add default file list name (otherwise use $1)

--- scripts/imgAndVideo/mkNumberedCopiesFromFileList.sh
+++ scripts/imgAndVideo/mkNumberedCopiesFromFileList.sh
@@ -2,16 +2,26 @@
 # Invoke this cript with one parameter:
 # $1 The file list name, for which every file name in the list there will be numbered junction (file names which are junctions) in a subdir.
 
-# WARNING: this script perhaps dangerously assumes all file names provided in the list have the same extension; which is done on these next lines:
-tempStr=`head -n 1 IMGlistByMostSimilar.txt`
-# NOTE that this assumes a closing apostraphe or single quote in the input file! :
+# WARNING: this script perhaps dangerously assumes all file names provided in the list have the same extension.
+
+# IF NO list file provided, assume it is IMGlistByMostSimilar.txt:
+if [ -z ${1+x} ]
+then
+	fileList=IMGlistByMostSimilar.txt; echo No file list parameter \(\$\1\) passed to script\; setting fileList to default IMGlistByMostSimilar.txt
+else
+	fileList=$1; echo fileList set to parameter \1\, $1
+fi
+
+tempStr=`head -n 1 $fileList`
+# NOTE that this script also assumes a closing apostraphe or single quote in the input file! :
 fileNameExt=`echo $tempStr | sed "s/.*\.\([^\.]\{1,5\}\)'.*/\1/g"`
 
 if [ -a numberedCopies ]; then rm -rf numberedCopies; mkdir numberedCopies; else mkdir numberedCopies; fi
 
-sed "s/file '\(.*\)'/\1/g" $1 > tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
+# NOTE ALSO that this script assumes a list formatted for concatenation by ffmpeg, and makes a temp copy of the list removing that syntax:
+sed "s/file '\(.*\)'/\1/g" $fileList > tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
 
-numElements=`wc -l < $1`
+numElements=`wc -l < $fileList`
 digitsCount=${#numElements}
 
 counter=0
