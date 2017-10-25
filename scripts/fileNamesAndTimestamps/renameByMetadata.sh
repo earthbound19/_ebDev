bcd62388d440049caf15cc02fda1a5acca49b96e
Alex Hall <ansible1@earthbound.io>
2017-10-25 09:01:04 -0600

    new scripts

---
+++ scripts/fileNamesAndTimestamps/renameByMetadata.sh
@@ -0,0 +1,43 @@
+# WARNING: this script renames files without backup and without any prompt. For this reason everything under the CODE comment is commented out, and you must manually uncomment the code to use it, and I urge you to then comment it out again when done using it.
+
+# DESCRIPTION
+# Renames image files and .MOV and .mp4 files after dateTimeOriginal and createDate metadata, respectively.
+
+# DEPENDENCY
+# This script must be in your $PATH.
+
+# USAGE
+# Invoke from a directory with media files you wish to so rename, e.g.:
+# ./thisScript.sh
+
+# KNOWN ISSUES
+# THIS MAY NOT PERFECTLY segregate by creation date metadata type; it potentially renames many files twice (first by creation date metadata, then dateTimeOriginal metadata). ALSO, for files from some sources mixed with others (something doing with dateTimeOriginal metadata in one jpg source and not another?) it may loop endlessly . . .
+
+# REFERENCE
+# https://gist.github.com/rjames86/33b9af12548adf091a26
+# https://ninedegreesbelow.com/photography/exiftool-commands.html#rename
+
+# UNRELATED REFERENCE
+# https://sno.phy.queensu.ca/~phil/exiftool/filename.html
+
+# TO DO
+# Update all dateTimeOriginal metadata which lacks milliseconds by adding random milliseconds before the next line of code which appears later:
+	# exiftool '-dateTimeOriginal<fileCreateDate' -if '(($dateTimeOriginal)) and ($filetype eq "JPEG")' .
+# Exploit that given at this URL? : https://smarnach.github.io/pyexiftool/
+
+# CODE
+
+# renames all image formats in current directory which exiftool decides to:
+exiftool -v -overwrite_original '-Filename<${dateTimeOriginal}${subsecTimeOriginal;$_.=0 x(3-length)}.%e' -d "%Y_%m_%d__%H_%M_%S" .
+# renames all .mov files in current directory:
+exiftool -v -overwrite_original '-Filename<${createDate}.%e' -d "%Y_%m_%d__%H_%M_%S" *.MOV
+# renames all .m4v files in current directory:
+exiftool -v -overwrite_original '-Filename<${createDate}.%e' -d "%Y_%m_%d__%H_%M_%S" *.m4v
+# It's important that these next commands are run last:
+# rename all JPEG format files that which lack a dateTimeOriginal metadata field by file creation date, with a %%c counter against any potential duplicate resulting file names (making unique file names where the time stamp is identical) ; re https://sno.phy.queensu.ca/~phil/exiftool/exiftool_pod.html#Advanced-formatting-feature :
+# perl expressions can be done on tags in this format:
+# ${TAG;EXPR}
+# (e.g. ) ${-FileName;somePerlExprThatMakesRandomCharacters}
+exiftool -v -overwrite_original '-FileName<FileCreateDate' -d %Y_%m_%d__%H_%M_%S__%%c.%%e -if '((not $dateTimeOriginal)) and ($filetype eq "JPEG")' .
+# For all JPEG format files which lack a dateTimeOriginal field, create one from fileCreateDate:
+exiftool -v -overwrite_original '-dateTimeOriginal<fileCreateDate' -if '(($dateTimeOriginal)) and ($filetype eq "JPEG")' .
---
+++ scripts/imgAndVideo/krita2IMG.sh
@@ -0,0 +1,16 @@
+# DESCRIPTION
+# krita CLI wrapper to convert from a krita source image to another destination image format (you might call this e.g. effectively kra2png).
+
+# DEPENDENCY
+# An install of krita http://krita.org in your $PATH.
+
+# USAGE
+# Pass this script two parameters, being:
+# $1 input .kra (or other formats too?) filename
+# $2 desired output extension, without a period, e.g. png
+# e.g.
+# thisScript.sh input.kra png
+
+# re: https://userbase.kde.org/Krita/Manual/CommandLine (it is actually enabled, apparently, on Windows) :
+
+krita $1 --export --export-filename $1.$2
