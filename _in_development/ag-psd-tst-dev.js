2a685650d6f8b77a5039df6898f44acd47092bf5
Alex Hall <ansible1@earthbound.io>
2017-10-29 21:10:59 -0600

    dev snapshot

--- _in_development/ag-psd-tst-dev.js
+++ _in_development/ag-psd-tst-dev.js
@@ -1,22 +1,24 @@
 // Working up this test re:  re: https://www.npmjs.com/package/ag-psd
-console.log("wut");
+console.log('wut')
 
 		// THE NEXT LINE IS DEPRECATED and updated to the line after it, re: https://stackoverflow.com/a/39436580
-		// import * as fs from 'fs';
-const fs = require("fs") ;
-		// import { writePsd } from 'ag-psd';
+		// import * as fs from 'fs'
+const fs = require('fs') 
+		// import { writePsd } from 'ag-psd'
 // CONTINUE CODING HERE:
-// const writePSD = require("ag-psd");
+// const agpsd = require('buzzphrase')
+// const agpsd = require('ag-psd')
+// const writePSD = require('ag-psd')
 
-const psd = {
-  width: 300,
-  height: 200,
-  children: [
-    {
-      name: 'Layer #1',
-    }
-  ]
-};
+// const psd = {
+  // width: 300,
+  // height: 200,
+  // children: [
+    // {
+      // name: 'Layer #1',
+    // }
+  // ]
+// }
  
-// const buffer = writePsd(psd);
-// fs.writeFileSync('my-file.psd', buffer);
+// const buffer = writePsd(psd)
+// fs.writeFileSync('my-file.psd', buffer)
