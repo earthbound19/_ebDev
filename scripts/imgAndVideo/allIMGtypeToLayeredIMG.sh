975c1e076fafbd2e28fc4b8ce2531d64f6ce06d6
Alex Hall <ansible1@earthbound.io>
2017-11-05 01:39:17 -0600

    convert to more workable format (.ora recommended) from tif, via krita

--- scripts/imgAndVideo/allIMGtypeToLayeredIMG.sh
+++ scripts/imgAndVideo/allIMGtypeToLayeredIMG.sh
@@ -1,13 +1,22 @@
 # DESCRIPTION
-# Combines all input image files of type $1 (parameter 1 to script) into a layered output image file (at this writing, tif format), _all$1_layered.tif
+# Combines all input image files of type $1 (parameter 1 to script) into a layered output image file. Suggested options: kra, ora, or psd; ora (works with Krita) recommended.
+
+# DEPENDENCIES
+# GraphicsMagick and Krita, both in your $PATH.
 
 # USAGE
-# Invoke this script with one parameter, being the file type to operate on, e.g.:
-# ./thisScript.sh png
+# Invoke this script with two parameters, being $1 the extension of all files in your current directory to operate on, and $2 (optional) the desired final layered format.
+# ./thisScript.sh tif ora
+
+# NOTES
+# FOR BEST RESULTS start with .tif images that store alpha information. Otherwise, IF YOUR SOURCE images are in a format (even with alpha information) other than .tif, graphicsmagick may replace alpha values in each layer with black, which you'll want to eliminate with an unmultiply filter (in photoshop and/or filter forge).
+# Script first converts to a layered .tif, then the target format. I discourage archiving images in layered .tif format, because Krita and Photoshop (at least) read them differently; Krita understands them, but Photoshop only reads one layer!
 
 # TO DO
-# Parameterize output file type
+# Set defaulf ora format for destination file.
 
+
+# CODE
 # Template command:
 # gm convert 1.png 2.png 3.png 4.png out.tif
 
@@ -24,4 +33,7 @@ rm all_$1.txt
 # echo $inputFiles
 gm convert $inputFiles _all_$1_layered.tif
 
-echo DONE. See result image _all_$1_layered.tif.
+krita _all_$1_layered.tif --export --export-filename _all_$1_layered.$2
+# rm _all_$1_layered.tif
+
+echo DONE. See result image _all_$1_layered.$2
