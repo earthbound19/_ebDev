50f9227d2b565dea06471029d059a44b1d0ea1a8
earthbound19 <>
2017-11-09 14:41:00 -0700

    compared extravehicular relationships

--- macCustomize.sh
+++ macCustomize.sh
@@ -7,5 +7,8 @@ done < ./npmPackages.txt
 # Enable "Allow from Anywhere" in app gatekeeper on macOS Sierra (seriously, Apple, you are AWOL with your controls--I have to enable even the *option* to install apps from anywhere by entering a super-user terminal command?! Isn't that anti-competitive?), re: http://osxdaily.com/2016/09/27/allow-apps-from-anywhere-macos-gatekeeper/
 sudo spctl --master-disable
 
+# date format "Phrase content" for PhraseExpress text expander for mac, for hotkey/sequence to type full date and time:
+#   {#datetime -f yyyy.mm.dd dddd t am/pm}
+
 # command for Atom open-terminal-here package on Mac (requires ttab to be installed) which allows opening any path to terminal by shortcut:
 # ttab && cd "$PWD"
