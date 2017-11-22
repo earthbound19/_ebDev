b5eef2d9e70280fc7616bfccddcbd94dd53a8393
Alex Hall <ansible1@earthbound.io>
2017-11-22 09:36:33 -0700

    figure bitmap size from input data file size (square bitmap, each side ~square root of data size in bytes)

--- scripts/imgAndVideo/data2BMPglitchArt.sh
+++ scripts/imgAndVideo/data2BMPglitchArt.sh
@@ -10,11 +10,24 @@
 # DETAILS
 # Does a square root calculation (rounded) from the byte size of the data to determine the bmp X and Y dimensions which this creates a header from.
 
-gm convert -compress none -size 500x500 xc:gray stub.bmp
+# TO DO
+# Various data mapping, signal processing/pattern matching type things to data to more meaningfully map it to color values. e.g. for every  raw RGB (three values from 0 to 255)-aligned datum map to fewer values approximating the range of that datum?
+# Make a script that does this in ppm format, translating hex-mapped values to RGB values?
+
+fileToMakeCorruptedCopyOf=$1
+__ln=( $( ls -Lon "$fileToMakeCorruptedCopyOf" ) )
+__size=${__ln[3]}
+		# echo file size in bytes is $__size
+		# echo square root of that (rounded to integer) is:
+		# echo "sqrt ($__size)" | bc
+side=`echo "sqrt ($__size)" | bc`
+		# echo side val is\: $side
+
+gm convert -compress none -size "$side"x"$side" xc:gray stub.bmp
 		# NOTE that we want 2 x 27 bytes (count=2, bs=27) for 54 bytes, the size of the header as I'm seeing it in a hex editor:
 		# Byte size of header figured from http://www.dragonwins.com/domains/getteched/bmp/bmpfileformat.htm (file header + image header size) and observing color values white FF FF FF and gray 7E 7E 7E at offset 0x36 (hex for decimal 54) in a hex editor.
-dd bs=27 count=2 if=stub.bmp of=stubHeader.dat
-cat stubHeader.dat tst4.bmp > wut.bmp
+dd status=none bs=27 count=2 if=stub.bmp of=stubHeader.dat
+cat stubHeader.dat $fileToMakeCorruptedCopyOf > "$fileToMakeCorruptedCopyOf"_asBMP.bmp
 
 
-rm stub.bmp stubHeader.dat
+# rm stub.bmp stubHeader.dat
