76cb0c84860a0614b7365580bea2a179bd469b01
earthbound19 <>
2017-11-23 20:02:44 -0700

    Fixed (it was all wrong)

--- scripts/imgAndVideo/data2PPMglitchArt.sh
+++ scripts/imgAndVideo/data2PPMglitchArt.sh
@@ -1,5 +1,12 @@
+# Make a script that does this in ppm format, translating hex-mapped values to RGB values? Could (slowly) convert hex pairs to decimal numbers between 0 and 255 this way:
+# valOne=`xxd -ps ocean2.wav`
+# ..read over pairs of those values (put each in valTwo) and convert them to decimal, by adapting this, assuming $x is a string variable, and $p is an incrementing position:
+# echo ${x:$p:2}          ;# syntax is ${string:index:length}
+# ..
+
+
 # DESCRIPTION
-# Makes glitch art from any data source by creating a ppm header approximating a defined image size (at this writing square) into which that data would fit; takes that image header and writes rows of copied hex value pairs (converted to decimal) into an RGB value array which composes the remainder of the PPM format file. The result may be converted to any other image format e.g. via GraphicsMagick. Results end up in a new PPM image file named "$inputDataFile"_asPPM.ppm
+# Makes glitch art from any data source by creating a ppm header approximating a defined image size (at this writing square) into which that data would fit; takes that image header and slaps raw copied hex value pairs (converted to decimal) into an RGB value array which composes the remainder of the PPM format file. The result may be converted to any other image format e.g. via GraphicsMagick.
 
 # USAGE
 # ./thisScript.sh dataSource.file
@@ -7,47 +14,61 @@
 # DEPENDENCIES
 # a 'nix environment, xxd
 
+# DETAILS
+# Does a square root calculation (rounded) from the byte size of the data to determine the bmp X and Y dimensions which this creates a header from.
+
+# pseudo-code:
+# get data size
+#     the calc of ~bmp script is wrong i think! TO DO: fix that.
+ # bcse 3 vals per px, sqrt(data size / 3) = x and y dim. of img?
+# hrm. if data size is 27:
+# sqrt (27 / 3) = 3
+# x and y ppx dim. = that (3)
+# f f f f f f f f f = triplet of pix (3 px wide); 9 vals
+# f f f f f f f f f
+# f f f f f f f f f = 3 rows of 9 vals = 27 = orig. data length.
+
+
 # CODE
-# Get size of input data in bytes
 inputDataFile=$1
 __ln=( $( ls -Lon "$inputDataFile" ) )
 __size=${__ln[3]}
-# Assume a square target image size for the bent data by square root of that size, divided by three (because every triplet of hex pairs will make up one RGB value (or array of three values between 0-255) :
-BMPsideLength=`echo "sqrt ($__size)" | bc`
-BMPsideLength=$((BMPsideLength / 3))
-    echo BMP length on each side will be $BMPsideLength. Churning HEX data into decimal triplets\, each of which will be an RGB value . . .
+IMGsideLength=`echo "sqrt ($__size / 3)" | bc`
+valsPerRow=$((IMGsideLength * 3))
 
-# Make P3 PPM format header, which a later generated array of RGB values will be concatenated to:
+# Make P3 PPM format header:
 echo "P3" > PPMheader.txt
-echo "# The P3 means colors are in ASCCI decimal, then $BMPsideLength columns and $BMPsideLength rows, then 255 for max color, then RGB triplets within that range:" >> PPMheader.txt
-echo $BMPsideLength $BMPsideLength >> PPMheader.txt
-echo 255 >> PPMheader.txt
+echo "# P3 means ASCCI, $IMGsideLength $IMGsideLength is cols x rows, ff is max color (hex 255), triplets of hex vals per RGB val." >> PPMheader.txt
+echo $IMGsideLength $IMGsideLength >> PPMheader.txt
+echo ff >> PPMheader.txt
 
-# Will this break for very large files? :
+echo impoarting data from $1 to local variable..
 hexMonolith=`xxd -ps $inputDataFile`
-# Remove spaces (and line breaks?) from that:
+# remove spaces (and line breaks?) from that:
 hexMonolith=`echo $hexMonolith | tr -d ' \n'`
 
+# Values used by the following for loop:
 hexPairIndexCounter=0
 colsCount=0
 printf "" > PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt
-RGBvalsRowTXT=
-for i in $( seq $__size )
+dataCutoff=$(( $valsPerRow * $IMGsideLength))   # For where there is more data than can fit in an image IMGsideLength x IMGsideLength
+valsRowTXT=
+
+echo Creating PPM $IMGsideLength x $IMGsideLength . . .
+for i in $( seq $dataCutoff )
 do
-      tmpHEX=${hexMonolith:$hexPairIndexCounter:2}
-      RGBval=`echo $((16#$tmpHEX))`
-      RGBvalsRowTXT="$RGBvalsRowTXT $RGBval"
+      HEXval=${hexMonolith:$hexPairIndexCounter:2}
+      # echo HEXval $HEXval
+      valsRowTXT="$valsRowTXT $HEXval"
+          # echo $valsRowTXT
       colsCount=$((colsCount + 1))
-      if (($colsCount == $BMPsideLength))
+      if (($colsCount == $valsPerRow))
       then
-        # append RGBvalsRowTXT row to PPM body buildup file, and reset colsCount and RGBvalsRowTXT for next row accumulation:
-        echo Made row at offset $i of $__size\; RGB decimal values\:
-        echo $RGBvalsRowTXT
-        echo appending that to PPM text body . . .
-        echo $RGBvalsRowTXT >> PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt
-        echo ~~
+        # append valsRowTXT row to PPM body buildup file, and reset colsCount and valsRowTXT for next row accumulation:
+        echo $valsRowTXT >> PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt
+        echo wrote new row at offset $1\: $valsRowTXT
         colsCount=0
-        RGBvalsRowTXT=
+        valsRowTXT=
       fi
       hexPairIndexCounter=$((hexPairIndexCounter + 2))
 done
@@ -56,5 +77,4 @@ cat PPMheader.txt PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt > "$inputDataFile"_a
 
 rm PPMheader.txt PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt
 
-# OPTIONAL open of the file immediately after render (for Cygwin, "open" must be "cygstart") :
 # open "$inputDataFile"_asPPM.ppm
