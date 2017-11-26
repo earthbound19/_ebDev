ad18057f6d49b51a92abc92b5ba26755e7b19b8c
Alex Hall <ansible1@earthbound.io>
2017-11-25 19:34:56 -0700

    Automatically use IrfanView (instead of GraphicsMagick) to convert if source format is PPM (GraphicsMagick isn't reading such files at this writing)

--- scripts/imgAndVideo/imgs2imgsNN.sh
+++ scripts/imgAndVideo/imgs2imgsNN.sh
@@ -1,16 +1,19 @@
 # DESCRIPTION
-# Resizes all images of type T (via parameter $1) in a path, by nearest-neighbor method, to target format F ($2), at size A x B ($3 x $4).
+# Resizes all images of type T (via parameter $1) in a path, by nearest-neighbor method, to target format F ($2), at size A x B ($3 x $4). Uses GraphicsMagick, unless the file is ppm format, and in that case it uses IrfanView.
 
 # USAGE
-# Invoke this script with four parameters:
+# Invoke this script with the following parameters
 # $1 source format (without .)
 # $2 destination format
 # $3 scale by nearest neighbor method to pixels X
-# $4 scale by nearest neighbor method to pixels Y. IF OMITTED, scales to $3 (X pix) by nearest neighbor preserving aspect ratio. Example command for that:
+# $4 scale by nearest neighbor method to pixels Y. IF OMITTED, scales to $3 (X pix) by nearest neighbor preserving aspect ratio. NOT ENABLED for ppm (IrfanView) conversion; for that use only $3. See irfanView2imgNN.sh comments for options you may manually use to specify non-ratio sizes.
+# Example command:
 # imgs2imgsnn.sh ppm png 640
 # OR, to force a given x by y dimension:
-# imgs2imgsnn.sh ppm png 640 480
+# imgs2imgsNN.sh ppm png 640 480
 
+# DEPENDENCIES
+# GraphicsMagick, possibly IrfanView, both in your $PATH.
 
 	# DEPRECATED command for unexpected behavior; it may be that the following command somehow caused nconvert to iterate over every source file format by wildcard? Removing the . from the command, it iterates over the list; whereas with the . it did so twice:
 	# find . *.$1 > all_$1.txt
@@ -18,21 +21,23 @@ find *.$1 > all_$1.txt
 
 while read img
 do
-			# echo img is $img
-	imgFileNoExt=`echo $img | gsed 's/\(.*\)\..\{1,4\}/\1/g'`
-	if [ ! -f $imgFileNoExt.$2 ]; then
-		echo ~~
-		echo RENDERING target file $imgFileNoExt.$2 as it does not exist . . .
-			# DEPRECATED:
-			# nconvert -rtype quick -resize $3 -out $2 -o $imgFileNoExt.$2 $img
-			# ex. command of newly preferred tool:
-			# gm convert 6x5gridRND_2017_05_06__01_51_14__099842100.ppm -scale 1200 out.png
-		# If params $3 or $4 were not passed to the script, the command will simply be empty where they are (on the following line of code), and it should still work:
-		gm convert $img -scale $3 $4 $imgFileNoExt.$2
-		echo ~~
-# exit
+	imgFileNoExt=`echo $img | sed 's/\(.*\)\..\{1,4\}/\1/g'`
+	imgFileExt=`echo $img | sed 's/.*\.\(.\{1,4\}\)/\1/g'`
+	targetFileName=$imgFileNoExt.$2
+	if [ ! -f $targetFileName ]; then
+		echo RENDERING target file $targetFileName as it does not exist . . .
+		# If the source file format is ppm, use Irfanview to do the conversion (at this writing, I find that only IrfanView reads ppm format). Otherwise, use GraphicsMagick.
+		if [ $imgFileExt == "ppm" ]; then
+			i_view32.exe $img /resize_long=$3 /aspectratio /convert=$targetFileName
 		else
-			echo Target file $imgFileNoExt.$2 already exists\; delete the file if you wish to re-render it\; SKIPPING RENDER . . .
+				# ex. GraphicsMagick command:
+				# gm convert 6x5gridRND_2017_05_06__01_51_14__099842100.ppm -scale 1200 out.png
+			# If params $3 or $4 were not passed to the script, the command will simply be empty where they are (on the following line of code), and it should still work:
+			gm convert $img -scale $3 $4 $targetFileName
+		fi
+		echo ~~
+	else
+			echo Target file $targetFileName already exists\; delete the file if you wish to re-render it\; SKIPPING RENDER . . .
 	fi
 done < all_$1.txt
 
