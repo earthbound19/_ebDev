fb16416b6c523f05645f170ed945b3e4a8abc0e4
Alex Hall <ansible1@earthbound.io>
2017-11-25 17:30:50 -0700

    upscale img by nearest neighbor method via various utils (created while searching for ppm upscale solution, which IrfanView could do)

---
+++ scripts/imgAndVideo/irfanView2imgNN.sh
@@ -0,0 +1,20 @@
+# USAGE
+# Invoke this script with three parameters, being:
+# $1 input file
+# $2 output format
+# $3 px wide to resize to by nearest neighbor method, maintaining aspect
+
+# TO DO
+# Name output file after input file base name plus new extension
+
+
+# CODE
+# Command switches used to build up command; re: http://www.robvanderwoude.com/files/iviewcli.txt
+# /resize_long=X        - resize input image: set long side to X
+# /aspectratio          - used for resizes: keep image proportions
+# /convert=filename     - convert input file(s) to "filename" and CLOSE IrfanView
+# example for resize:
+# i_view32.exe c:\test.jpg /resize=(300,300) /resample
+
+imgFileNoExt=`echo $1 | sed 's/\(.*\)\..\{1,4\}/\1/g'`
+i_view32.exe $1 /resize_long=$3 /aspectratio /convert=$imgFileNoExt.$2
