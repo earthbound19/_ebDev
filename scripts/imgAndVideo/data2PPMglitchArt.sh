7b7095b3f78ad4e77986e02a5e3121284c0dbd98
Alex Hall <ansible1@earthbound.io>
2017-11-25 17:28:58 -0700

    add irfanview convert and upscale NN to png options, comment updates

--- scripts/imgAndVideo/data2PPMglitchArt.sh
+++ scripts/imgAndVideo/data2PPMglitchArt.sh
@@ -1,44 +1,38 @@
-# Make a script that does this in ppm format, translating hex-mapped values to RGB values? Could (slowly) convert hex pairs to decimal numbers between 0 and 255 this way:
-# valOne=`xxd -ps ocean2.wav`
-# ..read over pairs of those values (put each in valTwo) and convert them to decimal, by adapting this, assuming $x is a string variable, and $p is an incrementing position:
-# echo ${x:$p:2}          ;# syntax is ${string:index:length}
-# ..
-
-
 # DESCRIPTION
-# Makes glitch art from any data source by creating a ppm header approximating a defined image size (at this writing square) into which that data would fit; takes that image header and slaps raw copied hex value pairs (converted to decimal) into an RGB value array which composes the remainder of the PPM format file. The result may be converted to any other image format e.g. via GraphicsMagick.
+# Makes glitch art from any data source by creating a ppm header approximating a defined image size (at this writing square) into which that data would fit; takes that image header and slaps raw copied hex value pairs (converted to decimal) into an RGB value array which composes the remainder of the PPM format file. The result may be converted to any other image format, apparently only by IrfanView (GraphicsMagick and NConvert choke on ppm files with hex values; IrfanView doesn't).
 
 # USAGE
 # ./thisScript.sh dataSource.file
 
 # DEPENDENCIES
-# a 'nix environment, xxd
-
-# DETAILS
-# Does a square root calculation (rounded) from the byte size of the data to determine the bmp X and Y dimensions which this creates a header from.
-
-# pseudo-code:
-# get data size
-#     the calc of ~bmp script is wrong i think! TO DO: fix that.
- # bcse 3 vals per px, sqrt(data size / 3) = x and y dim. of img?
-# hrm. if data size is 27:
-# sqrt (27 / 3) = 3
-# x and y ppx dim. = that (3)
-# f f f f f f f f f = triplet of pix (3 px wide); 9 vals
-# f f f f f f f f f
-# f f f f f f f f f = 3 rows of 9 vals = 27 = orig. data length.
+# a 'nix environment, xxd, optionally IrfanView and irfanView2imgNN.sh
 
 
 # CODE
+
+# pseudo-code:
+# get data size
+		# bcse 3 vals per px, sqrt(data size / 3) = x and y dim. of img
+# ex. if data size is 27:
+# sqrt (27 / 3) = 3
+# x and y ppx dim. = that (3)
+		# check:
+		# f f f f f f f f f = triplet of pix (3 px wide); 9 vals
+		# f f f f f f f f f
+		# f f f f f f f f f = 3 rows of 9 vals = 27 = orig. data length.
+
+imgFileNoExt=`echo $1 | sed 's/\(.*\)\..\{1,4\}/\1/g'`
+ppmDestFileName="$imgFileNoExt""_asPPM.ppm"
+
 inputDataFile=$1
 __ln=( $( ls -Lon "$inputDataFile" ) )
 __size=${__ln[3]}
-IMGsideLength=`echo "sqrt ($__size / 3)" | bc`
+IMGsideLength=`echo "sqrt ($__size / 3)" | bc`		# / 3 because three vals per pixel
 valsPerRow=$((IMGsideLength * 3))
 
 # Make P3 PPM format header:
 echo "P3" > PPMheader.txt
-echo "# P3 means ASCCI, $IMGsideLength $IMGsideLength is cols x rows, ff is max color (hex 255), triplets of hex vals per RGB val." >> PPMheader.txt
+echo "# P3 means text file, $IMGsideLength $IMGsideLength is cols x rows, ff is max color (hex 255), triplets of hex vals per RGB val." >> PPMheader.txt
 echo $IMGsideLength $IMGsideLength >> PPMheader.txt
 echo ff >> PPMheader.txt
 
@@ -58,9 +52,7 @@ echo Creating PPM $IMGsideLength x $IMGsideLength . . .
 for i in $( seq $dataCutoff )
 do
       HEXval=${hexMonolith:$hexPairIndexCounter:2}
-      # echo HEXval $HEXval
       valsRowTXT="$valsRowTXT $HEXval"
-          # echo $valsRowTXT
       colsCount=$((colsCount + 1))
       if (($colsCount == $valsPerRow))
       then
@@ -73,8 +65,19 @@ do
       hexPairIndexCounter=$((hexPairIndexCounter + 2))
 done
 
-cat PPMheader.txt PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt > "$inputDataFile"_asPPM.ppm
+cat PPMheader.txt PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt > $ppmDestFileName
+# Optional and preferred to make the ppm file useable to all image converters that I've found besides IrfanView: convert result to spec-compliance (it would seem?) via IrfanView; the only thing I *don't* like about this is it can make column counts no longer match--which I call a bug and yet other programs seem to convert the result ok--BIG BREATH--comment out the next line if you don't want this:
+i_view32.exe $ppmDestFileName /convert=tmp_Zfrffb9Zbp2VdN.ppm && rm $ppmDestFileName && mv tmp_Zfrffb9Zbp2VdN.ppm $ppmDestFileName
 
 rm PPMheader.txt PPMtableTemp_huuRgKWvYvNtw5jd5CWPyJMc.txt
 
-# open "$inputDataFile"_asPPM.ppm
+# Optionally open the file in the default associated program (Windows) :
+# cygstart $ppmDestFileName
+
+# optionally scale up by NN method by N pix:
+# $1 input file
+# $2 output format
+# $3 px wide to resize to by nearest neighbor method, maintaining aspect
+upscaleX=$((IMGsideLength * 36))
+		# echo upscaleX val is $upscaleX where orig was $IMGsideLength
+irfanView2imgNN.sh $ppmDestFileName png $upscaleX
