ae818d2f5db569dcc0cae26036a97c9edafdb86f
earthbound19 <>
2017-11-28 11:21:00 -0700

    saved extensible projections

--- .bash_profile
+++ .bash_profile
@@ -24,6 +24,8 @@ export PATH=/Users/earthbound/Documents/breakTime/_ebPathMan:$PATH
 export PATH=/Users/earthbound/Documents/breakTime/_ebDev:$PATH
 
 echo "APPLIED CUSTOM .bash_profile settings. Also $(buzzphrase)."
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# DEPRECATED? :
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- macDevSetup.sh
+++ macDevSetup.sh
@@ -1,8 +1,16 @@
-# npm packages I regularly use:
-# PREREQUISITE; npm:
-while read element
-  echo item is $element . .
-done < ./npmPackages.txt
+pushd .
+
+# install n node version manager; re: https://github.com/tj/n/issues/169
+cd ~
+curl -L https://git.io/n-install | bash
+# TO UNINSTALL:
+# ~/n/n-uninstall
+
+n latest
+
+popd
+
+./install_global_node_modules.sh
 
 # Enable "Allow from Anywhere" in app gatekeeper on macOS Sierra (seriously, Apple, you are AWOL with your controls--I have to enable even the *option* to install apps from anywhere by entering a super-user terminal command?! Isn't that anti-competitive?), re: http://osxdaily.com/2016/09/27/allow-apps-from-anywhere-macos-gatekeeper/
 sudo spctl --master-disable
