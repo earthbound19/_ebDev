eeeb2da39f353b9385799a815efe1076dfbc06a8
Hermione Granger <Hermione Granger>
2017-12-26 11:01:58 +0000

    See comments in files

---
+++ _in_development/renderAllHexPalettes-gm.sh
@@ -0,0 +1,26 @@
+# DESCRIPTION
+# Calls renderHexPalette-gm.sh for every .hexplt file in the path from which this script is invoked. Result: all hex palette files in the current path (and subpaths!) are rendered.
+
+# USAGE
+# thisScript.sh
+
+# DEV NOTE
+# This is geared for a new developing version of the script it invokes; which script takes a different parameter order.
+
+
+# CODE
+echo "finding all *.hexplt files in the current path and subpaths . . ."
+gfind . -regex .*.hexplt -type f > all_hexplt.txt
+# trim all leading ./ crud that ruins the script calls off the start of all those lines:
+gsed -i 's/^\.\/\(.*\)/\1/g' all_hexplt.txt
+dos2unix all_hexplt.txt
+
+while read fileName
+do
+	baseFileName=`basename $fileName`
+	echo ~~~~
+	# echo invoking renderHexPalette-gm.sh for $fileName . . .
+	renderHexPalette-gm.sh $baseFileName 50 0
+done < all_hexplt.txt
+
+echo "DONE. Color palettes have been rendered from all *.hexplt files in the current path and subpaths. Palette images are named after the source *.hexplt files."
