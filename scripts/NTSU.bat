670c88fab633b392c8d7c066d21ad3946f3812ce
Hermione Granger <Hermione Granger>
2017-12-28 04:47:23 +0000

    NT System privelges cmd (NTSU New Technology Super User) via paexec

---
+++ scripts/NTSU.bat
@@ -0,0 +1,9 @@
+REM DESCRIPTION
+REM By way of paexec -i interactive and -s system flags, invoke a cmd terminal with "NT Authority/System" priveleges. It may allow you to do things an Administrator terminal can't.
+
+REM USAGE
+REM thisBatch.bat
+
+
+REM CODE
+paexec -i -s cmd
