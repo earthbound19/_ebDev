87a563ea1430ed5b6037b4265962160e242a7858
Alex Hall <ansible1@earthbound.io>
2017-12-29 11:49:34 -0700

    add bc

--- installMyCygwinToolchain.sh
+++ installMyCygwinToolchain.sh
@@ -1,2 +1,2 @@
 # Copy the cygwin installer to this directory before executing this script:
-.Cygwin_setup-x86.exe -q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel
+./Cygwin_setup-x86_64.exe -q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -q -P bc
