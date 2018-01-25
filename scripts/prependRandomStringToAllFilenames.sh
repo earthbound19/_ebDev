d55ed168f10459d91d9a42fbe10c8e461606610c
Alex Hall <ansible1@earthbound.io>
2018-01-24 20:56:19 -0700

    change loop from iterating over array to read from file loop

--- scripts/prependRandomStringToAllFilenames.sh
+++ scripts/prependRandomStringToAllFilenames.sh
@@ -1,16 +1,21 @@
-# DESCRIPTION: Given a filename, appends one random string of length 20 characters or per paramaters you pass to the script.
-# USAGE: Pass this script two paramaters, the first being the file name which you want to rename by appending a random string to it, the second being the length of the random string (default 20; number of possible strings breaks past the nonillians).
+# DESCRIPTION
+# Given a filename, appends one random string of length 20 characters or per paramaters you pass to the script.
 
-ls > meerp.txt
-mapfile -t fileNamesArray < ./meerp.txt
+# USAGE
+# Pass this script one parameter $1, being the length of the random string (default 20; number of possible strings breaks past the nonillians) to prepend to every file name in the current folder.
 
-for n in ${fileNamesArray[@]}
+
+# CODE
+if [ -z ${1+x} ]; then echo No paramater one \(length of random string to prepend\)\. Defaulting to 20\.; rndStringlength=20; else rndStringlength=$1; fi
+
+ls > meerp_gvucavE6ahYEWmEJq267.txt
+while read n
 do
-	if [[ $1 == "" ]]; then fileName=; else fileName=$1; fi
-	if [[ $2 == "" ]]; then length=20; else length=$2; fi
 			# No l, L, ,i, I, O, 1, 0, as those can get confused:
-	randString=`cat /dev/urandom | tr -dc 'a-hj-km-np-zA-HJ-KM-NP-Z2-9' | head -c $length`
+	randString=`cat /dev/urandom | tr -dc 'a-hj-km-np-zA-HJ-KM-NP-Z2-9' | head -c $rndStringlength`
 	echo executing command mv $n $randString\_$n . . .
-	mv $n $randString\_$n
-	# That's an escaped underscore thar--the variables won't print if it isn't escaped.
-done
+	mv $n "$randString"_"$n"
+done < meerp_gvucavE6ahYEWmEJq267.txt
+
+# Funny, we have to use a wildcard here because by the time this script ends, the temp .txt file is renamed with a prepended random string:
+rm *meerp_gvucavE6ahYEWmEJq267.txt
