bf7ea0971a6b705561eea1ea09a029291c4d2ed9
Alex Hall <ansible1@earthbound.io>
2018-01-27 11:30:51 -0700

    new scripts

---
+++ scripts/imgAndVideo/ffmpegLosslessSoundExcerpt.sh
@@ -0,0 +1,21 @@
+# DESCRIPTION
+# Losslessly copies sound out of (virtually) any media file starting at $1 seconds for $2 duration (according to parameters passed to script)
+
+# USAGE
+# Invoke this script with two parameters, being $1 the file to excerpt from, $2 the seconds to start at (resulution 00:00:00.0 available) and $3 how long of a sound clip to losslessly extract (same time resolution available) from that position. If only seconds, leading zeros (00: etc) can be omitted. Example that would copy 30 seconds of sound starting at 12 seconds:
+# ./thisScript.sh inputFile.m4a 12 20
+
+# REFERENCE: ffmpeg parameters: -map 0:a : operate only on input audio stream, -ss : seconds to start at, -t: duration to copy, -acodec copy: copy contents without recompression of any kind (LOSSLESS).
+
+# TO DO
+# Create an optional parameter $3, which is the end position of the excerpt, which will be subtracted from $1 to produce (and override) $2? Would require parsing and converting format 00:00:00.0 into seconds, substracting, then returning to that time format.
+
+
+# CODE
+# TO DO: update all scripts that extract to variables fileNameNoExt etc. to use this far more elegant method found at: https://www.cyberciti.biz/faq/unix-linux-extract-filename-and-extension-in-bash/
+fileExt=`echo "${1##*.}"`
+		# echo fileExt val is $fileExt
+fileNameNoExt=`echo "${1%.*}"`
+		# echo fileNameNoExt val is $fileNameNoExt
+
+ffmpeg -y -i "$1" -ss $2 -t $3 -map 0:a -acodec copy "$fileNameNoExt"_soundLosslessExtract_ss"$2"_t"$3"."$fileExt"
