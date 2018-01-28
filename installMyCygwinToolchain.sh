58a660725ad989441e1d1af3efa9c79ecbf0020a
Alex Hall <ansible1@earthbound.io>
2018-01-27 22:32:50 -0700

    add used tool and update for x86 or x86_64 cygwin options

--- installMyCygwinToolchain.sh
+++ installMyCygwinToolchain.sh
@@ -1,2 +1,5 @@
-# Copy the cygwin installer to this directory before executing this script:
-./Cygwin_setup-x86_64.exe -q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -q -P bc
+# Copy the cygwin installer to this directory before executing this script. Comment out the line for the cygwin installer version you don't want to use.
+installParams="-q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -q -P bc -P xxd"
+
+./Cygwin_setup-x86.exe $installParams
+# ./Cygwin_setup-x86_64.exe $installParams
