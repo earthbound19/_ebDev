eba953fd60577be44c86981a764c35e442353cb5
Alex Hall <ansible1@earthbound.io>
2018-01-29 23:19:09 -0700

    read password from home directory file and pass as auth. parameter to new putty session (INSECURE!)

---
+++ scripts/puttyAuth.sh
@@ -0,0 +1,8 @@
+# WARNING: storing your password in an unencrypted file anywhere is a bad idea.
+
+# TO DO:
+# Use utils to encrypt the password with an at least more secure password in the file read into this; decrypt-able only with that password.
+# OR THIS: https://www.howtoforge.com/how-to-configure-ssh-keys-authentication-with-putty-and-linux-server-in-5-quick-steps
+
+pw=$(< ~/puttyAuth.txt)
+putty.exe ussinsor@ussins.org -pw $pw
