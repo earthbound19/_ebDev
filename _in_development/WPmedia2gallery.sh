428b1fb8d6a17832f1c8b9ba5f160b482396fe79
Alex Hall <ansible1@earthbound.io>
2018-02-05 22:36:40 -0700

    produces valid json result from valid query string parameter passed to script

--- _in_development/WPmedia2gallery.sh
+++ _in_development/WPmedia2gallery.sh
@@ -1,12 +1,15 @@
-# reference REST API queries:
+# USAGE
+# $1 "query string"
 
+# reference REST API queries:
 # Get image by title:
 # earthbound.io/blog/wp-json/wp/v2/media?filter[image]&fields=media_details.image_meta.title&search=narmth
-
 # Get image URL and title by title search:
 # earthbound.io/blog/wp-json/wp/v2/media?filter[image]&fields=media_details.image_meta.title,media_details.sizes.medium_large.source_url&search=narmth
-
 # get all image info by title (in quotes)
 # earthbound.io/blog/wp-json/wp/v2/media?filter[image]&search="Work 00088 Fractal Flame"
 
-curl earthbound.io/blog/wp-json/wp/v2/media?filter[image]&search="00076" > wut.txt
+queryString=$1
+queryString=`echo "$queryString" | sed -f /cygdrive/c/_ebdev/scripts/urlencode.sed`
+queryString="earthbound.io/blog/wp-json/wp/v2/media?filter\[image\]&search=\"$queryString\""
+curl $queryString > wut.txt
