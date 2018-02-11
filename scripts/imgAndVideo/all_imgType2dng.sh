576a3b0b0e747440ae8f3347b86365ed9fcfda4e
Alex Hall <ansible1@earthbound.io>
2018-02-10 17:14:06 -0700

    This shouldn't even register as a file change?

--- scripts/imgAndVideo/all_imgType2dng.sh
+++ scripts/imgAndVideo/all_imgType2dng.sh
@@ -5,11 +5,12 @@
 # Call this script with one parameter $1, being a file type in the directory from which you run this script--a file type for which all thing whom what this yes you wish to convert to Adobe digital negatives format (.dng), e.g.:
 # thisScript.sh CR2
 
+
 find . -iname \*.$1 > all_wut.txt
 while read element
 do
 # TO DO: check if target file exists (get base file name of element first), then only create it if it doesn't.
-	elementBaseName=`echo $element | sed "s/\(.*\)\.$1/\1/g"`
+	elementBaseName="${element%.*}"
 	if [ ! -f "$elementBaseName".dng ]
 	then
 		echo "$elementBaseName".dng does not exist\; will execute dng conversion command.
