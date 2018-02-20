6a56b2e281e5e5b4149a171163d32b87a02b1172
Alex Hall <ansible1@earthbound.io>
2018-02-19 22:21:27 -0700

    rename

--- scripts/imgAndVideo/WPmedia2markdownGallery.sh
+++ scripts/imgAndVideo/WPmedia2markdownGallery.sh
@@ -2,14 +2,17 @@
 # Makes a markdown gallery of remote images from a title query to the Wordpress JSON REST API.
 
 # USAGE
-# $1 "query string"
+# ./thisScript.sh $1 "An image title name"
 
 # DEPENDENCIES
-# Unix-y environment, jq CLI json parser
+# Unix-y environment, jq CLI json parser, an image in a wordpress media database (NOTE: hard-coded--TO DO--? make the WP-json URL a parameter?) bearing the same title (in metadata) which you pass this script via $1.
+
 
 # TO DO
 # - Have MD_ADDS2markdownGallery.sh call this after extracting a title from an ~MD_ADDS.txt file? Adapt code for that here?
 # - Turn any URL in the text pattern "See http://replaceThisURL for original, print and usage" into a markdown URL. OR turn all URLs into markdown links?
+# - HTML conversion: turn linked stylesheets into inline via some tool? Font references into google or other fonts? :/
+# - AND/OR strip styles and unused html info from html via some tool?
 
 # DEVELOPER NOTES
 # The WP-JSON REST API, it seems, forcibly farts if you set per_page greater than 100. If it ever comes to wanting more than 100 results per query, I'll have to hack Wordpress to overcome that or make this script paginate. Ugh.
@@ -55,3 +58,11 @@ curl $queryString > tmp_DVGXNAUaQe9298.json
 jq -r '.[] | "## \(.media_details.image_meta.title)\n\n[ ![\(.media_details.image_meta.title)](\(.media_details.sizes.medium_large.source_url)) ](\(.media_details.sizes.full.source_url))\n\n\(.alt_text)\n\n" ' tmp_DVGXNAUaQe9298.json >> $target_md_fileName
 
 rm tmp_DVGXNAUaQe9298.json tmp_DVGXNAUaQe9298_prettyPrinted.json
+
+# ALSO TEH BOUNOOS! Convert to HTML!
+
+# CONVERT MARKDOWN TO HTML
+publish-markdown-style.sh $target_md_fileName
+
+# OPTIONAL open of resultant html file, assuming the result file name matches that produced by publish-markdown-style.sh:
+	# cygstart ./_dist/$HTMLfinalFileName
