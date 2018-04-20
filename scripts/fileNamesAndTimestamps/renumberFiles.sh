80da5216469845534c5388a9bcf5f05483446a91
earthbound19 <ansible1@earthbound.io>
2018-04-19 20:02:19 -0600

    Take  out (fails on Mac) and create array in-memory.

--- scripts/fileNamesAndTimestamps/renumberFiles.sh
+++ scripts/fileNamesAndTimestamps/renumberFiles.sh
@@ -10,36 +10,28 @@
 # TO DO? give this script a warning y/n prompt.
 # TO DO? Make the option to move all renamed files in the path to the root folder this is invoked from a parameter option?
 
+
+# CODE
 echo Hi persnonzez!!!!!!!!!!!!!!! HI!! -Nem
 
-# find *.$1 > allFiles.txt
-
-	# OPTIONAL variation that renames all files of given extension *recursively--* WARNING; it moves all of them into whatever path root this is invoked from:
-	find $directory -type f -name "*.$1" > allFiles.txt
-
-# WORKAROUND for that or other versions of echo sometimes throwing in unwanted \r characters:
-tr -d '\r' < allFiles.txt > wut.txt
-rm allFiles.txt
-mv wut.txt allFiles.txt
-arraySize=$(wc -l < allFiles.txt)
-numDigitsOf_arraySize=${#arraySize}
-
-mapfile -t allFilesArray < allFiles.txt
-rm allFiles.txt
+# Get count of files we want, and from that digits to pad to.
+# The necessity of deleting leading blank space is because of Mac. Re: https://stackoverflow.com/a/30927885 : GYAH! This doom was had 2018-04-19 Thursday 07:33 PM:
+filesCount=`find . -maxdepth 1 -iname \*.$1 | wc -l | tr -d ' '`
+digitsToPadTo=${#filesCount}
+# Create array to run through of files.
+filesArray=`find . -maxdepth 1 -iname \*.$1`
 
 counter=0
-for filename in ${allFilesArray[@]}
+for filename in ${filesArray[@]}
 do
-			# echo filename is\:
-			# echo $filename
 	counter=$((counter + 1))
-	countString=`printf "%0""$numDigitsOf_arraySize""d\n" $counter`
-			# echo "mv $filename $countString.$1"
-	# echo . . .
+	countString=`printf "%0""$digitsToPadTo""d\n" $counter`
+			# echo old file name is\: $filename
+			# echo new file name is\: $countString.$1
 	mv $filename $countString.$1
 done
 
-
 # DEVELOPMENT HISTORY
+# 2018/04/19 Take `mapfile` out (fails on Mac) and create array in-memory. Wrangle how to get digitsToPadTo value meanwhile. (Do it before.)
 # 2016/07/17 I wish it hadn't taken me a silly half hour (more?) to write this. It used to be it would take much longer, so there's that. -RAH
 # 2016/10/12 7:16 PM Fixed bug (via workaround) for echo bug that throws in extra \r charactesr in some situations.
