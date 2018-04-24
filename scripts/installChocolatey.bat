8d3cdba62751f8f94ed17812b5187094fdd574a6
Alex Hall <ansible1@earthbound.io>
2018-04-24 04:14:23 -0600

    install chocolatey

---
+++ scripts/installChocolatey.bat
@@ -0,0 +1,10 @@
+C:
+
+mkdir tmp
+cd C:\tmp
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+cd /
+
+rd /s /q tmp
