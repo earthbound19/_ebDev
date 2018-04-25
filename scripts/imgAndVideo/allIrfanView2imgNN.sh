f3442d51f17207596e54787e8b47da896435053e
Alex Hall <ansible1@earthbound.io>
2018-04-25 08:56:00 -0600

    Invokes irfanView2imgNN.sh for all files of type $1 in the current directory. See USAGE.

---
+++ scripts/imgAndVideo/allIrfanView2imgNN.sh
@@ -0,0 +1,23 @@
+# DESCRIPTION
+# Invokes irfanView2imgNN.sh for all files of type $1 in the current directory. See USAGE.
+
+# USAGE
+# $1 input format
+# $2 output format
+# $3 px wide to resize to by nearest neighbor method, maintaining aspect
+# For example:
+# ./thisScript.sh ppm png 540
+
+
+# CODE
+# Store a list of all files of type $1 in an array, first trimming off any leading ./ from the listed files via sed:
+allParam1files=`find . -maxdepth 1 -iname "*.ppm" | sed 's/^\.\///g'`
+
+# Step through that array, converting each element from type $2 to $3 via irfanView2imgNN.sh:
+echo "Rendering images from type $2 to type $3 . . ."
+for element in ${allParam1files[@]}
+do
+			# echo Invoking command\:
+			# echo "irfanView2imgNN.sh $element $2 $3"
+	irfanView2imgNN.sh $element $2 $3
+done
