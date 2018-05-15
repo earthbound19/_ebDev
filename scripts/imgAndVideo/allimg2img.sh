d1a694aad3c48ef5c51104fe93aa2a0b52a517da
Alex Hall <ansible1@earthbound.io>
2018-05-15 10:42:16 -0600

    that should have been optional

--- scripts/imgAndVideo/allimg2img.sh
+++ scripts/imgAndVideo/allimg2img.sh
@@ -12,13 +12,16 @@
 img_format_1=$1
 img_format_2=$2
 
+# OPTIONAL e.g. resize command:
+# additionalParams="-scale 640 "
+
 find . -iname \*.$img_format_1 > all_"$img_format_1".txt
 while read element
 do
 	fileNameNoExtension=`basename $element .$img_format_1`
 			# REFERENCE for script hacking for custom runs: the [-scale n] switch will resize the image maintaining aspect with the longest side at n pixels.
 	# what parameter was I after here? : -size $1x$1
-	command="gm convert $element $fileNameNoExtension.$img_format_2 -scale 640"
+	command="gm convert $element $fileNameNoExtension.$img_format_2 $additionalParams"
 	echo running command\: $command
 	echo . . .
 	$command
