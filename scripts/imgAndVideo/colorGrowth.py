97eecddeb4fa904d199d67e64fca6820842fc48e
Alex Hall <ansible1@earthbound.io>
2018-07-28 13:16:10 -0600

    - bug fix: default background from mutation colorbase if not background not given, use if given - bug fix: number saved animation framed by +1, not +a - rearrange to dos by priority - add more to do

--- scripts/imgAndVideo/colorGrowth.py
+++ scripts/imgAndVideo/colorGrowth.py
@@ -11,16 +11,14 @@
 # python 3 with the various modules installed that you see in the import statements here near the start of this script.
 
 # TO DO:
-# - If -a is greater than 1 are saved frames numbered by multiples of a or a+1? If not the latter fix it so it's the latter.
-# - Option to use a parameter preset (which would be literally just an input file of desired parameters?). Is this a standardized nixy' CLI thing to do?
-# - Clamp randomly generated colors that are out of gamut (back into the gamut).
 # - Throw an error and exit script when conflicting CLI options are passed (a parameter that overrides another).
+# - Option to use a parameter preset (which would be literally just an input file of desired parameters?). Is this a standardized nixy' CLI thing to do?
+# - Initialize colorMutationBase by random selection from a .hexplt color scheme
+# - Coordinate mutation: optionally revert to coordinate before last known successful mutation on coordinate mutation fail (instead of continuing random walk)
+# - Color mutation: on coordinate mutation fail (and selection of new random coordinate), revert to colorMutationBase, OR select random new color (including from a .hexplt color scheme), OR continue with default behavior (continue color mutation from last reference color). Maybe make these options based on a string or letter value given for a switch.
+# - Clamp randomly generated colors that are out of gamut (back into the gamut).
 # - Have more than one bacterium alive at a time (and have all their colors evolve on creating new bacterium).
-# - Initialize mutationColorbase by random selection from a .hexplt color scheme
-# - Have optional random new color selection when failedMutationsThreshold is met (coordination mutation fails)?
-#  - Do random new color selection from a .hexplt color scheme
-# - Optionally revert to coordinate before last known successful mutation on coordinate mutation fail (instead of continuing random walk)
-# - Major new feature? : Initialize arr[] from an image, pick a random coordinate from the image, and use the color at that coordinate both as the origin coordinate and the color at that coordinate as mutationColorbase
+# - Major new feature? : Initialize arr[] from an image, pick a random coordinate from the image, and use the color at that coordinate both as the origin coordinate and the color at that coordinate as colorMutationBase. Could also be used to continue terminated runs with the same or different parameters.
 
 
 # CODE
@@ -31,14 +29,14 @@ from PIL import Image
 
 parser = argparse.ArgumentParser(description='Renders a PNG image like colored, evolved bacteria (they produce different colors as they evolve) grown randomly over a surface. Right now it is one virtual, undead bacterium which randomly walks and poops mutated colors. A possible future update will manage multiple bacteria. Output file names are random. Inspired and drastically evolved from colorFibers.py, which was horked and adapted from https://scipython.com/blog/computer-generated-contemporary-art/')
 parser.add_argument('-n', '--numberOfImages', type=int, default=7, help='How many images to generate. Default 7.')
-parser.add_argument('-w', '--width', type=int, default=250, help='Width of output image(s). Default 1200.')
-parser.add_argument('-t', '--height', type=int, default=125, help='Height of output image(s). Default 600.')
-parser.add_argument('-r', '--rshift', type=int, default=2, help='Vary R, G and B channel values randomly in the range negative this value or positive this value. Note that this means the range is rshift times two. Defaut 4. Ripped or torn looking color streaks are more likely toward 6 or higher.')
+parser.add_argument('-w', '--width', type=int, default=1200, help='Width of output image(s). Default 1200.')
+parser.add_argument('-t', '--height', type=int, default=400, help='Height of output image(s). Default 400.')
+parser.add_argument('-r', '--rshift', type=int, default=2, help='Vary R, G and B channel values randomly in the range negative this value or positive this value. Note that this means the range is rshift times two. Defaut 4. Ripped or torn looking color streaks are more likely toward 6 or higher. Default 2.')
 parser.add_argument('-c', '--colorMutationBase', default='[157, 140, 157]', help='Base initialization color for pixels, which randomly mutates as painting proceeds. Expressed as a python list or single number that will be assigned to every RGB value. If a list, put the parameter in quotes and give the RGB values in the format e.g. \'[255, 70, 70]\' (this example would produce a deep red, as Red = 255, Green = 70, Blue = 70). A single number example like just 150 will result in a medium-light gray of [150, 150, 150] (Red = 150, Green = 150, Blue = 150). All values must be between 0 and 255. Default [157, 140, 157] (a medium-medium light, slightly violet gray).')
-parser.add_argument('-b', '--backgroundColor', default='[157, 140, 157]', help='Canvas color. If omitted, defaults to whatever colorMutationBase is. If included, may differ from colorMutationBase. This option must be given in the same format as colorMutationBase.')
-parser.add_argument('-p', '--percentMutation', type=float, default=0.00248, help='(Alternate for -m) What percent of the canvas would have been covered by failed mutation before it triggers selection of a random new available unplotted coordinate. Percent expressed as a decimal (float) between 0 and 1.')
+parser.add_argument('-b', '--backgroundColor', help='Canvas color. If omitted, defaults to whatever colorMutationBase is. If included, may differ from colorMutationBase. This option must be given in the same format as colorMutationBase.')
+parser.add_argument('-p', '--percentMutation', type=float, default=0.02, help='(Alternate for -m) What percent of the canvas would have been covered by failed mutation before it triggers selection of a random new available unplotted coordinate. Percent expressed as a decimal (float) between 0 and 1. Default 0.043 (about 4 percent).')
 parser.add_argument('-f', '--failedMutationsThreshold', type=int, help='How many times coordinate mutation must fail to trigger selection of a random new available unplotted coordinate. Overrides -p | --percentMutation if present.')
-parser.add_argument('-s', '--stopPaintingPercent', type=float, default=0.65, help='What percent canvas fill to stop painting at. To paint until the canvas is filled (which is infeasible for higher resolutions), pass 1 (for 100 percent) If not 1, value should be a percent expressed as a decimal (float) between 0 and 1.')
+parser.add_argument('-s', '--stopPaintingPercent', type=float, default=0.475, help='What percent canvas fill to stop painting at. To paint until the canvas is filled (which is infeasible for higher resolutions), pass 1 (for 100 percent) If not 1, value should be a percent expressed as a decimal (float) between 0 and 1. Default 0.475 (about 48 percent).')
 parser.add_argument('-a', '--animationSaveEveryNframes', type=int, help='Every N successful coordinate and color mutations, save an animation frame into a subfolder named after the intended final art file. To save every frame, set this to 1, or to save every 3rd frame set it to 3, etc. Saves zero-padded numbered frames to a subfolder which may be strung together into an animation of the entire painting process (for example via ffmpegAnim.sh). May substantially slow down render, and can also create many, many gigabytes of data, depending. Off by default. To switch it on, use it (with a number).')
 
 args = parser.parse_args()		# When this function is called, if -h or --help was passed to the script, it will print the description and all defined help messages.
@@ -53,19 +51,18 @@ stopPaintingPercent = args.stopPaintingPercent
 animationSaveEveryNframes = args.animationSaveEveryNframes
 # Interpreting -c (or --colorMutationBase) argument as python literal and assigning that to a variable, re: https://stackoverflow.com/a/1894296/1397555
 colorMutationBase = ast.literal_eval(args.colorMutationBase)
-backgroundColor = ast.literal_eval(args.backgroundColor)
-purple = [255, 0, 255]	# Purple
-
+backgroundColor = args.backgroundColor
+# If no canvas color given, use colorMutationBase; if canvas color given, initialize backgroundColor from it:
+if backgroundColor == None:
+	backgroundColor = colorMutationBase
+else:
+	backgroundColor = ast.literal_eval(args.backgroundColor)
+# purple = [255, 0, 255]	# Purple. In prior commits of this script, this has been defined and unused, just like in real life. Now, it is commented out or not even defined, just like it is in real life.
 allesPixelCount = width * height
-
-# Conditional argument overrides logic.
 # If no specific threshold given, calculate it. Otherwise what is given will be used (it will not be altered):
 if failedMutationsThreshold == None:
 	failedMutationsThreshold = int(allesPixelCount * percentMutation)
 terminatePaintingAtFillCount = int(allesPixelCount * stopPaintingPercent)
-# If no canvas color given, use colorMutationBase:
-if backgroundColor == None:
-	backgroundColor = colorMutationBase
 
 print('Will generate ', numIMGsToMake, ' image(s).')
 
@@ -162,8 +159,8 @@ for n in range(1, (numIMGsToMake + 1) ):		# + 1 because it iterates n *after* th
 					frameFilePathAndFileName = animFramesFolderName + '/' + strOfThat.zfill(padAnimationSaveFramesNumbersTo) + '.png'
 					im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
 					im.save(frameFilePathAndFileName)
+					animationFrameCounter += 1		# Increment that *after* because by default ffmpeg expects frame count to start at 0.
 				animationSaveNFramesCounter += 1
-				animationFrameCounter += 1		# Increment that *after* because by default ffmpeg expects frame count to start at 0.
 
 		else:		# If the coordinate is NOT NOT used (is used), print a progress message.
 			failedCoordMutationCount += 1
