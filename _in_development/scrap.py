cb04d77d3b6c01c2dcebdb4e1f1a224c30f13b28
earthbound19 <ansible1@earthbound.io>
2018-08-02 07:34:10 -0600

    dev snapshot

--- _in_development/scrap.py
+++ _in_development/scrap.py
@@ -3,9 +3,9 @@ from PIL import Image
 
 # slots for higher memory / use efficiency re: https://stackoverflow.com/a/49789270
 class Coordinate:
-	__slots__ = ["x", "y", "isAlive", "isConsumed", "unoccupiedNeighbors"]
-	def __init__(self, x, y, isAlive, isConsumed, unoccupiedNeighbors):
-		self.x = x;	self.y = y;	self.isAlive = isAlive;	self.isConsumed = isConsumed
+	__slots__ = ["x", "y", "RGBcolor", "isAlive", "isConsumed", "unoccupiedNeighbors"]
+	def __init__(self, x, y, RGBcolor, isAlive, isConsumed, unoccupiedNeighbors):
+		self.x = x;	self.y = y;	self.RGBcolor = RGBcolor; self.isAlive = isAlive;	self.isConsumed = isConsumed
 		self.unoccupiedNeighbors = [ [x -1, y -1], [x -1, y], [x -1, y +1], [x, y-1], [x, y+1], [x+1, y-1], [x+1, y], [x+1, y+1] ]		# List of possible neighbor relative coordinates for any coordinate (unless the coordinate is at a border):
 			# x-1, y-1	:	left, up		ONE
 			# x-1, y	:	left			TWO
@@ -23,11 +23,12 @@ class Coordinate:
 # print('wut', p1.unoccupiedNeighbors)
 
 height = 4; width = 5; allCoordinates = []
+mediumPurplishGray = [157, 140, 157]
 
 # init. allCoordinates array:
 for yCoord in range(0, width):
 	for xCoord in range(0, height):
-		allCoordinates.append(Coordinate(yCoord, xCoord, False, False, None))
+		allCoordinates.append(Coordinate(yCoord, xCoord, mediumPurplishGray, False, False, None))
 # delete out-of-bounds values in that array (that result from the way a Coordinate object self-initializes unoccupiedNeighbors from x and y) :
 # for yCoord in range(0, width):
 	# print('allCoordinates at yCoord ', yCoord, ':')
@@ -37,8 +38,8 @@ for yCoord in range(0, width):
 # print('Intended bitmap width: ', width, '\nIntended bitmap height: ', height, '\nCoordinate objects in array allCoordinates:')
 # for loopCoord in allCoordinates:
 	# print(loopCoord.x, loopCoord.y, loopCoord.isAlive, loopCoord.isConsumed, loopCoord.unoccupiedNeighbors)
-	
-arr = np.ones((height, width, 3)) * [157, 140, 157]
-# im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
-# im.save('tstScrap-py.png')
+
+arr = np.ones((height, width, 3)) * mediumPurplishGray
+im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
+im.save('tstScrap-py.png')
 print(arr)
