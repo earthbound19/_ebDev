7721c1f5b206c9ce616b6ee2257844b83c53f833
earthbound19 <ansible1@earthbound.io>
2018-08-05 22:33:39 -0600

    move, colorpoop.py members alter

--- scripts/imgAndVideo/colorpoop.py
+++ scripts/imgAndVideo/colorpoop.py
@@ -9,9 +9,11 @@ import numpy as np
 
 class coordinate:
 	# slots for allegedly higher efficiency re: https://stackoverflow.com/a/49789270
-	__slots__ = ["x", "y", "maxX", "maxY", "RGBcolor", "isAlive", "isConsumed", "emptyNeighbors"]
+	__slots__ = ["XYtuple", "maxX", "maxY", "RGBcolor", "isAlive", "isConsumed", "emptyNeighbors"]
 	def __init__(self, x, y, maxX, maxY, RGBcolor, isAlive, isConsumed, emptyNeighbors):
-		self.x = x;	self.y = y;	self.RGBcolor = RGBcolor; self.isAlive = isAlive;	self.isConsumed = isConsumed
+
+		self.XYtuple = (x, y)
+		self.RGBcolor = RGBcolor; self.isAlive = isAlive;	self.isConsumed = isConsumed
 		# Adding all possible empty neighbor values even if they would result in values out of bounds of image (negative or past maxX or maxY), and will check for and clean up pairs with out of bounds values after:
 		tmpList = [ (x-1, y-1), (x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1) ]
 			# List of possible neighbor relative coordinates for any coordinate (unless the coordinate is at a border):
@@ -25,7 +27,9 @@ class coordinate:
 			# x+1, y+1	:	right, down		EIGHT
 			# -~-~ I DOUBLE-CHECKED and verified that all intialized values in tmpList in this class initalize according to this list.
 		# make lists of all tuples that contain out of range values, then use the list to delete those tuples from the list of tuples; re: https://www.quora.com/How-do-you-search-a-list-of-tuples-in-Python
+
 		deleteList = []
+
 		for element in tmpList:
 			if -1 in element:
 				deleteList.append(element)
@@ -35,13 +39,16 @@ class coordinate:
 		for element in tmpList:
 			if (maxY+1) in element:
 				deleteList.append(element)
+
 		# reduce deleteList to a list of unique tuples (in case of duplicates, which can lead us to attempt to remove something that ins't there, which throws an exception and stops the script) :
 		deleteList = list(set(deleteList))
+
 		# the deletions:
 		for no in deleteList:
 			tmpList.remove(no)
 		# finallu initialize the intended object member from that built list:
 		self.emptyNeighbors = list(tmpList)
+
 	def getRNDemptyNeighbors(self):
 		random.shuffle(self.emptyNeighbors)		# shuffle the list of empty neighbor coordinates
 		nNeighborsToReturn = np.random.random_integers(0, len(self.emptyNeighbors))		# Decide how many to pick
