72a815b4ed89e59d37be1951b3a0c26e61795678
earthbound19 <ansible1@earthbound.io>
2018-08-05 20:44:31 -0600

    cat and text editor plugins can do this. deleted

--- scripts/combineAlltxts.bat
+++
@@ -1,9 +0,0 @@
-REM Drop a lot of .txt files on this batch to combine them into one .txt file archive.
-
-SETLOCAL ENABLEDELAYEDEXPANSION
-
-FOR %%Y IN (%*.txt) DO (
-======================================
-REM ECHO %%Y
-TYPE %%Y >> __compilation.txt
-)
