31c120adea232bf5e6af12dfeda4f90d8f4fcf3d
earthbound19 <ansible1@earthbound.io>
2018-08-11 21:18:11 -0600

    Gutted and commented out to start refactor
    Will develop to use multiple Coordinate objects

--- scripts/imgAndVideo/colorGrowth.py
+++ scripts/imgAndVideo/colorGrowth.py
@@ -121,12 +121,13 @@ def mutateCoordinate(xCoordParam, yCoordParam):
 		yCoord = (height - 1)
 	return [xCoord, yCoord]
 
+# TO DO: REINTEGRATE AS NECESSARY:
 # function gets random unused coordinate:
-def getRNDunusedCoord():
-	unusedCoordsListSize = len(unusedCoords)
-	randomIndex = np.random.random_integers(0, unusedCoordsListSize-1)
-	chosenCoord = unusedCoords[randomIndex]
-	return chosenCoord
+# def getRNDunusedCoord():
+# 	unusedCoordsListSize = len(unusedCoords)
+# 	randomIndex = np.random.random_integers(0, unusedCoordsListSize-1)
+# 	initCoord = unusedCoords[randomIndex]
+# 	return initCoord
 
 # function prints coordinate plotting statistics (progress report):
 def printProgress():
@@ -142,25 +143,13 @@ for n in range(1, (numIMGsToMake + 1) ):		# + 1 because it iterates n *after* th
 	animationSaveNFramesCounter = 0
 	animationFrameCounter = 0
 	arr = np.ones((height, width, 3)) * backgroundColor
-# DEV NOTE: CORRECTION OF WHAT WAS WRONG in prior incarnation of script using a list of Coordinate objects for color mutation; that list should have been initialized like:
-	# compArr = []
-	# for yCoord in range(0, height):
-	# 	for xCoord in range(0, width):	# RGBcolor can also be initialized with: np.random.randint(0, 255, size=3)
-	# 		compArr.append(Coordinate(xCoord, yCoord, width, height, backgroundColor, False, False, None))
-	# print('arr:', arr)
-	# for element in compArr:
-	# 	print(element.x, element.y, element.XYtuple)
-	# sys.exit()
-# END CORRECTION NOTE--DELETE THIS after I decide whether to use it (take that approach of color mutation via Coordinate objects again).
+# TO DO: redefine that as a list of lists of Coordinates.
 
 	unusedCoords = []
-	for yCoord in range(0, width):
-		for xCoord in range(0, height):
-			unusedCoords.append([yCoord, xCoord])
+# TO DO: define this as a list of lists mappable to a list of lists of coordinates.
 
-	# Initialize chosenCoord:
-	chosenCoord = getRNDunusedCoord()
-	usedCoords = []
+# TO DO: REINTEGRATE AS NECESSARY:
+	# initCoord = getRNDunusedCoord()		# First coordinate to be used
 	color = colorMutationBase
 	previousColor = color
 	failedCoordMutationCount = 0
@@ -182,62 +171,60 @@ for n in range(1, (numIMGsToMake + 1) ):		# + 1 because it iterates n *after* th
 		os.mkdir(animFramesFolderName)
 
 	print('Generating image . . .')
-	while unusedCoords:
-		chosenCoord = mutateCoordinate(chosenCoord[0], chosenCoord[1])
-		boolIsInUsedCoords = chosenCoord in usedCoords
-		if not boolIsInUsedCoords:		# If the coordinate is NOT in usedCoords, use it (whether or not it is, the coordinate is still mutated; this loop keeps mutating the coordinate (and pooping colors on newly arrived at unused coordinates) until terminate conditions are met).
-			# print('chosenCoord ', chosenCoord, ' is NOT in usedCoords. Will use.')
-			usedCoords.append(chosenCoord)
-			arrXidx = chosenCoord[0]
-			arrYidx = chosenCoord[1]
-			newColor = previousColor + np.random.random_integers(-rshift, rshift, size=3) / 2
-			# Clip that within RGB range if it wandered outside of that range. If this slows it down too much and you don't care if colors randomly freak out (bitmap conversion seems to take colors outside range as wrapping around?) comment the next line out:
-			newColor = np.clip(newColor, 0, 255)
-			arr[arrYidx][arrXidx] = newColor
-			previousColor = newColor
-			unusedCoords.remove(chosenCoord)
-			# Also, if a parameter was passed saying to do so, save an animation frame (if we are at the Nth (-a) mutation:
-			if animationSaveEveryNframes:
-				if (animationSaveNFramesCounter % animationSaveEveryNframes) == 0:
-					strOfThat = str(animationFrameCounter)
-					frameFilePathAndFileName = animFramesFolderName + '/' + strOfThat.zfill(padAnimationSaveFramesNumbersTo) + '.png'
-					im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
-					im.save(frameFilePathAndFileName)
-					animationFrameCounter += 1		# Increment that *after* because by default ffmpeg expects frame count to start at 0.
-				animationSaveNFramesCounter += 1
+	continuePainting = True
+	while continuePainting:
+# TO DO: REINTEGRATE AS NECESSARY:
+			# newColor = previousColor + np.random.random_integers(-rshift, rshift, size=3) / 2
+			# newColor = np.clip(newColor, 0, 255)		# Clip that within RGB range if it wandered outside of that range.
+			# arr[arrYidx][arrXidx] = newColor
+			# previousColor = newColor
 
-		else:		# If the coordinate is NOT NOT used (is used), print a progress message.
-			failedCoordMutationCount += 1
-			# If coordiante mutation fails failedMutationsThreshold times, get a new random coordinate, and print a message saying so.
-			if failedCoordMutationCount == failedMutationsThreshold:
-				chosenCoord = getRNDunusedCoord()
-				print('Coordinate mutation failure threshold met at ', failedMutationsThreshold, '. New random, unused coordinate selected: ', chosenCoord)
-				printProgress()
-				failedCoordMutationCount = 0
-				# if a switch was passed saying to revert or randomise the color mutation base when we reach revertColorOnMutationFail, do so (actually, change the "previous color" to the mutation color base, and the next color mutation will be off that) :
-				if revertColorOnMutationFail == 1:
-					previousColor = colorMutationBase
-		# Running progress report:
-		if reportStatsNthLoopCounter == 0 or reportStatsNthLoopCounter == reportStatsEveryNthLoop:
-			# Save a progress snapshot image.
-			print('Saving prograss snapshot image ', stateIMGfileName, ' . . .')
-			im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
-			im.save(stateIMGfileName)
-			printProgress()
-			reportStatsNthLoopCounter = 1
-		reportStatsNthLoopCounter += 1
+# TO DO: REINTEGRATE AS NECESSARY:
+			# Save an animation frame if that variable has a value:
+			# if animationSaveEveryNframes:
+			# 	if (animationSaveNFramesCounter % animationSaveEveryNframes) == 0:
+			# 		strOfThat = str(animationFrameCounter)
+			# 		frameFilePathAndFileName = animFramesFolderName + '/' + strOfThat.zfill(padAnimationSaveFramesNumbersTo) + '.png'
+			# 		im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
+			# 		im.save(frameFilePathAndFileName)
+			# 		animationFrameCounter += 1		# Increment that *after*, for image tools expecting series starting at 0.
+			# 	animationSaveNFramesCounter += 1
+
+# TO DO: REINTEGRATE AS NECESSARY:
+		# If the coordinate is NOT NOT used (is used), print a progress message.
+		# 	failedCoordMutationCount += 1
+		# Get a new random coordinate if failedMutationsThreshold met.
+		# 	if failedCoordMutationCount == failedMutationsThreshold:
+		# 		printProgress()
+		# 		failedCoordMutationCount = 0
+# TO DO: REINTEGRATE AS NECESSARY:
+				# On color mutation fail, revert color to base.
+				# if revertColorOnMutationFail == 1:
+				# 	previousColor = colorMutationBase
+		# Save a snapshot/progress image:
+		# if reportStatsNthLoopCounter == 0 or reportStatsNthLoopCounter == reportStatsEveryNthLoop:
+		# 	print('Saving prograss snapshot image ', stateIMGfileName, ' . . .')
+		# 	im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
+		# 	im.save(stateIMGfileName)
+		# 	printProgress()
+		# 	reportStatsNthLoopCounter = 1
+		# reportStatsNthLoopCounter += 1
+# TO DO: REINTEGRATE AS NECESSARY:
+		# TO DO: PUT a terminate at arbitrary number of mutations control code here.
 		# This will terminate all coordinate and color mutation at an arbitary number of mutations.
-		usedCoordsCount = len(usedCoords)
-		if usedCoordsCount == terminatePaintingAtFillCount:
-			print('Pixel fill (successful mutation) termination count ', terminatePaintingAtFillCount, ' reached. Ending algorithm and painting.')
-			break
+		# if THAT:
+			# print('Pixel fill (successful mutation) termination count ', terminatePaintingAtFillCount, ' reached. Ending algorithm and painting.')
+			# break
 
+# TO DO: REINTEGRATE:
 	# Save final image file and delete progress (state, temp) image file.
-	print('Saving image ', imgFileName, ' . . .')
-	im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
-	im.save(imgFileName)
-	print('Created ', n, ' of ', numIMGsToMake, ' images.')
-	os.remove(stateIMGfileName)
+	# print('Saving image ', imgFileName, ' . . .')
+	# im = Image.fromarray(arr.astype(np.uint8)).convert('RGB')
+	# im.save(imgFileName)
+	# print('Created ', n, ' of ', numIMGsToMake, ' images.')
+	# os.remove(stateIMGfileName)
+# TO DO: set a condition for the following to turn false:
+		continuePainting = False
 
 
 
