24ca6f354f6b24df6c0aa5840f2385d7eac9c9e2
earthbound19 <ansible1@earthbound.io>
2018-09-17 21:42:34 -0600

    move usage notes to #NOTES secton

--- scripts/fountain2pdf.sh
+++ scripts/fountain2pdf.sh
@@ -1,8 +1,11 @@
 # DESCRIPTION
-# Converts a fountain format plain text file screenplay into a formatted PDF via a CLI tool (two options, uncomment the option you want). The optional last line of this script opens the output pdf on Mac, which (I think) won't work on Windows, and if it annoys you, leave that commented out.
+# Converts a fountain format plain text file screenplay into a formatted PDF via a CLI tool (two options, uncomment the option you want).
 
 # USAGE
 # thisScript.sh fountain-source-file.fountain
+
+# NOTES
+# The "wrap" CLI option expects those specific font files to be (I think) in the same PATH as the source fountain file. Also, the optional last line of this script opens the output pdf on Mac, which (I think) won't work on Windows, and if it annoys you, leave that commented out.
 
 
 # CODE
