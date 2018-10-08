bd191c3ed902bcd2e9ffcb72decf0d7e334800e1
earthbound19 <ansible1@earthbound.io>
2018-10-08 15:04:42 -0600

    upd. to integrate scripts, new prefs, help

--- scripts/imgAndVideo/allCR2toTIFF.sh
+++ scripts/imgAndVideo/allCR2toTIFF.sh
@@ -1,1 +1,5 @@
-dcraw -o 1 -W -q 3 -T *.CR2
+allCR2S=(*.CR2)
+for element in ${allCR2S[@]}
+do
+	dcraw2tif.sh $element
+done
--- scripts/imgAndVideo/dcraw2tif.sh
+++ scripts/imgAndVideo/dcraw2tif.sh
@@ -12,8 +12,47 @@
 fileName="${1%.*}"
 fileExt=`echo "${1##*.}"`
 
-	# - SCRATCHED -D switch; it makes the image very white. ? AND -W makes it very black. Together, they make it gray. (I assume that with good paramters they can adjust the white and black points.)
-	dcraw -6 -w +M -o 0 -q 0 -T -q 3 -v -O "$fileName"_dcraw.tif "$1"
+# - SCRATCHED -D switch; it makes the image very white. ? AND -W makes it very black. Together, they make it gray. (I assume that with good paramters they can adjust the white and black points.)
+dcraw -6 -v -w +M -o 1 -W -q 3 -T \
+-b 2.2 \
+"$1"
+# -h \
+# NOTE: move those additional optional switches line one up before the "$1" and uncomment them to use them.
 
-# ! :
-# -z        Change file dates to camera timestamp [this doesn't do any conversion--it only corrects the file time stamp]
+# dcraw options, used ones indented:
+# 	-6        Write 16-bit instead of 8-bit
+# 	-v        Print verbose messages
+# -c        Write image data to standard output
+# -e        Extract embedded thumbnail image
+# -i        Identify files without decoding them
+# -i -v     Identify files and show metadata
+# 	-z        Change file dates to camera timestamp
+#		-w        Use camera white balance, if possible
+# -a        Average the whole image for white balance
+# -A <x y w h> Average a grey box for white balance
+# -r <r g b g> Set custom white balance
+# 	+M		/-M     Use/don't use an embedded color matrix (using +M)
+# -C <r b>  Correct chromatic aberration
+# -P <file> Fix the dead pixels listed in this file
+# -K <file> Subtract dark frame (16-bit raw PGM)
+# -k <num>  Set the darkness level
+# -S <num>  Set the saturation level
+# -n <num>  Set threshold for wavelet denoising
+# -H [0-9]  Highlight mode (0=clip, 1=unclip, 2=blend, 3+=rebuild)
+# -t [0-7]  Flip image (0=none, 3=180, 5=90CCW, 6=90CW)
+# 	-o [0-6]  Output colorspace (raw,sRGB,Adobe,Wide,ProPhoto,XYZ,ACES) (using 1)
+# -o <file> Apply output ICC profile from file
+# -p <file> Apply camera ICC profile from file or "embed"
+# -d        Document mode (no color, no interpolation)
+# -D        Document mode without scaling (totally raw)
+# -j        Don't stretch or rotate raw pixels
+# 	-W        Don't automatically brighten the image
+# -b <num>  Adjust brightness (default = 1.0)
+# -g <p ts> Set custom gamma curve (default = 2.222 4.5)
+# 	-q [0-3]  Set the interpolation quality	(using 3 re: Use Adaptive Homogeneity-Directed (AHD) interpolation.)
+# -h        Half-size color image (twice as fast as "-q 0")
+# -f        Interpolate RGGB as four colors
+# 	-m <num>  Apply a 3x3 median filter to R-G and B-G (using 2)
+# -s [0..N-1] Select one raw image or "all" from each file
+# -4        Linear 16-bit, same as "-6 -W -g 1 1"
+# 	-T        Write TIFF instead of PPM
