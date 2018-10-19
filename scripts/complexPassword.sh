ca9625fc168a890a02e54f85592390c48a79f0a4
earthbound19 <ansible1@earthbound.io>
2018-10-19 17:05:04 -0600

    improve doc, add alt. possibility in comment

--- scripts/complexPassword.sh
+++ scripts/complexPassword.sh
@@ -1,16 +1,27 @@
-# DESCRIPTION: returns one password of length 44 characters or per paramaters you pass to the script.
+# DESCRIPTION
+# Returns one password of length 44 characters or per paramaters you pass to the script.
 
-# USAGE: pass this script two paramaters, the first being how many strings you want it to return, the second being the length of each string.
+# USAGE
+# pass this script two paramaters:
+# $1 how many strings you want it to return
+# $2 the length of each string. Example:
+# thisScript.sh 5 44
+# -- will return 5 passwords of length 44 characters each.
+
+# SEE ALSO: http://passwordmaker.sourceforge.net/downloads/cli.html
 
 
-# TO DO: update this to use the better means of empty variable checkign I found; e.g if [ -z ${1+x} ]; then img_size=4120; else img_size=$1; fi :
-if [[ $1 == "" ]]; then howMany=1; else howMany=$1; fi
-if [[ $2 == "" ]]; then length=44; else length=$2; fi
+# CODE
+if [ -z ${1+x} ]; then howMany=1; else howMany=$1; fi
+if [ -z ${2+x} ]; then length=44; else length=$2; fi
+
 # To force tr to operate on non-text (urandom) output:
 export LC_CTYPE=C
 for (( i=1; i<=$howMany; i++ ))
 do
 	cat /dev/urandom | tr -dc 'a-z0-9A-Z{}[]~!@#$%^&*()_+-' | head -c $length
+# Possibly more efficient option that sacrifices many unusual characters from entropy pool; re https://unix.stackexchange.com/a/476125/110338 :
+	# base64 < /dev/urandom | head -c $length
 		# For newline between printed strings:
 		echo
 done
