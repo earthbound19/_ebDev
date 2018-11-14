911e1bac3dc72d6a98a594d82aa54ff34d27c537
Alex Hall <ansible1@earthbound.io>
2018-11-14 15:08:08 -0700

    upd. per usage (leave history commented)

--- installUsedBrewPackages.sh
+++ installUsedBrewPackages.sh
@@ -7,60 +7,55 @@
 # TO DO: always install gnu-sed with default names (override mac sed?!) : brew install gnu-sed --with-default-names : re https://stackoverflow.com/a/27834828/1397555 -- also see: https://developer.apple.com/library/content/documentation/OpenSource/Conceptual/ShellScripting/PortingScriptstoMacOSX/PortingScriptstoMacOSX.html -- https://unix.stackexchange.com/questions/13711/differences-between-sed-on-mac-osx-and-other-standard-sed ("complex history")
 
 brewPackages=" \
+gnu-sed \
+ffmpeg \
+pandoc \
+coreutils \
 automake \
+autoconf \
+libyaml \
+readline \
 libxslt \
 unixodbc \
-argtable \
-autoconf \
-boost \
-cmake \
-cocoapods \
-coreutils \
-cryptopp \
-ffmpeg \
-findutils \
-fish \
-freetype \
-gdbm \
-ghostscript \
-git \
-gmp \
-gnu-sed \
-gperftools \
 graphicsmagick \
-hiredis \
-icu4c \
-jpeg \
-jsoncpp \
-lame \
-leveldb \
-libjson-rpc-cpp \
-libmicrohttpd \
-libpng \
-libssh2 \
-libtiff \
-libtool \
-libyaml \
-little-cms2 \
-miniupnpc \
-openssl \
-pandoc \
-pcre2 \
-perl \
-phantomjs \
-pkg-config \
-pyenv \
-python3 \
-qt \
-readline \
-ruby \
-rust \
-snappy \
-sqlite \
-x264 \
-xvid \
-xz \
-yarn"
+gnupg"
+
+# yarn \
+# argtable \
+# boost \
+# cmake \
+# cocoapods \
+# cryptopp \
+# findutils \
+# freetype \
+# gdbm \
+# ghostscript \
+# gmp \
+# gperftools \
+# hiredis \
+# icu4c \
+# jpeg \
+# jsoncpp \
+# lame \
+# leveldb \
+# libjson-rpc-cpp \
+# libmicrohttpd \
+# libpng \
+# libssh2 \
+# libtiff \
+# little-cms2 \
+# miniupnpc \
+# pcre2 \
+# perl \
+# phantomjs \
+# pkg-config \
+# ruby \
+# rust \
+# snappy \
+# sqlite \
+# x264 \
+# xvid \
+# xz \
 
 for element in ${brewPackages[@]}
 do
