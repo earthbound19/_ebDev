c5df93a4ec81f4637c28e45852d8cc0d6f2ffd63
Alex Hall <ansible1@earthbound.io>
2018-11-17 00:03:30 -0700

    merge notes into one doc (and delete orig. notes)

--- scripts/macDevSetup.sh
+++ scripts/macDevSetup.sh
@@ -1,5 +1,4 @@
-pushd .
-
+# pushd .
 # NOPE: maybe asdf instead:
 # install n node version manager; re: https://github.com/tj/n/issues/169
 # cd ~
@@ -7,8 +6,7 @@ pushd .
 # TO UNINSTALL:
 # ~/n/n-uninstall
 # n latest
-
-popd
+# popd
 
 ./install_global_node_modules.sh
 
@@ -18,9 +16,32 @@ sudo spctl --master-disable
 # Enable cut-paste in Mac Finder (why would you *not* have that there by default, Apple?) :
 defaults write com.apple.finder AllowCutForItems 1
 
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Possible additional commands to setup asdf and pyenv:
+# git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.0
+# echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
+# asdf plugin-add nodejs
+# asdf install nodejs 10.13.0
+# asdf global nodejs 10.13.0
+# xcode-select --install
+# RE: https://github.com/pyenv/pyenv/issues/1219 :
+# sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# git config user.email "earthbound19@users.noreply.github.com"
+# git credential-osxkeychain 
+# git config --global credential.helper osxkeychain
+# curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+
 # REFERENCE FOR OTHER TOOLS
 # date format "Phrase content" for PhraseExpress text expander for mac, for hotkey/sequence to type full date and time:
 #   {#datetime -f yyyy.mm.dd dddd t am/pm}
 
 # command for Atom open-terminal-here package on Mac (requires ttab to be installed) which allows opening any path to terminal by shortcut:
 # ttab && cd "$PWD"
+
+# path change notes:
+# see: https://coolestguidesontheplanet.com/add-shell-path-osx/
+# 
+# and / or http://hathaway.cc/post/69201163472/how-to-edit-your-path-environment-variables-on-mac
