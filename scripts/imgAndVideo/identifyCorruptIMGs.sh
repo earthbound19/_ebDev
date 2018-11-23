26b4802aaf49891b088d124f5421347051fbed01
Alex Hall <ansible1@earthbound.io>
2018-11-23 16:51:31 -0700

    create array in memory (no temp file). use gfind / gstat (not find / stat)

--- scripts/imgAndVideo/identifyCorruptIMGs.sh
+++ scripts/imgAndVideo/identifyCorruptIMGs.sh
@@ -4,27 +4,28 @@
 # USAGE
 # Invoke this script with one parameter, being an image file extension type without any . in it.
 
+# DEPENDENCIES
+# graphicsmagick (gm identify)
+
+# NOTES
 # Format of command that will dump any errors to a text file:
 # identify 1489.jpg 2> wut.txt
 
+
+# CODE
 if [ ! -d _irrecoverable ]; then mkdir _irrecoverable; fi
 
-find $directory -type f -name "*.$1" > all_"$1"_files.txt
-sed -i 's/^\.\/\(.*\)/\1/g' all_"$1"_files.txt
+array=`gfind . -maxdepth 1 -type f -name "*.$1" -printf '%f\n' | sort`
 
-mapfile -t allIMGsArray < all_"$1"_files.txt
-rm all_"$1"_files.txt
-
-for filename in ${allIMGsArray[@]}
+for filename in ${array[@]}
 do
-			# echo $filename
-	identify $filename 2> "$filename"_identify_log.txt
-	blocksUsed=`stat --format=%b "$filename"_identify_log.txt`
+	gm identify $filename 2> "$filename"_identify_log.txt
+	blocksUsed=`gstat --format=%b "$filename"_identify_log.txt`
 	if [ "$blocksUsed" != "0" ]
 		then
-				# echo $blocksUsed blocks used for error log "$filename"_identify_log.txt; moving associated file to dir for identifying possible irrepairably corrupted images.
-		mv "$filename" ./_irrecoverable
-				# mv "$filename"_identify_log.txt ./_irrecoverable
+			echo $blocksUsed blocks used for error log "$filename"_identify_log.txt\; moving associated file to dir for identifying possible irrepairably corrupted images.
+			mv "$filename" ./_irrecoverable
+			mv "$filename"_identify_log.txt ./_irrecoverable
 	fi
 	rm "$filename"_identify_log.txt
 done
