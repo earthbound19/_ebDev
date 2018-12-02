e68e119203cfbd808e474cddb8a6b5aa57676033
Alex Hall <ansible1@earthbound.io>
2018-12-01 23:41:22 -0700

    efficiency / comment / layout fixes
    Generate file list (array) in memory (skip writing temp files), layout niggles, delete some comments

--- scripts/fileNamesAndTimestamps/allRandomFileNames.sh
+++ scripts/fileNamesAndTimestamps/allRandomFileNames.sh
@@ -27,23 +27,20 @@ fi
 
 if ! [ -z ${2+x} ]
 	then
-		find ./*.$2 > files_list.txt
+		array=(`gfind . -maxdepth 1 -type f -iname \*.$1 -printf '%f\n'`)
 	else
-		find ./* > files_list.txt
+		array=(`gfind . -maxdepth 1 -type f -iname \* -printf '%f\n'`)
 fi
 
-mapfile -t array < files_list.txt
-rm files_list.txt
-
 arrSize=${#array[@]}
-# echo arrSize val is $arrSize
 
 	# Pregenerate random characters to pull shorter random character strings from:
 	# re: http://stackoverflow.com/a/1405641
-	numRandomCharsToGet=`echo $(( arrSize * getNrandChars ))`
-		# echo numRandomCharsToGet val is $numRandomCharsToGet
-	randomCharsString=`cat /dev/urandom | tr -cd 'a-km-np-zA-KM-NP-Z2-9' | head -c $numRandomCharsToGet`
-		# echo randomCharsString val is $randomCharsString
+numRandomCharsToGet=`echo $(( arrSize * getNrandChars ))`
+	# echo numRandomCharsToGet val is $numRandomCharsToGet
+export LC_CTYPE=C
+randomCharsString=`cat /dev/urandom | tr -cd 'a-km-np-zA-KM-NP-Z2-9' | head -c $numRandomCharsToGet`
+	# echo randomCharsString val is $randomCharsString
 
 # TO DO: check if the following math is right. I think it isn't, and effectively sets it to $(( getNrandChars * (-1) )) ; although it probably works out for the same intended effect; ALSO SEE rndHexColorsGen.sh for a possibly more elegant way to do this:
 # Initialize counter at negative the number of getNrandChars, so that the first iteration in the following loop will set it to 0, which is where we need it to start:
@@ -51,20 +48,15 @@ multCounter=-$getNrandChars
 	# echo multCounter val is $multCounter
 for filename in ${array[@]}
 do
-		# Get file extension, re: http://stackoverflow.com/a/30863119/1397555
-		# Extension (all) : '1.0.1.tar.gz'
-			# DEPRECATED; apparently less portable:
-			# fileExt=`echo "$filename" | awk '\{sub(/[^.]*[.]/, "", $0)\} 1'`
-		fileExt=`echo "$filename" | sed 's/.*\.\([^\.]*\)/\1/g'`
-			# echo fileExt val is $fileExt
-		# For file renaming, grab next n random characters from pre-generated randomCharsString:
-		# num=$(($multCounter + $num2))
-			# echo multCounter val is $multCounter
-			# echo getNrandChars vlas is $getNrandChars
+	fileExt=${filename##*.}
+	# For file renaming, grab next n random characters from pre-generated randomCharsString:
+	num=$((multCounter + num2))
+		# echo multCounter val is $multCounter
+		# echo getNrandChars vlas is $getNrandChars
 		multCounter=$(($multCounter + $getNrandChars))
-			# echo getNrandChars val is $getNrandChars
+		# echo getNrandChars val is $getNrandChars
 		newFileBaseName=${randomCharsString:$multCounter:$getNrandChars}
-			# echo ~----- "renaming $filename to $newFileBaseName"."$fileExt"
+		echo ~----- "renaming $filename to $newFileBaseName"."$fileExt"
 	mv ./$filename ./$newFileBaseName.$fileExt
 done
 
@@ -73,3 +65,4 @@ done
 # made this keep extensions. this is quick and dirty for flam3 files. 06/12/2016 09:15:44 AM -RAH -- DONE 2016-07-16 6:33 PM -RAH
 # Prior to now: script that was inefficient by reading 4 bytes from /dev/urandom with every single file rename.
 # 2016-07-16 Made script much more efficient by prefetching necessary number of random characters into a variable, and fetching iterative groups of chars from said variable (in memory, instead of using a file on disk). -RAH
+# 2018-12-01 11:33 PM Generate file list (array) in memory (skip writing temp files), layout niggles, delete some comments
