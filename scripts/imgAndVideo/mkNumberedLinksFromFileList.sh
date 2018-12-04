4ac97bff2d8ba6648ad7921f04bde83cb2ee3f7e
Alex Hall <ansible1@earthbound.io>
2018-12-04 03:31:14 -0700

    dunno how I used this before--did it ever work? fixed.

--- scripts/imgAndVideo/mkNumberedLinksFromFileList.sh
+++ scripts/imgAndVideo/mkNumberedLinksFromFileList.sh
@@ -1,19 +1,38 @@
 # USAGE
-# Invoke this cript with one parameter:
-# $1 The file list name, for which every file name in the list there will be numbered junction (file names which are junctions) in a subdir.
+# Invoke this cript with one optional parameter:
+# $1 A file name of a list. The list is in the format:
+#
+# file '263.jpg'
+# file '363.jpg'
+# file '064.jpg'
+# file '145.jpg'
+#
+# from this list there will be made numbered junctions (file names which are junctions) in a subdir.
 
-# WARNING: this script perhaps dangerously assumes all file names provided in the list have the same extension; which is done on these next lines:
-tempStr=`head -n 1 IMGlistByMostSimilar.txt`
-echo that is $tempStr
-# NOTE that this assumes a closing apostraphe or single quote in the input file! :
-fileNameExt=`echo $tempStr | sed "s/.*\.\([^\.]\{1,5\}\)'.*/\1/g"`
+# WARNING: this script perhaps dangerously assumes all file names provided in the list have the same extension.
 
+
+# CODE
+# IF NO list file provided, assume it is IMGlistByMostSimilar.txt:
+if [ -z ${1+x} ]
+then
+	fileList=IMGlistByMostSimilar.txt; echo No file list parameter \(\$\1\) passed to script\; setting fileList to default IMGlistByMostSimilar.txt
+else
+	fileList=$1; echo fileList set to parameter \1\, $1
+fi
+
+# the mentioned "all files have the same extension" assumption is used here:
+tempStr=`tail -n 1 IMGlistByMostSimilar.txt`
+
+# NOTE that this assumes a closing apostraphe or single quote in the input file!-- and that we therefore here need to use gsed instead of fileExt=${filename##*.} :
+fileNameExt=`echo $tempStr | gsed "s/.*\.\([^\.]\{1,5\}\)'.*/\1/g"`
+
+digitsCount=${#fileNameExt}
+
+# If the numberedLinks directory already exists, TOAST IT without warning, then recreate it:
 if [ -a numberedLinks ]; then rm -rf numberedLinks; mkdir numberedLinks; else mkdir numberedLinks; fi
 
-sed "s/file '\(.*\)'/\1/g" $1 > tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
-
-numElements=`wc -l < $1`
-digitsCount=${#numElements}
+gsed "s/file '\(.*\)'/\1/g" $fileList > tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
 
 counter=0
 while read element
