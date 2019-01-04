bffef2ef25504b63d26c4539b585c2d0987753bc
Alex Hall <ansible1@earthbound.io>
2019-01-03 23:05:52 -0700

    improve desc., use array not file list, gfind use

--- scripts/imgAndVideo/allMOV2mp4Lossless.sh
+++ scripts/imgAndVideo/allMOV2mp4Lossless.sh
@@ -1,11 +1,11 @@
 # DESCRIPTION
-# Converts all .mov format video files in the current path to .mp4 containers, losslessly (no recompression: direct stream copy).
+# Converts all .mov and .avi format video files in the current directory (ignores subdirectories) to .mp4 containers, losslessly (no recompression: direct stream copy).
 
 # USAGE
 # thisScript.sh
 
 # These rediculous pipe acrobatics are to trim ./ off the start and any windows newlines that gnu ports of windows utilities create:
-list=`find . -name '*.mov' -o -name '*.MOV' | sed 's|^./||' | tr -d '\15\32'`
+list=(`gfind . -maxdepth 1 \( -iname \*.mov -o -iname \*.MOV -o -iname \*.avi \) -printf '%f\n' | sort`)
 
 # Optional extra parameters:
 # extraParams="-acodec aac -crf 17 -strict -2"		# Because ffmpeg can't handle pcm for mp4 right now, and that would be a silly waste of space for distribution anyway (compress it to aac) -- and it throws an error instructing me to add -strict -2 to that if I use aac
