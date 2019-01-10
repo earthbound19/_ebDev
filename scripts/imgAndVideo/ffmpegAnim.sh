eebb81c7521dc8c6b7e1bb57afd54b9cf4c2651b
Alex Hall <ansible1@earthbound.io>
2019-01-09 23:33:25 -0700

    use gfind (not find)

--- scripts/imgAndVideo/ffmpegAnim.sh
+++ scripts/imgAndVideo/ffmpegAnim.sh
@@ -48,7 +48,7 @@ fi
 # an example command wut does some math as would be needed per this algo: echo "scale=5; 3298 / 1296" | bc
 
 # Assumes that all input files have the same character count in the file base name; I wonder whether I've gone full circle on going away from and back to the exact form of the following command, but *right now* it's testing ok on Cygwin and Mac; re https://stackoverflow.com/a/40876071 ; ALSO gnuWin32 gfind it seems has a bug; the command throws an error. Here using Cygwin find on Windows and find on Mac; maybe I'll do that everywhere..
-lastFoundTypeFile=`find . -iname \*.$4 | sort | tail -n 1 | sed 's/\.\/\(.*\)/\1/g'`
+lastFoundTypeFile=`gfind . -iname \*.$4 | sort | tail -n 1 | sed 's/\.\/\(.*\)/\1/g'`
 lastFoundTypeFileNameNoExt=${lastFoundTypeFile%.*}
 digitsPadCount=${#lastFoundTypeFileNameNoExt}
 
