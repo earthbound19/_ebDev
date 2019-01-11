9fb8ea6e91def0325426d0d6308ebe0c70500259
Alex Hall <ansible1@earthbound.io>
2019-01-10 20:34:42 -0700

    overhauled to match process of mkNumberedCopies~

--- scripts/imgAndVideo/mkNumberedLinksFromFileList.sh
+++ scripts/imgAndVideo/mkNumberedLinksFromFileList.sh
@@ -11,6 +11,21 @@
 
 # WARNING: this script perhaps dangerously assumes all file names provided in the list have the same extension.
 
+# USAGE
+# Invoke this script with one parameter:
+# $1 The file list name, for which every file name in the list will have numbered copies made in a subdir. This is preparation for other scripts which must operate on numbered files.
+
+# WARNING: this script perhaps dangerously assumes all file names provided in the list have the same extension. Also, it clobbers any files that already exist when it copies (overwrites without prompt).
+
+# DEPENDENCIES
+# IMGlistByMostSimilar.txt as prepared by imgsGetSimilar.sh and/or re_sort_imgsMostSimilar.sh
+
+# USAGE
+# TO DO
+# - detail usage
+# - BUG FIX: on copy it is overwriting the last image with the second-to-last. ?
+# - Document how to use this script :)
+
 
 # CODE
 # IF NO list file provided, assume it is IMGlistByMostSimilar.txt:
@@ -21,19 +36,24 @@ else
 	fileList=$1; echo fileList set to parameter \1\, $1
 fi
 
-# the mentioned "all files have the same extension" assumption is used here:
-tempStr=`tail -n 1 IMGlistByMostSimilar.txt`
+# If the numberedCopies directory already exists, TOAST IT without warning, then recreate it; otherwise create it:
+if [ -d numberedCopies ]; then rm -rf numberedCopies; mkdir numberedCopies; else mkdir numberedCopies; fi
 
-# NOTE that this assumes a closing apostraphe or single quote in the input file!-- and that we therefore here need to use gsed instead of fileExt=${filename##*.} :
-fileNameExt=`echo $tempStr | gsed "s/.*\.\([^\.]\{1,5\}\)'.*/\1/g"`
+tempStr=`ghead -n 1 $fileList`
+# NOTE that this script assumes a closing apostraphe or single quote in the input file! :
+# No, the fileExt=${filename##*.} doesn't work here as there's a trailing ' to trim:
+fileNameExt=`echo $tempStr | sed "s/.*\.\([^\.]\{1,5\}\)'.*/\1/g"`
 
-digitsCount=${#fileNameExt}
-
-# If the numberedLinks directory already exists, TOAST IT without warning, then recreate it:
-if [ -a numberedLinks ]; then rm -rf numberedLinks; mkdir numberedLinks; else mkdir numberedLinks; fi
-
+# NOTE this script assumes a list formatted for concatenation by ffmpeg, and makes a temp copy of the list removing that syntax:
 gsed "s/file '\(.*\)'/\1/g" $fileList > tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
+dos2unix tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
 
+# Because some platforms pad wc output with spaces:
+numElements=`wc -l < tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt | tr -d ' '`
+digitsCount=${#numElements}
+
+# create new IMGlistByMostSimilar.txt in the subdir the copies will be written to, only with the new file names (as those file names will be appended to that file in the following block) :
+printf "" > ./numberedCopies/$fileList
 counter=0
 while read element
 do
@@ -41,10 +61,11 @@ do
 			# ex. to pad numbers to number of digits in %0n:
 			# var=`printf "%05d\n" $element`
 	paddedNum=`printf "%0"$digitsCount"d\n" $counter`
-	command="link ./$element ./numberedLinks/$paddedNum.$fileNameExt"
-	echo executing command\:
-	echo $command
-	$command
+	echo "executing command: link ./$element ./numberedCopies/$paddedNum.$fileNameExt"
+	link ./$element ./numberedCopies/$paddedNum.$fileNameExt
+	# Because Cygwin can be silly with permissions (I can't use the .png images afterward without special access!) :
+	chmod 777 ./numberedCopies/$paddedNum.$fileNameExt
+	echo "file '$paddedNum.$fileNameExt'" >> ./numberedCopies/$fileList
 done < ./tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
 
 rm ./tmp_kHDcaVmKUgsZp9cvU2QezUsZ3EYHAWbqkr.txt
