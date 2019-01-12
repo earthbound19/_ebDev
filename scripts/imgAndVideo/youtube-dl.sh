4ae51f4e4ac2c099fc112230e11f7eac01a2c35b
Alex Hall <ansible1@earthbound.io>
2019-01-12 13:20:13 -0700

    youtube-dl wrapper

---
+++ scripts/imgAndVideo/youtube-dl.sh
@@ -0,0 +1,10 @@
+# DESCRIPTION
+# Wrapper for youtube-dl which passes preferred CLI options to program before download URL $1
+
+# USAGE
+# Invoke this script with one parameter, being the URL to a video to download:
+# youtube-dl.sh URL-to-video
+
+# additionalParams="--audio-quality 0"
+
+youtube-dl --no-playlist --skip-unavailable-fragments -w, --no-overwrites --write-description --all-formats --youtube-skip-dash-manifest --merge-output-format mp4 --extract-audio --audio-format best  --recode-video mp4 --keep-video $additionalParams $1
