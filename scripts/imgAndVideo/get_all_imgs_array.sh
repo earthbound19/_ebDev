a1eb900b96a920d4a93df55d21b8bda93bcd84c2
Alex Hall <ansible1@earthbound.io>
2019-01-19 12:08:16 -0700

    delete vestigal lines from copy from other script

--- scripts/imgAndVideo/get_all_imgs_array.sh
+++ scripts/imgAndVideo/get_all_imgs_array.sh
@@ -18,35 +18,18 @@ echo "(if that was a sentence fragment, invisibl_switch was blank)"
 # CODE
 imgs_arr=(`gfind . -maxdepth 1 \( \
 -iname \*$_MTPL*.tif \
--o -iname \*"$_MTPL"var*.tif \
 -o -iname \*$_MTPL*.tiff \
--o -iname \*"$_MTPL"VAR*.tiff \
 -o -iname \*$_MTPL*.png \
--o -iname \*"$_MTPL"VAR*.png \
 -o -iname \*$_MTPL*.psd \
--o -iname \*"$_MTPL"VAR*.psd \
 -o -iname \*$_MTPL*.psb \
--o -iname \*"$_MTPL"*.psb \
 -o -iname \*$_MTPL*.ora \
--o -iname \*"$_MTPL"VAR*.ora \
 -o -iname \*$_MTPL*.rif \
--o -iname \*"$_MTPL"VAR*.rif \
 -o -iname \*$_MTPL*.riff \
--o -iname \*"$_MTPL"VAR*.riff \
 -o -iname \*$_MTPL*.jpg \
--o -iname \*"$_MTPL"VAR*.jpg \
 -o -iname \*$_MTPL*.jpeg \
--o -iname \*"$_MTPL"VAR*.jpeg \
 -o -iname \*$_MTPL*.gif \
--o -iname \*"$_MTPL"VAR*.gif \
 -o -iname \*$_MTPL*.bmp \
--o -iname \*"$_MTPL"VAR*.bmp \
 -o -iname \*$_MTPL*.cr2 \
--o -iname \*"$_MTPL"VAR*.cr2 \
 -o -iname \*$_MTPL*.raw \
--o -iname \*"$_MTPL"VAR*.raw \
 -o -iname \*$_MTPL*.crw \
--o -iname \*"$_MTPL"VAR*.crw \
--o -iname \*$_MTPL*.svg \
--o -iname \*"$_MTPL"VAR*.svg \
  \) -printf '%f\n' | sort $invisibl_switch`)
