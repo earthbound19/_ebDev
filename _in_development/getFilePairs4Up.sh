834eb651c68307e6a7b63ee39c7de5ab32e378ce
Alex Hall <ansible1@earthbound.io>
2019-01-19 05:24:48 -0700

    trivial tweaks, moves from autobrood repo

--- _in_development/getFilePairs4Up.sh
+++ _in_development/getFilePairs4Up.sh
@@ -7,12 +7,11 @@
 # thisScript.sh sourceFilesExtension targetFilesToMoveHereExtension
 # EXAMPLE: suppose you've got some files named rnd_43aB.png and rnd_44Cd.png in the current path, which you rendered from source files rnd_43aB.flame and rnd43Cd.flame which are scattered one or more directories up from the current path, and that you want to move those matching .flame files into the current path. From the directory with those .png files, invoke this script thusly:
 # thisScript.sh png flame
-# This will result in all those matching .flame file names being moved from up to four paths above this path into this path.
+# This will result in all those matching .flame file names being moved from up to four paths above this path into this path. WARNING: if there is more than one of the same file name in different directories, one will clobber the other even if they have different content, without warning.
 
 # TO DO:
 # Separate script that searches down directories and moving file here if it exists; re a genius breath yon: http://stackoverflow.com/a/37012114
 # gfind ./ -name "$element" -exec mv '{}' './' ';'
-# TO DO: fix probs. with that; see comments in fetchGenomesImages.sh
 
 
 # CODE
