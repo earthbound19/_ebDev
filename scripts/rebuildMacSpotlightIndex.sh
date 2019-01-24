c2120e76b85978806d66144baaeb5f58eca00e75
Alex Hall <ansible1@earthbound.io>
2019-01-24 09:54:26 -0700

    tweak that improves execution

--- scripts/rebuildMacSpotlightIndex.sh
+++ scripts/rebuildMacSpotlightIndex.sh
@@ -1,5 +1,4 @@
 # re mdutil help (when run without any switch) :
+sudo mdutil -Esa -i on;
 
-# NOTE: this may not work if invoked from this script; this is more for reference:
-
-sudo mdutil -Esa -i on
+# Without the semicolon (";") at the end of this command, it doesn't return to the terminal again (and allow you to enter another command) until re-indexing completes. But with the & echo, it forces the command to effectively run in the background, by "finishing" the command and moving to the "next" command, which is nothing.
