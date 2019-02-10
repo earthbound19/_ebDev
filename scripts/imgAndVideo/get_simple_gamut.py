9f24429c9d3455715d9788978f56cf8fddae8455
Alex Hall <ansible1@earthbound.io>
2019-02-09 23:30:22 -0700

    exclusive to HCL--my eyes say too many colors :(

--- scripts/imgAndVideo/get_simple_gamut.py
+++ scripts/imgAndVideo/get_simple_gamut.py
@@ -1,14 +1,11 @@
 # DESCRIPTION
-# Creates a list of colors expressed as RGB hexadecimal values, from a simplified L*a*b gamut, in D (-d <number> switch) domains per permutation of L, a, and b in the gamut. Capture the list with the > operator via terminal (see USAGE). Can be hacked to do this with several gamuts (L*a*b of particular interest?). The script stretches the shade and hue range a bit and produces more colors than you would expect, to that end. Also writes new files (or clobbers them if they already exist) all_gamut_two_pairs.txt, which is a list of all possible combinations of two colors in the simplified gamut (without repetition of any color--every combination will be two different colors).
+# Creates a list of colors expressed as RGB hexadecimal values, from a simplified LCH (a.k.a. HCL) gamut. Capture the list with the > operator via terminal (see USAGE). Can be hacked to do this with several gamuts which the supporting "spectra" python library can use. To change the step over various components in the gamut, hack the A_step, B_step and C_step variables in this script.
 
 # USAGE
-# python get_simple_gamut.py -n 7
-# -- where -n is the number of shades to include for every hue in the gamut (default 7).
-# capture the output to a file with the > operator, e.g.
 # python get_simple_gamut.py -n 7 > HSL_simplified_gamut.hexplt
 
 # DEPENDENCIES
-# Python 3, spectra, argparse, numpy
+# Python 3, spectra (python) library
 # spectra library via:
 # pip install spectra
 # -- and documented at:
@@ -17,21 +14,6 @@
 
 # CODE
 import spectra
-import argparse
-import numpy as np
-
-DEFAULT_DOMAIN = 5.5
-
-PARSER = argparse.ArgumentParser(description='Creates a list of colors expressed as RGB hexadecimal values, from a simplified HSL gamut, in D (-d <number> switch) domains per permutation of L, a, and b in the gamut. Capture the list with the > operator via terminal e.g. "python get_simple_gamut.py -d 5.5 > Lab_simplified_gamut.hexplt" (without the double quote marks). Note that -d is a float.')
-PARSER.add_argument('-d', '--DOMAIN', type=float, help='Domains per permutation of L, a, and b in the L*a*b gamut. Default ' + str(DEFAULT_DOMAIN))
-ARGS = PARSER.parse_args()
-
-# If -n <a number> was passed to script, generate that many colors; otherwise generate DEFAULT_DOMAIN:
-if ARGS.DOMAIN:
-    DOMAIN = ARGS.DOMAIN
-else:
-    DOMAIN = DEFAULT_DOMAIN
-
 
 # FAILED to do what I want implemented with color print; from the spectra tutorial page; but handy reference for creating gradients (scales) :
 # scale = spectra.scale([ start, end ])
@@ -40,7 +22,7 @@ else:
 
 # Lab max values; IF YOU USE THESE as max it gives a grayscale output:
 # 99.99998453333127, -0.0004593894083471106, -0.008561457924405325
-# A_max = 99.99998453333127
+# L_max = 99.99998453333127
 # B_max = -0.0004593894083471106
 # C_max = -0.008561457924405325
 
@@ -53,41 +35,33 @@ else:
 
 # Lab min and max values? : L: 0 to 100; a: -110 to 110; b: -110 to 110? re: http://davidjohnstone.net/pages/lch-lab-colour-gradient-picker
 
-# The clipping of the low L*a*b range compensates for the fact that few actually decipher color that dark.
-# The additions to the max range compensate for the non-inclusive upper np.arange.
-A_min = 2
-A_max = 100 + (100 / DOMAIN * 1.8)
-B_min = -100
-B_max = 110 + (110 / DOMAIN)
-C_min = -100
-C_max = 110 + (110 / DOMAIN)
+# LCH (Luminance, Chroma, Hue) (also known as HCL) min and max values? : L 0 to 150 [but probably really to 100!], C 0 to 100, H 0 to 360? re: https://bl.ocks.org/mbostock/3e115519a1b495e0bd95 -- OR more likely L is 0 to 100, re: http://hclwizard.org/hclwizard/ -- and another color manipulation python package, "colorspace!" : https://python-colorspace.readthedocs.io/en/latest/hclcolorspace.html -- and another page supports L 0 to 100, C 0 to 100, and H 0 to 360: http://www.hclwizard.org/why-hcl/ -- AND ALSO here: https://www.colourphil.co.uk/lab_lch_colour_space.shtml
 
-# creates numpy array of NUMBER_OF_COLORS (count) values over L (Lab) range; dividing final parameter further results in more colors used:
-A_domain = np.arange(A_min, A_max, A_max / DOMAIN)
-B_domain = np.arange(B_min, B_max, B_max / DOMAIN)
-C_domain = np.arange(C_min, C_max, C_max / DOMAIN)
-
-# convert from numpy arrays to lists for combinatronics:
-A_domain = list(A_domain)
-B_domain = list(B_domain)
-C_domain = list(C_domain)
+L_min = 0
+L_max = 100
+C_min = 0
+C_max = 100
+H_min = 0
+H_max = 360
 
 simplified_gamut = []
 
-# print all possible combinations from every domain:
-for i in A_domain:
-	for j in B_domain:
-		for k in C_domain:
-			this_color = spectra.lab(i, j, k)
-			# print(i, j, k)
+# print all possible combinations over the distribution via step_increment:
+L_step = 10		# luminance step in LCH
+C_step = 12		# chroma step in LCH
+H_step = 7		# hue step in LCH
+# The (val + val) for the maxrange is because the range function excludes the highest value (assumes zero-based counting), so that here we _do_ get the end range (it stops _past_ the range--maybe quite a bit but we dedup the results so it doesn't matter) -- although for LCH I don't worry about H; the range is broad enough without doing that:
+for hue in range(H_min, (H_max + H_step), H_step):
+	for chroma in range(C_min, (C_max + C_step), C_step):
+		for lum in range(L_min, (L_max + L_step), L_step):
+			this_color = spectra.lch(lum, chroma, hue)
 			simplified_gamut.append(this_color.hexcode)
-
 
 # Deduplicate list but maintain order; re: https://stackoverflow.com/a/17016257/1397555
 # -- or TO DO--this without importing another library, re: https://thispointer.com/python-how-to-remove-duplicates-from-a-list/
 from more_itertools import unique_everseen
 simplified_gamut = list(unique_everseen(simplified_gamut))
-# 
+
 for element in simplified_gamut:
 	print(element)
 
