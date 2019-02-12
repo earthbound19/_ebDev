1f398184929fc11a6e0329d50c350643fbdace7a
Alex Hall <ansible1@earthbound.io>
2019-02-11 23:49:02 -0700

    v1 proof of concept complete

--- scripts/imgAndVideo/get_CIECAM02_simplified_gamut.py
+++ scripts/imgAndVideo/get_CIECAM02_simplified_gamut.py
@@ -15,39 +15,46 @@
 # - winner if it works: https://colorspacious.readthedocs.io/en/latest/reference.html#ciecam02 : "If you just want a better replacement for traditional ad hoc spaces like “Hue/Saturation/Value”, then use the string "JCh" for your colorspace (see Perceptual transformations for a tutorial) and be happy."
 # - OR maybe better?! : https://pypi.org/project/ciecam02/
 
-A_min = 0
-A_max = 100
-A_step = int(100 / 18)
+# cicam02 doc:
+# CIECAM02 produces multiple correlates, like H, J, z, Q, t, C, M, s. Some of them represent similar concepts, such as C means chroma and M colorfulness s saturation correlate the same thing in different density. We need only 3 major property of these arguments to completely represent a color, and we can get other properties or reverse algorithms..
+# Color type jch is a float list like [j, c, h], where 0.0 < j < 100.0, 0.0 < h < 360.0, and 0.0 < c. the max value of c does not limit, and may produce exceeds when transform to rgb. The effective value of max c varies. Probablly for red color h 0.0, and brightness j 50.0, c reach the valid maximum, values about 160.0.
+# IN OTHER WORDS: j (brightness) range is 0 to 100, h (hue) is 0 to 360, c (chroma) can be 0 to any number (I don't beleive that there _is_ a max from whatever correlary/inputs produces the max), maybe max 160. I'll start with max 182 (this seemed a thing with L in HCL).
 
-B_min = 0
-B_max = 182
-B_step = int(182 / 24)
+import numpy as np
+from ciecam02 import jch2rgb
 
-C_min = 0
-C_max = 360
-C_step = int(360 / 7)
+j_min = 0
+j_max = 100
+j_step = int(100 / 18)
+
+h_min = 0
+h_max = 182
+h_step = int(182 / 24)
+
+c_min = 0
+c_max = 360
+c_step = int(360 / 7)
 
 # Stepping (counting) down because it's difficult (or with this code depending, impossible) to hit the max ranges counting up:
 simplified_gamut = []
-for i in range(C_max, C_min, -C_step):
-	for j in range(A_max, A_min, -A_step):
-		for k in range(B_max, B_min, -B_step):
-			this_color = spectra.lch(j, k, i)
-			simplified_gamut.append(this_color.hexcode)
+for c in range(c_max, c_min, -c_step):
+	for h in range(j_max, j_min, -j_step):
+		for j in range(h_max, h_min, -h_step):
+			ferge = "boo"
+			jch = np.array([ [c, h, j] ])
+			jch_as_str = str(jch[0])
+			simplified_gamut.append(jch_as_str)
+			# rgb_array = jch2rgb(jch)
+			# print(rgb_array[0])
+			# hex_string = "#" + str(rgb_array[0][0]) + str(rgb_array[0][1]) + str(rgb_array[0][2])
+			# print(hex_string) 
+			# OUTPUT: array([[255,   0, 255]], dtype=uint8)
 
 # Deduplicate list but maintain order; re: https://stackoverflow.com/a/17016257/1397555
 # -- or TO DO--this without importing another library, re: https://thispointer.com/python-how-to-remove-duplicates-from-a-list/
 from more_itertools import unique_everseen
 simplified_gamut = list(unique_everseen(simplified_gamut))
-
-
-# WIP: DELETE COLORS WHICH ARE SO SIMILAR as to be practically the same.., via colormath (to reduce the list further) :
-#list_len = len(simplified_gamut)
-#print("list_len val:", list_len)
-#for outer_idx in range(list_len -1, 0, -1):
-#	for inner_idx in range(outer_idx-1, 0, -1):
-
-
+ 
 for element in simplified_gamut:
 	print(element)
 
