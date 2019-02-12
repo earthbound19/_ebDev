629b2cbc4851d4e9e91d1ed70959ad55216a461b
Alex Hall <ansible1@earthbound.io>
2019-02-11 20:59:19 -0700

    greater accuracy I think, more notes, dev snapshot

--- scripts/imgAndVideo/get_simple_gamut.py
+++ scripts/imgAndVideo/get_simple_gamut.py
@@ -1,19 +1,24 @@
 # DESCRIPTION
-# Creates a list of colors expressed as RGB hexadecimal values, from a simplified LCH (a.k.a. HCL) gamut. Capture the list with the > operator via terminal (see USAGE). Can be hacked to do this with several gamuts which the supporting "spectra" python library can use. To change the step over various components in the gamut, hack the A_step, B_step and C_step variables in this script.
+# Creates a list of colors expressed as RGB hexadecimal values, from a simplified LCH (a.k.a. HCL) gamut. Capture the list with the > operator via terminal (see USAGE). Can be hacked to do this with several gamuts which the supporting "spectra" python library can use. To change the step over various components in the gamut, hack the A_domain, B_domain and C_domain variables in this script.
 
 # USAGE
 # python get_simple_gamut.py -n 7 > HSL_simplified_gamut.hexplt
 
 # DEPENDENCIES
-# Python 3, spectra (python) library
+# Python 3, spectra, argparse, numpy
 # spectra library via:
 # pip install spectra
 # -- and documented at:
 # https://github.com/jsvine/spectra/blob/master/docs/walkthrough.ipynb
 
+# TO DO: or instead of using colormath, use? : https://github.com/colour-science/colour which has _a lot_ of functionality (including Colour Difference calculation), a ton of commits, and at this writing the newest commit only 4 days ago -- or any of the packages _it_ lists? : https://github.com/colour-science/colour#see-also
+
 
 # CODE
 import spectra
+from colormath.color_objects import LabColor
+from colormath.color_objects import LCHabColor
+from colormath.color_diff import delta_e_cie2000
 
 # FAILED to do what I want implemented with color print; from the spectra tutorial page; but handy reference for creating gradients (scales) :
 # scale = spectra.scale([ start, end ])
@@ -22,7 +27,7 @@ import spectra
 
 # Lab max values; IF YOU USE THESE as max it gives a grayscale output:
 # 99.99998453333127, -0.0004593894083471106, -0.008561457924405325
-# L_max = 99.99998453333127
+# A_max = 99.99998453333127
 # B_max = -0.0004593894083471106
 # C_max = -0.008561457924405325
 
@@ -35,26 +40,27 @@ import spectra
 
 # Lab min and max values? : L: 0 to 100; a: -110 to 110; b: -110 to 110? re: http://davidjohnstone.net/pages/lch-lab-colour-gradient-picker
 
-# LCH (Luminance, Chroma, Hue) (also known as HCL) min and max values? : L 0 to 150 [but probably really to 100!], C 0 to 100, H 0 to 360? re: https://bl.ocks.org/mbostock/3e115519a1b495e0bd95 -- OR more likely L is 0 to 100, re: http://hclwizard.org/hclwizard/ -- and another color manipulation python package, "colorspace!" : https://python-colorspace.readthedocs.io/en/latest/hclcolorspace.html -- and another page supports L 0 to 100, C 0 to 100, and H 0 to 360: http://www.hclwizard.org/why-hcl/ -- AND ALSO here: https://www.colourphil.co.uk/lab_lch_colour_space.shtml
+# LCH (Luminance, Chroma, Hue) (also known as HCL) min and max values? : L 0 to 100, C 0 to 182 (not 100?!), H 0 to 360? (Documentation doesn't say!) re: http://www.multipole.org/discourse-server/viewtopic.php?t=24834 -- with those max ranges I get hex ff00ff, whereas with what many web sites led me to believe the ranges should be (with C max 100) I get ff95ff.
+# NOTE my eyes say, assuming those values, do these step values: L_step 18, C_domain 24, H_step 7
 
-L_min = 0
-L_max = 100
+A_min = 0
+A_max = 100
+A_step = int(100 / 18)
+
+B_min = 0
+B_max = 182
+B_step = int(182 / 24)
+
 C_min = 0
-C_max = 100
-H_min = 0
-H_max = 360
+C_max = 360
+C_step = int(360 / 7)
 
+# Stepping (counting) down because it's difficult (or with this code depending, impossible) to hit the max ranges counting up:
 simplified_gamut = []
-
-# print all possible combinations over the distribution via step_increment:
-L_step = 10		# luminance step in LCH
-C_step = 12		# chroma step in LCH
-H_step = 7		# hue step in LCH
-# The (val + val) for the maxrange is because the range function excludes the highest value (assumes zero-based counting), so that here we _do_ get the end range (it stops _past_ the range--maybe quite a bit but we dedup the results so it doesn't matter) -- although for LCH I don't worry about H; the range is broad enough without doing that:
-for hue in range(H_min, (H_max + H_step), H_step):
-	for chroma in range(C_min, (C_max + C_step), C_step):
-		for lum in range(L_min, (L_max + L_step), L_step):
-			this_color = spectra.lch(lum, chroma, hue)
+for i in range(C_max, C_min, -C_step):
+	for j in range(A_max, A_min, -A_step):
+		for k in range(B_max, B_min, -B_step):
+			this_color = spectra.lch(j, k, i)
 			simplified_gamut.append(this_color.hexcode)
 
 # Deduplicate list but maintain order; re: https://stackoverflow.com/a/17016257/1397555
@@ -62,19 +68,51 @@ for hue in range(H_min, (H_max + H_step), H_step):
 from more_itertools import unique_everseen
 simplified_gamut = list(unique_everseen(simplified_gamut))
 
+
+# WIP: DELETE COLORS WHICH ARE SO SIMILAR as to be practically the same, via colormath (to reduce the list further) :
+#list_len = len(simplified_gamut)
+#print("list_len val:", list_len)
+#for outer_idx in range(list_len -1, 0, -1):
+#	outer_check_hex = simplified_gamut[outer_idx]
+#	compare_color1 = spectra.html(outer_check_hex).to("lch")
+#	LabColor_compare1 = LCHabColor(lch_l=compare_color1.values[0], lch_c=compare_color1.values[1], lch_h=compare_color1.values[2])
+#	print("----? :")
+#	print(outer_check_hex)
+#	print(compare_color1.hexcode)	# DOESN'T ALWAYS MATCH outer_check_hex!
+#	spectra_color_reconstructed = spectra.lab(LabColor_compare1.lch_l, LabColor_compare1.lch_c, LabColor_compare1.lch_h)
+#	print(spectra_color_reconstructed.hexcode)
+# TO DO: rework this script to use only colormath?! Because THE COLORS CHANGE IN A ROUNDTRIP FROM SPECTRA TO COLORMATH. Maybe that's inevitable?
+# color2.lab_a, color2.lab_b, color2.lab_l
+#	for inner_idx in range(outer_idx-1, 0, -1):
+		# print("outer check val is:", outer_check_hex, "idx", outer_idx)
+		# print("inner check val is:", inner_check_hex, "idx", inner_idx)
+		# color comparison reference; re: https://python-colormath.readthedocs.io/en/latest/delta_e.html
+		# (import statements moved to start of script)
+		# # Reference color.
+		# color1 = LabColor(lab_l=0.9, lab_a=16.3, lab_b=-2.22)
+		# # Color to be compared to the reference.
+		# color2 = LabColor(lab_l=0.7, lab_a=14.2, lab_b=-1.80)
+		# # This is your delta E value as a float.
+		# delta_e = delta_e_cie1976(color1, color2)
+#		inner_check_hex = simplified_gamut[inner_idx]
+#		compare_color2 = spectra.html(inner_check_hex).to("lab")
+		# LabColor_compare1 = LabColor(lab_l=compare_color1.values[0], lab_a=compare_color1.values[1], lab_b=compare_color1.values[2])
+		# delta_e = delta_e_cie2000(compare_color1, compare_color2)
+
+
 for element in simplified_gamut:
 	print(element)
 
 
 # Write lists of all two-and-three pairs from reduced gamut:
-import string
-import itertools
-all_two_permutations = list(itertools.permutations(simplified_gamut, 2))
-outfile = open('all_gamut_two_pairs.txt', 'w')
-
-for i in all_two_permutations:
-	outfile.write(i[0] +"," + i[1] +'\n')
-outfile.close()
+# import string
+# import itertools
+# all_two_permutations = list(itertools.permutations(simplified_gamut, 2))
+# outfile = open('all_gamut_two_pairs.txt', 'w')
+# 
+# for i in all_two_permutations:
+# 	outfile.write(i[0] +"," + i[1] +'\n')
+# outfile.close()
 
 # all_three_permutations = list(itertools.permutations(simplified_gamut, 3))
 # outfile = open('all_gamut_three_pairs.txt', 'w')
