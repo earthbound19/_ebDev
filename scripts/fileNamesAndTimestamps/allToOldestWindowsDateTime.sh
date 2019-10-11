ff67f339cacd1912a83a517cb49a07a64e1a4bcb
Alex Hall <ansible1@earthbound.io>
2019-10-11 09:15:42 -0600

    new?? undeleted?

---
+++ scripts/fileNamesAndTimestamps/allToOldestWindowsDateTime.sh
@@ -0,0 +1,13 @@
+# DESCRIPTION
+# Invokes toOldestWindowsDateTime.sh for all image formats (hard coded) in the current directory.
+
+# USAGE
+# allToOldestWindowsDateTime.sh
+
+list=(`gfind . -maxdepth 1 \( -iname \*.jpg -o -iname \*.png -o -iname \*.cr2 -o -iname \*.tif -o -iname \*.mov \) -printf '%f\n' | sort`)
+
+for element in ${list[@]}
+do
+	echo "INVOKING toOldestWindowsDateTime.sh for file \"$element\" . . ."
+	toOldestWindowsDateTime.sh $element
+done
