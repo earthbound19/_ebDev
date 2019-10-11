810a69e3803dc5b4c32b8304ff47e4e124dbb2e2
Alex Hall <ansible1@earthbound.io>
2019-10-11 09:15:42 -0600

    new?? undeleted?

---
+++ _in_development/getAll2colorHexpltCombos.py
@@ -0,0 +1,11 @@
+# THIS IS A STUB with some starting reference code.
+
+# import string
+# import itertools
+# all_hardchars = (['b','c','d','f']) 
+# four_hardchars_allpermutations = list(itertools.permutations(all_hardchars, 4))
+# outfile = open('all_hardchars_four_permutations.txt', 'w')
+
+# for i in four_hardchars_allpermutations:
+	# outfile.write(i[0] +i[1]  +i[2]  +i[3] +'\n')
+# outfile.close()
