e6b7cf3726aa2a176fdd0b904f77428187231ae8
Alex Hall <ansible1@earthbound.io>
2019-10-28 10:39:09 -0600

    comments / defaults tweaks

--- scripts/imgAndVideo/get_CIECAM02_simplified_gamut.py
+++ scripts/imgAndVideo/get_CIECAM02_simplified_gamut.py
@@ -9,6 +9,8 @@
 # DEPENDENCIES
 # python, with these packages: numpy, ciecam02, colormap, more_itertools
 
+# TO DO? parameterize J, C and h steps.
+
 
 # CODE
 
@@ -18,14 +20,15 @@
 # - https://colorspacious.readthedocs.io/en/latest/reference.html#supported-colorspaces
 # - https://colour.readthedocs.io/en/latest/colour.appearance.html#ciecam02
 # - winner if it works: https://colorspacious.readthedocs.io/en/latest/reference.html#ciecam02 : "If you just want a better replacement for traditional ad hoc spaces like “Hue/Saturation/Value”, then use the string "JCh" for your colorspace (see Perceptual transformations for a tutorial) and be happy."
-# - OR maybe better?! : https://pypi.org/project/ciecam02/
+# - OR maybe better?! : https://pypi.org/project/ciecam02/ -- or maybe not as it says "_simplified_ form  of CEICAM02.." (emphasis added)
 
 # cicam02 doc:
 # CIECAM02 produces multiple correlates, like H, J, z, Q, t, C, M, s. Some of them represent similar concepts, such as C means chroma and M colorfulness s saturation correlate the same thing in different density. We need only 3 major property of these arguments to completely represent a color, and we can get other properties or reverse algorithms..
-# Color type jch is a float list like [j, c, h], where 0.0 < j < 100.0, 0.0 < h < 360.0, and 0.0 < c. the max value of c does not limit, and may produce exceeds when transform to rgb. The effective value of max c varies. Probablly for red color h 0.0, and brightness j 50.0, c reach the valid maximum, values about 160.0.
+# Color type jch is a float list like [j, c, h], where 0.0 < j < 100.0, 0.0 < h < 360.0, and 0.0 < c. the max value of c does not limit, and may produce exceeds when transform to rgb. The effective value of max c varies. Probably for red color h 0.0, and brightness j 50.0, c reach the valid maximum, values about 160.0.
 # IN OTHER WORDS: j (brightness) range is 0 to 100, h (hue) is 0 to 360, c (chroma) can be 0 to any number (I don't beleive that there _is_ a max from whatever correlary/inputs produces the max), maybe max 160. I'll start with max 182 (this seemed a thing with L in HCL).
 
 # NOTE my eyes said, assuming those values, for HCL, do these step values: L_step 18, C_domain 24, H_step 7
+# (later self: WHAT?! 7 colors? How about 12 * how many perceived steps in between all?!)
 
 
 import numpy as np
@@ -34,22 +37,22 @@ from colormap import rgb2hex	# which also needs easydev (pip install easydev)
 
 j_min = 0
 j_max = 100
-j_step = int(100 / 18)
+j_step = int(100 / 15)
 
 c_min = 0
 c_max = 162
-c_step = int(162 / 24)
+c_step = int(162 / 11)
 
 h_min = 0
 h_max = 360
-h_step = int(360 / 7)
+h_step = int(360 / 150)
 
 # Stepping (counting) down because it's difficult (or with this code depending, impossible) to hit the max ranges counting up:
 simplified_jch_gamut = []
 simplified_jch_gamut_as_RGB_hex = []
-for c in range(c_max, c_min, -c_step):
-	for j in range(j_max, j_min, -j_step):
-		for h in range(h_max, h_min, -h_step):
+for h in range(h_max, h_min, -h_step):
+	for c in range(c_max, c_min, -c_step):
+		for j in range(j_max, j_min, -j_step):
 			# build jch array:
 			jch = np.array([ [j, c, h] ])
 			jch_as_str = str(jch[0])
@@ -64,7 +67,9 @@ for c in range(c_max, c_min, -c_step):
 # -- or TO DO--this without importing another library, re: https://thispointer.com/python-how-to-remove-duplicates-from-a-list/
 from more_itertools import unique_everseen
 simplified_jch_gamut = list(unique_everseen(simplified_jch_gamut))
-# simplified_jch_gamut_as_RGB_hex = list(unique_everseen(simplified_jch_gamut_as_RGB_hex))
+simplified_jch_gamut_as_RGB_hex = list(unique_everseen(simplified_jch_gamut_as_RGB_hex))
+# ALTERNATE AWK method of deduping that after its piped to file from this script:
+# awk -v BINMODE=rw '!($0 in a){a[$0];print}' inputFile.hexplt >> tmp.hexplt
 
 # OUTPUT OPTIONS:
 # UNCOMMENT whichever for loop you wish to produce output from (jch values or RGB hex):
