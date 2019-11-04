cb9012f63e9863c2bc554d2b434332f339d3ac85
Alex Hall <ansible1@earthbound.io>
2019-11-04 09:54:13 -0700

    correct param (elim. redundant -q)

--- scripts/admin/installMyCygwinToolchain.sh
+++ scripts/admin/installMyCygwinToolchain.sh
@@ -1,5 +1,7 @@
-# Copy the cygwin installer to this directory before executing this script. Comment out the line for the cygwin installer version you don't want to use.
-installParams="-q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -q -P bc -P xxd"
+# NOTE: to install xxd, see installCygwinXXD.sh
 
-./Cygwin_setup-x86.exe $installParams
-# ./Cygwin_setup-x86_64.exe $installParams
+# Run this script from the directory with the installer, with this script in your PATH, or copy the cygwin installer to this directory before executing this script. Comment out the line for the cygwin installer version you don't want to use.
+installParams="-q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -P bc"
+
+# ./Cygwin_setup-x86.exe $installParams
+./Cygwin_setup-x86_64.exe $installParams
