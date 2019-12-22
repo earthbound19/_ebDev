7722c3673bc78471617730a1aa06034a249f8980
Alex Hall <ansible1@earthbound.io>
2019-12-22 07:22:13 -0700

    copies fonts from dir. above if exist

--- scripts/fountain2pdf.sh
+++ scripts/fountain2pdf.sh
@@ -8,15 +8,15 @@
 # fountain2pdf.sh fountain-source-file.fountain foo
 # --where "foo" is optional, and may be anything, which if present, causes the script to join
 # semantic linefeeds in action or dialogue (to a temp file) before PDF conversion.
-# SEE comments in "START PDF RENDER OPTIONS" section for different possible dependencies to use (via comment/uncomment of code lines)).
+# WARNING: only use this on a fountain source that is backed up, because it backs it up, modifies, the original, then restores from backup, which will break things if anything interrupts or goes wrong with script execution.
+# ALSO, SEE comments in "START PDF RENDER OPTIONS" section for different possible dependencies to use (via comment/uncomment of code lines)).
 
 # NOTES
 # THIS SCRIPT expects everything after the title page of the screenplay (the actual
 # screenplay body text) to start with:
 # > FADE IN:
 # and you may expect this script to not work if that is not the case.
-# ALSO, the "wrap" CLI option expects those specific font files (given in this script)
-# to be (I think) in the same PATH as the source fountain file. Also, the optional last
+# Also, the optional last
 # line of this script opens the output pdf.
 
 # DEPENDENCIES
@@ -32,7 +32,7 @@ fileNameNoExt=${1%.*}
 
 # if optional parameter 2 provided, join semantic linefeeds into a temp doc and swap it for
 # the original before PDF print:
-if ! [ -z ${2+x} ]
+if [ ${2+x} ]
 then
 	# Kludgy and arbitrarily inflexible (instead of pattern matching any title page elements
 	# and temporarily cutting them out), but:
@@ -65,17 +65,29 @@ then
 	rm ./tmp_head_wYSNpHgq.fountain ./tmp_tail_wYSNpHgq.fountain
 fi
 
+# LOCAL FONTS for wrap render setup:
+# conditional copy of fonts if they exist in parent dir:
+if [ -e ../CourierMegaRS-SemiCondensed.ttf ]; then cp ../CourierMegaRS-SemiCondensed.ttf .; fi
+if [ -e ../CourierMegaRS-SemiCondensedBold.ttf ]; then cp ../CourierMegaRS-SemiCondensedBold.ttf .; fi
+if [ -e ../CourierMegaRS-SemiCondensedBoldItalic.ttf ]; then cp ../CourierMegaRS-SemiCondensedBoldItalic.ttf .; fi
+if [ -e ../CourierMegaRS-SemiCondensedItalic.ttf ]; then cp ../CourierMegaRS-SemiCondensedItalic.ttf .; fi
+# TO DO: get this param. working:
+# if those copies work (we assume from one check), set an opt to use them:
+# if [ -e ./CourierMegaRS-SemiCondensed.ttf ]; then FONT_ARG="\"--font \"CourierMegaRS-SemiCondensed.ttf, CourierMegaRS-SemiCondensedBold.ttf, CourierMegaRS-SemiCondensedItalic.ttf, CourierMegaRS-SemiCondensedBoldItalic.ttf\""; fi
+
 # ====
 # START PDF RENDER OPTIONS:
 # UNCOMMENT THIS: "wrap" CLI option, uses specific fonts--
 wrap pdf $1 --font "CourierMegaRS-SemiCondensed.ttf, CourierMegaRS-SemiCondensedBold.ttf, CourierMegaRS-SemiCondensedItalic.ttf, CourierMegaRS-SemiCondensedBoldItalic.ttf"
-
 # _OR_ UNCOMMENT THIS: "afterwriting" CLI option--
 # afterwriting --source $1 --overwrite --pdf
 # I gave my best effort and the following method of loading fonts is *stupid* arcane (*_two_ json files?!_*) and doesn't seem to work:
 # --config courierMegaConfig.json --fonts CourierMega.json
 # END PDF RENDER OPTIONS
 # ====
+
+# CLEAN UP the set up local fonts for wrap render; this will throw a non-fatal error if those files don't exist;
+rm CourierMegaRS-SemiCondensed.ttf CourierMegaRS-SemiCondensedBold.ttf CourierMegaRS-SemiCondensedBoldItalic.ttf CourierMegaRS-SemiCondensedItalic.ttf
 
 # If we joined semantic linefeeds, restore backed-up fountain file over original:
 if ! [ -z ${2+x} ]
@@ -84,4 +96,4 @@ then
 fi
 
 # Optionally open result PDF; change `open` to `cygstart` for Cygwin:
-open ./$fileNameNoExt.pdf
+# open ./$fileNameNoExt.pdf
