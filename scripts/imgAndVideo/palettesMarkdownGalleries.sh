5ef92fd7b8ba3a678269372e5eaca536cbb1f241
Alex Hall <ansible1@earthbound.io>
2019-12-22 07:21:04 -0700

    upd.

--- scripts/imgAndVideo/palettesMarkdownGalleries.sh
+++ scripts/imgAndVideo/palettesMarkdownGalleries.sh
@@ -1,16 +1,8 @@
 # DESCRIPTION
 # Crawls all directories in the current path and runs palettesMarkdownGallery.sh in every one.
 
-array=(`gfind . -type d -printf '%f\n'`)
+# VASTLY simplified from previous incarnations of this script, and works recursively besides
+# (those didn't) :
+gfind . -type d -exec sh -c 'cd "{}" && palettesMarkdownGallery.sh' \;
 
-currdir=`pwd`
-
-for element in ${array[@]}
-do
-	pushd .
-	cd $currdir/$element
-	palettesMarkdownGallery.sh
-	popd .
-done
-
-# echo "DONE running palettesMarkdownGallery.sh in every directory in this path."
+echo "DONE running palettesMarkdownGallery.sh in every directory in this path."
