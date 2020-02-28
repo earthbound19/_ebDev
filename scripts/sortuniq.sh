4ac441c404b4a6d0a7d18aa9ecb321933df4b934
Alex Hall <ansible1@earthbound.io>
2020-02-28 12:43:33 -0700

    simplify

--- scripts/sortuniq.sh
+++ scripts/sortuniq.sh
@@ -1,13 +1,16 @@
 # DESCRIPTION
-# Takes input file $1, sorts it via `sort` to a temp file, then deduplicates lines in it via `uniq` back to the original file, and deletes the temp file. WARNING: Overwrites the original file without warning. (That was your only warning--and you missed it if you didn't read this comment in the source code.)
+# Takes input file $1, sorts it, reduces it to unique entries, and writes that back to $1.
+# WARNING: Overwrites the original file without warning. (That was your only warning--and
+# you missed it if you didn't read this comment in the source code.)
 
 # USAGE
 # sortuniq.sh fileNameToSortAndDedup.txt
 
 
 # CODE
-sort $1 > _tmp_HWPqYyXjv7pGCN.txt
-uniq _tmp_HWPqYyXjv7pGCN.txt > $1
-rm ./_tmp_HWPqYyXjv7pGCN.txt
+sort $1 | guniq > _tmp_HWPqYyXjv7pGCN.txt
+mv -f _tmp_HWPqYyXjv7pGCN.txt $1
+
 
 echo DONE. Mein Krummspugerlt ist nicht mein Krummspugelnd.
+
