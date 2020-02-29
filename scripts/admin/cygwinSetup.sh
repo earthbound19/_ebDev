95fce7fbb27d2f326be393bdc74e1259f4585f2f
earthbound19 <earthbound19@users.noreply.github.com>
2020-02-28 22:24:09 -0700

    comment changes / trivial changes

--- scripts/admin/cygwinSetup.sh
+++ scripts/admin/cygwinSetup.sh
@@ -1,8 +1,23 @@
-# Run this script from the directory with the installer, with this script in your PATH, or copy the cygwin installer to this directory before executing this script. Comment out the line for the cygwin installer version you don't want to use.
-installParams="-q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -P bc"
+# DESCRIPTION
+# Commands to install wanted tools and custom configuration for a Cygwin install.
+
+# USAGE
+# Run this script from the directory with the installer, with this script
+# and the other files it needs (CYGWIN_fstab, my.minttyrc.settings.txt) in
+# your PATH, or copy the cygwin installer to this directory before executing
+# this script. Comment out the line for the cygwin installer version you don't
+#want to use.
+
+
+# CODE
+installParams="-q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -P bc -P lynx -P chere"
 
 # ./Cygwin_setup-x86.exe $installParams
 ./Cygwin_setup-x86_64.exe $installParams
+
+lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
+dos2unix apt-cyg
+install apt-cyg /bin
 
 username=`whoami`
 echo "Username is $username. OVERWRITING configuration of /home/$username/.minttyrc with my.minttyrc.settings.txt. Continue?"
