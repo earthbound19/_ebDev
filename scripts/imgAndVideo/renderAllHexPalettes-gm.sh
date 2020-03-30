192958210ee659160b122d24a292705d896cf953
earthbound19 <earthbound19@users.noreply.github.com>
2020-03-29 22:25:39 -0600

    tweak (no rnd palette sort)

--- scripts/imgAndVideo/renderAllHexPalettes-gm.sh
+++ scripts/imgAndVideo/renderAllHexPalettes-gm.sh
@@ -12,7 +12,7 @@ for element in ${array[@]}
 do
 	echo ~~~~
 	echo invoking renderHexPalette-gm.sh for $element . . .
-	renderHexPalette-gm.sh $element 80
+	renderHexPalette-gm.sh $element 80 0
 done
 
 echo "DONE. Color palettes have been rendered from all *.hexplt files in the current path and subpaths for which there was not already a corresponding .png image. Palette images are named after the source *.hexplt files."
