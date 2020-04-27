eeff1f2e6c5b17fa092c419eed33043e860852ad
earthbound19 <earthbound19@users.noreply.github.com>
2020-04-26 20:06:08 -0600

    new

---
+++ _in_development/ffmpegScrollImage.sh
@@ -0,0 +1,10 @@
+# TO DO:
+# - parameterize.
+# - adapt 0.08 via math, re this comment at the post:
+https://stackoverflow.com/a/56302143/1397555
+# "The -t is added for the image so that we have a stream with 2 frames.
+# (25 fps x 0.08 = 2). The setpts sets the timestamp for the 2nd frame
+# to the inverse of the scroll rate, which represents a fraction of the
+# height. The fps filter fiils in the timestamp gaps with cloned frames."
+
+ffmpeg -f lavfi -i color=s=1920x1080 -loop 1 -t 0.08 -i "input.png" -filter_complex "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+1/0.02/TB),fps=25[fg]; [0:v][fg]overlay=y=-'t*h*0.02':eof_action=endall[v]" -map "[v]" output.mp4
