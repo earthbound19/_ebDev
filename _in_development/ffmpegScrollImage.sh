bf22a635adac50bf6882e4452b0b1c5e137afa22
earthbound19 <earthbound19@users.noreply.github.com>
2020-04-28 01:10:36 -0600

    oops those comments weren't supposed to be there

--- _in_development/ffmpegScrollImage.sh
+++ _in_development/ffmpegScrollImage.sh
@@ -8,7 +8,3 @@ https://stackoverflow.com/a/56302143/1397555
 # height. The fps filter fiils in the timestamp gaps with cloned frames."
 
 ffmpeg -f lavfi -i color=s=1920x1080 -loop 1 -t 0.08 -i "input.png" -filter_complex "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+1/0.02/TB),fps=25[fg]; [0:v][fg]overlay=y=-'t*h*0.02':eof_action=endall[v]" -map "[v]" output.mp4
-
-
-# 3672 seconds
-# gm convert *.png -append ___supahighout.png
