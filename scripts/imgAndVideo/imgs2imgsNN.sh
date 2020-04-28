33aa31123c4679fbeb02a5db034bd3e5fdd01c0a
earthbound19 <earthbound19@users.noreply.github.com>
2020-04-28 00:21:02 -0600

    supercedes irfanview scripts via ppm format detect

--- scripts/imgAndVideo/imgs2imgsNN.sh
+++ scripts/imgAndVideo/imgs2imgsNN.sh
@@ -1,35 +1,32 @@
 # DESCRIPTION
-# Resizes all images of type T (via parameter $1) in a path, by nearest-neighbor method, to target format F ($2), at size A x B ($3 x $4). Uses GraphicsMagick, unless the file is ppm format, and in that case it uses IrfanView.
+# Resizes all images of type T (via parameter $1) in a path, by
+# nearest-neighbor method, to target format F ($2), at size A x B ($3 x $4).
+# (Nearest neighbor method will keep hard egdes, or look "pixelated.")
+# Uses GraphicsMagick, unless the file is ppm format, and in that case it
+# uses IrfanView.
 
 # USAGE
+# NOTE that as this auto-switches to IrfanView as needed, it supercedes
+# the now deleted irfanView2imgNN.sh and allIrfanView2imgNN.sh scripts.
+# To use:
 # Invoke this script with the following parameters
-# $1 source format (without .)
+# $1 source file format (or extension, without .)
 # $2 destination format
 # $3 scale by nearest neighbor method to pixels X
-# $4 scale by nearest neighbor method to pixels Y. IF OMITTED, scales to $3 (X pix) by nearest neighbor preserving aspect ratio. NOT ENABLED for ppm (IrfanView) conversion; for that use only $3. See irfanView2imgNN.sh comments for options you may manually use to specify non-ratio sizes.
+# $4 Optional. Force this Y-dimension, regardless of aspect. Scales by
+#  nearest neighbor method to pixels Y. ONLY USED for ppms. Ignored for
+#  all other types (aspect kept). SEE COMMENTS in i_view32.exe code
+#  lines area for options to maintain aspect and/or rotate image (wanted
+#  for my purposes at times).
 # Example command:
 # imgs2imgsnn.sh ppm png 640
-# OR, to force a given x by y dimension:
+# OR, to force a given x by y dimension for a ppm:
 # imgs2imgsNN.sh ppm png 640 480
 
 # DEPENDENCIES
-# GraphicsMagick, possibly IrfanView, both in your $PATH.
-
-# NOTES:
-# A previous verson of this script conditionally used IrfanView for ppms
-# because another script I developed made ppms that only IrfanView could convert
-# to pngs (I think I was making non-standard ppms). That script now makes
-# proper ppms (it fills them with integer instead of hex RGB values). If you
-# have ppms with hex values, hack this script to use the IrfanView option,
-# though it might be better to recreate or conver the ppms to a format that
-# has wider acceptance.
-
-# TO DO
-# Assign script paramaters to named variables and use the named variables.
-
+# GraphicsMagick and/or IrfanView, both in your $PATH.
 
 # CODE
-
 array=(`gfind . -maxdepth 1 -type f -iname \*.$1 -printf '%f\n'`)
 for img in ${array[@]}
 do
@@ -37,13 +34,23 @@ do
 	imgFileExt=${img##*.}
 	targetFileName=$imgFileNoExt.$2
 	if [ ! -f $targetFileName ]; then
-			# IrfanView option:
-			# i_view32.exe $img /resize_long=$3 /aspectratio /convert=$targetFileName
-				# ex. GraphicsMagick command:
-				# gm convert 6x5gridRND_2017_05_06__01_51_14__099842100.ppm -scale 1200 out.png
+		# if source file is ppm, use IrfanView or graphicsmagic
+		# (uncomment your preference) to convert.
+		if [ $imgFileExt == ppm ]; then
+			echo converting ppm file via i_view32 . . .
+			# re: http://www.etcwiki.org/wiki/IrfanView_Command_Line_Options
+			# ROTATE 90 DEGREES OPTION; uncomment next line (used with other options) :
+			# extraIrfanViewParam1="/rotate_r"
+				# FORCE ARBITRARY DIMENSIONS (aspect) OPTION:
+			i_view32.exe $img /resize_long=$3 /resize_short=$4 $extraIrfanViewParam1 /convert=$targetFileName
+				# MAINTAIN ASPECT OPTION:
+			# i_view32.exe $img /resize_long=$3 /aspectratio $extraIrfanViewParam1 $extraIrfanViewParam2 /convert=$targetFileName
+		# otherwise use graphicsmagic:
+		else
+			echo converting image via graphicsmagick . . .
 			# If params $3 or $4 were not passed to the script, the command will simply be empty where they are (on the following line of code), and it should still work:
-			gm convert $img -scale $3 $4 $targetFileName
-#		fi
+			gm convert $img -scale $3 $targetFileName
+		fi
 		echo converted to $targetFileName . .
 	else
 		echo target file $targetFileName already exists\; skipping.
