8a2c185d0c025e83cf2d6e0f6225c88684211da0
Alex Hall <earthbound19@users.noreply.github.com>
2020-04-30 21:17:39 -0600

    rewrite scripts and rename (was .bat; now .sh)

--- scripts/font/FontForgeTTFs2OTFs.bat
+++
@@ -1,6 +0,0 @@
-REM revamped to use simpler script. 08/29/2014 12:49:24 PM -RAH
-REM usage: first open a console using fontforge-console.bat (which comes with the fontforge win distribution), then invoke this batch with a parameter: the name of a .tff font to convert to .otf (Open Type Font) and .woff (web font).
-
-FOR /f "tokens=* delims= " %%F IN ('DIR *.ttf /B') DO (
-fontforge.exe  -script convert.pe %%F
-)
