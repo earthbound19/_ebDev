342fb2943facae914ff9c57595c6edf959a60363
Alex Hall <earthbound19@users.noreply.github.com>
2020-05-22 15:32:23 -0600

    handles single and double quotes now

--- scripts/admin/ftunStr.sh
+++ scripts/admin/ftunStr.sh
@@ -1,12 +1,18 @@
 # DESCRIPTION
-# Takes a string parameter $1 and echoes a terminal-friendly string (e.g. for file names); Fixes Terminal Un-friendly Strings.
+# Takes a string parameter $1 and echoes a terminal-friendly string
+# (e.g. for file names); Fixes Terminal Un-friendly Strings.
 
 # USAGE
-# Invoke this script with one parameter surrounded in double-quotes; e.g.
-# ./ThisScript.sh "Mountain Landscape over Downtown Provo UT"
-# To make use of the echoed result in another script by storing it in a variable, surround the call with backticks and assign it to a variable:
-# terminalFriendlyString=`./thisScript.sh "Mountain Landscape over Downtown Provo UT"`
+# Invoke this script with one parameter surrounded either in double
+# or singl quotes: e.g.
+# ./ftunStr.sh "Mountain@Landscape over] Downtown'' Provo UT"
+#  OR
+# ./ftunStr.sh 'Mountain@Landscape over]"" Downtown Provo UT'
+# To make use of the echoed result in another script by
+#  storing it in a variable, surround the call with backticks
+#  and assign it to a variable:
+# terminalFriendlyString=`./ftunStr.sh "Mountai'n Landscape#$ over Downtown Provo UT"`
 
 
 # CODE
-echo $1 | tr \=\@\`~\!#$%^\&\(\)+[{]}\;\ , _
+echo $1 | tr \"\'\=\@\`~\!#$%^\&\(\)+[{]}\;\ , _
