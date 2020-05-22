e7accb37aac82b7b4abcd1c059eb5b43acee5613
Alex Hall <earthbound19@users.noreply.github.com>
2020-05-22 15:45:32 -0600

    somewhat imrove doc., add to dos

--- scripts/imgAndVideo/EXIFdataBatch/__DigitalImagePress.sh
+++ scripts/imgAndVideo/EXIFdataBatch/__DigitalImagePress.sh
@@ -1,19 +1,29 @@
-# USAGE: comment out steps unecessary for your current run of the image press (this script). Then run the script. Also NOTE comments in ALL CAPS in this script.
+# DESCRIPTION
+# Invokes a series of scripts to prepare and embed metadata in final exported (pre-publication) art etc. files.
 
-# FIRST, USE; PREFERRED: http://sourceforge.net/projects/file-folder-ren/?source=typ_redirect with MetamorphoseBadFileNameCharacterRemoval.cfg (and adjust the search/select folder as necessary), to remove unwanted characters from file names. NOTE: When it asks about creating a t_e_m_p folder, you may answer no (it only creates a folder maintaining the folder structure of the renamed files, which you may paste back into the root operating folder. Depending on circumstance, you may want to do that).
-		# Deprecated (formerly preferred):
-						# cleanup_bad_fileNames__RenameFiles2.3b.exe (*without* using the white spaces option) and file_renamer_install_flex73.exe respectively to remove all special characters and replace all spaces with underscores. NOTE: it freaks out unless you put a \ at the end of the path to scan. THEN, use fixIMGnames.sh, which fails on some or perhaps too many special characters repair :/ but that is mitigated by using the first mentioned tools. FAIL for that, but in development: cleanupGarbageFileNames.sh
-				# THE ABOVE accomplished with the next two lines:
-				# renameFiles.exe
-				# fixIMGnames.sh
+# USAGE
+# Not currently well documented. A start at documentation:
+# - If necessary, use rename.pl to get all files this script will operate
+#  on to have only terminal-friendly characters in their files names.
+#  This is a manual process to do before running this script. Find it in
+#  rename.pl in this repository and read the USAGE comments I have added to it.
+#  Previous solutions are in:
+#  _THE_SAD_EPIC_OF_TERMINAL_FRIENDLY_FILE_RENAMING.txt.
+# - Comment out steps unecessary for your current run of the image press (this script). Then run the script.
 
-# NOTE: metamorphose2 is not used because of a strange bug listing files to be renamed (it shows an entirely wrong target file name which is from anoter file name in the tree).
 
-# TO DO: Revise this to describe how metamorphose is needed for folder renames and ftun.sh for this (I think--test that?)--and to also call ftun.sh
-# TO DO: update documentation in comments and have it refer to an outside file (move relevant comments from here to said file).
-# TO DO: Move the relevant instructions from bunnieselow, or all of them, into echo statements in the files to which they relate, for cases of using those files independent of this script.
-# TO DO? : split this into two scripts (one for file name and metadata fixup, another for custom metadata creation), and update documentation referencing the (to be former) file name of this script accordingly.
+# TO DO
+# - move this to _ebArt? It's a "recipe" in the sense given there.
+# - Update documentation in comments and have it refer to an outside file?
+# - Move the relevant instructions from below, or all of them, into
+#  echo statements in the files to which they relate, for cases of using
+#  those files independent of this script.
+# - Split this into two scripts (one for file name and metadata fixup, another for custom metadata creation), and update documentation referencing the (to be former) file name of this script accordingly?
+# Do I want this to use archiveMetaDataWin.sh before prepping/updating metadata?
+# Do I want this to also use exportIMGsMetadataSimple.sh?
 
+
+# CODE
 echo "!============================================================"
 	echo "WARNING: Before using this script, ensure all file names in your execution path do not have any console-unfriendly characters in their file names, including no spaces (replace those with underscores). OTHERWISE, this script will not work as hoped. This script will first attempt to run metamorphose (see https://sourceforge.net/projects/file-folder-ren/files/Metamorphose/1.1.2%20stable/ ). Load the Metamorphose1BadFileNameCharacterRemoval_step01.cfg configuration into metamorphose, adjust the search folder as necessary, and execute it. Then reload the file list, and repeat this with ~02.cfg, and any others after that. ALTERNATE option for this: manually use the Flexible Renamer program, operating only on folders, with the regex in badCharsFRregex.txt, replacing with _. ALSO, to include all files in subdirectories, look for a \"walk\" checkbox and tick it. If metamorphose fails to open from this script, you will have a chance to abort this script. NOTE: if metamorphose gives you a permissions or other access error, it may be that a path name is too long. Shorten it up or temporarily move a long folder name to a root dir, then run this script against that temporarily moved folder. ALSO NOTE: you might additionally or alternately try ftun.sh."
 	echo "Do you wish to run this script?"
