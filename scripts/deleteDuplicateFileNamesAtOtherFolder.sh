e3eda649c14fc0768e81f842e257679a8ee954e9
Alex Hall <earthbound19@users.noreply.github.com>
2020-05-24 20:58:26 -0600

    make dangerous scripts safer (warnings/file sort)

--- scripts/deleteDuplicateFileNamesAtOtherFolder.sh
+++ scripts/deleteDuplicateFileNamesAtOtherFolder.sh
@@ -1,10 +1,24 @@
+# DESCRIPTION
+# Permanently deletes all files in path $1 (parameter one)
+# which are in the directory this script is run from.
+# Warns you and asks you to type password to confirm.
+
 # USAGE
-# pass one parameter, being another path. All files in that other path will be deleted which have a duplicate file name in the current path, PERMAENENTLY AND WITHOUT WARNING. You must comment out the echo line and uncomment the delete line in this code for this to happen, though.
+# pass one parameter, being another path, e.g.:
+# deleteDuplicateFileNamesAtOtherFolder.sh /c/whydothosefilesexist/there
 
-filesHere=(`gfind . -maxdepth 1 -type f -iname \*.* -printf '%f\n'`)
+thisDir=`pwd`
+read -p "WARNING: this script will delete all files in path $1 which have a duplicate file in the immediate path, $thisDir. If that is what you want, type YES_DESTROY_THEM and then press ENTER or RETURN: " FLEURF
 
-for element in "${filesHere[@]}"
-do
-  echo "COMMAND WOULD BE: $1/$element"
-  # rm $1/$element
-done
+if [ "$FLEURF" == "YES_DESTROY_THEM" ]
+then
+	filesHere=(`gfind . -maxdepth 1 -type f -iname \*.* -printf '%f\n'`)
+
+	for element in "${filesHere[@]}"
+	do
+	  echo "RUNNING COMMAND: rm $1/$element"
+	  rm $1/$element
+	done
+else
+	echo YES_DESTROY_THEM was not typed\; exiting script and doing noththing else.
+fi
--- scripts/pruneByUnmatchedExtension.sh
+++ scripts/pruneByUnmatchedExtension.sh
@@ -1,14 +1,26 @@
 # DESCRIPTION
-# Deletes all files in the current folder (non-recursive) with a given extension (e.g. .png) that have no companion file with the same base file name and a different extension (e.g. .ppm, .hexplt, anything). Useful for discarding e.g. undesired source format files whose undesirability have been ascertained by converting them to a target format and viewing, then deleting the rendered target image. NOTE that this will not actually execute any delete commands: rather, it makes a proposed script for the delete commands, with instructions on how to run it.
-
+# Helps discard files in the current folder (non-recursive) with a given 
+# extension that have no companion file with the same file but a 
+# different extension. Does this by moving the identified files into a 
+# temp folder for review before permanent and manual deletion. Scenarios 
+# this is useful in: suppose you have a set of render source or 
+# configuration files (in my case it might be files of extension .cgp, or 
+# .hexplt) for which you have rendered targets (e.g. .png images of color 
+# growth script settings or palettes). You may then go through and delete 
+# the .png results you don't like, and run this script to move all the 
+# associated .cgp or .hexplt files into a temp folder. You can then review 
+# the files in the temp folder and manually delete them. 
 # WARNING
 # If you use this on files with unintended dissimilar base file names such as thisFractalRenderFlame.flame.png, you will lose work!
 
 # USAGE
 # With this script in your PATH:
-# pruneByUnmatchedExtension.sh extensionOfSourceFilesToDelete ifNoMatchedFileNameWithThisExtension, e.g.:
+# pruneByUnmatchedExtension.sh extensionOfFilesToMove ifNoMatchedFilesWithThisExtension, e.g.:
 # pruneByUnmatchedExtension.sh hexplt png
-# -- will result in the delete of every file with an extension .hexplt that has no same-named file with a .png extension. NOTE that extensions passed as parameters must not include the dot (.).
+# -- will sort every file with extension extension .hexplt that has no
+# same-named file with a .png extension into a temporary folder for manual
+# analysis and deletion or recovery. NOTE that extensions passed as
+# parameters must not include the dot (.).
 # READ ON for a detailed explanation.
 # Suppose you have so many .ppm files which you have converted to .png:
 # ~
@@ -21,7 +33,9 @@
 # img_04.ppm
 # img_04.png
 # ~
-# -- and you decide you want to delete some of the source .ppm files because you don't like the pngs they render to. First delete the rendered pngs you don't like:
+# -- and you decide you want to delete some of the source .ppm files 
+# because you don't like the pngs they render to. First delete the 
+#rendered pngs you don't like: 
 # ~
 # img_01.ppm
 # img_02.ppm
@@ -30,13 +44,21 @@
 # img_03.png
 # img_04.ppm
 # ~
-# -- and then run this script with parameters that tell this script $1 the source file extension to search for matching file names with extension $2, where $1 will be deleted if no file with extension $2 is found. Example:
+# -- and then run this script with parameters that tell this script
+# $1 the source file extension to search for matching file names
+# with extension $2, where $1 will be deleted if no file with
+# extension $2 is found. Example:
 # ./thisScript.sh ppm png
-# After the script run, ppm files the had no png with the same base file name will be deleted:
+# After the script run, ppm files that had no png with the same
+# base file name:
 # img_02.ppm
 # img_02.png
 # img_03.ppm
 # img_03.png
+# -- will be sorted into a new folder with a random name like:
+# ___delete_candidates_tmp__BB01BF75857DDFE0
+# -- where you can manually analyze them and delete them
+# after you are sure you don't want them.
 
 # NOTES
 # This script intended to run e.g. after NrandomHexColorSchemes.sh and renderAllHexPalettes-gm.sh (which invokes renderHexPalette-gm.sh repeatedly for every .hexplt file in a directory), and renders and deletes of not desired converted pngs of those palettes, or after autobrood fractorium renders to prune undesired fractal flame genomes.
@@ -45,10 +67,23 @@
 # orphan, unmatched, unpaired, no pair, extension, prune, delete, not found, pair
 
 # CODE
-list=`gfind . -maxdepth 1 -iname "*.$1" | gsed 's/^\.\///g' | tr -d '\15\32'`
+if ! [ "$1" ]
+then
+	echo "No parameter \$1 passed to script. Exit."
+	exit
+fi
+if ! [ "$2" ]
+then
+	echo "No parameter \$2 passed to script. Exit."
+	exit
+fi
 
-# empty tmp_Dn6M_proposed_deletes.sh.txt whether it exists or not (recreate it blank) :
-printf "" > tmp_Dn6M_proposed_deletes.sh.txt
+list=`gfind . -maxdepth 1 -type f -iname \*.$1 -printf '%f\n' | tr -d '\15\32'`
+
+RND16HEX=`cat /dev/urandom | tr -dc '0-9A-F' | head -c 16`
+delete_candidates_temp_folder=___delete_candidates_tmp__"$RND16HEX"
+mkdir $delete_candidates_temp_folder
+
 for element in ${list[@]}
 do
 	fileNameNoExt=${element%.*}
@@ -56,11 +91,21 @@ do
 			# echo searchFileName is\: $searchFileName
 	if ! [ -f $searchFileName ]
 	then
-# FOR SAFE MODE, uncomment the next line and comment out the line after it! For DANGER MODE, reverse those directions!
-	# echo File matching source file name $element but with $2 extension NOT FOUND\; will PROPOSE TO DELETE source file\! && echo "rm $element" >> tmp_Dn6M_proposed_deletes.sh.txt
-    echo "File matching source file name $element but with $2 extension NOT FOUND\: DELETING\!" && rm $element
+		echo "File matching source file name $element but with $2"
+		echo " extension NOT FOUND\: SORTING into folder"
+		echo " $delete_candidates_temp_folder"
+		mv $element ./$delete_candidates_temp_folder
 	fi
 done
 
-echo ~-
-echo "DONE. If you ran this script in SAFE MODE, open the file tmp_Dn6M_proposed_deletes.sh.txt and\, if the delete commands in it are agreeable, rename the file to a .sh script, give it execute permissions, and run it. Otherwise, this script may have permanently deleted things you want to keep, and you are a fool. If you ran this in danger mode you may want to delete the empty, extraneous tmp_Dn6M_proposed_deletes.sh.txt file. To switch between safe and danger mode see the SAFE MODE comment near the end of this script's source code."
+echo ""
+echo "DONE. All files proposed for deletion have been moved"
+echo " to the folder $delete_candidates_temp_folder."
+echo "!- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo "!- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo " WARNING: please be very sure you have not renamed any"
+echo " associated e.g. render or target files to something"
+echo " that does not match a source/configuration file before"
+echo " you delete them!"
+echo "!- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo "!- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
