19de3911178d5821032ac7cfd178dc74afb65145
Alex Hall <earthbound19@users.noreply.github.com>
2020-06-07 12:13:59 -0600

    new

---
+++ scripts/admin/winsat_drive_test.bat
@@ -0,0 +1,4 @@
+REM RUN WITH one parameter, being the drive letter (no : or \ in it) you want to test.
+REM Re: https://superuser.com/questions/130143/how-to-measure-disk-performance-under-windows
+
+winsat disk -drive %1
