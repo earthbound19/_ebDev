7de6e86f2a02870a70c1cd94f9b56927f1a6b526
earthbound19 <earthbound19@users.noreply.github.com>
2020-06-15 01:26:51 -0600

    new

---
+++ _in_development/pypy_install_pip.sh
@@ -0,0 +1,11 @@
+# DESCRIPTION
+# Installed pip with pypy3, so that you can install other packages (for use with pypy3) via pip.
+# re: https://stackoverflow.com/a/44737687/1397555
+# From the directory with pypy3 in it, run (can alter it withuot the ./ if you put pypy3 in your PATH) :
+
+./pypy3 -m ensurepip
+./pypy3 -m pip install --upgrade pip
+# ./pypy3 -m pip install whatever_package_name
+# ./pypy3 -m pip install whatever_package_name
+# ./pypy3 -m pip install whatever_package_name
+# etc. . . .
