b82c469303a273f270ffed71b5855d827743b687
Alex Hall <earthbound19@users.noreply.github.com>
2020-06-23 21:50:39 -0600

    add notes in comments

--- scripts/admin/MSYS2/install_msys2_packages.sh
+++ scripts/admin/MSYS2/install_msys2_packages.sh
@@ -1,12 +1,27 @@
 # DESCRIPTION
 # Installs desired msys2 packages.
 
+# USAGE
+# Run this script.
+# NOTES
+# To inherit the windows PATH in the MSYS2 terminal, uncomment lines in the MSYS2 launch batch scripts (such as msys2_shell.cmd) and/or .ini files which set this value:
+#
+# MSYS2_PATH_TYPE=inherit
+#
+# h/t: https://www.rjh.io/blog/20190722-msys2_setup/
+#
+# To get a right-click menu to open any folder in the MSYS2 terminal, double-click the installer .reg file in this folder, which was created via my fork of a tool: https://github.com/earthbound19/msys2-mingw-shortcut-menus
+
 # TO DO
 # Auto-mod (on install) the files mentioned in MSYS2_notes.txt
 # so that MSYS2 inherits the system PATH.
 
 
 # CODE
+# OPTIONAL pre-package install steps: upgrade/sync MSYS2:
+# pacman -Syy
+# pacman -Suu
+
 echo "u go kaboomy haha now you dead moldy voldy -Snep"
 
 # In my cygwin setup and I don't know why:
