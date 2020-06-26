2af6c621a3a0d11f5edeec0b70cb307436ff34c0
earthbound19 <earthbound19@users.noreply.github.com>
2020-06-26 17:37:46 -0600

    upd. script path detect, param. handling

--- scripts/imgAndVideo/getNshadesOfColorsCIECAM02.sh
+++ scripts/imgAndVideo/getNshadesOfColorsCIECAM02.sh
@@ -1,29 +1,24 @@
 # DESCRIPTION
-# Repeatedly invokes getNshadesOfColorCIECAM02.py for every color in a .hexplt file $1,
-# with optional $2 colors (default 13 may be overriden with optional 2nd parameter).
+# Repeatedly invokes getNshadesOfColorCIECAM02.py for every color in
+# a .hexplt file $1, with optional $2 colors (default hard-coded
+# number may be overriden with optional 2nd parameter).
 
 # USAGE
-# With this and thy python script it invokes, and the .hexplt file you'll work against all in
-# your PATH, invoke this script with minimum one argument (the .hexplt file to work with)
-# and optional second argument, being the number of shades to create:
+# With this and thy python script it invokes, and the .hexplt file
+# you'll work against all in your PATH, invoke this script with
+# minimum one argument (the .hexplt file to work with) and optional
+# second argument, being the number of shades to create:
 #
 # getNshadesOfColors.sh inputPalette.hexplt [number_of_shades]
 #
-# To hack other parameters modify the command=".." assignment directly (e.g. with -b 100),
-# referring to the python script.
+# To hack other parameters modify the command=".." assignment
+# directly (e.g. with -b 100), referring to the python script.
 
 # DEPENDENCIES
 # Bash, and all the dependencies of the python script this calls.
 
 
-whereScriptIs=`which getNshadesOfColorCIECAM02.py`
-# if operating system is windows, assume we're using cygin (I dunna use any other 'nixy thing on windows, but ubuntu might be poss.? -- anyhoo --
-# and adapt that var per:
-if [ "$OS" == "Windows_NT" ]
-then
-	whereScriptIs=`cygpath -w $whereScriptIs`
-fi
-
+# CODE
 if [ "$1" ]; then
   INFILE=$1
 else
@@ -37,6 +32,13 @@ else
   N=13
   echo "no parameter 2 for N passed, defaulting to $N."
 fi
+
+  # DEPRECATED OS check:
+  # if [ "$OS" == "Windows_NT" ]
+  # then
+whereScriptIs=`whereis getNshadesOfColorCIECAM02.py`
+whereScriptIs=`echo $whereScriptIs | gsed 's/getNshadesOfColorCIECAM02: //'`
+  # fi
 
 arr=$(<$INFILE)
 for element in ${arr[@]}
