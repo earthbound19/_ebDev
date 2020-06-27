80d0cfc82104158c75950d7794f015b2ec07b2f1
earthbound19 <earthbound19@users.noreply.github.com>
2020-06-26 23:33:32 -0600

    dev

---
+++ _in_development/get_colormind_RND_palette.sh
@@ -0,0 +1,3 @@
+echo "This script is in development. TO DO: parse output and format it to a .hexplt file."
+
+curl 'http://colormind.io/api/' --data-binary '{"model":"default"}' > colormind_RND_palette.txt
