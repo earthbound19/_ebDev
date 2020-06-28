48fb0d9304919cd208eb20dfe4bb6ddf75cff886
earthbound19 <earthbound19@users.noreply.github.com>
2020-06-28 16:12:04 -0600

    add rnd string to temp scrpt to enable simultaneous runs
    without clobbering one runnig insances' scripts
    
    also add optional commented alt command with custom coord but still paramaterized color

--- scripts/recipes/color_growth_hexplt.sh
+++ scripts/recipes/color_growth_hexplt.sh
@@ -24,21 +24,24 @@
 # .rendering file. You can do multiple simultaneous batch
 # renders (exploiting multiple processors/threads) this way too.
 
+
+# CODE
 if ! [ "$1" ]; then echo "No parameter \$1 (a .hexplt file). Exit."; exit; fi
 
 hexplt2rgbplt.sh $1
 
 paletteFileNoExt=`echo "${1%.*}"`
 convertedPaletteFile=$paletteFileNoExt.rgbplt
-
 pathToScript=`whereis color_growth.py | gsed 's/color_growth: \(.*\)/\1/g'`
 
-# uncomment the next line to add these optional additional arguments:
+# uncomment any of the following to add its optional additional arguments:
 # extraArgs='--SAVE_EVERY_N 6'
 # extraArgs='--WIDTH 20480 --HEIGHT 720 --SAVE_EVERY_N 0 --BORDER_BLEND False'
 # extraArgs='--WIDTH 8640 --HEIGHT 1800 --SAVE_EVERY_N 0 --BORDER_BLEND False'
-extraArgs='--WIDTH 9600 --HEIGHT 2400 --SAVE_EVERY_N 0 --BORDER_BLEND False'
- 
+# extraArgs='--WIDTH 9600 --HEIGHT 2400 --SAVE_EVERY_N 0 --BORDER_BLEND False'
+extraArgs='--WIDTH 3600 --HEIGHT 3600 --SAVE_EVERY_N 0'
+
+# I treid iterating over a created array from the palette file, and different commands expecting different IFS delimiters I know not which scewed it up. Just iterate over lines in a file:
 while read -r element
 do
 	# Only render if there is no .rendering stub file named after the
@@ -73,10 +76,20 @@ do
 		# reduce the upper value to max if it's beyond max:
 		if [ $highGrowthClipParam -gt 8 ]; then echo highGrowthClipParam value $highGrowthClipParam greater than max allowed \(8\)\; reducing to 8\.; highGrowthClipParam=8; fi
 		echo "Calling color_growth.py with custom values rgbValForParam $rgbValForParam, invertRGBvalForParam $invertRGBvalForParam, rndSeedValForParam $rndSeedValForParam, rshiftParam $rshiftParam, lowGrowthClipParam $lowGrowthClipParam, highGrowthClipParamAddend $highGrowthClipParamAddend, highGrowthClipParam $highGrowthClipParam . ."
+		# random string component to add to temp shell script name, so multiple insances of this can run simultaneously without clobbering one another's temp scripts:
+		rndString=`cat /dev/urandom | tr -dc 'a-f0-9' | head -c 6`
+		tmp_script_file_name=RUN_this__gQThHuC5RTeeRn__"$rndString".sh
+# echo that is $tmp_script_file_name
 		# Works around problems escaping characters by writing the whole command
 		# to a script (which still needs " ( and ) escaped though?!) :
-		echo "python $pathToScript --WIDTH 200 --HEIGHT 100 --RSHIFT $rshiftParam -b [$rgbValForParam] -c [$rgbValForParam] --RECLAIM_ORPHANS True --BORDER_BLEND True --TILEABLE False --STOP_AT_PERCENT 1 -a 0 --RANDOM_SEED $rndSeedValForParam -q 1 --GROWTH_CLIP \($lowGrowthClipParam,$highGrowthClipParam\) --SAVE_PRESET True $extraArgs" > RUN_this__gQThHuC5RTeeRn.sh
-		./RUN_this__gQThHuC5RTeeRn.sh
+		echo "python $pathToScript --WIDTH 200 --HEIGHT 100 --RSHIFT $rshiftParam -b [$rgbValForParam] -c [$rgbValForParam] --RECLAIM_ORPHANS True --BORDER_BLEND True --TILEABLE False --STOP_AT_PERCENT 1 -a 0 --RANDOM_SEED $rndSeedValForParam -q 1 --GROWTH_CLIP \($lowGrowthClipParam,$highGrowthClipParam\) --SAVE_PRESET True $extraArgs" > $tmp_script_file_name
+# ALT COMMAND with custom hard-coded coord but still using color parameter:
+# echo "python $pathToScript --WIDTH 200 --HEIGHT 100 --RSHIFT $rshiftParam -b [$rgbValForParam] --CUSTOM_COORDS_AND_COLORS '[[(1799,1799),[$rgbValForParam]]]' --RECLAIM_ORPHANS True --BORDER_BLEND True --TILEABLE False --STOP_AT_PERCENT 1 -a 0 --RANDOM_SEED $rndSeedValForParam -q 1 --GROWTH_CLIP \($lowGrowthClipParam,$highGrowthClipParam\) --SAVE_PRESET True $extraArgs" > RUN_this__gQThHuC5RTeeRn.sh
+		./$tmp_script_file_name
+		echo ""
+		echo "pausing 4 seconds before deleting temp shell script . . ."
+		sleep 4
+		rm ./$tmp_script_file_name
 		# When I got this to work and saw initial output of so many colors, I exclaimed:
 		# OH MY HONKEY-TONK STARS OF WRATH!
 		#
@@ -88,5 +101,3 @@ do
 		echo "SKIPPING render . . ."
 	fi
 done < $convertedPaletteFile
-
-# echo $convertedPaletteFile
