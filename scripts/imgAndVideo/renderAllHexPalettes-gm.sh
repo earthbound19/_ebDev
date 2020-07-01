b5b9dc934b393b8a76e090c8eaf2d38d10395fa5
earthbound19 <earthbound19@users.noreply.github.com>
2020-07-01 15:18:53 -0600

    now passes parameters through to renderHexPalette-gm.sh

--- scripts/imgAndVideo/renderAllHexPalettes-gm.sh
+++ scripts/imgAndVideo/renderAllHexPalettes-gm.sh
@@ -4,11 +4,15 @@
 # USAGE
 # To render all palettes in the current directory, invoke the script without any argument:
 #  ./renderAllHexPalettes-gm.sh
-# To recurs into all subdirectories and render all palettes in them, pass any parameter for $1:
+# To recurse into all subdirectories and render all palettes in them, pass any parameter for $1:
 #  ./renderAllHexPalettes-gm.sh YORP
+# To pass additional parameters, examine the positional parameters in renderHexPalette-gm.sh and position them the same here, but don't use $1 for that script here, because $1 is provided by this script as $element in a loop repeatedly calling renderHexPalette-gm.sh.
+# To NOT recurse into subdirectories but also use additional parameters, pass the keyword NULL for $1, e.g.:
+#  ./renderAllHexPalettes-gm.sh NULL 250 NULL 5
+
 
 # CODE
-if [ "$1" ]
+if [ "$1" ] && [ "$1" != "NULL" ]
 then
 	# no -maxdepth 1 switch; recurse through subdirectories
 	hexpltFilesArray=(`gfind . -type f -iname \*.hexplt`)
@@ -21,7 +25,7 @@ for element in ${hexpltFilesArray[@]}
 do
 	echo ~~~~
 	echo invoking renderHexPalette-gm.sh for $element . . .
-	renderHexPalette-gm.sh $element
+	renderHexPalette-gm.sh $element $2 $3 $4 $5 $6
 done
 
 echo "DONE. Color palettes have been rendered from all *.hexplt files in the current path for which there was not already a corresponding .png image. Palette images are named after the source *.hexplt files. If you passed any parameter to this script, this has been done recursively through all subfolders also."
