298742805bad4a7ca45dcbf6e17719fd7f58ad75
earthbound19 <earthbound19@users.noreply.github.com>
2020-07-04 10:48:09 -0600

    allTo.*: aktully do al files. toOldest.*: tweaks

--- scripts/fileNamesAndTimestamps/allToOldestWindowsDateTime.sh
+++ scripts/fileNamesAndTimestamps/allToOldestWindowsDateTime.sh
@@ -2,7 +2,7 @@
 # Invokes toOldestWindowsDateTime.sh for all image formats (hard coded) in the current directory.
 
 # CODE
-list=(`gfind . -maxdepth 1 \( -iname \*.jpg -o -iname \*.png -o -iname \*.cr2 -o -iname \*.tif -o -iname \*.mov \) -printf '%f\n' | sort`)
+list=$(ls *.*)
 
 for element in ${list[@]}
 do
