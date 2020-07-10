6918d52c297b130eacb5dcc75933170f94dcc48b
Alex Hall <earthbound19@users.noreply.github.com>
2020-07-10 09:45:00 -0600

    no need for--see details
    just do this in any script, to get a random integer in between e.g. 1 and 10, as I already do:
    rndNum=`shuf -i 1-10 -n 1`

--- scripts/nRandomNumbers.sh
+++
@@ -1,9 +0,0 @@
-# DESCRIPTION: Generate $1 random numbers in range $2-$3 (min range and max range, inclusive)
-
-howMany=$1
-
-shuf -i $2-$3 -n $1
-
-# DEVELOPMENT HISTORY
-# Not sure what I wanted this script for; but I just found it was here and totally not working as intended? Fixed. 2016-06-19 06:35:35 AM -RAH
-# And modded again because it didn't work in a range (fixed number of digits). Now it works with a range. 2020-04-23
