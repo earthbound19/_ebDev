accacdef94950fbdb3a8daeb271f9f9035f6239f
earthbound19 <earthbound19@users.noreply.github.com>
2020-07-09 19:47:45 -0600

    comment/documentation, loop fixup, param check

--- scripts/imgAndVideo/stripSoundAllVideos.sh
+++ scripts/imgAndVideo/stripSoundAllVideos.sh
@@ -1,18 +1,23 @@
-# Invoke this with one paramater, being the extension of videos you wish to strip of sound.
+# DESCRIPTION
+# Losslessly removes sound from all videos of type $1 (parameter 1), renaming the previous version of the file to <filename>_backup.ext, and putting the video with sound stripped in place of the original file name.
 
+# USAGE
+# Invoke with one paramater, being the extension of videos you wish to strip of sound, e.g.:
+#  stripSoundAllVideos.sh
+
+
+# CODE
 fileExt=$1
-allFilesTXTfile="all_""$fileExt"
 
-find ./*.$1 > $allFilesTXTfile
+if ! [ "$1" ]; then printf "\nNo parameter 1 (video file type). Exit."; exit; fi
 
-while read filename
+allVideosArray=(`find . -maxdepth 1 -type f -iname \*.$1 -printf '%f\n'`)
+
+for filename in ${allVideosArray[@]}
 do
-	fileBasename=`basename $filename ".""$fileExt"`
 	ffmpeg -y -i "$filename" -map 0:v -vcodec copy "filename"_temp.mp4
 	mv "$filename" "$filename""_backup"."$fileExt"
 	mv "filename"_temp.mp4 "$filename"
-done < $allFilesTXTfile
+done
 
-rm $allFilesTXTfile
-
-echo DONE. Check all the converted videos for errors. If there were any conversion errors, you can restore from the originals which were renamed \<filename\>_backup.$fileExt. To save space you may want to delete originals after verifying the sound-stripped video stream copy files are okay.
+printf "\nDONE. Check all the converted videos for errors. If there were any conversion errors, you can restore from the originals which were renamed \<filename\>_backup.$fileExt. To save space you may want to delete originals after verifying the sound-stripped video stream copy files are okay."
