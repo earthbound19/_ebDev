499bd3a2db2217960845fca0f5ea15933315bdef
earthbound19 <earthbound19@users.noreply.github.com>
2020-07-11 00:32:35 -0600

    oops correct log blanking command

--- scripts/imgAndVideo/allPalettesCompareCIECAM02.sh
+++ scripts/imgAndVideo/allPalettesCompareCIECAM02.sh
@@ -9,8 +9,8 @@
 hexpltFileNames=(`find . -maxdepth 1 -type f -iname \*.hexplt -printf '%f\n'`)
 comparePyScriptFullPath=`whereis paletteCompareCIECAM02.py | sed 's/.* \(.*\/paletteCompareCIECAM02.py\).*/\1/g'`
 
-# blank/create new palettesListByMostSimilar.txt:
-printf "" > palettesListByMostSimilar.txt
+# blank/create new palette comparisons ranking file:
+printf "" > paletteDifferenceRankings.txt
 # List all possible pairs of file type .hexplt, order is not important, repetition is not allowed (math algorithm N pick 2).
 i_count=0
 for i in ${hexpltFileNames[@]}
