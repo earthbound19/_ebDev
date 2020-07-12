0fcac5d4bcc6afd896c836aeab24a5a89684b48e
Alex Hall <earthbound19@users.noreply.github.com>
2020-07-12 13:31:59 -0600

    see details
    - update palette render script call per that scripts' updated number of columns logic
    - delete vestigal comment
    - technical clarification in description
    - comment out script that's only in dev. now

--- scripts/recipes/tapColorMindDotIO.sh
+++ scripts/recipes/tapColorMindDotIO.sh
@@ -1,11 +1,10 @@
 # DESCRIPTION
-# Via other scripts, obtains N perceptually unique (vs. one another) color palettes, and deletes every palette that was found against another palette above a threshold.
+# Via other scripts, obtains N perceptually unique (vs. one another) color palettes. The last script this calls lists palettes that are perceptually similar (technically and logically not very different below a threshold), for proposed deleting.
 
 # USAGE
 # Hack the global values right after the CODE comment per your want. Then invoke the script:
 #  fetch_and_render_Ncolormind_palettes.sh
 # NOTES
-# - For palette comparison that uses an advanced color model, see paletteCompareCIECAM02.py.
 # - To remove palettes for which you manually delete a rendered PNG (because you don't want the palette), see listUnmatchedExtensions.sh or pruneByUnmatchedExtension.sh.
 
 
@@ -18,12 +17,9 @@ echo "Will obtain, compare and possibly delete similar palettes from $numberOfPa
 for i in `seq 1 $numberOfPalettesToGet`; do get_colormind_RND_palette.sh; sleep 1; done
 
 allRGBhexColorSortIn2CIECAM02.sh
-
-renderAllHexPalettes-gm.sh NULL 250 NULL 5
-
+renderAllHexPalettes-gm.sh NULL 250 NULL
 allPalettesCompareCIECAM02.sh
-
 listPaletteDifferencesBelowThreshold.sh $deletePalettesBelowDifferenceThreshold
-
-deletePalettesDifferentBelowThreshold.sh
+# PENDING DEVELOPMENT:
+# deletePalettesDifferentBelowThreshold.sh
 
