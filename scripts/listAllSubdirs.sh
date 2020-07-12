398e4678a2a153b9e29ccc9511e012d0ef2845d3
Alex Hall <earthbound19@users.noreply.github.com>
2020-07-12 13:30:04 -0600

    add usage from other scripts comment

--- scripts/listAllSubdirs.sh
+++ scripts/listAllSubdirs.sh
@@ -3,10 +3,12 @@
 
 # USAGE
 #  listAllSubdirs.sh
+# To call this from another script and create an array of the output, do:
+# allSubdirectoriesArray=(`listAllSubdirs.sh`)
 
 
 # CODE
-# If there's a printf command that chops off the leading ./ but still prints all subdirs to, I don't know it yet:
+# If there's a printf command that chops off the leading ./ but still prints all subdirs also, I don't know it yet:
 directories_list=`find . -type d | sed 's/\.\///g' | tr -d '\15\32' | sort -n`
 # :1 cuts off the first element, '.' :
 for element in "${directories_list[@]:1}"
