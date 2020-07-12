7fadc87e1c0066f45991fd938d4da2e763e93f31
earthbound19 <earthbound19@users.noreply.github.com>
2020-07-12 13:47:25 -0600

    Change hard-coded arbitrary first sort color to almost black magenta

--- scripts/imgAndVideo/allRGBhexColorSortInCAM16-UCS.sh
+++ scripts/imgAndVideo/allRGBhexColorSortInCAM16-UCS.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# runs RGBhexColorSortInCAM16-UCS.py with original file overwrite parameter against all .hexplt files in the current directory (no recursion into subdirectories), comparing them by sorting nearest to black first (makes a temporary copy of every hexplt file with black as the first color, runs the comparison, then removes the added color).
+# runs RGBhexColorSortInCAM16-UCS.py with original file overwrite parameter against all .hexplt files in the current directory (no recursion into subdirectories), comparing them by sorting nearest to black-black-black-magenta first (makes a temporary copy of every hexplt file with black as the first color, runs the comparison, then removes the added color).
 
 # USAGE
 # With more than one .hexplt file in your current directory, and RGBhexColorSortInCAM16-UCS.py either in your PATH or a copy of it in the current directory, and with this script also in your PATH or the current directory, invoke this script:
@@ -9,6 +9,8 @@
 
 
 # CODE
+# TO DO
+# Parameterize arbitrary sort color; default to black-black-black-magenta if not provided.
 scriptLocation=`whereis RGBhexColorSortInCAM16-UCS.py | sed 's/.* \(.*\/RGBhexColorSortInCAM16-UCS.py\).*/\1/g'`
 
 array=(`find . -maxdepth 1 -type f -iname \*.hexplt -printf '%f\n'`)
@@ -16,5 +18,5 @@ array=(`find . -maxdepth 1 -type f -iname \*.hexplt -printf '%f\n'`)
 for element in ${array[@]}
 do
 	echo "Running comparisons for file $element . . ."
-	python $scriptLocation $element foo '#000000'
+	python $scriptLocation $element foo '#0a000a'
 done
