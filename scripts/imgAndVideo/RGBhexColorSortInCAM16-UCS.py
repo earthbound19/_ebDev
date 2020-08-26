5e6ee1d4bea102f510caafbf4cd985275dcfea2b
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-26 16:52:29 -0600

    improved parameter checking, comment updated per doc. pref.

--- scripts/imgAndVideo/RGBhexColorSortInCAM16-UCS.py
+++ scripts/imgAndVideo/RGBhexColorSortInCAM16-UCS.py
@@ -1,85 +1,71 @@
 # DESCRIPTION
-# variant of RGBhexColorSortInCIECAM02.py which uses the color-science library
-# and CAM16 color model for color sorting (supposedly superior to CIECAM02).
-# In my tests, RGBhexColorSortInCIECAM02.py (which uses the colorspacious library) does
-# better for hue matching independent of brightness; this (which uses colour-science) does
-# better for matching first brightness/saturation, then hue. Both give good results. To my eye colorspacious
-# is better overall (maybe I reckon hue before brightness/saturation).
-# Also, it may be a matter of here figuring the best illuminant and transform matrix paramters.
-# You can also try different chromatic_adaptation_transform values from here:
-# https://colour.readthedocs.io/en/develop/generated/colour.RGB_to_XYZ.html
-# Also, I may not know what I'm doing here -- the illuminants and transform matrix have me scratching my head.
-# A test array for this: #E33200 #FF8B94 #54F1F1 #499989 #AAEFCB #8E4C5C #F7B754 #8AC2B0 #DE9D38 #FA9394 #ADDCCA #2DD1AA #E5E4E9 #02547D #78BF82 #745D5F #414141 #958F95 #FDE182 #7699C7 #FF5933 #D9BB93 #F1E5E9 #EA5287 #E3DB9A #95B6BA #59746E #333388 #008D94 #2EC1B1 #FF534E #367793 #00A693
-
-# NOTES: this script expects perfect data. If there's a blank line anywhere in the
-# input file, or any other unexpected data, it may stop with an unhelpful error.
-# ALSO, this script will eliminate duplicates in the input list.
-
-# USAGE
-# With this script and a .hexplt file both in your immediate path, and
-# Python in your PATH, call this script with one parameter, being a hex palette list:
-# Python hex2CIECAM02colorSort2hex.py PrismacolorMarkers.hexplt
-#
-# It will print the sort result to stdout, so that you may for example redirect the output to a new .hexplt file, e.g.:
-# Python RGBhexColorSortInCAM16-UCS.py inputColors.hexplt
-#
-# OR, if you pass an optional 2nd argument (which may be anything), it will overwrite the source file with the sorted colors.
+# Variant of RGBhexColorSortInCIECAM02.py which uses the color-science library and CAM16 color model for color sorting (supposedly superior to CIECAM02). In my tests, RGBhexColorSortInCIECAM02.py (which uses the colorspacious library) does better for hue matching independent of brightness; this (which uses colour-science) does better for matching first brightness/saturation, then hue. Both give good results. To my eye colorspacious is better overall (maybe I reckon hue before brightness/saturation).
 
 # DEPENDENCIES
-# Python 3, various python packages (see import list at start of code) which you may install with
-# easy_install or pip; NOTE that the colour import is actually from the colour-science package;
-# you must install colour-science.
+# Python 3, various python packages (see import list at start of code) which you may install with easy_install or pip; NOTE that the colour import is actually from the colour-science package; you must install colour-science.
 
-# KNOWN ISSUES
-# The same input sorted in a different order always produces the same output, but you must
-# decide what the first color in the list is--because if you change the first color, the
-# sorting changes.
-
-# LICENSE
-# This is my original code and I release it to the Public Domain. -RAH 2019-09-23 09:18 PM
-
-# REFERENCE and dev notes
-# - Also, CAM16 has superceded CIECAM02: https://arxiv.org/abs/1802.06067
-# CAM16 defines via tristimulus values XYZ: lightness J, brightness Q, chroma C, colorfulness M, saturation s, and hue h. re https://observablehq.com/@jrus/cam16
-# - maybe yet better, as it's referenced in a paper -- https://arxiv.org/pdf/1802.06067.pdf --
-# describing the very same implementation as a solution to found problems in CIECAM02;
-# it is implemented in https://github.com/nschloe/colorio
-# DEV NOTE: 
-# "The results showed that J'a'b' gave the second best (CAM16-UCS was the best) performance
-# for small color difference data sets and the best for experimental data corresponding to
-# large color differences." -- https://www.osapublishing.org/DirectPDFAccess/83324CA3-D13F-9C21-A2C857331BED04A3_368272/oe-25-13-15131.pdf?da=1&id=368272&seq=0&mobile=no (from https://www.osapublishing.org/oe/abstract.cfm?uri=oe-25-13-15131) and https://www.osapublishing.org/DirectPDFAccess/72B3A417-CBFB-5C71-084E496F156FBA4E_368272/oe-25-13-15131.pdf?da=1&id=368272&seq=0&mobile=no
-# in J'a'b, "J refers to lightness, a refers to redness-to-greenness, and b refers to blueness-to-yellowness." re https://github.com/connorgr/d3-cam02 (that is a javascript library I think)
-# also re https://colour.readthedocs.io/en/develop/generated/colour.XYZ_to_JzAzBz.html#colour.XYZ_to_JzAzBz
-# "Returns:	JzAzBz colourspace array where Jz is Lightness, Az is redness-greenness and Bz is yellowness-blueness."
-# - to get all convert functions starting with a string:
-# print([name for name in colour.__all__ if name.startswith('XYZ_to')])
-# - to get conversion path from one color space / model to another
-# (but I got errors trying) :
-# colour.describe_conversion_path('sRGB', 'CAM16UCS', width=75)
-# re https://colour.readthedocs.io/en/develop/generated/colour.describe_conversion_path.html#colour.describe_conversion_path
-# JzAzBz is based on CAM16? re: http://files.cie.co.at/x046_2019/x046-OP71.pdf: "A new whiteness formula, named white J’a’b’ (WJ’a’b’) was derived using the datasets introduced in the last section..where J’, a’, b’ are the colour coordinates of the test stimulus in CAM16-UCS within the colour boundary."
-# - yet another library: https://www.tandfonline.com/doi/full/10.1080/15502724.2018.1518717
+# USAGE
+# Run this script through a Python interpreter, with these positional parameters:
+# - sys.argv[1] A list of RGB colors as hexadecimal numbers, in .hexplt format (a simple text list, one color per line).
+# - sys.argv[2] OPTIONAL. May be anything, and if present, the script will overwrite the original hex palette list with the sorted colors. To use positional parameter 3 (the next one) but not overwrite the original hex palette list, pass this as the string 'NULL' (with or without quote marks).
+# - sys.argv[3] OPTIONAL. Any hex color code string in the format #fa24cf (RGB color expressed in hex format), which the script will use as the first color to compare other colors to, even if it's not in the list. The list will therefore be sorted by next nearest color starting with this arbitrary color. If the color happens to be in the original list file (first argument), it will remain in the final list. If the color is not in the original list, it will not appear in the final sorted list.
+# Example run with only a hexplt file given:
+#    python /path/to_this_script/RGBhexColorSortInCAM16-UCS.py PrismacolorMarkers.hexplt
+# The result will be printed to stdout, so that you may for example redirect the output to a new .hexplt file, e.g.:
+#    python /path/to_this_script/RGBhexColorSortInCAM16-UCS.py inputColors.hexplt > result.hexplt
+# Example run with the optional second parameter, which will cause the script to overwrite the original .hexplt file with the sorted version of it:
+#    python /path/to_this_script/RGBhexColorSortInCAM16-UCS.py inputColors.hexplt foo
+# Example run with the optional third parameter, an arbitrary color to start comparisons with (here, magenta):
+#    python /path/to_this_script/RGBhexColorSortInCAM16-UCS.py inputColors.hexplt foo #ff00ff
+# NOTES
+# - It seems the library this uses for CAM16-UCS is slower than the library used in the other mentioned script (for CIECAM02).
+# - This expects perfect data. If there's a blank line anywhere in the
+# input file, or any other unexpected data, it may stop with an unhelpful error.
+# - This will also delete duplicate colors in the list.
+# - The same input always produces the same output, though if you change the order of colors in the list, the results may change. This is because the script uses the first color in the list to begin its comparisons (but you may override that with parameter 3). You therefore may wish to decide which color you want first in the list for those comparison purposes, or pass an arbitrary color via parameter 3.
+# - Good comparisons with this script may be a matter of here figuring the best illuminant and transform matrix parameters, and hacking that. You can try different chromatic_adaptation_transform values from here:
+# https://colour.readthedocs.io/en/develop/generated/colour.RGB_to_XYZ.html
+# Also, I may not know what I'm doing here -- the illuminants and transform matrix have me scratching my head.
 
 
 # CODE
+# See the "REFERENCE and dev notes" section at the end of this script.
+# A test array: #E33200 #FF8B94 #54F1F1 #499989 #AAEFCB #8E4C5C #F7B754 #8AC2B0 #DE9D38 #FA9394 #ADDCCA #2DD1AA #E5E4E9 #02547D #78BF82 #745D5F #414141 #958F95 #FDE182 #7699C7 #FF5933 #D9BB93 #F1E5E9 #EA5287 #E3DB9A #95B6BA #59746E #333388 #008D94 #2EC1B1 #FF534E #367793 #00A693
+
 import sys
-import colour        # this must be the colour-science package, NOT the colour package.
+import colour        # The colour-science package must be installed and imported under this name, NOT the colour package. It's confusing, because colour-science is imported under the name colour. But I think there is another package named just colour which imports under the same name.
 from itertools import combinations
 from more_itertools import unique_everseen
 
+# GLOBALS and positional parameters check, with clear boolean/value inits to describe:
+overwriteOriginalList = False
+arbitraryFirstCompareColor = False
+countOfArbitraryFirstColorInOriginalList = 0
+if len(sys.argv) > 1:
+    hexpltFileNamePassedToScript = sys.argv[1]
+else:
+    print('\nNo parameter 1 (source .hexplt file) passed to script. Exit.')
+    sys.exit(1)
+if len(sys.argv) > 2:
+    if sys.argv[2] != 'NULL':   # Only set the following to true if user did not pass the string NULL (allows using positional parameter 2 without "using" it, and also using positional parameter 3:
+        overwriteOriginalList = True
+if len(sys.argv) > 3:
+    arbitraryFirstCompareColor = sys.argv[3]
+# END GLOBALS and positional parameters check
 
 # split input file into list on newlines:
-inputFile = sys.argv[1]
-f = open(inputFile, "r")
+f = open(hexpltFileNamePassedToScript, "r")
 colors_list = list(f.read().splitlines())
 f.close()
+
+if arbitraryFirstCompareColor != False:     # if a parameter was passed to script (arbitrary color in format '#ffffff' to start comparisons with), dynamically alter the list to insert that color at the start. BUT FIRST, track (with the following variable, countOfArbitraryFirstColorInOriginalList) the count of that color in the list before we add it to the list, because if that count is zero before adding, we will want to remove it from the list after comparisons and sorting (we don't want to permanently add it to the list; we only want to add it temporarily). (Because we don't want to add colors permanently to the list that were never there; we want to leave the list as it was found.) But if it _was_ originally in the list, we want to keep it there after adding, because the list will be deduplicated. Because the list will be deduplicated, the color we add to the list (even if it was in the list already before we added it) it would end up appearing only once in the list, and removing it after that would remove it entirely. That is a lot of explanation for the following two lines of code :)
+    countOfArbitraryFirstColorInOriginalList = colors_list.count(arbitraryFirstCompareColor)
+    colors_list.insert(0, arbitraryFirstCompareColor)
 # strip beginning '#' character off every string in that list:
 colors_list = [element[1:] for element in colors_list]
 # deduplicate the list, but maintain same order:
 colors_list = list(unique_everseen(colors_list))
 
-
-# GLOBALS:
 pair_deltaEs = []
 # going with example parameters from:
 # https://colour.readthedocs.io/en/develop/generated/colour.RGB_to_XYZ.html
@@ -104,9 +90,10 @@ RGB_to_XYZ_matrix = [
 # get a list of lists of all possible color two-combinations, with an deltaE (distance
 # measurement between the colors) as a value in the lists; doing this
 # with manual nested for loops because itertools returns tuples (and other reasons that
-# may not be valid) :
-if len(sys.argv) > 1:    # if a second parameter was passed to script, do these things:
-    print('Input file is ', inputFile)
+# may not be valid) ;
+# Conditional print: only print this information if we're overwriting original palette file (because if we aren't overwriting it, the user may be piping output to a new palette file that they wouldn't want cluttered with other information that breaks the file format:
+if overwriteOriginalList == True:
+    print('Input file is ', hexpltFileNamePassedToScript)
     print('Getting deltaE for all color combinations . . .')
 for i in range(len(colors_list)):
     for j in range(i + 1, len(colors_list)):
@@ -139,12 +126,14 @@ for i in range(len(colors_list)):
         pair_deltaEs.append([distance, colors_list[i], colors_list[j]])
 
 # results in lowest deltaE values first; may cause searches to run faster (I don't know):
-if len(sys.argv) > 1:
+# Again, conditional print:
+if overwriteOriginalList == True:
     print('Sorting deltaEs . . .')
 pair_deltaEs.sort()
 sorted_colors = list()
 
-if len(sys.argv) > 1:
+# Again, conditional print:
+if overwriteOriginalList == True:
     print('Sorting colors by nearest deltaE . . .')
 search_color = colors_list[0]
 sorted_colors.append(search_color)  # starts list
@@ -177,17 +166,46 @@ while len(sorted_colors) < len(colors_list):
 
 
 # FINALE
-# If a second argument (which may be anything) was passed to the script, 
-# overwrite the source read file with the sorted copy of it.
-# Otherwise, print the sorted list to stdout.
-if len(sys.argv) > 2:
+# add '#' back to the start of every element in the sorted color list before print:
+for IDX, color in enumerate(sorted_colors):
+    fixedTehColorFormat = '#' + color
+    sorted_colors[IDX] = fixedTehColorFormat
+
+# If we inserted an arbitrary color at start of list (to make first comprarison from), but it wasn't in the original list, remove it from the new sorted colors list; otherwise leave it there:
+if countOfArbitraryFirstColorInOriginalList == 0 and arbitraryFirstCompareColor != False:
+    sorted_colors.remove(arbitraryFirstCompareColor)
+
+# Again, conditional print if we're overwriting original list, and if we are overwriting original list, this is where in code we do that:
+if overwriteOriginalList == True:
     print('Writing sorted color list back over original file . . .')
-    with open(inputFile, 'w') as f:
+    with open(hexpltFileNamePassedToScript, 'w') as f:
         for element in sorted_colors:
-            print_element = '#' + element + '\n'
-            f.write(print_element)
+            f.write(element + "\n")
     f.close()
 else:
     for element in sorted_colors:
-        print_element = '#' + element
-        print(print_element)
+        print(element)
+
+
+
+# REFERENCE and dev notes
+# - Also, CAM16 has superceded CIECAM02: https://arxiv.org/abs/1802.06067
+# CAM16 defines via tristimulus values XYZ: lightness J, brightness Q, chroma C, colorfulness M, saturation s, and hue h. re https://observablehq.com/@jrus/cam16
+# - maybe yet better, as it's referenced in a paper -- https://arxiv.org/pdf/1802.06067.pdf --
+# describing the very same implementation as a solution to found problems in CIECAM02;
+# it is implemented in https://github.com/nschloe/colorio
+# DEV NOTE: 
+# "The results showed that J'a'b' gave the second best (CAM16-UCS was the best) performance
+# for small color difference data sets and the best for experimental data corresponding to
+# large color differences." -- https://www.osapublishing.org/DirectPDFAccess/83324CA3-D13F-9C21-A2C857331BED04A3_368272/oe-25-13-15131.pdf?da=1&id=368272&seq=0&mobile=no (from https://www.osapublishing.org/oe/abstract.cfm?uri=oe-25-13-15131) and https://www.osapublishing.org/DirectPDFAccess/72B3A417-CBFB-5C71-084E496F156FBA4E_368272/oe-25-13-15131.pdf?da=1&id=368272&seq=0&mobile=no
+# in J'a'b, "J refers to lightness, a refers to redness-to-greenness, and b refers to blueness-to-yellowness." re https://github.com/connorgr/d3-cam02 (that is a javascript library I think)
+# also re https://colour.readthedocs.io/en/develop/generated/colour.XYZ_to_JzAzBz.html#colour.XYZ_to_JzAzBz
+# "Returns:	JzAzBz colourspace array where Jz is Lightness, Az is redness-greenness and Bz is yellowness-blueness."
+# - to get all convert functions starting with a string:
+# print([name for name in colour.__all__ if name.startswith('XYZ_to')])
+# - to get conversion path from one color space / model to another
+# (but I got errors trying) :
+# colour.describe_conversion_path('sRGB', 'CAM16UCS', width=75)
+# re https://colour.readthedocs.io/en/develop/generated/colour.describe_conversion_path.html#colour.describe_conversion_path
+# JzAzBz is based on CAM16? re: http://files.cie.co.at/x046_2019/x046-OP71.pdf: "A new whiteness formula, named white J’a’b’ (WJ’a’b’) was derived using the datasets introduced in the last section..where J’, a’, b’ are the colour coordinates of the test stimulus in CAM16-UCS within the colour boundary."
+# - yet another library: https://www.tandfonline.com/doi/full/10.1080/15502724.2018.1518717
