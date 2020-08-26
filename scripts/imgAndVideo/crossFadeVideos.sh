cdf9212b3917a700436e92217d1cd503310007a1
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-26 16:46:16 -0600

    fix: no dest file create if no audio. new logic handles no audio case

--- scripts/imgAndVideo/crossFadeVideos.sh
+++ scripts/imgAndVideo/crossFadeVideos.sh
@@ -1,24 +1,40 @@
+# DESCRIPTION
+# ffmpeg wrapper that crossfades two videos with custom fade parameters. Result file name is `<input_video_one_base_file_name>_xFade_<input_video_two_base_file_name>.mp4`.
+
 # USAGE
-# Pass this script three parameters, being:
-# $1 the first video
-# $2 the second video
-# $3 the duration of the crossfade between them
-# $4 what time (in seconds, allowing for decimals) the crossfade will start for both videos (in and out).
-# TO DO:
-# Make $3 and $4 optional and default them to 3 and 1, respectively.
+# Run with these parameters:
+# - $1 File name of the first video
+# - $2 File name of the second video
+# - $3 OPTIONAL. The duration of the crossfade to make between them (in seconds, which may include decimals). If not provided, a default is used.
+# - $4 OPTIONAL. Time in the video that the crossfade will begin (in seconds, which may include decimals). If not provided, a default is used.
+# Example that will crossfade input1.mp4 to input2.mp4, with a crossfade of 2.3 seconds, which begins at 7.6 seconds into the video:
+#    crossFadeVideos.sh input1.mp4 input2.mp4 2.3 7.6
 
-# NOTE; Both videos are assumed to be the same length in my dev tests. I haven't found out what happens if they aren't:
-inputVideoOne=$1
-inputVideoTwo=$2
-crossFadeLength=$3			# Set this to half the length of both videos
-crossFadeStart=$4
 
-# MAKE AUDIO crossfade.
-ffmpeg -y -i $inputVideoOne -r 11500 -i $inputVideoTwo -filter_complex \
-"[0:a]afade=t=out:st=$crossFadeStart:d=$crossFadeLength[a0];\
-[1:a]afade=t=in:st=$crossFadeStart:d=$crossFadeLength[a1];\
-[a0][a1]amix[over]" \
--map [over] _aud.aac
+# CODE
+# PARAMETERS CHECK and globals set:
+if [ ! "$1" ]; then printf "\nNo parameter \$1 (source video one file name) passed to script. Exit."; exit 1; else inputVideoOne=$1; fi
+if [ ! "$2" ]; then printf "\nNo parameter \$2 (source video two file name) passed to script. Exit."; exit 1; else inputVideoTwo=$2; fi
+if [ "$3" ]; then crossFadeLength=$3; else crossFadeLength=4.25; fi
+if [ "$4" ]; then crossFadeStart=$4; else crossFadeStart=0.75; fi
+
+# Construct render target file name variable:
+inputVideoOneFileNameNoExt=${inputVideoOne%.*}
+inputVideoTwoFileNameNoExt=${inputVideoTwo%.*}
+renderTargetFileName="$inputVideoOneFileNameNoExt"_xFade_"$inputVideoTwoFileNameNoExt".mp4
+
+# IF VIDEO HAS AUDIO, make audio crossfade. The following command will print nothing if there is no audio:
+#audioLog=$(ffprobe -i $inputVideoOne -show_streams -select_streams a -loglevel error)
+if [ "$audioLog" != "" ]
+then
+	ffmpeg -y -i $inputVideoOne -r 11500 -i $inputVideoTwo -filter_complex \
+	"[0:a]afade=t=out:st=$crossFadeStart:d=$crossFadeLength[a0];\
+	[1:a]afade=t=in:st=$crossFadeStart:d=$crossFadeLength[a1];\
+	[a0][a1]amix[over]" \
+	-map [over] _crossFadeVideos_sh_tmp_PgfJnBGc9.aac
+else
+	echo "No audio; will not construct audio crossfade."
+fi
 
 # MAKE VIDEO crossfade.
 # Adapted from: http://superuser.com/a/778967/130772
@@ -26,11 +42,13 @@ ffmpeg -y -i $inputVideoOne -i $inputVideoTwo -filter_complex \
 "[0:v]fade=t=out:st=0:d=$crossFadeLength:alpha=1,setpts=PTS-STARTPTS[va0];\
 [1:v]fade=t=in:st=0:d=$crossFadeLength:alpha=1,setpts=PTS-STARTPTS[va1];\
 [va0][va1]overlay[over]" \
--map [over] _vid.mp4
+-map [over] _crossFadeVideos_sh_tmp_PgfJnBGc9.mp4
 
-# Mux the two; sound is shorter at this writing :(  :
-ffmpeg -y -i _aud.aac -i _vid.mp4 -crf 17 "$inputVideoOne"_xFade_"$inputVideoTwo".mp4
-rm _aud.aac _vid.mp4
-
-# Cygwin option: auto-launch the completed cross-faded video:
-# cygstart ./_crossfadeOut.mp4
+# IF SOURCE VIDEO has audio, mux video and audio crossfade (and render to final render target file name); otherwise don't (and just rename the crossfade video to the final render target file name), because we made no audio crossfade. Alas, sound is shorter [I noted at one point; does that mean sound goes out of sync?] at this writing:
+if [ "$audioLog" != "" ]
+then
+	echo ffmpeg -y -i _crossFadeVideos_sh_tmp_PgfJnBGc9.aac -i _crossFadeVideos_sh_tmp_PgfJnBGc9.mp4 -crf 17 $renderTargetFileName
+	rm _crossFadeVideos_sh_tmp_PgfJnBGc9.aac _crossFadeVideos_sh_tmp_PgfJnBGc9.mp4
+else
+	mv _crossFadeVideos_sh_tmp_PgfJnBGc9.mp4 $renderTargetFileName
+fi
