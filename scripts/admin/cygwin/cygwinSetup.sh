3c6035211c6a8e15249bdc09b4e59e415f69196c
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-26 16:48:45 -0600

    comment to state pref. for MSYS2

--- scripts/admin/cygwin/cygwinSetup.sh
+++ scripts/admin/cygwin/cygwinSetup.sh
@@ -2,17 +2,13 @@
 # Commands to install wanted tools and custom configuration for a Cygwin install.
 
 # USAGE
-# Run this script from the directory with the installer, with this script
-# and the other files it needs (CYGWIN_fstab, my.minttyrc.settings.txt) in
-# your PATH, or copy the cygwin installer to this directory before executing
-# this script. Comment out the line for the cygwin installer version you don't
-# want to use.
-# To run it, open the cygwin prompt, cd to this path, then run:
-# ./cygwinSetup.sh
+# Maybe don't use this. I have come to far prefer MSYS2 over Cygwin. See MSYS2setup.sh. But if you use this, run it from the directory with the installer, with this script and the other files it needs (CYGWIN_fstab, my.minttyrc.settings.txt) in your PATH, or copy the Cygwin installer to this directory before executing this script. Comment out the line for the Cygwin installer version you don't want to use.
+# Then open the Cygwin prompt, cd to this path, and run the script without any parameter:
+#    cygwinSetup.sh
 
 
 # CODE
-installParams="-q -P perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -P bc -P lynx -P chere"
+installParams="-q -P Perl -P gcc-g++ -P make -P diffutils -P libmpfr-devel -P libgmp-devel -P libmpc-devel -P bc -P lynx -P chere"
 
 # ./Cygwin_setup-x86.exe $installParams
 ./Cygwin_setup-x86_64.exe $installParams
@@ -24,10 +20,9 @@ lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
 dos2unix apt-cyg
 install apt-cyg /bin
 
-# temporarily switch apt-cyg mirror to source that has XXD; install that and
-# switch it back:
+# temporarily switch apt-cyg mirror to source that has XXD; install that and switch it back:
 apt_cyg_mirror=`apt-cyg mirror`
-apt-cyg mirror "ftp://mirrors.syringanetworks.net/cygwin/"
+apt-cyg mirror "ftp://mirrors.syringanetworks.net/Cygwin/"
 apt-cyg install xxd
 apt-cyg mirror "$apt_cyg_mirror"
 
@@ -48,9 +43,7 @@ fi
 # RE: https://superuser.com/a/392598/130772 :
 cp ./CYGWIN_fstab /etc/fstab
 
-# DESTROY . . . okay, just rename . . . the utils I use a lot but adopt
-#  in _ebSuperBin from MSYS2, but which here cause problems; this first
-#  rename is funny because not finding it is exactly what will happen:
+# DESTROY . . . okay, just rename . . . the utils I use a lot but adopt in _ebSuperBin from MSYS2, but which here cause problems; this first rename is funny because not finding it is exactly what will happen:
 mv /usr/bin/find.exe /usr/bin/no-find.exe
 mv /usr/bin/sed.exe /usr/bin/no-sed.exe
 mv /usr/bin/sort.exe /usr/bin/no-sort.exe
