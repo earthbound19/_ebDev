b30afc6cdac6c3df72495e95278bd7649e984a7e
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-26 14:49:18 -0600

    clearer description of file history

--- scripts/imgAndVideo/color_growth_fast.py
+++ scripts/imgAndVideo/color_growth_fast.py
@@ -1,1 +1,5 @@
-print('What was this script (a forked version of color_growth.py) is now that again :) see color_growth.py. See color_growth_v1.py's first print statement for history.')
+# DESCRIPTION
+# The content of this file was pasted over color_growth.py; SEE THAT. This file is where an improved version of color_growth.py was forked to (by scribblemaniac), but that improvement was pasted over the original. This file is only here because web links may point to this, and I don't want anyone to hit a dead end looking for it.
+
+# CODE
+print("The content of this file was pasted over color_growth.py; SEE THAT. This file is where an improved version of color_growth.py was forked to, but that improvement was pasted over the original. This file is only here because web links may point to this, and I don't want anyone to hit a dead end looking for it.")
