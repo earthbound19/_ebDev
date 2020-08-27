1f1a3b14d4b49cc86e21af81678d3d35ed222cf2
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-26 21:10:19 -0600

    - all_imgType2dng.sh: upd. to call: - new script: img2dng.sh

--- scripts/imgAndVideo/all_imgType2dng.sh
+++ scripts/imgAndVideo/all_imgType2dng.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# Converts all files of a given type in your current path to Adobe digital negatives (.dng). But don't use DNG. At best you have to keep the original raw image around anyway (in case the DNG toolchain becomes outdated), and DNG has not seen wide adoption.
+# Calls img2dng for every file of type $1 in the current directory.
 
 # USAGE
 # Call this script with one parameter $1, being a file type in the directory from which you run this script--a file type for which all thing whom what this yes you wish to convert to Adobe digital negatives format (.dng), e.g.:
@@ -7,22 +7,15 @@
 
 
 # CODE
-find . -iname \*.$1 > all_wut.txt
-while read element
+imgs=($(find . -maxdepth 1 -iname \*.$1))
+for fileName in ${imgs[@]}
 do
-# TO DO: check if target file exists (get base file name of element first), then only create it if it doesn't.
-	elementBaseName="${element%.*}"
-	if [ ! -f "$elementBaseName".dng ]
+	elementBaseName="${fileName%.*}"
+	if [ -f "$elementBaseName".dng ]
 	then
-		echo "$elementBaseName".dng does not exist\; will execute dng conversion command.
-		# NOTE if you add the -e switch it will embed the original raw (/CR2 etc) file in the dng.
-		command="AdobeDNGConverter.exe -c -fl -cr5.4 $element"
-		echo running command\: $command
-		echo . . .
-		$command
-	else
 		echo Would-be target file "$elementBaseName".dng already exists\; will not re-create.
+	else
+		echo "$elementBaseName".dng does not exist\; will execute dng conversion command.
+		img2dng.sh $elementBaseName
 	fi
-done < all_wut.txt
-
-rm ./all_wut.txt
+done
