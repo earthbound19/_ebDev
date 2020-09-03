4361abeaf459cc02041939e00e534bebd3bec0c1
Alex Hall <earthbound19@users.noreply.github.com>
2020-09-03 00:10:16 -0600

    correct gtail -> tail, create render target file name variable for script that calls this via `source` to use, document that

--- scripts/fountain2fountain.sh
+++ scripts/fountain2fountain.sh
@@ -5,6 +5,16 @@
 # USAGE
 # Run with one parameter, which is the input fountain file:
 #    fountain2fountain.sh input-file.fountain
+# NOTES
+# - This script sets a variable named targetFountainFileName, which is a string variable for the target file name. If you call this script from another script, via `source`, like this:
+#        source fountain2fountain.sh input-file.fountain
+# -- then the set variable will be available to the calling script after this script terminates, as `source` does magicky things to cause that. You can then use that variable in the calling script to do other things with that file name.
+# - If you have text connecting punctuation like a double-dash (for emdash) :
+#        one phrase--another phrase
+# --and you break the connecting punctuation over lines of ventilated prose, like this:
+#        one phrase--
+#        another phrase
+# --the script will add a space in the punctuation when it joins lines, which is probably not what you want. To avoid this, don't break connecting punctuation over lines: keep it on the same line.
 
 
 # CODE
@@ -20,7 +30,7 @@ fileNameNoExt=${1%.*}
 tail_from=`awk '/> FADE IN:/{print NR;exit}' $1`
 let head_to=tail_from-1
 head -n $head_to $1 > tmp_head_wYSNpHgq.fountain
-gtail -n +$tail_from $1 > tmp_tail_wYSNpHgq.fountain
+tail -n +$tail_from $1 > tmp_tail_wYSNpHgq.fountain
 dos2unix tmp_head_wYSNpHgq.fountain tmp_tail_wYSNpHgq.fountain
 #  - join semantic linefeeds into that tail file, in-place:
 # Adapted from: https://backreference.org/2009/12/23/how-to-match-newlines-in-sed/
@@ -29,5 +39,6 @@ dos2unix tmp_head_wYSNpHgq.fountain tmp_tail_wYSNpHgq.fountain
 #   - Also don't match [ .@~] characters at start of line (don't join if those fountain syntax
 # marks are present:
 sed -i ':begin;$!N;s/\(^[^ .\(~@\n].*[a-z].*\)\n\(^[^ .\(~@\n].*[a-z].*\)/\1 \2/;tbegin;P;D' tmp_tail_wYSNpHgq.fountain
-cat tmp_head_wYSNpHgq.fountain tmp_tail_wYSNpHgq.fountain > "$fileNameNoExt"_unventilated.fountain 
+targetFountainFileName="$fileNameNoExt"_unventilated.fountain
+cat tmp_head_wYSNpHgq.fountain tmp_tail_wYSNpHgq.fountain > $targetFountainFileName
 rm ./tmp_head_wYSNpHgq.fountain ./tmp_tail_wYSNpHgq.fountain
