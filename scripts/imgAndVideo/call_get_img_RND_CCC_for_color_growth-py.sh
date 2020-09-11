b6cf8fbbc3ffb2780c30f029c213ac7bd72ea159
Alex Hall <earthbound19@users.noreply.github.com>
2020-09-10 20:48:01 -0600

    better command substitution. comments on full paths to hard code option in my setup

--- scripts/imgAndVideo/call_get_img_RND_CCC_for_color_growth-py.sh
+++ scripts/imgAndVideo/call_get_img_RND_CCC_for_color_growth-py.sh
@@ -24,7 +24,7 @@ else
 	inputFile=$1
 	printf "\nInput file is $inputFile."
 fi
-inputFileNoExt=`echo "${inputFile%.*}"`
+inputFileNoExt=$(echo "${inputFile%.*}")
 if ! [ "$2" ]
 then
 	printf "\n\nNo parameter \$2 (number of rnd coordinates and colors to get) passed to script. Exit."
@@ -42,17 +42,20 @@ else
 	printf "\n\nParameter \$3 (extra parameters for color_growth.py) passed to script. Received and will be used as: $additionalParams"
 fi
 
+# At this writing, the full path to the script in my setup is the following, and for speed sake I can just uncomment the next line (and comment the line after it) :
+# pathToCCCgetterScript='/c/_ebDev/scripts/imgAndVideo/get_img_RND_CCC_for_color_growth.py'
+pathToCCCgetterScript=$(getFullPathToFile.sh get_img_RND_CCC_for_color_growth.py)
 
-pathToCCCgetterScript=`whereis get_img_RND_CCC_for_color_growth.py | sed 's/get_img_RND_CCC_for_color_growth: \(.*\)/\1/g'`
+# ~in my setup I can uncomment the following and comment the line after it:
+# pathToColorGrowth_py='/c/_ebDev/scripts/imgAndVideo/color_growth.py'
+pathToColorGrowth_py=$(getFullPathToFile.sh color_growth.py)
 
-var_CUSTOM_COORDS_AND_COLORS=`python $pathToCCCgetterScript $inputFile $numRNDcoordsToGet`
-
-pathToColorGrowth_py=`whereis color_growth.py | sed 's/color_growth: \(.*\)/\1/g'`
+var_CUSTOM_COORDS_AND_COLORS=$(python $pathToCCCgetterScript $inputFile $numRNDcoordsToGet)
 
 # some parsing error or summat leads to "out of bounds" error if we just run command directly; so write it to temp script, then execute temp script and del temp script.
 # ALSO (and before that), because the shell can throw an error if I pass a ridiculous number of characters via the CLI (ha!), but python throws no errors loading so many characters from a file, do this: create an RND temp .cgp preset file name, load that via color_growth.py's --LOAD_PRESET switch in the temp script; then delete the temp preset after (saving the new preset with '--SAVE_PRESET True'):
 echo "Writing switches for color_growth.py to temporary .cgp file (it will re-save them with other information via '--SAVE_PRESET True') . . ."
-rndStringForTempPreset=`cat /dev/urandom | tr -dc 'a-f0-9' | head -c 6`
+rndStringForTempPreset=$(cat /dev/urandom | tr -dc 'a-f0-9' | head -c 6)
 presetFileBaseName=color_growth_py__RND_CCC_"$numRNDcoordsToGet"_from_"$inputFileNoExt"__"$rndStringForTempPreset"
 presetFileName="$presetFileBaseName".cgp
 echo "$additionalParams --SAVE_PRESET True $var_CUSTOM_COORDS_AND_COLORS" > $presetFileName
