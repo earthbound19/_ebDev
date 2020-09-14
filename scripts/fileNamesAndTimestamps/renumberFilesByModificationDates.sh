a79aa2b5b8a113a6d41843979e512b7fd1964b1e
Alex Hall <earthbound19@users.noreply.github.com>
2020-09-14 10:57:13 -0600

    - add parameter check and error out if not provided - better command substitution - true array - improve doc

--- scripts/fileNamesAndTimestamps/renumberFilesByModificationDates.sh
+++ scripts/fileNamesAndTimestamps/renumberFilesByModificationDates.sh
@@ -1,28 +1,31 @@
 # DESCRIPTION
-# Renames files to numbers by reverse sort of file modification date (one use case is this script helping make anims of modifications to files over time).
+# Renames all files of type $1 to numbers by reverse sort of file modification date (one use case is this script helping make anims of modifications to files over time).
 
 # USAGE
-# Run without any parameter:
-#    renumberFilesByModificationDates.sh
+# Run with this parameter:
+# - $1 the file extension (type) to renumber, without the . in the extension.
+# Example:
+#    renumberFilesByModificationDates.sh png
 
 
 # CODE
-echo Hi persnonzez!!!!!!!!!!!!!!! HI!! -Nem
+if [ ! "$1" ]; then printf "\nNo parameter \$1 (file extension (type) to renumber) passed to script. Exit."; exit 1; else fileTypeToRenumber=$1; fi
 
-filesCount=`find . -maxdepth 1 -iname \*.$1 | sort | wc -l | tr -d ' '`
-digitsToPadTo=${#filesCount}
+echo "Hi persnonzez!!!!!!!!!!!!!!! HI!! -Nem"
 
 # Create array to use to loop over files. Sort by modified date stamp re genius breath yon: https://superuser.com/a/294164
-# filesArray=`find . -maxdepth 1 -iname "*.$1" | sort -zk 1n | sed -z 's/^[^ ]* //' | tr '\0' '\n'`
+# filesArray=`find . -maxdepth 1 -iname "*.$fileTypeToRenumber" | sort -zk 1n | sed -z 's/^[^ ]* //' | tr '\0' '\n'`
 # NOPE, that dunna work, but this does:
-filesArray=`ls --sort=time --reverse *$1 | tr '\0' '\n'`
+filesArray=($(ls --sort=time --reverse *."$fileTypeToRenumber" | tr '\0' '\n'))
+# Get digits to pad to from length of array.
+digitsToPadTo=${#filesArray[@]}; digitsToPadTo=${#digitsToPadTo}
 
 counter=0
 for filename in ${filesArray[@]}
 do
 	counter=$((counter + 1))
-	countString=`printf "%0""$digitsToPadTo""d\n" $counter`
+	countString=$(printf "%0""$digitsToPadTo""d\n" $counter)
 			# echo old file name is\: $filename
-			# echo new file name is\: $countString.$1
-	mv $filename $countString.$1
+			# echo new file name is\: $countString.$fileTypeToRenumber
+	mv $filename $countString.$fileTypeToRenumber
 done
