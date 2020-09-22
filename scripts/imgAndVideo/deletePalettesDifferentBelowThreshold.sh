03a8d6bea6acab722d788d960f37d531435f2f8a
Alex Hall <earthbound19@users.noreply.github.com>
2020-09-21 20:23:42 -0600

    optional print count of pngs/hexplts via everythingCLI

--- scripts/imgAndVideo/deletePalettesDifferentBelowThreshold.sh
+++ scripts/imgAndVideo/deletePalettesDifferentBelowThreshold.sh
@@ -1,5 +1,11 @@
 # DESCRIPTION
 # Last of a series of scripts designed to eliminate similar .hexplt format palettes. Interactively launches pairs of rendered palettes corresponding to .hexplt files found to be similar (not so different below a rating threshold), as discovered by listPaletteDifferencesBelowThreshold.sh (which must be run first). Logs how many comparisons have been examined and allows resume from Nth comparison (see USAGE).
+# For assistance with managing copies of palettes (in favorites collections), this script optionally uses everythingCLI (if it makes a successful attempt to run an executable of that name in a way that will produce no error), for statistics on how many copies of given images are found on your (Windows) computer. See USAGE for details.
+
+# DEPENDENCIES
+# - run of a previous script and anything it depends on (see USAGE)
+# - A default image editor associated with the image files in the list which this script depends on (paletteDifferencesBelowThreshold.txt)
+# - Optionally, everythingCLI (see DESCRIPTION)
 
 # USAGE
 # Before this script, run listPaletteDifferencesBelowThreshold.sh (which tells you to run another script before it), to generate the list paletteDifferencesBelowThreshold.txt. Then run with or without this parameter, and follow the prompts:
@@ -11,20 +17,30 @@
 # The script will skip all iterations up to 320 and resume from there.
 # To examine all iterations, run this script without any parameter:
 #    deletePalettesDifferentBelowThreshold.sh 320
-
+# NOTES
+# - This script detects whether everythingCLI is installed and executes normally, and if so, it enables you to pick favorite palettes and copy them into palette collections, by printing information about how many copies of the palette (ASSUMED: in .hexplt format) are found on your computer. If this script prints information on the count of that palette file found on the computer, you'll know you don't need to copy the palette to another location (a collection), as it has already been copied (there are 2 or more of it).
+# - If this script gets an error on attempt to run everythingCLI, it will print a count for a palette with a question mark, which indicates that the number of them on the computer is unknown.
 
 # CODE
 if [ "$1" ]; then echo "Parameter \$1 passed to script (comparison number to resume from). Will use that"; resumeFromNumber=$1; else resumeFromNumber=0; fi
 
+everythingCLIworking='False'
+everythingCLI ANtUeH52yu9rtxWaBEBAqm5UsJ34Sghjz9vzRujyVN
+if [ $(echo $?) == "0" ]
+then
+	everythingCLIworking='True'
+fi
+echo value of everythingCLIworking is $everythingCLIworking
+
 printf "\nThis script launches pairs of palette image renders associated with palettes ranked below a dissimilarity threhshold, in a comparison log file, paletteDifferencesBelowThreshold.txt. It uses the 'start' command repeatedly to launch images. The intent is that an image editor with a hotkey to delete an image will make it easier to delete one of a pair of palettes that you like less. Press any key to continue . . ."
 read -rsn1
 
-arrayOfFilesToDelete=$(<paletteDifferencesBelowThreshold.txt)
+arrayOfFilesToScan=$(<paletteDifferencesBelowThreshold.txt)
 
 # variables for feedback print:
 linesCount=$(cat paletteDifferencesBelowThreshold.txt | wc -l)
 loopCount=0
-for element in ${arrayOfFilesToDelete[@]}
+for element in ${arrayOfFilesToScan[@]}
 do
 	loopCount=$((loopCount + 1))
 	if [ "$loopCount" -ge "$resumeFromNumber" ]
@@ -35,6 +51,8 @@ do
 		B_pair=$(echo $element | sed 's/\([^|]*\)|\([^|]*\)|\([^|]*\)/\3/g')
 		PNGmatchFor_A=${A_pair%.*}.png
 		PNGmatchFor_B=${B_pair%.*}.png
+		HEXPLTmatchFor_A=${A_pair%.*}.hexplt
+		HEXPLTmatchFor_B=${B_pair%.*}.hexplt
 		printf "\nPair $PNGmatchFor_A | $PNGmatchFor_B . . ."
 		# Skip delete prompt if either of pair not found:
 		if [ ! -e $PNGmatchFor_A ] || [ ! -e $PNGmatchFor_B ]
@@ -43,8 +61,20 @@ do
 		# Otherwise prompt to launch image pair and delete one:
 		else
 			# write to log (so user can look in log to know where to resume if process interrupted) :
+			if [ "$everythingCLIworking" == 'True' ]
+			then
+				count_pngs_A=$(everythingCLI $PNGmatchFor_A | wc -l)
+				count_pngs_B=$(everythingCLI $PNGmatchFor_B | wc -l)
+				count_hexplts_A=$(everythingCLI $HEXPLTmatchFor_A | wc -l)
+				count_hexplts_B=$(everythingCLI $HEXPLTmatchFor_B | wc -l)
+			else
+				count_pngs_A='n?'
+				count_pngs_B='n?'
+				count_hexplts_A='n?'
+				count_hexplts_B='n?'
+			fi
 			printf "Iteration $loopCount of $linesCount, pair $PNGmatchFor_A | $PNGmatchFor_B . . ." > deletePalettesDifferentBelowThresholdLog.txt
-			printf '\n~\nAttempting "start" command to launch corresponding palette images for A and B in similarity pair. Delete the one you like less, then press any key to continue . . .'
+			printf "\n~\nAttempting \"start\" command to launch corresponding palette images for A and B in similarity pair:\n~\n$PNGmatchFor_A ($count_pngs_A) : $HEXPLTmatchFor_A ($count_hexplts_A)\n$PNGmatchFor_B ($count_pngs_B) : $HEXPLTmatchFor_B ($count_hexplts_B)\n~\nDelete the one you like less, or alternately maybe copy one or both to a palette/similar palettes collection, then press any key to continue . . .\n"
 			start $PNGmatchFor_A
 			start $PNGmatchFor_B
 			# Waits for keypess (no need to press key and then <enter>; it is immediate) :
