14619f2c49338cbfc52a04df7b3b338cfac4731c
Alex Hall <earthbound19@users.noreply.github.com>
2020-10-06 16:50:00 -0600

    exclude any path with '.git' in it

--- scripts/imgAndVideo/palettesMarkdownGalleries.sh
+++ scripts/imgAndVideo/palettesMarkdownGalleries.sh
@@ -5,6 +5,6 @@
 # CODE
 # VASTLY simplified from previous incarnations of this script, and works recursively besides
 # (those didn't) :
-find . -type d -exec sh -c 'cd "{}" && palettesMarkdownGallery.sh' \;
+find . -type d -not -path "./.git*" -exec sh -c 'cd "{}" && current_dir=$(pwd) && echo working in path $current_dir . . . && palettesMarkdownGallery.sh' \;
 
 echo "DONE running palettesMarkdownGallery.sh in every directory in this path."
