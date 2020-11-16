da8afdd8e5fc9184368971473fe636be37a57e21
Alex Hall <earthbound19@users.noreply.github.com>
2020-11-15 21:50:41 -0700

    Updated to use better array creation and command substitution. Re-tested on windows. Corrected some documentation errors.

--- scripts/font/FontForgeFonts2Fonts.sh
+++ scripts/font/FontForgeFonts2Fonts.sh
@@ -2,14 +2,15 @@
 # Converts all FontForge-compatible files in the current directory of type $1 to type $2.
 
 # DEPENDENCIES
-# FontForge installed and in your PATH.
+# - FontForge installed and in your PATH
+# - for Windows, cygpath (which comes with both Cygwin and MSYS2)
 
 # USAGE
 # Run with these parameters:
-# - $1 Source type to convert from.
-# - $2 Target type to convert from.
+# - $1 Source type to convert from
+# - $2 Target type to convert from
 # Example that converts all .sfd files to .otf:
-#    FontForgeFont2Font.sh sfd otf
+#    FontForgeFonts2Fonts.sh sfd otf
 
 
 # CODE
@@ -26,49 +27,45 @@ if [ ! "$2" ]
 	exit
 else
 	destFormat=$2
-	echo source format passed to script\: $2;
+	echo target format passed to script\: $2;
 fi
 
 # get full path to FontForge:
-fullPathToFFscript=`whereis FontForgeConvert.pe | sed 's/.*: \(.*\)/\1/g' | tr -d '\15\32'`
-# conver to windows path format if Windows:
+fullPathToFFscript=$(getFullPathToFile.sh FontForgeConvert.pe)
+# convert to windows path format if Windows:
 if [ $OS == "Windows_NT" ]
 then
-	fullPathToFFscript=`cygpath -w "$fullPathToFFscript" | tr -d '\15\32'`
+	# cygpath is also shipped with MSYS2, so this works with cygwin and MSYS2! :
+	fullPathToFFscript=$(cygpath -w "$fullPathToFFscript" | tr -d '\15\32')
 fi
 
-currDir=`pwd`
-IFS=""
-find . -maxdepth 1 -type f -iname \*.$sourceFormat -printf '%f\n' > all_"$sourceFormat"s.txt
-while IFS= read -r element || [ -n "$element" ]
+currDir=$(pwd)
+source_files=($(find . -maxdepth 1 -type f -iname \*.$sourceFormat -printf '%f\n'))
+for element in ${source_files[@]}
 do
-	# If we're running Windows, assume Cygwin and convert to windows path.
-	# otherwise leave path as-is:
+	# If we're running Windows, build a Windows-style path (backslashes); otherwise leave path as-is:
 	if [ $OS == "Windows_NT" ]
 	then
 		# escaping \:
 		fullPathToSourceFile="$currDir"\\"$element"
-		fullPathToSourceFile=`cygpath -w $fullPathToSourceFile`
+		fullPathToSourceFile=$(cygpath -w $fullPathToSourceFile)
 	else
 		# oy with the need to escape \:
 		fullPathToSourceFile="$currDir"/"$element"
 	fi
+
 	fullPathMinusExt=${fullPathToSourceFile%.*}
 	fullPathToTargetFile="$fullPathMinusExt"."$destFormat"
-	# THIS IS INSANITY: I can't get it to run the command
-	# in-line; but it will if run from a variable with
-	# the same string?! Is something weird with
-	# double-quote marks and/or spaces in paths going on? :
-	# AMENDED: no, I can only get it to work if I create and execute a temp
-	# script! :
+	# THIS IS INSANITY: I can't get it to run the command in-line; but it will if run from a variable with the same string?! Is something weird with double-quote marks and/or spaces in paths going on? I can only get it to work if I create and execute a temp script! :
 	echo "FontForge -script \"$fullPathToFFscript\" \"$fullPathToSourceFile\" .$destFormat" > gs42BeyT_tmpScript.sh
 	chmod +x ./gs42BeyT_tmpScript.sh
 	source ./gs42BeyT_tmpScript.sh
-done < all_"$sourceFormat"s.txt
-rm all_"$sourceFormat"s.txt ./gs42BeyT_tmpScript.sh
+	rm ./gs42BeyT_tmpScript.sh
+done
 
 
 # DEV HISTORY:
+# 2020-11-15 Updated to use better array creation and command substitution. Re-tested on windows. Corrected some documentation errors.
 # 2020-08-29 Revamped to use simpler script. 
 # 2020-04-30 Rewrote as bash script, parameterizing source and dest format.
 # 2020-04-30 Pulled my hair out with problems of spaces in file name and arrays, made "array" with text file and iterated over lines of it instead.
