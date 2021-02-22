84ffe3a7576e16cd5f577895997ae0ec8450b074
Alex Hall <earthbound19@users.noreply.github.com>
2021-02-21 19:13:22 -0700

    add resize hack note

--- scripts/imgAndVideo/ffmpeg2imgNN.sh
+++ scripts/imgAndVideo/ffmpeg2imgNN.sh
@@ -14,4 +14,6 @@
 # example ffmpeg command; resizes to x800 px maintaining aspect ratio:
 # ffmpeg -y -i in.png -vf scale=1280:-1:flags=neighbor out.png
 imgFileNoExt=${1%.*}
+# NOTE: for an arbitrary resolution hack the next line like:
+# ffmpeg -y -i $1 -vf scale=1280:720:flags=neighbor $imgFileNoExt.$2
 ffmpeg -y -i $1 -vf scale=$3:-1:flags=neighbor $imgFileNoExt.$2
