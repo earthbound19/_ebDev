d37c5e4494fac3778026728c32d947bcb04c2d20
Alex Hall <earthbound19@users.noreply.github.com>
2021-03-26 01:26:10 -0600

    - split color removal option into options to remove colors at start of tints and end of shades - so much debugging my head hurts. all working I think.

--- scripts/recipes/getN_hexplt_shadesXchromas_Oklab.sh
+++ scripts/recipes/getN_hexplt_shadesXchromas_Oklab.sh
@@ -9,13 +9,16 @@
 # - $1 source file name of palette in .hexplt format (a list of RGB color hex codes)
 # - $2 how many tints and shades to obtain for each color
 # - $3 how many chromacities to get (toward gray) for every one of those resultant tints and shades
-# - $4 anything, such as the word FLOOFARF, which will cause the palette renders stage of the script to be skipped.
+# - $4 OPTIONAL. How many tints to remove from the start of the tints gradient (which begins nearest white).
+# - $5 OPTIONAL. How many shades to remove from the end of the shades gradient (which ends nearest black).
+# - $6 OPTIONAL. Any other arbitrary switches (with their optional values) that you want to pass to `get_color_gradient_culori.js`. If there are spaces in these switches, surround all the switches and their values with single or double quote marks. To not use any arbitrary (additional) switches, but use $7, pass the word NULL for this parameter.
+# - $7 OPTIONAL. Anything, such as the word FLOOFARF, which will cause the palette renders stage of the script to be skipped.
 # Example that will get 3 tints, 3 shades, and 4 chromacities for every tint and shade, for every color in 16_max_chroma_med_light_hues.hexplt:
 #    getN_hexplt_shadesXchromas_Oklab.sh 16_max_chroma_med_light_hues.hexplt 3 4
-# NOTE
-# The result count of tints and shades is ($2 * 2) + 1, because it is $2 tints + $2 shades + the original color
-# The result count of chromas is that many + (chromas * those tints and shades), because those tints and shades are included unmodified (you get the original tints and shades plus chroma variants).
-
+# NOTES
+# - The result count of tints and shades is ($2 * 2) + 1, because it is $2 tints + $2 shades + the original color
+# - The result count of chromas is that many + (chromas * those tints and shades), because those tints and shades are included unmodified (you get the original tints and shades plus chroma variants).
+# - See the optional `extraParams` variable to pass additional arguments in calls to `get_color_gradient_culori.js`
 
 # CODE
 # START MAIN SETUP AND CHECKS
@@ -24,8 +27,19 @@ fullPathToCuloriScript=$(getFullPathToFile.sh get_color_gradient_culori.js)
 if [ "$fullPathToCuloriScript" == "" ]; then printf "\n~\nERROR: dependency script get_color_gradient_culori.js not found in your \$PATH. Will exit."; exit 1; fi
 
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (source .hexplt file name) passed to script. Exit."; exit 1; else sourceHexpltFile=$1; fi; if [ ! -e $sourceHexpltFile ]; then printf "\n~\nERROR: file $sourceHexpltFile not found. Will exit."; exit 1; fi
-if [ ! "$2" ]; then printf "\nNo parameter \$2 (how many tints and shades to get for each color in palette) passed to script. Exit."; exit 1; else nShades=$2; nShades2moar=$(($nShades + 2)); fi		# +2 because this script removes the start and end of gradients (-f and -l switches to get_color_gradient_culori.js)
-if [ ! "$3" ]; then printf "\nNo parameter \$3 (how many chroma to get per tint and shade) passed to script. Exit."; exit 1; else nChroma=$3; nChroma2moar=$(($nChroma + 2));fi
+if [ ! "$2" ]; then printf "\nNo parameter \$2 (how many tints and shades to get for each color in palette) passed to script. Exit."; exit 1; else nShades=$2; fi
+if [ ! "$3" ]; then printf "\nNo parameter \$3 (how many chroma to get per tint and shade) passed to script. Exit."; exit 1; else nChroma=$3; fi
+nColorsRemoved=0
+if [ "$4" ]; then tintsRemoveSwitch="-f $4"; nColorsRemoved=$(($nColorsRemoved + $4)); fi
+if [ "$5" ]; then shadesRemoveSwitch="-l $5"; nColorsRemoved=$(($nColorsRemoved + $5)); fi
+if [ "$6" ] && [ "$6" != "NULL" ]; then extraParameters=$7; fi
+		# DEV / DEBUG PRINTS; in production, comment out everthing here at this indent:
+		# echo sourceHexpltFile $sourceHexpltFile
+		# echo nShades $nShades
+		# echo nChroma $nChroma
+		# echo tintsRemoveSwitch $tintsRemoveSwitch
+		# echo shadesRemoveSwitch $shadesRemoveSwitch
+		# echo nColorsRemoved $nColorsRemoved
 # END MAIN SETUP AND CHECKS
 
 # create array of RGB hex color codes from source palette:
@@ -42,12 +56,12 @@ do
 	colorNoHashSign=$(echo ${color:1})
 	destFileName="$paddedCounterString"__x"$colorNoHashSign"_"$nShades"tints_"$nShades"shades_times"$nChroma"chromas.hexplt
 	printf "" > $destFileName
-	# write tints to palette files, whiter first:
-	node $fullPathToCuloriScript -s $colorNoHashSign -e ffffff -n $nShades2moar -r -f -l >> $destFileName
-	# write original color to palette file next:
-	echo $color >> $destFileName
-	# write lower luminance toward black to palette files, luminance nearer original color first:
-	node $fullPathToCuloriScript -s $colorNoHashSign -e $colorNoHashSign -n $nShades2moar -b 0 -f -l >> $destFileName
+	# write tints to palette files, whiter first; also doing nShades + 1 else we get fewer than wanted; we want tints not including orig. color:
+	node $fullPathToCuloriScript -s ffffff -e $colorNoHashSign -n $(($nShades +1)) $tintsRemoveSwitch $extraParameters >> $destFileName
+	# write lower luminance toward black to palette files, luminance nearer original color first;
+	# removing first color here, even though user switch doesn't say so, else the original color will be duplicated;
+	# same addition applies here:
+	node $fullPathToCuloriScript -s $colorNoHashSign -e $colorNoHashSign -n $(($nShades + 1)) -b 0 -f 1 $shadesRemoveSwitch $extraParameters >> $destFileName
 	# META! :
 	# Read that result file into an array:
 	chromaSourceColorsArray=$(<$destFileName)
@@ -56,14 +70,14 @@ do
 	for chromaSource in ${chromaSourceColorsArray[@]}
 	do
 		chromaSourceNoHashSign=$(echo ${chromaSource:1})
-		node $fullPathToCuloriScript -s $chromaSourceNoHashSign -e $chromaSourceNoHashSign -n $nChroma2moar -c 0 -l >> $destFileName
+		node $fullPathToCuloriScript -s $chromaSourceNoHashSign -e $chromaSourceNoHashSign -n $nChroma -c 0 $extraParameters >> $destFileName
 	done
 done
 
-if [ ! "$4" ]
+if [ ! "$7" ]
 then
-	renderColumns=$(($nChroma + 1))
-	renderRows=$((($nShades * 2) + 1))
+	renderColumns=$nChroma
+	renderRows=$(( ($nShades * 2) + 1 - $nColorsRemoved))
 	renderAllHexPalettes.sh YORP 260 NULL $renderColumns $renderRows
 fi
 
