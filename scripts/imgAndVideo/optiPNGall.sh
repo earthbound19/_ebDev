8643a7a6e1922ebe772b6bd1e9287905d8bdfd86
Alex Hall <earthbound19@users.noreply.github.com>
2021-06-02 22:42:12 -0600

    uh WHAT? pngquant is not color / original image quality faithful. comment out and upd. doc.

--- scripts/imgAndVideo/optiPNGall.sh
+++ scripts/imgAndVideo/optiPNGall.sh
@@ -1,15 +1,17 @@
 # DESCRIPTION
-# Recursively optimizes all pngs in the current directory and subdirectories, via pngquant and optpng, preserving full fidelity of original pixels.
+# Recursively optimizes all pngs in the current directory and subdirectories, via optpng, preserving full fidelity of original pixels. See NOTE for pngquant option.
 
 # USAGE
 #    optiPNGall.sh
+# NOTE To additionally use pngquant but lose color fidelity in the process, uncomment the line in the for loop which begins with pngquant.
 
 
 # CODE
 pngsFileNamesArray=( $(find . -type f -name "*.png") )
+# NOTE
 
 for element in ${pngsFileNamesArray[@]}
 do
-	pngquant --skip-if-larger --ext=.png --force --quality 100 --speed 1 --nofs --strip --verbose $element
+	# pngquant --skip-if-larger --ext=.png --force --quality 100 --speed 1 --nofs --strip --verbose $element
 	optipng -o7 $element
 done
