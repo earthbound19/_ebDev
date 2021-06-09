d13ff5a2e2da38b4369ebdd01972feadbf37740c
Alex Hall <earthbound19@users.noreply.github.com>
2021-06-09 12:40:41 -0600

    - add $4 password bypass warning prompt option - bug fix of erred print

--- scripts/fileNamesAndTimestamps/rmnn.sh
+++ scripts/fileNamesAndTimestamps/rmnn.sh
@@ -5,7 +5,8 @@
 # Run with these parameters:
 # - $1 file type to delete count after N of.
 # - $2 start count of files to delete.
-# - $3 OPTIONAL. End count of files to delete. If omitted, all files of type $1 after count $2 are deleted (and so is file at count $2).
+# - $3 OPTIONAL. End count of files to delete. If omitted, all files of type $1 after count $2 are deleted (and so is file at count $2). If you wish to use $4 but not this parameter, pass this parameter as the word 'NULL'.
+# - $4 OPTIONAL. The word BLUBARG, which if you pass it, will bypass warning prompt and delete the files without asking. Useful for using this script from other scripts.
 # EXAMPLES
 # To delete all files of type png which are listed by the `find` command from the 51st to the last found png:
 #    rmn.sh png 51
@@ -21,21 +22,24 @@ array=($(find . -maxdepth 1 -type f -iname \*.$fileType))
 array_length=${#array[@]}
 # conditional exit without print but with error code if practical:
 if (( array_length == "0" )); then exit 1; fi
-if [ ! "$3" ]; then endCount=$array_length; else endCount=$3; fi
+if [ ! "$3" ] || [ "$3" == 'NULL' ]; then endCount=$array_length; else endCount=$3; fi
 
-echo ""
-echo "WARNING: This script will delete all files of type $fileType found at"
-echo "list count $startCount and ending at list count $endCount."
-echo "If this is not what you want to do, press ENTER or RETURN, or"
-echo "CTRL+C or CTRL+Z. If this _is_ what you want to do, type"
-echo "BLUBARG and then press ENTER or RETURN."
-read -p "TYPE HERE: " SILLYWORD
-
-if [ ! "$SILLYWORD" == "BLUBARG" ]
+if [ ! "$4" ] || [[ "$4" != "BLUBARG" ]];
 then
 	echo ""
-	echo Typing mismatch\; exit.
-	exit 2
+	echo "WARNING: This script will delete all files of type $fileType found at"
+	echo "list count $startCount and ending at list count $endCount."
+	echo "If this is not what you want to do, press ENTER or RETURN, or"
+	echo "CTRL+C or CTRL+Z. If this _is_ what you want to do, type"
+	echo "BLUBARG and then press ENTER or RETURN."
+	read -p "TYPE HERE: " SILLYWORD
+
+	if [ ! "$SILLYWORD" == "BLUBARG" ]
+	then
+		echo ""
+		echo Typing mismatch\; exit.
+		exit 2
+	fi
 fi
 
 # adjust those counts inline by -1 for zero-based indexing:
@@ -44,6 +48,6 @@ seqEnd=$(($endCount - 1))
 for idx in $(seq $seqStart $seqEnd)
 do
 	fileNameToDelete="${array[$idx]}"
-	echo "DELETING file idx $seq, file name $fileNameToDelete . . ."
+	echo "DELETING file idx $idx, file name $fileNameToDelete . . ."
 	rm $fileNameToDelete
 done
