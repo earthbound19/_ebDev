dc2eef75551939a31dccdf2b189e0fba4712b4bb
Alex Hall <earthbound19@users.noreply.github.com>
2021-12-04 00:45:27 -0700

    Rewrites to fix bug (no aktul print) and make overwrite of original optional

--- scripts/getUniqueWords.sh
+++ scripts/getUniqueWords.sh
@@ -1,11 +1,33 @@
 # DESCRIPTION
-# Creates a list of unique words in file $1 (parameter), without duplicates. It will create an identically named file but adding _unique_words.txt to the end of the file name.
+# Prints a list of unique words in file $1 (parameter), without duplicates, and maintaining order. (the `uniq` utility only removes consequtive duplicates, but this removes all duplicates). Optionally writes the filtered result over the original file instead of printing it to the screen. To instead sort and deduplicate, use sortuniq.sh.
 
 # USAGE
-# Pass this script the name of a text file which you want to filter unique words from, e.g.:
+# Run with these parameters:
+# - $1 the name of a text file (in your current directory) which you want to filter unique words from
+# - $2 OPTIONAL Anything, such as the word FLOURBALP, which will cause the script to overwrite the original file with the filtered result.
+# For example, to print unique words in their original order of appearance from the file gibberwords.txt, run:
 #    getUniqueWords.sh gibberwords.txt
+# To write the result to a file instead of printing it to the screen, pipe it like this:
+#    getUniqueWords.sh gibberwords.txt > gibberwords_deduplicated.txt
+# To overwrite the original file, pass anything as parameter $2, for example:
+#    getUniqueWords.sh gibberwords.txt FLOURBALPER
 
 
 # CODE
-# Re http://stackoverflow.com/a/16489444
-grep -o -E '\w+' $1 | sort -u -f > "$1"_unique_words.txt
+
+# re http://stackoverflow.com/a/16489444
+# DEPRECATED, becuase it sorts the list (changes order)
+# grep -o -E '\w+' $1 | sort -u -f > "$1"_unique_words.txt
+
+# re https://superuser.com/a/1480765/130772
+lines=($(awk '!visited[$0]++' "$1"))
+if [ ! "$2" ]
+then
+	# Again saved by a genius yonder: https://stackoverflow.com/a/15692004/1397555
+	printf '%s\n' "${lines[@]}"
+else
+	printf '%s\n' "${lines[@]}" > $1
+	echo
+	echo "DONE. Mein Kirchewenig ist nicht mein Punktezahlen."
+	echo "(File $1 was overwritten with a copy of itself, but with duplicate lines removed, and the original order of first appearance of lines maintained.)"
+fi
--- scripts/sortuniq.sh
+++ scripts/sortuniq.sh
@@ -1,18 +1,32 @@
 # DESCRIPTION
-# Takes input file $1, sorts it, reduces it to unique entries, and writes that back to $1.
-
-# WARNING
-# This overwrites the original file without warning. That was your only warning--and you missed it if you didn't read this comment in the source code.
+# Takes input file $1, sorts its lines, reduces it to unique entries, and prints that. Optionally writes that back to the file $1. To maintain order (or in other words to _not_ sort), but eliminate duplicates, see getUniqueWords.sh.
 
 # USAGE
-# Run with one parameter, which is a file name to operate on:
+# Run with these parameters:
+# - $1 a file name to operate on
+# - $2 OPTIONAL. Anything, such as the word FLOURPESCENSE, which will cause the script to overwrite the original file without warning.
+# For example, to print unique lines from fileNameToSortAndDedup.txt, run
 #    sortuniq.sh fileNameToSortAndDedup.txt
-
+# To write the result to a new file, pipe it:
+#    sortuniq.sh fileNameToSortAndDedup.txt > fileName_sorted_and_deduped.txt
+# To filter fileNameToSortAndDedup.txt to unique lines and overwrite the original with that result, run:
+#    sortuniq.sh fileNameToSortAndDedup.txt FLOURPESCENSE
 
 # CODE
-arr=$(sort -n -u $1)
-echo "${arr[@]}" > $1
+lines=($(<$1))
+# Saved by a genius yonder: https://stackoverflow.com/a/11789688/1397555
+IFS=$'\n'
+lines=($(sort <<<"${lines[*]}"))
+lines=($(uniq <<<"${lines[*]}"))
+unset IFS
 
-echo ""
-echo "DONE. Mein Krummspugerlt ist nicht mein Krummspugelnd."
-
+if [ ! "$2" ]
+then
+	# Again saved by a genius yonder: https://stackoverflow.com/a/15692004/1397555
+	printf '%s\n' "${lines[@]}"
+else
+	printf '%s\n' "${lines[@]}" > $1
+	echo
+	echo "DONE. Mein Krummspugerlt ist nicht mein Krummspugelnd."
+	echo "(File $1 was overwritten with a copy of itself, but with duplicate lines removed, and the original order of first appearance of lines maintained.)"
+fi
