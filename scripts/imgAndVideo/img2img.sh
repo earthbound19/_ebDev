0e215329fe83ab5e93fb313cdfd2b790ddf7fef3
Alex Hall <>
2021-12-27 23:05:56 -0700

    add resize option (with good hard-coded color space / filter conversion)

--- scripts/imgAndVideo/img2img.sh
+++ scripts/imgAndVideo/img2img.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# Creates a converted copy of image file name $1 to format $2, via ImageMagick. Will not convert if render target already exists.
+# Creates a converted copy of image file name $1 to format $2, via ImageMagick. Will not convert if render target already exists. Optionally downsizes (with good downsizing method) via $3.
 
 # DEPENDENCY
 # ImageMagick installed in your PATH.
@@ -9,6 +9,7 @@
 # Run this script with these parameters::
 # - $1 the file name of the file to convert
 # - $2 the image extension (format) to convert it to (without any . in the extension).
+# - $3 OPTIONAL. New image X (pixels across) dimension. Smaller strongly recommended. Input image will be converted to linear RGB, downsized with Lanczos method, maintaining aspect, converted back to sRGB, and saved. This preserves lightness much better in my tests. Photoshop default downscaling produces a happy medium between preserved brightness and perceptual surrounding/mixed darkness. Direct sRGB (no linear intermediary) is worst.
 # For example, to create a converted copy of color_growth_title.psd to a .png image, run:
 #    img2img.sh color_growth_title.psd png
 # NOTES
@@ -19,12 +20,14 @@
 # CODE
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (source image type to convert) passed to script. Exit."; exit 1; else sourceIMG=$1; fi
 if [ ! "$2" ]; then printf "\nNo parameter \$2 (format or extension to convert to) passed to script. Exit."; exit 1; else destIMGformat=$2; fi
+additionalConvertOptions=""
+if [ "$3" ]; then additionalConvertOptions="-colorspace RGB -filter Lanczos -resize $3 -colorspace sRGB"; dimSTR=_"$3x"; fi
 
-renderTarget=${sourceIMG%.*}.$destIMGformat
+renderTarget=${sourceIMG%.*}$dimSTR.$destIMGformat
 if [ -e $renderTarget ]
 then
 	printf "\n~\nRender target $renderTarget already exists; skipping render. To recreate it, delete it and run this script again with the same parameters.\n"
 else
 	printf "\n~\nWill create converted copy of $sourceIMG as $renderTarget . . .\n"
-	magick convert $sourceIMG -flatten $renderTarget 2>/dev/null
+	magick convert $sourceIMG -flatten $additionalConvertOptions $renderTarget 2>/dev/null
 fi
