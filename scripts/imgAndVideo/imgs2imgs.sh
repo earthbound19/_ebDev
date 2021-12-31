05d31bb29188577eabdff44784e3a68062e7972c
Alex Hall <>
2021-12-30 18:05:54 -0700

    add parameter 3 per updated img2img.sh

--- scripts/imgAndVideo/imgs2imgs.sh
+++ scripts/imgAndVideo/imgs2imgs.sh
@@ -6,6 +6,7 @@
 # Run this script with these parameters::
 # - $1 the source file format e.g. bmp or png
 # - $2 the target file format e.g. tif or jpg
+# - $3 OPTIONAL. New image X (pixels across) dimension. Smaller strongly recommended. Aspect matching this many X pixels will be maintained. See details in img2img.sh
 # Example that will convert all png images in the current directory to jpgs:
 #    imgs2imgs.sh png jpg
 
@@ -13,10 +14,11 @@
 # CODE
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (source image type to convert) passed to script. Exit."; exit 1; else sourceIMGformat=$1; fi
 if [ ! "$2" ]; then printf "\nNo parameter \$2 (source image type to convert) passed to script. Exit."; exit 1; else destIMGformat=$2; fi
+if [ "$3" ]; then param3=$3; fi
 
 fileNamesList=$(find . -maxdepth 1 -type f -name \*.$sourceIMGformat -printf "%f\n")
 
 for fileName in ${fileNamesList[@]}
 do
-	img2img.sh $fileName $destIMGformat
+	img2img.sh $fileName $destIMGformat $param3
 done
