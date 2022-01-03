7d0365217284d8305c0f6cd487adc63e55aeb52c
Alex Hall <earthbound19@users.noreply.github.com>
2022-01-02 21:11:17 -0700

    another dev snapshot

--- scripts/axeNfiles.sh
+++ scripts/axeNfiles.sh
@@ -35,44 +35,40 @@ fi
 if [ ! "$4" ]
 then
 	# if $4 was not provided, use default sort of find (don't append | shuf) :
-	allFileTypeList=( $(find . -maxdepth 1 -iname "*.$fileExt" -printf "%P\n") )
+	allFilesType=( $(find . -maxdepth 1 -iname "*.$fileExt" -printf "%P\n") )
 else
 	# if $4 was provided, after file list via find, shuffle it (before put in array) :
-	allFileTypeList=( $(find . -maxdepth 1 -iname "*.$fileExt" -printf "%P\n" | shuf) )
+	echo "Parameter \$4 provided; shuffling file list before sort into folders."
+	allFilesType=( $(find . -maxdepth 1 -iname "*.$fileExt" -printf "%P\n" | shuf) )
 fi
-
-for i in ${allFileTypeList[@]}
-do
-	echo $i
-done
-
-exit
-# Count number of files so we can figure out how many 0 columns to pad numbers with via printf:
-numberOfFiles=$(find . -maxdepth 1 -iname "*.$fileExt" | wc -l | tr -d ' ')
-		echo Found $numberOfFiles files of type $fileExt.
-padToDigits=${#numberOfFiles}
-		echo Will pad numbers in folder names to $padToDigits digits.
 # ====
 # END SET GLOBALS
 
+# Count number of files so we can figure out how many 0 columns to pad numbers with via printf:
+numberOfFiles=${#allFilesType[@]}
+		echo Found $numberOfFiles files of type $fileExt.
+padToDigits=${#numberOfFiles}
+		echo Will pad numbers in folder names to $padToDigits digits.
+exit
 # MAIN LOGIC
-# Adapted from and thanks to a genius breath yon; https://stackoverflow.com/a/29118145 -- for axing files in subdirs into subdirs by count, check another answer there:
-n=$((`find . -maxdepth 1 -iname "*.$fileExt" | wc -l`/$numberToAxeOn+1))
+# Figure out how many folders we'll need to create to move $numberToAxeOn into each:
+n=$(($numberOfFiles / $numberToAxeOn+1))
 # Variables used in the coming control block to break up lines of a text file (created by and useful for other scripts) into partitioned copies of it in created subfolders:
 linesCPmultiplier=1
 linesCPStartAtMultiple=1
 # For folder name by number zero-padding digits:
 highestAxeFolderNumberWillBe=$(($n * $numberToAxeOn))
 folderNumberDigitsPadding=${#highestAxeFolderNumberWillBe}
-for i in `seq 1 $n`;
+for i in $(seq 1 $n);
 do
-	zeroPaddedNumber=`printf "%0"$padToDigits"d" $i`
+	zeroPaddedNumber=$(printf "%0"$padToDigits"d" $i)
 	toEndFrameMultiple=$(($i * $numberToAxeOn))
-	paddedToEndFrameMultiple=`printf "%0"$folderNumberDigitsPadding"d" $toEndFrameMultiple`
+	paddedToEndFrameMultiple=$(printf "%0"$folderNumberDigitsPadding"d" $toEndFrameMultiple)
 	folderName=$folderPrefix"$paddedToEndFrameMultiple"
 	if [ $i == 1 ]; then helpFirstFolderName=$folderName; fi    # Store first folder name in variable for later help text.
 	if [ $i == $n ]; then helpLastFolderName=$folderName; fi    # Store last folder name in variable for later help text.
 	if ! [ -d $folderName ]; then mkdir $folderName; fi
+# TO DO: get array item by index here and rewrite the next line to move command using that:
 	find . -maxdepth 1 -iname "*.$fileExt" | sort -n | head -n $numberToAxeOn | tr -d '\15\32' | xargs -i mv "{}" $folderName
 		# Only do anything with IMGlistByMostSimilar.txt if it exists:
 	if [ -f ./IMGlistByMostSimilar.txt ]
@@ -86,5 +82,5 @@ do
 	fi
 done
 
-numeralOneToPadToDigits=`printf "%0"$padToDigits"d" 1`
+numeralOneToPadToDigits=$(printf "%0"$padToDigits"d" 1)
 echo DONE. All files in this folder of type $fileExt have been axed by count $numberToAxeOn into folders named starting $helpFirstFolderName and ending $helpLastFolderName \(if those are both the same folder name\, they are all in that one folder\)\. If the number of files of type $fileExt evenly divided by $numberToAxeOn\, the highest numbered folder will be empty\, so you know.
