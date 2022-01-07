11137a37c4896f478797e957dc765cae901b4693
Alex Hall <>
2022-01-07 04:39:39 -0700

    uncomment code? that did work I want to do? haha. (Allmost all functional code was commented out!)

--- scripts/recipes/genRandomColorsGrayMathGallery.sh
+++ scripts/recipes/genRandomColorsGrayMathGallery.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# Creates a markdown image gallery of randomly generated color palettes using "gray math" (where the sum of colors by additive color mixing will make a shade of white). Not platform-neutral (uses irfanview).
+# Creates a markdown image gallery of randomly generated color palettes using "gray math" (where the sum of colors by additive color mixing will make a shade of white). At this writing and possibly forever, the palette rending script (which this calls) that calls another script is not platform-neutral (uses irfanview).
 
 # USAGE
 #    genRandomColorsGrayMathGallery.sh
@@ -8,14 +8,13 @@
 # CODE
 # TO DO
 # parameterize number of images to make.
-# pythonScriptPath=$(getFullPathToFile.sh NrandomHexColorSchemesGrayMath.py)
+pythonScriptPath=$(getFullPathToFile.sh NrandomHexColorSchemesGrayMath.py)
 
-# python $pythonScriptPath
+python $pythonScriptPath
 
-# TO DO: make a directory named after the date and a random string and cd into it, then:
-# allhexplt2ppm.sh
-# imgs2imgsNN.sh ppm png 540 270
-# rm *.ppm
+allhexplt2ppm.sh
+imgs2imgsNN.sh ppm png 540 270
+rm *.ppm
 
 echo ~~~~
 read -r -p "To delete .hexplt files which resulted in color palette images you don't like, look through all the resultant .png images in this directory and delete the ones you don't like, then enter Y to continue. Otherwise enter N to stop." response
