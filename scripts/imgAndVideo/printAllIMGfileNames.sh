a87ed3c6ef33aa646e2ea711526177a87b460ac0
Alex Hall <>
2022-01-10 01:36:32 -0700

    doc. clarify

--- scripts/imgAndVideo/printAllIMGfileNames.sh
+++ scripts/imgAndVideo/printAllIMGfileNames.sh
@@ -2,14 +2,16 @@
 # Prints a list of all files matching many image etc. file types in the current directory, and optionally all subdirectories. To create an array from the list, see USAGE.
 
 # USAGE
-# Run without any parameter:
+# Run without these parameters:
+# - $1 OPTIONAL. Anything, for example the word 'BROGNALF', which will cause the script to find and print image file names also in subdirectories (and not only the current directory). If omitted, the script only finds and prints file names from the current directory.
+# Example command to find and print files from the current directory only:
 #    printAllIMGfileNames.sh
+# Example command to find and print files from the current directory and all subdirectories:
+#    printAllIMGfileNames.sh BROGNALF
 # To use this from another script to create an array from the output, do this:
-#    allIMGfileNamesArray=($(printAllIMGfileNames.sh))
+#    allIMGfileNamesArray=( $(printAllIMGfileNames.sh) )
 # -- you may then iterate through it like this:
 #    for element in ${allIMGfileNamesArray[@]}; do <something with $element>; done
-# By default, the script only prints files in the current directory, but if you pass any parameter to the script (for example the word 'BROGNALF'), it will also (find and) print image file names from subdirectories:
-#    printAllIMGfileNames.sh BROGNALF
 # NOTE
 # Because some tools are silly and create files with uppercase extensions, this script searches for both lowercase and uppercase extensions of every file type in its list.
 
@@ -45,13 +47,13 @@ tiff
 )
 
 # build string listing lowercase and also uppercase extensions list section for `find` command:
-fileTypesWithAlsoUppercase=
 for type in ${filetypes[@]}
 do
 	typesParam+="-o -iname \*.$type -o -iname \*.${type^^} "
 done
 
 # I'm only getting this to work in a temp script that I create, write the command to, executed and then delete. By itself with whatever escaping I find, or in a variable expanded to a command, it breaks; CHORFL is just to meet a requirement of starting the list withuot -o:
+# TO DO? : option to search with ~+ (causes full path to be prints) instead of ./
 echo "find ./ $maxdepthParameter -type f \( -iname \*.CHORFL $typesParam \) -printf \"%P\n\"" > tmpScript_bxJZuSvKq.sh
 ./tmpScript_bxJZuSvKq.sh
 rm ./tmpScript_bxJZuSvKq.sh
