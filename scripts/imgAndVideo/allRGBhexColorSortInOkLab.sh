2f97d285eac154597250c9b41325cb8722ff9d6c
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2022-01-26 14:11:00 -0700

    new -- sorts all hexplts in the current directory by next nearest in okLab space

---
+++ scripts/imgAndVideo/allRGBhexColorSortInOkLab.sh
@@ -0,0 +1,28 @@
+# DESCRIPTION
+# runs `rgbHexColorSortInOkLab.js` against all .hexplt files in the current directory (no recursion into subdirectories), overwrtiting the original files with the result.
+
+# DEPENDENCIES
+# `getFullPathToFile.sh`, `rgbHexColorSortInOkLab.js`.
+
+# USAGE
+# With more than one .hexplt file in your current directory, and `rgbHexColorSortInOkLab.js` in your PATH, run this script:
+#    allRGBhexColorSortInOkLab.sh
+# SEE ALSO `allRGBhexColorSortInCAM16-UCS.sh` and `allRGBhexColorSortInCIECAM02.sh`.
+
+
+# CODE
+# TO DO
+# Parameterize arbitrary sort color; default to black-black-black-magenta if not provided.
+scriptLocation=$(getFullPathToFile.sh rgbHexColorSortInOkLab.js)
+
+array=( $(find . -maxdepth 1 -type f -iname \*.hexplt -printf '%f\n') )
+# or to find every file,`find .` . .
+for element in ${array[@]}
+do
+	echo "Running comparisons for file $element . . ."
+	results=( $(node $scriptLocation -i $element) )
+	# print array re: https://stackoverflow.com/a/15692004/1397555
+	printf '%s\n' "${results[@]}" > $element
+	echo "DONE and result written back over file $element."
+	echo ""
+done
