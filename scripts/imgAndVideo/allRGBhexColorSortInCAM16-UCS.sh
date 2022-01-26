80f5a9edfc27e02bf0920f345f10ef80d0c73b4e
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2022-01-26 13:49:15 -0700

    doc/print improvements

--- scripts/imgAndVideo/allRGBhexColorSortInCAM16-UCS.sh
+++ scripts/imgAndVideo/allRGBhexColorSortInCAM16-UCS.sh
@@ -1,11 +1,14 @@
 # DESCRIPTION
 # runs RGBhexColorSortInCAM16-UCS.py with original file overwrite parameter against all .hexplt files in the current directory (no recursion into subdirectories), comparing them by sorting nearest to black-black-black-magenta first (makes a temporary copy of every hexplt file with black as the first color, runs the comparison, then removes the added color).
 
+# DEPENDENCIES
+# `getFullPathToFile.sh`, `RGBhexColorSortInCAM16-UCS.py`
+
 # USAGE
-# With more than one .hexplt file in your current directory, and RGBhexColorSortInCAM16-UCS.py either in your PATH or a copy of it in the current directory, and with this script also in your PATH or the current directory, run this script:
+# With more than one .hexplt file in your current directory, and RGBhexColorSortInCAM16-UCS.py in your PATH, run this script:
 #    allRGBhexColorSortInCAM16-UCS.sh
 # NOTE
-# See also allRGBhexColorSortIn2CIECAM02.sh.
+# See also `allRGBhexColorSortInOkLab.sh' and `allRGBhexColorSortInCIECAM02.sh'.
 
 
 # CODE
@@ -19,4 +22,5 @@ for element in ${array[@]}
 do
 	echo "Running comparisons for file $element . . ."
 	python $scriptLocation $element foo '#0a000a'
+	echo ""
 done
--- scripts/imgAndVideo/allRGBhexColorSortInCIECAM02.sh
+++ scripts/imgAndVideo/allRGBhexColorSortInCIECAM02.sh
@@ -1,10 +1,14 @@
 # DESCRIPTION
 # runs RGBhexColorSortInCIECAM02.py with original file overwrite parameter against all .hexplt files in the current directory (no recursion into subdirectories), comparing them by sorting nearest to black-black-black-magenta first (makes a temporary copy of every hexplt file with black as the first color, runs the comparison, then removes the added color).
 
+# DEPENDENCIES
+# `getFullPathToFile.sh`, `RGBhexColorSortInCIECAM02.py`
+
 # USAGE
-# With more than one .hexplt file in your current directory, and RGBhexColorSortInCIECAM02.py either in your PATH or a copy of it in the current directory, and with this script also in your PATH or the current directory, run this script:
+# With more than one .hexplt file in your current directory, and RGBhexColorSortInCIECAM02.py in your PATH, run this script:
 #    allRGBhexColorSortInCIECAM02.sh
-# SEE ALSO `allRGBhexColorSortInCAM16-UCS.sh.`
+# NOTE
+# See also `allRGBhexColorSortInOkLab.sh` and `allRGBhexColorSortInCAM16-UCS.sh`.
 
 
 # CODE
@@ -13,9 +17,9 @@
 scriptLocation=$(getFullPathToFile.sh RGBhexColorSortInCIECAM02.py)
 
 array=($(find . -maxdepth 1 -type f -iname \*.hexplt -printf '%f\n'))
-# or to find every file,`find .` . .
 for element in ${array[@]}
 do
 	echo "Running comparisons for file $element . . ."
 	python $scriptLocation $element foo '#0a000a'
+	echo ""
 done
