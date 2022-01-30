7309867b7e5db3e8dd3fc9c81dd72ae2b3e8ed4b
Alex Hall <>
2022-01-30 16:24:22 -0700

    rewrite: add optional color parameter (default sizes and color if neither provided).

--- scripts/imgAndVideo/newBlankIMG.sh
+++ scripts/imgAndVideo/newBlankIMG.sh
@@ -2,41 +2,62 @@
 # Creates a blank, transparent background png image of dimensions $1 (nn..Xnn..), via GraphicsMagick, named <imageDimensions>_blank.png
 
 # DEPENDENCIES
-# graphicsmagic (gm) in your PATH, grep.
+# imagemagick in your PATH, grep.
 
 # USAGE
 # Run the script with one parameter:
-# - $1 OPTIONAL. The dimensions of the image to create in format NxN, for example 1200x800 or 4000x4000. If not provided, or if not in that format (for example, if you pass the word 'CHULFOR'), the script will print a message saying so (however, the script will not print the word 'CHULFOR', alas), and the script will create a medium-hugoriomongoumoungous image of a default resolution, which I have determined through quantitative quality-aspect-averaged holistic optimization to be the best medium-hugorious-average-widish-aspected image size.
+# - $1 OPTIONAL. The dimensions of the image to create in format NxN, for example 1200x800 or 4000x4000. If not provided, or if provided as the word DEFAULT, a default image size is used.
+# - $2 OPTIONAL. sRGB hex color code (without any # etc. prefix) to fill blank image with. Must be hex digits in format rrggbbaa, where rr, gg and bb are digit placeholders for the colors red, green and blue, which must be expressed in sRGB hex digits (0-9 and a-f), and aa is a placeholder for hex digits indicating alpha, or transparency. For example, a fully opaque black image would be 000000ff. If not provided, image will be color 00000000 (black but fully transparent -- a blank transparent image (png format). To specify fully opaque, use ff for alpha. For example, a fully opaque black image would be 000000ff. To use this ($2) and not specify the size for $1 (use the default), pass the word DEFAULT for $1.
 # Example command that will create a 5240x2620, blank, transparent png image named 5240x2620_blank.png:
 #    newBlankIMG.sh 5240x2620
+# Example command that will create an opaque magenta image:
+#    newBlankIMG.sh 5240x2620 f800fcff
 # NOTES
 # - This script will not clobber a target image that already exists, and will notify you of its existence.
 # - This script does not print the word 'CHULFOR'.
 
 
 # CODE
-# UNCOMMENT only one of the following:
-defaultResolution="5240x3166"		# 1.655:1, best medium-hugorious-average-widish-aspected image size.
-# defaultResolution="5240x2620"		# 2:1, same but if you want that aspect.
+# GLOBAL DEFAULTS:
+imageResolution="5240x3166"		# 1.655:1, best medium-hugorious-average-widish-aspected image size. If your goal is that but you want a 2:1 aspect, use 5240x2620
+imageColor=f800fc00		# This is magenta but totally transparent, because if I do black, photoshop incorrectly interprets it as opaque black but IrfanView correctly interprets it as transparent. I don't know if that's a bug with Photoshop, or ImageMagick, or both.
 
-# use grep to check $1 for required parameter pattern (will set $? to 0 (no error) if match), and will record that result and do different things depending) :
-echo $1 | grep -q "^[0-9]\{1,\}[Xx][0-9]\{1,\}$"
-dimensionParamPatternMatchError=`echo $?`
-
-if [ "$dimensionParamPatternMatchError" == "0" ]
+# Check $1 for required parameter pattern (will set $? to 0 (no error) if match), and error out if provided and wrong.
+if [ "$1" ] && [ "$1" != "DEFAULT" ]
 then
-	if [ ! -e "$1"_blank.png ]
+	echo $1 | grep -q "^[0-9]\{1,\}[Xx][0-9]\{1,\}$"
+	dimensionParamPatternMatchError=$(echo $?)
+	if [ "$dimensionParamPatternMatchError" != "0" ]
 	then
-		gm convert -size $1 xc:transparent "$1"_blank.png
+		echo "ERROR: provided dimensions parameter \$1 ($1) doesn't meet format requirement nXn (numbers (pixels across), X or x, and numbers (pixels down), without any space in between). Exit."
+		exit 1
 	else
-		printf "\nTarget image "$1"_blank.png already exists; will not clobber. If you want to re-create it, rename the existing image and run this script again."
+		imageResolution=$1
 	fi
+fi
+
+# Override image color with parameter $2 if provided, warning if no hex color pattern match:
+if [ "$2" ]
+then
+	echo $2 | grep -i -q "^[0-9a-z]\{8\}$"
+	sRGBcolorPatternMatchError=$(echo $?)
+	if [ $sRGBcolorPatternMatchError != 0 ]
+	then
+		echo "ERROR: provided sRGB hex color pattern \$2 ($2) doesn't match requirement. Must be hex digits in format rrggbbaa, where rr, gg and bb are digit placeholders for the colors red, green and blue, which must be expressed in sRGB hex digits (0-9 and a-f), and aa is a placeholder for hex digits indicating alpha, or transparency. For example, a fully opaque black image would be 000000ff."
+		exit 2
+	else
+		imageColor="$2"
+	fi
+fi
+
+targetFileName="$imageResolution"_sRGB_"$imageColor".png
+
+if [ ! -e $targetFileName ]
+then
+	echo "runnning command:"
+	echo "magick convert -size $imageResolution xc:#$imageColor $targetFileName"
+	magick convert -size $imageResolution xc:#$imageColor $targetFileName
+	echo "Created target image $targetFileName."
 else
-	printf "\nParameter 1 nonexistent or doesn't match pattern ^[0-9]{1,}[Xx][0-9]{1,}\$ (NxN). Will use default $defaultResolution.\n"
-	if [ ! -e "$defaultResolution"_blank.png ]
-	then
-		gm convert -size $defaultResolution xc:transparent "$defaultResolution"_blank.png
-	else
-		printf "\nTarget image "$defaultResolution"_blank.png already exists; will not clobber. If you want to re-create it, rename the existing image and run this script again."
-	fi
+	echo "Target image $targetFileName already exists; will not clobber. If you want to re-create it, rename or delete the existing image and run this script again."
 fi
