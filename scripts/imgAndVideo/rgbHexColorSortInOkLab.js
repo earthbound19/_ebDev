d1960b9bbe190dc2d2011bfa7475b136c2b927ac
Alex Hall <earthbound19@users.noreply.github.com>
2022-02-15 22:46:34 -0700

    add arbitrary first comparison color option

--- scripts/imgAndVideo/rgbHexColorSortInOkLab.js
+++ scripts/imgAndVideo/rgbHexColorSortInOkLab.js
@@ -14,7 +14,6 @@
 
 
 // CODE
-// TO DO: option to sort on arbitrary first color, but don't add to output print if the color is not in the input file. But keep it if it's in the original input file.
 // DEV NOTE: OH HOW BADLY I WANTED TO FIND A PYTHON LIBRARY that works with okLab + does distance calculations (DeltaE) between colors and adapt one of the Python color sorting scripts to do that, because OH MY UNHOLY NACHOS OF WRATH this was so, so, SOOOOO much more difficult to implement in JavaScript. But no such Python library exists, that I could find. okLab is in development for colour-science as of winter 2021..
 
 // DEPENDENCIES (INCLUDES)
@@ -27,36 +26,52 @@ const { program } = require('commander');
 program
   // the <fileName> thing here leads to capture of a series of values (file name):
   .requiredOption('-i --inputFile <fileName>', '\n\tInput palette file name (e.g. \'floral_print_00002.hexplt\'), which is a list of sRGB colors in hex format (e.g. #f800fc).\n')
-  // .requiredOption('-f, --firstComparisonColor <digits>', '\n\tFirst color to start comparisons with, in sRGB hex format (e.g. 0a000a\n')
+  .option('-f, --firstComparisonColor <digits>', '\n\tFirst color to start comparisons with, in sRGB hex format (e.g. 0a000a) (NOTE: no number/hex/pound (\'#\') symbol should be in the color code)\n')
 program.parse();
 const options = program.opts();
+
+// INPUT FILE
 // convert input file parameter to the string it's intended to be:
 const inputFileString = String(options.inputFile);
-// also first comparison color:
-// var compareColorSrgbHex = String(options.firstComparisonColor);
-
 // get array of sRGB hex values from input file:
 try {
   var inputFileContent = fs.readFileSync(inputFileString).toString();
 }
 // print error if unable to read specified file:
 catch(err) {
-  console.log("\n\n!========\nERROR: unable to open specified -i --inputFile ", inputFileString, ". Exit.\n!========\n");
+  console.log("\n\n!========\nERROR: unable to open specified -i --inputFile ", inputFileString, ". Exit code 1.\n!========\n");
   process.exit(1);
+}
+// ARBITRARY FIRST SORT COLOR
+var arbitraryFirstCompareColor = '';		// set default blank
+if(typeof options.firstComparisonColor !== 'undefined') {
+	// if it does not match sRGB hex color code pattern (including exactly six characters, no more or less), throw error and exit; otherwise assign the value to arbitraryFirstCompareColor and continue:
+	var pattern = new RegExp("\^[0-9a-fA-F]{6}$")
+	if (pattern.test(options.firstComparisonColor) == false) {
+		console.log("\n\n!========\nERROR: provided value for parameter -f --firstComparisonColor is not in sRGB hex format (six hex digits, e.g. 0a000a); value is:\n\t", options.firstComparisonColor, "\nExit code 2.\n!========\n");
+		process.exit(2);
+	} else {
+		arbitraryFirstCompareColor = '#' + String(options.firstComparisonColor);
+	}
 }
 
 // get array of sRGB hex format colors from the file:
 const regexp = /#[a-fA-F0-9]{6}/g;
 const searchResults = [...inputFileContent.matchAll(regexp)];
 // init array of colors from file + regex search result:
-var originalColorsArray = [];
+var comparisonColorsArray = [];
 for (const element of searchResults) {
-  originalColorsArray.push(element[0]);
+  comparisonColorsArray.push(element[0]);
 }
 
 // remove duplicates from original list, but maintain order (keep all unique in the same order), re: https://stackoverflow.com/a/15868720/1397555
-originalColorsArray = [ ... new Set(originalColorsArray) ];
-const deduplicatedOriginalColorsArrayLength = originalColorsArray.length
+comparisonColorsArray = [ ... new Set(comparisonColorsArray) ];
+// if the value of arbitraryFirstCompareColor was changed from default empty string (''), because a valid sRGB hex color value for the -f option was passed to the script, add it to the start of the list; colors will therefore be sorted by first comparing to it; will remove it afterward:
+if (arbitraryFirstCompareColor != '') {
+	comparisonColorsArray.unshift(arbitraryFirstCompareColor);
+}
+
+const comparisonColorsArrayLength = comparisonColorsArray.length
 
 // get a color space converter function varaible noun noun noun noun (okLCHconverter):
 let okLCHconverter = culori.converter('oklch');
@@ -72,35 +87,40 @@ okLCHdistance = culori.differenceEuclidean(mode = 'oklch', weights = [1, 1, 1]);
 
 // init final sort list (as empty):
 finalSortedList = [];
-// Add first item to final list, as the first item will be the first in the original list; originalColorsArray[0].sRGBhex and searchResults[0][0] SHOULD both be the first color in the list:
-finalSortedList.push(originalColorsArray[0]);
+// Add first item to final list, as the first item will be the first in the original list; comparisonColorsArray[0].sRGBhex and searchResults[0][0] SHOULD both be the first color in the list (although arbitraryFirstCompareColor can mess with that, but the result we intent will be the same):
+finalSortedList.push(comparisonColorsArray[0]);
 
-while (finalSortedList.length < deduplicatedOriginalColorsArrayLength) {
-	var sRGB_hex_A = originalColorsArray[0];
+while (finalSortedList.length < comparisonColorsArrayLength) {
+	var sRGB_hex_A = comparisonColorsArray[0];
 	var sRGB_hex_B;
-	var okLCHval_A = okLCHconverter(originalColorsArray[0]);
+	var okLCHval_A = okLCHconverter(comparisonColorsArray[0]);
 	var okLCHval_B;
 	var okDist = 58848;								// way beyond any distance that will be found.
 	var lowestFoundDistanceForThisPair = 58849;		// "
 	var nearestFoundColorHEX;
 	// figuring out this i = 1 (not zero) was the final thing that finished this script:
-	for (var i = 1; i < originalColorsArray.length; i++) {
-		sRGB_hex_B = originalColorsArray[i]
-		okLCHval_B = okLCHconverter(originalColorsArray[i]);
+	for (var i = 1; i < comparisonColorsArray.length; i++) {
+		sRGB_hex_B = comparisonColorsArray[i]
+		okLCHval_B = okLCHconverter(comparisonColorsArray[i]);
 		okDist = okLCHdistance(okLCHval_A, okLCHval_B);
 		if (okDist < lowestFoundDistanceForThisPair) {
 			lowestFoundDistanceForThisPair = okDist;
-			nearestFoundColorHEX = originalColorsArray[i];
+			nearestFoundColorHEX = comparisonColorsArray[i];
 		}
 	}
 	finalSortedList.push(nearestFoundColorHEX);
-	// recreate originalColorsArray with sRGB_hex_A removed and nearestFoundColorHEX moved to start:
+	// recreate comparisonColorsArray with sRGB_hex_A removed and nearestFoundColorHEX moved to start:
 	// remove sRGB_hex_A; re this horror: https://stackoverflow.com/a/20690490/1397555 :
-	originalColorsArray = originalColorsArray.filter(item => item !== sRGB_hex_A);
+	comparisonColorsArray = comparisonColorsArray.filter(item => item !== sRGB_hex_A);
 	// remove nearestFoundColorHEX:
-	originalColorsArray = originalColorsArray.filter(item => item !== nearestFoundColorHEX);
+	comparisonColorsArray = comparisonColorsArray.filter(item => item !== nearestFoundColorHEX);
 	// add nearestFoundColorHEX to start:
-	originalColorsArray.unshift(nearestFoundColorHEX);
+	comparisonColorsArray.unshift(nearestFoundColorHEX);
+}
+
+// if the value of arbitraryFirstCompareColor was changed from default empty string (''), because a valid sRGB hex color value for the -f option was passed to the script, we earlier added arbitraryFirstCompareColor to the list; in that case remove it now from the final list (it will be the first item in the list):
+if (arbitraryFirstCompareColor != '') {
+	finalSortedList.shift();
 }
 
 // print final perceptually sorted color list:
