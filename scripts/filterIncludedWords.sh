d3b362e1e592dff7b74cc0fdd7c3ed7faf442a6f
Alex Hall <>
2022-02-20 14:20:18 -0700

    - new include word filter script - typo fixes in two other scripts - rename script reference in filterWords.sh doc, will rename that to filterExcludedWords.sh on next commit

---
+++ scripts/filterIncludedWords.sh
@@ -0,0 +1,20 @@
+# DESCRIPTION
+# Inclusion text filter. Prints text file $2, modified, with only words that are also in text file $1. Another way of saying that is: prints only words in $2 that appear in $1. Both files must have one word per line.
+
+# USAGE
+# Run with these parameters:
+# - $1 text file name of inclusion filter words (words you want printed)
+# - $2 text file name to print words for only if they also appear in $1.
+# For example:
+#    filterIncludedWords.sh incudeWords.txt textToFilter.txt
+# NOTES
+# - The source text files are expected to have one word per line. This will not work the way you might want with paragraphs.
+# - To capture results to a new file, use a redirect operator, like this:
+#    filterIncludedWords.sh incudeWords.txt textToFilter.txt > filteredWords.txt
+
+
+# CODE
+if [ ! "$1" ]; then printf "\nNo parameter \$1 (file name of text file of include filter words) passed to script. Exit."; exit 1; else includeFilterWordsFileName=$1; fi
+if [ ! "$2" ]; then printf "\nNo parameter \$2 (file name of text file to print words from only if they appear in $\1) passed to script. Exit."; exit 2; else textToFilterFileName=$2; fi
+
+awk '{if (f==1) { r[$0] } else if ($0 in r) { print $0 } } ' f=1 $includeFilterWordsFileName f=2 $textToFilterFileName
--- scripts/filterWords.sh
+++ scripts/filterWords.sh
@@ -1,20 +1,20 @@
 # DESCRIPTION
-# Prints all words in text file $2 that don't appear in text file $1 (excludes all words $1 from $2), provided that both files have one word per line.
+# Prints text file $2, modified, with all words in text file $1 removed from it. Another way of saying that is: prints all words in $2 that don't appear in $1. Both files must have one word per line.
 
 # USAGE
 # Run with these parameters:
 # - $1 text file name of filter words (words you do not want printed)
 # - $2 text file name to filter words out of (words you want printed minus filter words).
 # For example:
-#    filterWords.sh filterWords.txt fileToFilterWordsFrom.txt
+#    filterExcludedWords.sh filterExcludeWords.txt fileToFilterWordsFrom.txt
 # NOTES
 # - The source text files are expected to have one word per line. This will not work the way you might want with paragraphs.
 # - To capture results to a new file, use a redirect operator, like this:
-#    filterWords.sh filterWords.txt fileToFilterWordsFrom.txt > filteredWords.txt
+#    filterExcludeWords.sh filterExcludeWords.txt fileToFilterWordsFrom.txt > filteredWords.txt
 
 
 # CODE
-if [ ! "$1" ]; then printf "\nNo parameter \$1 (file name of text file with filter words) passed to script. Exit."; exit 1; else fitlerWordsFileName=$1; fi
+if [ ! "$1" ]; then printf "\nNo parameter \$1 (file name of text file with filter words) passed to script. Exit."; exit 1; else filterWordsFileName=$1; fi
 if [ ! "$2" ]; then printf "\nNo parameter \$2 (file name of text file to print with words filtered out of it) passed to script. Exit."; exit 2; else fileNameToFilterWordsFrom=$2; fi
 
 # Thanks yet again to yet another genius breath yonder: http://stackoverflow.com/a/18477228/1397555
@@ -22,4 +22,4 @@ if [ ! "$2" ]; then printf "\nNo parameter \$2 (file name of text file to print 
 	# awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 exclude-these.txt f=2 from-this.txt
 	# ADAPTED e.g. for removal of all actual English words (english_dictionary.txt) from gibberishWords.txt:
 	# awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 english_dictionary.txt f=2 gibberishWords.txt > gibberishWords_real_words_excluded.txt
-awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 $fitlerWordsFileName f=2 $fileNameToFilterWordsFrom
+awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 $filterWordsFileName f=2 $fileNameToFilterWordsFrom
