8cfa3e04065f72e255efc4df7ca3ee1dcf02de1d
Alex Hall <>
2022-03-06 10:26:07 -0700

    remove extraParams that had been used for either audio codec copy or aac encode, set -c copy flag for all streams, point in doc to other script for audio re-encode

--- scripts/imgAndVideo/allVideo2mp4Lossless.sh
+++ scripts/imgAndVideo/allVideo2mp4Lossless.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# Converts all of many video media container (file) types in the current directory to mp4 containers, losslessly; there is no recompression: it directly copies the video streams into a new container. It also copies the file timestamps (including Windows-unique ones) and relevant metadata from the original file to the converted target file.
+# Converts all of many video media container (file) types in the current directory to mp4 containers, losslessly; there is no recompression: it directly copies the video streams into a new container. It also copies the file timestamps (including Windows-unique ones) and relevant metadata from the original file to the converted target file. For options for lossless video but lossy sound, see NOTES.
 
 # DEPENDENCIES
 # ffmpeg, GNU touch, ExifTool
@@ -7,17 +7,12 @@
 # USAGE
 # Run without any parameters:
 #    allVideo2mp4Lossless.sh
-# Also, see the extraParams variable (and commented options for it) and maybe hack its assignment per your preference.
+# NOTES
+# If you have a .mov container as source with PCM sound, you may get an error copying (maybe mp4 files can't have PCM audio??). In that case try `copyVidLosslessEncodeSound.sh` or `copyVidLosslessEncodeSoundAllType.sh`
 
 
 # CODE
 mediaList=$(printAllVideoFileNames.sh)
-
-# OPTIONAL EXTRA PARAMETERS
-# Because ffmpeg uncompressed/PCM audio is a silly waste of space for distribution, the following is an option commented out in distribution because encoding to aac isn't lossless! -crf 15 is very high quality encoding (practically though not actually lossless?) :
-# extraParams="-acodec aac -crf 15"
-# OR just straight copy the sound (default archived code option) even if it's a Canon DSLR .MOV pcm space hog sound channel:
-# extraParams="-c:a copy"
 
 for fileName in ${mediaList[@]}
 do
@@ -31,7 +26,7 @@ do
 		fi
 	echo "Converting $fileName to mp4 container as $fileNameNoExt.mp4 . . ."
 	renderTarget=$fileNameNoExt.mp4
-	ffmpeg -y -i $fileName $extraParams -c:v copy $renderTarget
+	ffmpeg -y -i $fileName -c copy $renderTarget
 	# Copy metadata from original file to render target:
 	exiftool -overwrite_original -TagsFromFile $fileName $renderTarget
 	# Update time stamp of file to metadata creation date; uses a conditional like is given in this post: https://exiftool.org/forum/index.php?topic=6519.msg32511#msg32511 -- but adding an -else clause:
