4503a64ac549be39b5275cd9134a6622389b64dd
Alex Hall <>
2022-04-02 15:22:59 -0600

    add info print about attempting to update timestamp from metadata creation date

--- scripts/imgAndVideo/copyMetadataFromSourceFileToTarget.sh
+++ scripts/imgAndVideo/copyMetadataFromSourceFileToTarget.sh
@@ -32,5 +32,6 @@ exiftool -overwrite_original -TagsFromFile $sourceFileName $targetFileName
 # OPTIONAL via anything for paratmer $3: Update time stamp of target file to metadata creation date; uses a conditional like is given in this post: https://exiftool.org/forum/index.php?topic=6519.msg32511#msg32511 -- but adding an -else clause:
 if [ "$3" ];
 then
+	echo "${0##*/} : attempting to modify file time stamp to match any creation date metadata, for file $targetFileName . . ."
 	exiftool -if "defined $CreateDate" -v -overwrite_original '-FileModifyDate<CreateDate' -d "%Y_%m_%d__%H_%M_%S%%-c.%%e" -else -v -overwrite_original '-FileModifyDate<DateTimeOriginal' -d "%Y_%m_%d__%H_%M_%S%%-c.%%e" $targetFileName
 fi
