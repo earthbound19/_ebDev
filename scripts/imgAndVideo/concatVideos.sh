c4b3f908a1ba586b9665edb9db6a5c3999ae531b
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2022-04-20 19:47:03 -0600

    improve doc

--- scripts/imgAndVideo/concatVideos.sh
+++ scripts/imgAndVideo/concatVideos.sh
@@ -9,7 +9,7 @@
 
 # USAGE
 # Run with these parameters:
-# - $1 OPTIONAL. Extension of e.g. mp4 files that are all encoded the same way. If not provided, defaults to mp4.
+# - $1 OPTIONAL. Extension of source video files to concatenate into one larger file. They must all be encoded the same way. If not provided, defaults to mp4.
 # - $2 OPTIONAL. File name of list of files. The contents of this file must be in this format:
 #    file 'file1.avi'
 #    file 'file2.avi'
@@ -18,7 +18,7 @@
 # If $2 is used, $1 is ignored (and can technically be anything in that case, like the word NULL). It may be desirable to use $2 if you want a custom ordering for the concatenation of the files. If you only use $1, whatever order results from the file list wildcard portion of the printf command in code (see) is the concatenation order.
 # Example with only parameter $1:
 #    concatVideos.sh avi
-# Example without any parameter, which would concatenate all mp4 files:
+# Example without any parameter, which would concatenate all mp4 files, because mp4 is the default if you don't provide a file type:
 #    concatVideos.sh
 # The result concatenated file name in that case is _mp4sConcatenated.mp4`.
 # Example with parameters $1 and $2:
@@ -26,6 +26,7 @@
 # NOTES
 # This script sets an environment variable $concatenatedVideoFileName which will be set in a calling shell if you call this script this way:
 #    source concatVideos.sh YORGBLAF fadeSRCvideosList.txt
+# The purpose of setting that variable is to use it in other scripts that need to know the result encoded file name (to do things with it).
 
 # CODE
 if [ "$1" ]; then vidExt=$1; else vidExt=mp4; fi
