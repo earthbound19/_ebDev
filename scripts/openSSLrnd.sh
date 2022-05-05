7f6cc0dc86a272e1c9a8cdd75e6121167b21e3dd
Alex Hall <earthbound19@users.noreply.github.com>
2022-05-05 07:31:26 -0600

    improve doc

--- scripts/openSSLrnd.sh
+++ scripts/openSSLrnd.sh
@@ -1,11 +1,15 @@
 # DESCRIPTION
-# Get N random bytes (from parameter $1) using openssl.
+# Very simple openSSL wrapper: get N ($1) random bytes from it.
 
 # DEPENDENCIES
-# openssl
+# OpenSSL.
 
 # USAGE
-# To write N random bytes to a file via this script, run it this way; here, 512 can be changed to any number of bytes you wish to generate:
+# Call with these parameters:
+# - $1 number of random bytes to generate using OpenSSL.
+# Example that will get 512 random bytes:
+#    openSSLrnd.sh 512
+# To write N random bytes to a file via this script, pipe the output to a file:
 #    openSSLrnd.sh 512 > rnd.dat
 
 
