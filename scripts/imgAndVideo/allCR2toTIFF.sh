3685e08c2fa320205cb3dafa84bee3ddfb6d2179
Alex Hall <>
2022-05-29 21:28:24 -0600

    operate on lowercase extensions also

--- scripts/imgAndVideo/allCR2toTIFF.sh
+++ scripts/imgAndVideo/allCR2toTIFF.sh
@@ -1,9 +1,9 @@
 # DESCRIPTION
-# Calls dcraw2tif.sh for every .CR2 format file in the current directory (thereby making .tif format conversions of them).
+# Calls dcraw2tif.sh for every .CR2 (OR .cr2 (lowercase extension) format file in the current directory, thereby making .tif format conversions of them.
 
 
 # CODE
-allCR2S=(*.CR2)
+allCR2S=(*.CR2 *.cr2)
 for element in ${allCR2S[@]}
 do
 	dcraw2tif.sh $element
