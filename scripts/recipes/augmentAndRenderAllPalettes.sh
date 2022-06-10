0f8fee6c8d1c3d2433687106bcbbae3574eec850
Alex Hall <>
2022-06-09 23:16:49 -0600

    - add option for adjacent color deduplication parameter - add check for target render and skip if exists, with notification print (no clobber)

--- scripts/recipes/augmentAndRenderAllPalettes.sh
+++ scripts/recipes/augmentAndRenderAllPalettes.sh
@@ -4,12 +4,19 @@
 # USAGE
 # Run with these parameters:
 # - $1 how many linearly interpolated colors to insert between each color in each palette.
-# For example:
+# - $2 OPTIONAL. Anything, such as the word FULFAR, which will cause this script to pass parameter $3 in augmentPalette.sh (which causes any repeat colors right next to each other to be removed, so that a color shows only once, never twice or more).
+# An example that will cause 32 colors to be interpolated between every color in every palette in the current directory:
 #    augmentAndRenderAllPalettes.sh 32
+# An example that will cause 100 colors to be interpolated between every color, but also remove any repeat colors:
+#    augmentAndRenderAllPalettes.sh 100 CHOOFY
+# NOTES
+# An excellent way to destroy (or nurture!) your soul with frustrated impatience is to run this repeatedly in the same directory where . . . you just augmented a palette. This is also known as Recursive Waiting Despair. It really doesn't take a terrible lot for this multiplication, either. 80 * 80 = waiting for 6,400 interpolated colors to calculate and write . . .
 
 
 # CODE
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (how many linearly interpolated colors to insert between each color in palette file \$1.) passed to script. Exit."; exit 1; else interpolationSteps=$1; fi
+# set deduplicateAdjacentSamplesParameter as '' (which will do nothing, and it will be overrided in next check if parameter $2 passed to script) :
+if [ "$2" ]; then deduplicateAdjacentSamplesParameter=$2; fi
 
 hexpltFileNamesArray=( $(find . -maxdepth 1 -type f -iname "*.hexplt" -printf "%P\n") )
 
@@ -19,12 +26,19 @@ fullPathToOKLABAugmentationScript=$(getFullPathToFile.sh get_color_gradient_OKLA
 
 for fileName in ${hexpltFileNamesArray[@]}
 do
-	echo "Creating augmented palette (gradient) for file $fileName . . ."
-	augmentedPaletteArray=( $(source augmentPalette.sh $fileName $interpolationSteps) )
 	paletteRenderTargetFileName=${fileName%.*}_gradient.hexplt
-	echo "${augmentedPaletteArray[@]}" > $paletteRenderTargetFileName
-	# get length of augmented palette:
-	augmentedPaletteArrayLength=${#augmentedPaletteArray[@]}
-	echo "Length of augmented palette is $augmentedPaletteArrayLength colors. Rendering palette . . ."
-	renderHexPalette.sh $paletteRenderTargetFileName 'NULL' 'NULL' $augmentedPaletteArrayLength 1
+	# only do render work if the target file does not exist:
+	if [ ! -f $paletteRenderTargetFileName ]
+	then
+		echo "Creating augmented palette (gradient) for file $fileName . . ."
+		augmentedPaletteArray=( $(source augmentPalette.sh $fileName $interpolationSteps $deduplicateAdjacentSamplesParameter) )
+		# print augmented palette, one line per color:
+		printf '%s\n' "${augmentedPaletteArray[@]}" > $paletteRenderTargetFileName
+		# get length of augmented palette:
+		augmentedPaletteArrayLength=${#augmentedPaletteArray[@]}
+		echo "Length of augmented palette is $augmentedPaletteArrayLength colors. Rendering palette . . ."
+	#	renderHexPalette.sh $paletteRenderTargetFileName 'NULL' 'NULL' $augmentedPaletteArrayLength 1
+	else
+		printf "Render target $paletteRenderTargetFileName already exists; will not clobber. To recreate it, delete it and run this script the same way again.\n\n"
+	fi
 done
