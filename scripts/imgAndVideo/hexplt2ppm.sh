3f2e35d5f6307d84456a89d8d811da2baeb7d876
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2022-07-28 04:51:18 -0600

    - bug fix: calculated rows could be off. - simplify / improve source palette color counting, column/row calculations - change gray to true neutral - other code style/preference improvements

--- scripts/imgAndVideo/hexplt2ppm.sh
+++ scripts/imgAndVideo/hexplt2ppm.sh
@@ -4,8 +4,8 @@
 # USAGE
 # Run this script with the following parameters:
 # - $1 hex color palette flat file list (input file).
-# - $2 OPTIONAL. Number of tiles across of tiles-assembled image (columns). If not provided, automatically calculated as approximate square root of total number of colors in source .hexplt file.
-# - $3 OPTIONAL. IF $2 IS PROVIDED, you probably want to provide this also, as the script does math you may not want if you don't provide $3. Number of tiles down of tiles-assembled image (rows).
+# - $2 OPTIONAL. Number of columns. If not provided, automatically calculated as approximate square root of total number of colors in source .hexplt file.
+# - $3 OPTIONAL. Number of rows. If omitted, the script does math automatically to have enough rows for all colors.
 # For example, to create a tiny ppm palette 5 tiles across and 40 tiles down from colors in the list `RAHfavoriteColorsHex.hexplt`, run:
 #    hexplt2ppm.sh RAHfavoriteColorsHex.hexplt 5 40
 # To create a tiny ppm image with columns and rows calculated by the script from that same palette, run:
@@ -17,6 +17,10 @@
 
 
 # CODE
+# TO DO
+# - Get number of colors via grep
+# - Possibly make an array of colors via grep before that, and use the array for color value write operations)
+
 
 # =============
 # BEGIN SETUP GLOBAL VARIABLES
@@ -36,8 +40,8 @@ else	# Search for specified palette file in palettesRootDir (if that dir exists;
 				echo searching in path $palettesRootDir --
 				echo for file $paletteFile . . .
 						# FAIL:
-						# hexColorSrcFullPath=`find "$palettesRootDir" -iname *$paletteFile`
-		hexColorSrcFullPath=`find $palettesRootDir -iname "$paletteFile"`
+						# hexColorSrcFullPath=$(find "$palettesRootDir" -iname *$paletteFile)
+		hexColorSrcFullPath=$(find $palettesRootDir -iname "$paletteFile")
 		echo -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 		if [ "$hexColorSrcFullPath" == "" ]
 			then
@@ -58,59 +62,39 @@ else	# Search for specified palette file in palettesRootDir (if that dir exists;
 	fi
 fi
 
-# Do whitespace / extraneous lines cleanup of .hexplt source file (saving result to temp file which we will operate from) :
-			# commands iterated through in development:
-			# replaces whitespace with ____:
-			# sed -e "s/\s/____/g" ColorSchemeHexBurntSandstone.hexplt
-			# isolates six-character hex:
-			# sed -e "s/#\([0-9a-fA-F]\{6\}\)/\1/g" ColorSchemeHexBurntSandstone.hexplt
-			# combines stripping leading whitespace and isolating sextuple hex strings:
-			# sed -e "s/^\s\{1,\}//g" -e "s/#\([0-9a-fA-F]\{6\}\).*/\1/g" ColorSchemeHexBurntSandstone.hexplt
-# I don't know *how* this combination of flags works; it was a shot in the dark (!), but it works; hints from https://stackoverflow.com/a/1665574/1397555 ; ALSO, the \L converts all preceding characters which are uppercase to lowercase :
-sed -n -e "s/^\s\{1,\}//g" -n -e "s/#\([0-9a-fA-F]\{6\}\).*/\L\1/p" $hexColorSrcFullPath > tmp_djEAM2XJ9w.hexplt
-# Reassign name of that temp file to hexColorSrcFullPath to work from:
-hexColorSrcFullPath=tmp_djEAM2XJ9w.hexplt
+# get array of colors from file by extracting all matches of a pattern of six hex digits preceded by a #:
+colorsArray=( $(grep -i -o '#[0-9a-f]\{6\}' $hexColorSrcFullPath | tr -d '#') )		# tr command removes pound symbol, and surrounding () makes it an actual array
+# Get number of colors (from array):
+numColors=${#colorsArray[@]}
 
 # WHETHER NUM tiles across (and down) is specified; if so, use as specified, if not so, do some math to figure for a 2:1 aspect;
 # $2 is across. If $2 is not specified, do some math. Otherwise use $2:
-if [ -z "$2" ]
+if [ ! "$2" ]
 then
-	# Get number of lines (colors). Square root of that x2 will be the number of columns in the rendered palette:
-	# Works around potential incorrect line count; re: https://stackoverflow.com/a/28038682/1397555 :
-		# echo attempting awk command\:
-# echo "awk 'END{print NR}' $hexColorSrcFullPath"
-	numColors=`awk 'END{print NR}' $hexColorSrcFullPath`
-	numColors=`awk 'END{print NR}' $hexColorSrcFullPath`
-			echo number of colors found in $paletteFile is $numColors.
-	sqrtOfColorCount=`echo "sqrt ($numColors)" | bc`
-			# echo sqrtOfColorCount is $sqrtOfColorCount \(first check\)
+	sqrtOfColorCount=$(echo "sqrt ($numColors)" | bc)
+	# echo sqrtOfColorCount is $sqrtOfColorCount \(first check\)
 	tilesAcross=$(( $sqrtOfColorCount * 2 ))
-			# echo tilesAcross is $tilesAcross\.
 else
 	tilesAcross=$2
 fi
+echo tilesAcross is $tilesAcross\.
+
 # $3 is down. If $3 is not specified, do some math. Otherwise use $3.
-if [ -z "$3" ]
+if [ ! "$3" ]
 then
-	# Get number of lines (colors, yes again, if so). Square root of that / 2 will be the number of rows in the rendered palette.
-	numColors=`awk 'END{print NR}' $hexColorSrcFullPath`
-			# echo numColors is $numColors\.
-	sqrtOfColorCount=`echo "sqrt ($numColors)" | bc`
-			# echo sqrtOfColorCount is $sqrtOfColorCount
-	tilesDown=$(( $sqrtOfColorCount / 2 ))
-	# If the value of ($tilesAcross times $tilesDown) is less than $numColors (the total number of colors), we will fail to print all colors in the palette; add rows to the print queue for as long as necessary:
-	tilesToRender=$(( $tilesAcross * $tilesDown ))
-			# echo tilesToRender is $tilesToRender\.
-	while [ $tilesToRender -lt $numColors ]
-	do
-			# echo tilesDown was $tilesDown.
+	sqrtOfColorCount=$(echo "sqrt ($numColors)" | bc)
+	# the following math and logic is much simpler than in a previous iteration of the script :/ and more to the point, it always produces the result I want:
+	tilesDown=$(( $numColors / $tilesAcross ))
+	# if this modulo returns nonzero, we need to add a row; do so:
+	modulo=$(( $numColors % $tilesAcross ))
+	if [ $modulo != "0" ]
+	then
 		tilesDown=$(( $tilesDown + 1 ))
-		tilesToRender=$(( $tilesAcross * $tilesDown ))
-	done
-			# echo tilesDown is $tilesDown\.
+	fi
 else
 	tilesDown=$3
 fi
+# echo tilesDown is $tilesDown\.
 # END SETUP GLOBAL VARIABLES
 # =============
 
@@ -129,40 +113,26 @@ else
 	echo "255" >> PPMheader.txt
 
 	# TO DO: this simpler conversion method for sRGB hex code to decimal: https://stackoverflow.com/a/7254022/1397555
-	# each hex color is a triplet of hex pairs (corresponding to 0-255 RGB values) ; concatenate temp hexplt file to uninterrupted hex pairs (no newlines) to parse as one long string into the ppm body:
-	# with | tr -d '\15\32' to delete windows newlines from the resultant array
-	#  and this is the seventeen-thousandth time this doom has caused issues without this:
-	ppmBodyValues=`tr -d '\n' < $hexColorSrcFullPath | tr -d '\15\32'`
-			rm $hexColorSrcFullPath
+	# create superstring from array (all hex values, no spaces) :
+	# print array re: https://stackoverflow.com/a/15692004/1397555
+	# deleting any Windows newline(s) in that also with | tr -d '\15\32' :
+	ppmBodyValues=$(printf '%s' "${colorsArray[@]}" | tr -d '\15\32')
 	# Split that superstring with spaces every two hex chars:
-	ppmBodyValues=`echo $ppmBodyValues | sed 's/../& /g' | tr -d '\15\32'`
-	# ppmBodyValues=`echo $((16#"$thisHexString")) $((16#"$thatHexString"))`
-	ppmBodyValues=`echo $ppmBodyValues | sed 's/[a-zA-Z0-9]\{2\}/$((16#&))/g' | tr -d '\15\32'`
+	ppmBodyValues=$(echo $ppmBodyValues | sed 's/../& /g' | tr -d '\15\32')
+	# ppmBodyValues=$(echo $((16#"$thisHexString")) $((16#"$thatHexString")))
+	ppmBodyValues=$(echo $ppmBodyValues | sed 's/[a-zA-Z0-9]\{2\}/$((16#&))/g' | tr -d '\15\32')
 	# If I echo that\, it prints it literally instead of interpretively. Ach! Workaround: make a temp shell script that echoes it interpretively (and assign the result to a variable) :
 	printf "echo $ppmBodyValues" > tmp_hsmwzuF64fEWmcZ2.sh
 	chmod +x tmp_hsmwzuF64fEWmcZ2.sh
-	ppmBodyValues=`./tmp_hsmwzuF64fEWmcZ2.sh`
+	ppmBodyValues=$(./tmp_hsmwzuF64fEWmcZ2.sh)
 	rm tmp_hsmwzuF64fEWmcZ2.sh
 	echo $ppmBodyValues > ppmBody.txt
-			# DEV NOTES
-			# including "simulate ? (optionals) with \{0,1\}", re: https://stackoverflow.com/a/6157705/1397555
-			# ex. command: echo "a aa a aa aaa a " | sed "s/ \{0,1\}\(a\)/\1\n/g"
-			# (The space needs to be optional because the start of a line isn't going to have a space in our case.)
-			# further worked up to break all three-groups of the letter a separated by spaces:
-			# echo "aaaaaaa aaaaa a aa aaaa a a a a aaa aa aaaaaaaaaaaaaaaaa" | sed "s/\( \{0,1\}a\{1,\}\)\{3\}/&\n/g"
-			# further worked up to capture digits, not "a"s:
-			# echo "8 11 254 4 100 2 36 1 100" | sed "s/\( \{0,1\}[0-9]\{1,\}\)\{3\}/&\n/g"
 	# Because each pixel is three values, each tile (tilesAcross) will be * 3 numeric values:
 	splitAtCount=$(( $tilesAcross * 3 ))
 	echo $ppmBodyValues | sed "s/\( \{0,1\}[0-9]\{1,\}\)\{$splitAtCount\}/&\n/g" > ppmBody.txt
 	# Strip resultant leading spaces off that, in place (in file):
 	sed -i 's/^ \(.*\)/\1/g' ppmBody.txt
-# CONTINUE CODING HERE; NOTE copied from above:
 	echo Padding any empty columns on last row of .ppm file with middle gray pixels . . .
-	# Fill in any empty columns on the last line with gray:
-	# - Count number of value triplets on last line, and subtract that tilesAcross; if the result is nonzero, add that many to the row.
-	#  - Isolate last line of ppmBody.txt to count those values; re http://www.theunixschool.com/2012/05/7-different-ways-to-print-last-line-of.html :
-	#  - pipe the last line of ppmBody to grep and search for number pattern, then pipe to word count (wc), and store all that in the variable lastLineValuesCount:
 	# Because AGAIN windows line endings created by one ported gnu utility are conflicting with Unix line endings created by another ported gnu utility:
 	dos2unix ppmBody.txt
 	lastLineString=`tail -1 ppmBody.txt`
@@ -171,14 +141,15 @@ else
 	lastLineTilesCount=$(( $lastLineValuesCount / 3))
 			# echo lastLineTilesCount is\: $lastLineTilesCount
 	#  - Which gives is our value to subtract from tilesAcross as described above:
-	tilesToAdd=$(( $tilesAcross - $lastLineTilesCount ))
+	tilesToAdd=$(( $tilesAcross * $tilesDown - $numColors ))
 			# echo tilesToAdd is\: $tilesToAdd
 	valuesToAdd=$(( $tilesToAdd * 3))
 			# echo valuesToAdd is\: $valuesToAdd
 	#  - Alas this can't be done as elegantly as printf "ha "%.0s {1..5} with a variable in the range, re https://stackoverflow.com/questions/19432753/brace-expansion-with-variable :
 	for i in $(seq 1 $valuesToAdd)
 	do
-		lastLineValuesPadString="$lastLineValuesPadString 127"
+		# 145, not 127, is neutral gray (as far as sRGB range is concerned), according to color-sciency analysis:
+		lastLineValuesPadString="$lastLineValuesPadString 145"
 	done
 	lastLineValuesPadString="$lastLineString $lastLineValuesPadString"
 	# remove last line from ppmBody.txt, then append the replacement line to it:
