c8ca180ac2c2b1eda99a4a17df4e4fb05ec54775
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2022-08-01 05:27:16 -0600

    Fix straggling documentation (was sentence fragment), add examples re new option, add to do

--- scripts/fileNamesAndTimestamps/renumberFiles.sh
+++ scripts/fileNamesAndTimestamps/renumberFiles.sh
@@ -1,14 +1,25 @@
 # DESCRIPTION
-# Renames all files of a given extension (via parameter) in the path from which this script is called--renames them to zero-padded numbers matching the number of digit columns of the count of all said files. WARNINGS: 1) use this only in directories where you actually want _all_ files of the given extension renamed by numbers. 2) If any of your file names are numeric-only (e.g. 005.png) *before* you run this script against them, files may disappear via overwrite, effectively erasing that file by replacing it with new content. For example, a file named 005.png may be overwritten when a file named someOtherFile.png is renamed to 005.png, overwriting the original file named 005.png.
+# Renames all files of a given extension (via parameter) in the path from which this script is called--renames them to zero-padded numbers matching the number of digit columns of the count of all said files.
+
+# WARNINGS
+# - Use this only in directories where you actually want _all_ files of the given extension renamed by numbers.
+# - If any of your file names are numeric-only (e.g. 005.png) *before* you run this script against them, files may disappear via overwrite, effectively erasing that file by replacing it with new content. For example, a file named 005.png may be overwritten when a file named someOtherFile.png is renamed to 005.png, overwriting the original file named 005.png.
 
 # USAGE
 # Run with these parameters:
 # - $1 the file extension you wish for it to operate on, for example png
-# - $2 OPTIONAL. Anything, such as the word FLUBNOR, which will cause the script to 
+# - $2 OPTIONAL. Anything, such as the word FLUBNOR, which will cause the script to recurse into all subdirectories of the current directory and renumber files in every directory. Meaning, it repeats the operation in every directory. So for example `dir01` would end up with files inside it renamed `001.png`, `002.png`, `003.png`, and `dir02` would also end up with files named `001.png`, `002.png`, `003.png`, etc.
+# Example that will renumber all png format files in the current directory:
 #    renumberFiles.sh png
+# Example that will renumber all png format files in the current directory and all subdirectories (as explained for parameter $2) :
+#    renumberFiles.sh png FLUBNOR
 # NOTE: this will choke on file names with console-unfriendly characters e.g. spaces, parenthesis and probably others.
 
+
 # CODE
+# TO DO
+# - Mitigate what is warned about (in the WARNINGS section) of overwrite clobbers. Or error out or skip if it would happen in a given directory?
+
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (file extension (type) to renumber) passed to script. Exit."; exit 1; else fileTypeToRenumber=$1; fi
 
 if [ "$2" ]
