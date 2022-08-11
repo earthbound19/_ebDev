1ee9440d26893beed47a8d66ce4eb7cbf0ebd96a
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2022-08-11 12:30:34 -0600

    - improve delete feedback print with display of count order deleted file - update logic to make that correct (it still deleted correctly before that) - clarify documentation

--- scripts/fileNamesAndTimestamps/decimateFileType.sh
+++ scripts/fileNamesAndTimestamps/decimateFileType.sh
@@ -1,16 +1,21 @@
 # DESCRIPTION
-# Counts all files of type $1 in the current directory, then deletes every one that is not a multiple of $2 minus one.
+# Counts all files of type $1 in the current directory, then deletes all files counted between the 1st file and all multiples of $2. See NOTES for a more detailed explanation.
 
 # USAGE
 # With this script in your PATH, run it with these parameters:
 # - $1 the file type to search for (in the current directory only).
-# - $2 the multiple of those files to _keep_ when counting and deleting them. All files of type $1 which are _not_ multiples of $2 will be deleted. (Actually $2 minus 1; see DESCRIPTION.)
-# Example which will delete every png image in the current directory that is not a multiple of 3:
+# - $2 the multiple of those files to _keep_ when counting and deleting them.
+# Example which will delete every png image in the current directory which, by counted order, is not 1 or a multiple of 3:
 #    decimateFileType.sh png 3
 # NOTES
 # - You may wish, after running this script, to run renumberFiles.sh (see the USAGE comment in that script) to make e.g. animation frame numbers in files contiguous again.
 # - The script sorts listed files by create date before deleting, so that e.g. animation frames will stay in order.
 # - The formula for listed file numbers to delete is $2 minus one because this preserves the first frame.
+# - Here is a way of detailing the file delete pattern:
+#    1. file number 1: KEEP
+#    2. all files counted between that and the next multiple of $2: DELETE
+#    3. file counted on multiple of $2: KEEP
+#    4. repeat steps 2 and 3 until there are no more files to delete
 
 
 # CODE
@@ -25,15 +30,14 @@ if [[ "$florf" != "YORFEL" ]]; then echo "No input match. Exit."; exit; else ech
 # Sorts by time stamp:
 array=( $(find . -maxdepth 1 -name "*.$fileTypeToDelete" -print0 -printf "%T@ %Tc %p\n" | sort -n | sed 's/.* [0-9]\{3,\} \.\/\(.*\)/\1/g') )
 
-counter=-1
+counter=0
 for element in ${array[@]}
 do
-	counter=$((counter + 1))
 	result=$((counter % $divisor))
+	counter=$((counter + 1))
 	if [[ ! result -eq 0 ]]
 	then
-		echo "DELETING $element . . ."
+		echo "DELETING $element at count $counter . . ."
 		rm $element
-		echo " . . . DELETED!"
 	fi
 done
