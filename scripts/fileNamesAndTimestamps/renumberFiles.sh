77e822715e7ee1ee692e06a1328ed264c3c69c70
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2022-08-17 15:59:20 -0600

    - CHANGE default sort (before renumbering) to what is default for `find` command - Add parameter $3 for optional custom sort of files by oldest file first before renumbering. This was the default but I no longer want that default, but may still want this option. - Add option to parameter $2 to use $3 but not $2 via NULL keyword

--- scripts/fileNamesAndTimestamps/renumberFiles.sh
+++ scripts/fileNamesAndTimestamps/renumberFiles.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# Renames all files of a given extension (via parameter) in the path from which this script is called--renames them to zero-padded numbers matching the number of digit columns of the count of all said files.
+# Renames all files of a given extension (via parameter) in the path from which this script is called--renames them to zero-padded numbers matching the number of digit columns of the count of all said files. By default sorts files by `find` command's default option before renumbering (which it seems does well for maintaining the sort order of numbered files). Can be overriden to sort by oldest file first before renumbering, so that for example the oldest file would be numbered 01, the next-oldest would be 02, the next-oldest 03, and so on.
 
 # WARNINGS
 # - Use this only in directories where you actually want _all_ files of the given extension renamed by numbers.
@@ -8,11 +8,16 @@
 # USAGE
 # Run with these parameters:
 # - $1 the file extension you wish for it to operate on, for example png
-# - $2 OPTIONAL. Anything, such as the word FLUBNOR, which will cause the script to recurse into all subdirectories of the current directory and renumber files in every directory. Meaning, it repeats the operation in every directory. So for example `dir01` would end up with files inside it renamed `001.png`, `002.png`, `003.png`, and `dir02` would also end up with files named `001.png`, `002.png`, `003.png`, etc.
+# - $2 OPTIONAL. Anything, such as the word FLUBNOR, which will cause the script to recurse into all subdirectories of the current directory and renumber files in every directory. Meaning, it repeats the operation in every directory. So for example `dir01` would end up with files inside it renamed `001.png`, `002.png`, `003.png`, and `dir02` would also end up with files named `001.png`, `002.png`, `003.png`, etc. If you wish to use $3 (see next) but not this, pass the word NULL for this.
+# - $3 OPTIONAL. Anything, such as the word WHELF, which will cause sort by oldest file first before renumbering. If omitted, uses the `find` command's default sort (which seems to do well for maintaining the ordering of numbered files in renumbering).
 # Example that will renumber all png format files in the current directory:
 #    renumberFiles.sh png
 # Example that will renumber all png format files in the current directory and all subdirectories (as explained for parameter $2) :
 #    renumberFiles.sh png FLUBNOR
+# Example that will renumber all png format files in the current directory and all subdirectories, but override default sort (before rename) of oldest file first to number-oriented sort of `find` command:
+#    renumberFiles.sh png FLUBNOR WHELF
+# Example that will do the same thing but not over subfolders:
+#    renumberFiles.sh png NULL WHELF
 # NOTE: this will choke on file names with console-unfriendly characters e.g. spaces, parenthesis and probably others.
 
 
@@ -22,7 +27,7 @@
 
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (file extension (type) to renumber) passed to script. Exit."; exit 1; else fileTypeToRenumber=$1; fi
 
-if [ "$2" ]
+if [ "$2" ] && [ "$2" != "NULL" ]
 then
 	# if $2 was passed to script, put folder names of all subdirectories into an array, and remove the first element ('.', or this folder) :
 	directories=($(find -type d))
@@ -40,10 +45,17 @@ do
 	cd $directory
 	echo in directory $directory . . .
 	# Create array to use to loop over files.
-	# previous version of command; doesn't sort by file date:
-	# filesArray=`find . -maxdepth 1 -iname "*.$fileTypeToRenumber" | sort`
-	# new command; sorts by file date (oldest first); re: https://superuser.com/a/546900/130772
-	filesArray=( $(find . -maxdepth 1 -type f -iname "*.$fileTypeToRenumber" -printf "%T@ %Tc %p\n" | sort -n | sed 's/.* \.\///g') )
+	# if $3 was passed, do custom sort, otherwise use `find` command's custom sort:
+	if [ "$3" ]
+	then
+		# previous version of command; doesn't sort by file date:
+		# filesArray=`find . -maxdepth 1 -iname "*.$fileTypeToRenumber" | sort`
+		# new command; sorts by file date (oldest first); re: https://superuser.com/a/546900/130772
+		filesArray=( $(find . -maxdepth 1 -type f -iname "*.$fileTypeToRenumber" -printf "%T@ %Tc %p\n" | sort -n | sed 's/.* \.\///g') )
+	else
+		filesArray=( $(find . -maxdepth 1 -type f -iname "*.$fileTypeToRenumber" -printf "%P\n") )
+	fi
+	
 	# Get digits to pad to from length of array.
 	digitsToPadTo=${#filesArray[@]}; digitsToPadTo=${#digitsToPadTo}
 
