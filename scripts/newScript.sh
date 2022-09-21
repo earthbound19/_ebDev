8cfc20a80603a93dfd7c037b471750d536de4e88
Alex Hall <earthbound19@users.noreply.github.com>
2022-09-21 08:55:09 -0600

    proper escaping of intended backslash

--- scripts/newScript.sh
+++ scripts/newScript.sh
@@ -13,6 +13,6 @@
 # CODE
 rndString=$(cat /dev/urandom | tr -dc 'a-f0-9' | head -c 9)
 
-printf "# DESCRIPTION\n# omigoshomigoshomigoshomigoshomigoshomigoshomigoshomigosh\n\n# USAGE\n# Run with these parameters:\n# - \$1 (describe parameter)\n# For example:\n#    scriptFileName.sh parameterOne\n\n\n# CODE\nif [ \"\$1\" ]; then param1=\$1; else printf \"\\\\nNo parameter \$1 (type short explanation of parameter) passed to script. Exit.\"; exit 1; fi" > "$rndString".sh.txt
+printf "# DESCRIPTION\n# omigoshomigoshomigoshomigoshomigoshomigoshomigoshomigosh\n\n# USAGE\n# Run with these parameters:\n# - \$1 (describe parameter)\n# For example:\n#    scriptFileName.sh parameterOne\n\n\n# CODE\nif [ \"\$1\" ]; then param1=\$1; else printf \"\\\\nNo parameter \\\$1 (type short explanation of parameter) passed to script. Exit.\"; exit 1; fi" > "$rndString".sh.txt
 
 start "$rndString".sh.txt
