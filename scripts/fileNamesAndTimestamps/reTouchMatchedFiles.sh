82f6d19780d1440b3fd3b1cef785f613ec3d39d5
Alex Hall <>
2022-09-24 12:22:58 -0600

    new: solves a problem of file system views that sort by date not displaying related files of the same base file name near each other (by aligning timestamps)

---
+++ scripts/fileNamesAndTimestamps/reTouchMatchedFiles.sh
@@ -0,0 +1,35 @@
+# DESCRIPTION
+# Solves a problem of file system views that sort by date not displaying related files of the same base file name near each other. For every file of type $1 in the current directory, finds all files with the same base file name and updates thier creation/modification date to match that corresponding file $1.
+
+# USAGE
+# Run with these parameters:
+# - $1 source file type (extension) to find and update timestamps of all corresponding files with the same base file name for any given file of this extension.
+# For example, if you have source art files like this:
+    # woodcut_inspired_by_choral_00c8c538.png
+    # woodcut_inspired_by_choral_00c8c538.txt
+    # woodcut_inspired_by_choral_013973d3.png
+    # woodcut_inspired_by_choral_013973d3.txt
+# -- and you want to follow this process for them, run:
+#    reTouchMatchedFiles.sh png
+# -- and the script will update e.g. `woodcut_inspired_by_choral_00c8c538.txt` with the same time stamp as `woodcut_inspired_by_choral_00c8c538.png`, and `woodcut_inspired_by_choral_013973d3.txt` with the same time stamp as `woodcut_inspired_by_choral_013973d3.png`, and so on, for every png/txt file pair where they have the same file name.
+# This process will also work where files of multiple extensions have the same base file name as (for example as given here) .png.
+
+
+# CODE
+if [ "$1" ]; then sourceExtension=$1; else printf "\nNo parameter \$1 (type short explanation of parameter) passed to script. Exit."; exit 1; fi
+
+# I tried this more elegant solution; re: https://stackoverflow.com/a/28574445
+# for f in *.$sourceExtension; do touch -r "$f" "${f%.a}".*; done
+# -- but it produced mangled file names newly created by touch; I haven't got that working. Meanwhile the below works:
+
+for i in *.sourceExtension
+do
+	echo working on $i
+	touch $i
+	files=($(listMatchedFileNames.sh $i))
+	for j in ${files[@]}
+	do
+		echo working on related file $j . . .
+		touch $j -r $i
+	done
+done
