87be345373caf787eda4d82638704ddcf018e209
Alex Hall <earthbound19@users.noreply.github.com>
2022-10-01 22:20:50 -0600

    - remove redundant sampler method print from output - format in colon-separated csv for potential file sorting if called from another script

--- scripts/imgAndVideo/stableDiffusionRenderGetSamplerMethodFromMetadata.sh
+++ scripts/imgAndVideo/stableDiffusionRenderGetSamplerMethodFromMetadata.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# Extracts sampler method from metadata of Stable Diffusion renders, from all file types $1 in the current directory, and prints them with the file name. May only work from a custom metadata setup I have.
+# Extracts sampler method from metadata of Stable Diffusion renders, from all file types $1 in the current directory, and prints them with the file name in csv format "Sampler method:filename". May only work from a custom metadata setup I have.
 
 # USAGE
 # Run with these parameters:
@@ -14,7 +14,7 @@ if [ "$1" ]; then srcFileType=$1; else printf "\nNo parameter \$1 (file type to 
 srcFiles=($(find . -maxdepth 1 -iname \*.$srcFileType -printf "%P\n"))
 for srcFile in ${srcFiles[@]}
 do
-	sampleMethodFromMetadata=$(exiftool $srcFile | sed -n 's/.*Sampler: \([^.]\{0,\}\)\..*/\1/p')
-	printf "SOURCE FILE: $srcFile -- METHOD: $sampleMethodFromMetadata\n"
-	printf "\n"
+	sampleMethodFromMetadata=($(exiftool $srcFile | sed -n 's/.*Sampler: \([^.]\{0,\}\)\..*/\1/p'))
+	# because that can be an array (because there can be more than one line/field of metadata giving the Sampler method), print only the first one via array subscripting; it will be the same as the second if there is more than one; although just "printing" the array might also always only print the first element:
+	printf "${sampleMethodFromMetadata[0]}:$srcFile\n"
 done
