92450331256a8bf22bdf56287f0ebba28d03636b
Alex Hall <earthbound19@users.noreply.github.com>
2022-10-06 08:32:23 -0600

    add comments including reference a similar script

--- scripts/imgAndVideo/stableDiffusionRenderGetSamplerMethodFromMetadata.sh
+++ scripts/imgAndVideo/stableDiffusionRenderGetSamplerMethodFromMetadata.sh
@@ -1,11 +1,13 @@
 # DESCRIPTION
-# Extracts sampler method from metadata of Stable Diffusion renders, from all file types $1 in the current directory, and prints them with the file name in csv format "Sampler method:filename". May only work from a custom metadata setup I have.
+# Extracts sampler method from metadata of Stable Diffusion renders, from all file types $1 in the current directory, and prints them with the file name in csv format "Sampler method:filename". May only work from a custom metadata setup I have. To get seeds from metadata, see stableDiffusionRenderGetSeedsFromMetadata.sh.
 
 # USAGE
 # Run with these parameters:
 # - $1 file type to scan all files in the current directory for render method metadata
 # For example, if the metadata is stored in .jpg images, run:
 #    stableDiffusionRenderGetSamplerMethodFromMetadata.sh jpg
+# NOTE: if you're not concerned with printing file names, you can far more quickly print all sampler methods wit this command, assuming you're searching through png files metadata:
+#    exiftool "*.png" | sed -n 's/.*Sampler: \([^,]\{0,\}\)\,.*/\1/p'
 
 
 # CODE
