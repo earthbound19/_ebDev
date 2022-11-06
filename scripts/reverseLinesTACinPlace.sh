a6a6f18461cf2c7173c89fc6e86b1394448afdbc
Alex Hall <>
2022-11-06 16:38:08 -0700

    new: reverses the lines of all files of type $1 in the current directory, in place. Overwrites the original files.

---
+++ scripts/reverseLinesTACinPlace.sh
@@ -0,0 +1,20 @@
+# DESCRIPTION
+# Reverses the lines of all files of type $1 in the current directory, in place. Overwrites the original files. TAC in this script name as a keyword: it's after tac or cat or concatenate backward.
+
+# USAGE
+# Run with these parameters:
+# - $1 type of file to operate on
+# For example, to reverse the lines of all .hexplt format files in the current directory, run:
+#    reverseLinesTACinPlace.sh hexplt
+
+
+# CODE
+if [ "$1" ]; then srcFileType=$1; else printf "\nNo parameter \$1 (file type to operate on) passed to script. Exit."; exit 1; fi
+
+sourceFiles=($(find . -type f -iname \*.$srcFileType -printf "%P\n"))
+
+for fileName in ${sourceFiles[@]}
+do
+	contents=$(tac $fileName)
+	printf "$contents" > $fileName
+done
