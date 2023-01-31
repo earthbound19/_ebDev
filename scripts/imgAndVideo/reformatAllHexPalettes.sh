53216db68d010daffc6ed51359e811814d3a258e
Alex Hall <earthbound19@users.noreply.github.com>
2023-01-30 17:58:43 -0700

    refactor reformatHexPalette.sh to use getopt: - change input file $1 to -i - split $2 columns options into -c and -a switches - drop $3 rows option (just do math for the columns you want from N rows) that led to wonky too much logic - add -n --no-comment (no layout print) option rework reformatAllHexPalettes.sh to use changed/new parameters to reformatHexPalette.sh

--- scripts/imgAndVideo/reformatAllHexPalettes.sh
+++ scripts/imgAndVideo/reformatAllHexPalettes.sh
@@ -1,30 +1,34 @@
 # DESCRIPTION
-# runs `reformatHexPalette.sh` with the same parameters for every `.hexplt` file in the current directory. Uses same parameters as that script (SEE) but shifted. See USAGE.
+# runs `reformatHexPalette.sh` repeatedly with every .hexplt file in the current directory as -i, and optionally different paramters that script can use via #1. See USAGE.
 
 # DEPENDENCIES
 # `reformatHexPalette.sh` in your PATH.
 
 # USAGE
 # Call this script with these parameters:
-# - $1 OPTIONAL. Number of columns. If omitted, defaults to 1. May also be keyword 'ALL' to make columns equal to number of colors.
-# - $2 OPTIONAL. Number of rows. If omitted, the script this calls (`reformatHexPalette.sh) will use its defaults for its parameter $3 (see USAGE details for $3 for that script), with handling to fit rows/columns as also described in that script.
+# - $1 OPTIONAL. Any string of parameters/switches usable by `reformatHexPalette.sh` which you wish to pass to it; see documentation for that script. NOTE that if you use more than one paramters, you must surround them all with quotes. If omitted, the defaults of the called script will be used.
 # Example that passes 16 for the columns parameter:
-#    reformatAllHexPalettes.sh 16
-# Example that passes 16 for the columns parameter and 8 for the rows parameter:
-#    reformatAllHexPalettes.sh 16 8
+#    reformatAllHexPalettes.sh -c16
+# Example that passes 5 for the columns parameter, with the -n switch to print no columns and rows layout comment:
+#    reformatAllHexPalettes.sh '-c16 -n'
+# Example that uses the -a switch to make as many columns as colors:
+#    reformatAllHexPalettes.sh 16 '-a'
 # NOTES
-# This script provides $1 as a different file name (pulling from every `.hexplt` format file in the current directory), for every call it makes to `reformatHexPalette.sh`. What is provided as $1 for this script is passed as $2 for that script, and $2 is passed for $3.
+# This script provides a different file name (pulling from every `.hexplt` format file in the current directory) for every call it makes to `reformatHexPalette.sh`. For each call it provides a new file name for -i, as if it were many calls like this:
+#    reformatHexPalette.sh -i <palette1.hexplt> <any parameters from $1>
+#    reformatHexPalette.sh -i <palette2.hexplt> <any parameters from $1>
+#    reformatHexPalette.sh -i <palette3.hexplt> <any parameters from $1>
+#    ..
 
 
 # CODE
-if [ "$1" ]; then cols=$1; fi
-if [ "$2" ]; then rows=$2; fi
+if [ "$1" ]; then extraParameters=$1; fi
 
 allHexpltFileNames=( $(find . -maxdepth 1 -type f -iname "*.hexplt" -printf "%P\n") )
 
 for hexpltFileName in ${allHexpltFileNames[@]}
 do
-	reformatHexPalette.sh $hexpltFileName $cols $rows
+	reformatHexPalette.sh -i $hexpltFileName $extraParameters
 done
 
 printf "DONE reformatting all .hexplt format files in the current directory.\n"
--- scripts/imgAndVideo/reformatHexPalette.sh
+++ scripts/imgAndVideo/reformatHexPalette.sh
@@ -2,57 +2,69 @@
 # Reformats a .hexplt file (a list of sRGB colors in hex format) to remove all comments and arrange colors on an $1 column by $2 rows grid, then add back a comment that tells the grid dimension (appended to the first row).
 
 # USAGE
-# Run with these parameters:
-# - $1 source hexplt format file name.
-# - $2 OPTIONAL. Number of columns. If omitted, defaults to 1. If passed as the keyword 'ALL', the columns will equal the number of colors in the source hexplt.
-# - $3 OPTIONAL. Number of rows. If omitted, will be calculated as the number of rows that fit specified number of columns (including if the last row has empty remainder columns). If specified and the number of rows will not fit all colors, the script overrides what you specify to give enough rows to fit them.
-# For example, to reformat a .hexplt file with defaults, run:
-#    reformatHexPalette.sh hobby_art_0001-0003.hexplt
-# To reformat the same with 
+function print_halp {
+	echo "
+Options:
+    -i, --input-file <source hexplt format file name>
+    -c<integer or keyword>, --columns=<integer or keyword> Number of columns. OPTIONAL. If omitted, defaults to 1.
+    -a, --all-columns OPTIONAL flag to set number of columns to all colors. Overrides any value of -c, --columns.
+	-n, --no-comment OPTIONAL flag: No \"columns: <n> rows: <n>\" comment in reformatted file
+For example, to reformat a .hexplt file with defaults, run:
+    reformatHexPalette.sh -i hobby_art_0001-0003.hexplt
+"
+}
 
 
 # CODE
+# START PARAMETER PARSING AND GLOBALS SETUP
+if [ ${#@} == 0 ]; then print_halp; exit 1; fi
+PROGNAME=$(basename $0)
+OPTS=`getopt -o hi:c::an --long help,input-file:,columns::,all-columns,no-comment -n $PROGNAME -- "$@"`
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+eval set -- "$OPTS"
+# --- END PARSING STUFF ---
+
 # Global function takes number of specified columns and calculates number of rows to fit all colors: ASSUMES AND OPERATES ON GLOBAL VARIABLES:
 setRowsToFitColors() {
-	rows=$(($howManyColors / $cols))
-	# check if there's a remainder from the division (if cols * rows less than howManyColors); if so, add another row:
-	if [[ $(($cols * $rows)) < $howManyColors ]]
+	rows=$(($howManyColors / $columns))
+	# check if there's a remainder from the division (if columns * rows less than howManyColors); if so, add another row:
+	if [[ $(($columns * $rows)) < $howManyColors ]]
 	then
-		# echo adding an extra row to fit all colors into cols * row print.
+		# echo adding an extra row to fit all colors into columns * row print.
 		rows=$(($rows + 1))
 	fi
 }
 
-# START PARAMETER PARSING AND GLOBALS SETUP
-if [ ! "$1" ]; then printf "\nNo parameter \$1 (source .hexplt format file name) passed to script. Exit."; exit 1; else srcHexplt=$1; fi
-# check that source hexplt exists; exit with error if it does not.
-if [ ! -f $srcHexplt ]
-then
-	echo "ERROR: source file $srcHexplt not found in this directory. Exit."
-	exit 1
-else
-	echo Loading source .hexplt file . . .
-	# get array of colors from file by extracting all matches of a pattern of six hex digits preceded by a #:
-	colorsArray=( $(grep -i -o '#[0-9a-f]\{6\}' $srcHexplt) )
-	# Get number of colors (from array):
-	howManyColors=${#colorsArray[@]}
-	if [ ! "$2" ]; then cols=1; else cols=$2; fi
-	if [ "$2" == "ALL" ]; then cols=$howManyColors; fi
-	if [ ! "$3" ]
-	then
-		# Function call:
-		setRowsToFitColors
-	else
-		rows=$3
-		# Function call:
-		setRowsToFitColors
-	fi
-fi
+# SET ANY DEFAULTS that would be overriden by optional arguments here:
+columns=1
+while true; do
+  case "$1" in
+    -h | --help ) print_halp; exit 0 ;;
+    -i | --inputfile ) srcHexplt=$2; if [ ! -f $srcHexplt ]; then echo "WARNING: source file $srcHexplt not found. Specify an existing file. Exit."; exit 1; fi; shift; shift ;;
+    -c | --columns ) if [ "$2" == "" ]; then echo "WARNING: No value or a space (resulting in empty value) after optional parameter -c --columns. Pass a value without any space after -c (for example: -c5), or else don't pass -c and a default value will be used for it. Exit."; exit 2; fi; columns=$2; shift; shift ;;
+	-a | --all-columns ) columnForEveryColor=true; shift; ;;
+	-n | --no-comment ) noLayoutComment=true; shift; ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+echo Loading source .hexplt file $srcHexplt . . .
+# get array of colors from file by extracting all matches of a pattern of six hex digits preceded by a #:
+colorsArray=( $(grep -i -o '#[0-9a-f]\{6\}' $srcHexplt) )
+# Get number of colors (from array):
+howManyColors=${#colorsArray[@]}
+
+# WHOPAS; you have to create a new flag for this case; do -a --allcolumns:
+if [ "$columnForEveryColor" ]; then columns=$howManyColors; fi
+
+# Function call:
+setRowsToFitColors
 # END PARAMETER PARSING AND GLOBALS SETUP
 
 # MAIN WORK
 echo Reformatting palette in memory . . .
-
+echo noLayoutComment $noLayoutComment
 colorPrintCounter=0
 rowsArray=()
 OIFS="$IFS"
@@ -60,15 +72,17 @@ IFS=$'\n'
 for r in $(seq 1 $rows)
 do
 	rowSTR=""
-	for q in $(seq 1 $cols)
+	for q in $(seq 1 $columns)
 	do
 		rowSTR="$rowSTR ${colorsArray[$colorPrintCounter]}"
 		colorPrintCounter=$((colorPrintCounter + 1))
 	done
 	
-	if [[ $colorPrintCounter == $cols ]]
+	# if we are at number of columns for color count; we are at the end of the first row;
+	# if that is the case and there is no flag to not write a columns and rows layout comment, write one:
+	if [[ $colorPrintCounter == $columns ]] && [[ ! "$noLayoutComment" ]]
 	then
-		rowSTR="$rowSTR  columns: $cols rows: $rows"
+		rowSTR="$rowSTR  columns: $columns rows: $rows"
 	fi
 	# trim resultant leading space off string:
 	rowSTR="${rowSTR:1}"
