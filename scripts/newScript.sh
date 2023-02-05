34ebc3329115c79b98afd7ab3f78084474eca00b
Alex Hall <earthbound19@users.noreply.github.com>
2023-02-04 22:34:47 -0700

    move intended notes comments into printf that creates new script

--- scripts/newScript.sh
+++ scripts/newScript.sh
@@ -11,14 +11,8 @@
 
 
 # CODE
-# NOTES: if you want to get all command line parameters to a script as a single string;
-# how to get command line arguments . . . collection? Array? Re: https://stackoverflow.com/a/12711837/1397555
-# -- and print array re: https://stackoverflow.com/a/15692004/1397555
-# -- you can put those together to get the following:
-# scriptArgumentsAsString=$(printf '%s ' "$@")
-
 rndString=$(cat /dev/urandom | tr -dc 'a-f0-9' | head -c 9)
 
-printf "# DESCRIPTION\n# omigoshomigoshomigoshomigoshomigoshomigoshomigoshomigosh\n\n# USAGE\n# Run with these parameters:\n# - \$1 (describe parameter)\n# For example:\n#    scriptFileName.sh parameterOne\n\n\n# CODE\nif [ \"\$1\" ]; then param1=\$1; else printf \"\\\\nNo parameter \\\$1 (type short explanation of parameter) passed to script. Exit.\"; exit 1; fi" > "$rndString".sh.txt
+printf "# DESCRIPTION\n# omigoshomigoshomigoshomigoshomigoshomigoshomigoshomigosh\n\n# USAGE\n# Run with these parameters:\n# - \$1 (describe parameter)\n# For example:\n#    scriptFileName.sh parameterOne\n\n\n# CODE\n# NOTES: if you want to get all command line parameters to a script as a single string;\n# how to get command line arguments . . . collection? Array? Re: https://stackoverflow.com/a/12711837/1397555\n# -- and print array re: https://stackoverflow.com/a/15692004/1397555\n# -- you can put those together to get the following:\n# scriptArgumentsAsString=$(printf '%s ' "$@")\n\nif [ \"\$1\" ]; then param1=\$1; else printf \"\\\\nNo parameter \\\$1 (type short explanation of parameter) passed to script. Exit.\"; exit 1; fi" > "$rndString".sh.txt
 
 start "$rndString".sh.txt
