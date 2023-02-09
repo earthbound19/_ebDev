09e28dc3061d841d4f390767b6350347cfe6c2b3
Alex Hall <earthbound19@users.noreply.github.com>
2023-02-08 23:35:19 -0700

    - only concatenate palettes if any were found in the current directory; print note and exit otherwise - complete accidentally trailed off sentence in doc

--- scripts/imgAndVideo/catHexpltsGrid.sh
+++ scripts/imgAndVideo/catHexpltsGrid.sh
@@ -1,5 +1,5 @@
 # DESCRIPTION
-# concatenates all .hexplt files in the current directory into one hexplt named after the directory, and reformats it to include only one comment if any. May be useful after a run of 
+# concatenates all .hexplt files in the current directory into one hexplt named after the directory, and reformats it to include only one comment if any. May be useful after a run of `paletteRenamedCopiesByNextMostSimilar.sh`, as the files created by that and concatenated to a new palette by this will give a palette that visually lays out next most similar colors (from the palettes it was made of).
 
 # DEPENDENCIES
 # `reformatHexPalette.sh`
@@ -22,10 +22,16 @@ outputFileName="$currentDirNoPath"_palettesGrid.hexplt
 # warn and exit if output file already exists, which could result in redundant concatenation:
 if [ -f $outputFileName ]; then echo "WARNING: would-be result file name $outputFileName already exists. To re-create it delete it and run this script again. Exit."; exit 1; fi
 
-# concatenate palettes to output file:
-cat *.hexplt > $outputFileName
-
-# reformat it if $columns variable was set (by passing $1) :
-reformatHexPalette.sh -i $outputFileName $columnsParameter
-
-echo "DONE. Resulting palette file is $outputFileName."
+# concatenate palettes to array and check if it is length 0 (no palettes found); only continue if that is not the case; if it is the case print an info log and exit:
+paletteFileNames=( $(find . -maxdepth 1 -iname "*.hexplt" -printf "%P\n") )
+if [ ${#paletteFileNames[@]} == 0 ]
+then
+	thisDir=$(pwd)
+	echo "NOTE: No .hexplt files found in directory $thisDir. Exit."; exit 2;
+else
+	foof=blor
+	cat *.hexplt > $outputFileName
+	# reformat it if $columns variable was set (by passing $1) :
+	reformatHexPalette.sh -i $outputFileName $columnsParameter
+	echo "DONE. Resulting palette file is $outputFileName."
+fi
