7d629d6adc34ef1c2153afe091598bcb2100bb81
Alex Hall <earthbound19@users.noreply.github.com>
2023-02-14 19:09:11 -0700

    new art toy script -- uses other scripts to print colored block character etc. art

---
+++ scripts/recipes/randomNsetCharsAltTerminalRNDretrievedPalette_mintty.sh
@@ -0,0 +1,30 @@
+# DESCRIPTION
+# I know, the script name is ridiculously long. To print random two-color block etc. character art. Runs `randomNsetCharsAlt.sh` repeatedly, interspersed with calls to `printContentsOfRandomlyChosenPalette.sh`, to print the contents of a randomly chosen palette), and custom code, to get two random colors from the randomly retrieved palette, and set the mintty terminal foreground and background color to those two random colors.
+
+# DEPENDENCIES
+# `printRandomPaletteContents.sh` in your PATH. Probably the mintty terminal; maybe compatible things would work.
+
+# USAGE
+# Run without any parameters:
+#    randomNsetCharsAltTerminalRNDretrievedPalette_mintty.sh
+
+# infinite loop
+while :
+do
+	# get three random palette colors (shuffling result as we get it) :
+	randomColors=( $(printContentsOfRandomlyChosenPalette.sh | shuf) )
+	# only do anything if the retrieved palette has at least 2 colors; otherwise skip this loop iteration:
+	arrayLength=${#randomColors[@]}
+	if [ $arrayLength -ge 2 ]
+	then
+		# 1st color of that shuffle will be background, 2nd will be foreground and cursor; only use them if there are at least 2 colors in the palette though:
+		BGcolor=${randomColors[0]}
+		FGcolor=${randomColors[1]}
+		# cursorColor=${randomColors[2]}
+		# set colors from those; 11 is bg, 10 is fg, 12 is cursor:
+		echo -ne "\e]11;$BGcolor\a"
+		echo -ne "\e]10;$FGcolor\a"
+		echo -ne "\e]12;$FGcolor\a"
+		randomNsetCharsAlt.sh 8 CHALPUR 1
+	fi
+done
