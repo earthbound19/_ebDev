629336214910040417354a5c681f81c07b1f1ff3
Alex Hall <earthbound19@users.noreply.github.com>
2023-03-17 20:05:25 -0600

    fix incorrect printf escape sequences, change target script name format, print new script name when done

--- scripts/newScript.sh
+++ scripts/newScript.sh
@@ -12,7 +12,9 @@
 
 # CODE
 rndString=$(cat /dev/urandom | tr -dc 'a-f0-9' | head -c 9)
+newScriptFileName="$rndString".sh
 
-printf '# DESCRIPTION\n# omigoshomigoshomigoshomigoshomigoshomigoshomigoshomigosh\n\n# USAGE\n# Run with these parameters:\n# - $1 (describe parameter)\n# For example:\n# scriptFileName.sh parameterOne\n\n\n# CODE\n# NOTES: if you want to get all command line parameters to a script as a single string, re: https://stackoverflow.com/a/12711837/1397555\n#    scriptArgumentsAsString=$@\n# -- although probably you can just use $@ directly without assigning it to any other variable.\n\nif [ \"\$1\" ]; then param1=\$1; else printf \"\\\\nNo parameter \\\$1 (type short explanation of parameter) passed to script. Exit.\"; exit 1; fi' > "$rndString".sh.txt
+printf '# DESCRIPTION\n# omigoshomigoshomigoshomigoshomigoshomigoshomigoshomigosh\n\n# USAGE\n# Run with these parameters:\n# - $1 (describe parameter)\n# For example:\n# scriptFileName.sh parameterOne\n\n\n# CODE\n# NOTES: if you want to get all command line parameters to a script as a single string, re: https://stackoverflow.com/a/12711837/1397555\n#    scriptArgumentsAsString=$@\n# -- although probably you can just use $@ directly without assigning it to any other variable.\n\nif [ \"$1\" ]; then param1=$1; else printf \"\\nNo parameter \$1 (type short explanation of parameter) passed to script. Exit.\"; exit 1; fi' > $newScriptFileName
 
-start "$rndString".sh.txt
+start $newScriptFileName
+printf "\nNew script template file name:\n$newScriptFileName\n"
