604dc98bd87abf05f7bfc84d35640c484f8aaa8e
Alex Hall <earthbound19@users.noreply.github.com>
2023-04-20 22:39:11 -0600

    calls `oklabGradientFromFirstAndLastHexpaletteColor.sh` for every .hexplt format file in the current directory, overwriting the source files with the color gradient result

---
+++ scripts/imgAndVideo/oklabGradientFromFirstAndLastHexpaletteColors_allPalettes.sh
@@ -0,0 +1,60 @@
+# DESCRIPTION
+# Repeatedly calls `oklabGradientFromFirstAndLastHexpaletteColor.sh`, doing this:
+# - for every .hexplt format file (hex palette) in the current directory:
+# - retrieve the first and last color in the palette
+# - interpolate N ($1) colors (to create a gradient) from the first to last color through oklab space
+# - write that result over the original palette file
+
+# WARNING
+# DESTRUCTIVE. Overwrites the original files, very probably permanently modifying their contents. (The only way it wouldn't be a change is if the operation happens to create the same result as the original file.) Prompts for a given password to verify the operation, or takes the password as a parameter to overwrite files without warning.
+
+# USAGE
+# To do the operation in DESCRIPTION for every .hexplt format file in the current directory, run with these parameters:
+# - $1 REQUIRED. The number of colors in the intended gradient of each modified .hexplt file
+# - $2 OPTIONAL. The word SYMBCOZ, which will cause the script to perform operations without warning. If omitted, you are prompted to type this password to continue.
+# For example, to run the script and be prompted for this password, run it without any parameter:
+#    oklabGradientFromFirstAndLastHexpaletteColors_allPalettes.sh
+# To run the script and perform the changes on all files as described without warning, run the script with that password as the first parameter:
+#    oklabGradientFromFirstAndLastHexpaletteColors_allPalettes.sh SYMBCOZ
+
+# CODE
+if [ "$1" ]; then gradientColorsN=$1; else printf "\nNo parameter \$1 (the number of colors in the intended gradient of each modified .hexplt file) passed to script. Exit."; exit 1; fi
+
+# if $2 passed and equals SYMBCOZ, bypass check. Otherwise do check.
+if [ "$1" != "SYMBCOZ" ]
+then
+	echo ''
+	echo 'WARNING: this script will overwrite every .hexplt format file'
+	echo 'in the current directory with changes as given in the'
+	echo '"DESCRIPTION" comment in the source code (SEE).'
+	echo 'If this is what you want to do, type:'
+	echo ''
+	echo 'SYMBCOZ'
+	echo ''
+	echo '-- and then press ENTER.'
+	echo "If that is NOT what you want to do, press CTRL+z or CTRL+c,"
+	echo " or type something else and press ENTER, to terminate this"
+	echo "script."
+
+	read -p "TYPE HERE: " USERINPUT
+
+	if [ $USERINPUT != "SYMBCOZ" ]
+	then
+		echo "User input does not equal SYMBCOZ. Script will exit without doing anything."
+		exit 1
+	else
+		echo "User input equals pass string. Will proceed."
+	fi
+fi
+
+# get array of all .hexplt format files in the current directory:
+filesArray=( $(find . -maxdepth 1 -type f -iname "*.hexplt" -printf "%P\n") )
+
+for file in ${filesArray[@]}
+do
+	echo "interpolating $gradientColorsN colors from first to last in file $file and overwriting it . . ."
+	# make an array of the interpolation result, because it failed if I tried to pipe that directly back to the file:
+	newGradient=( $(oklabGradientFromFirstAndLastHexpaletteColors.sh $file $gradientColorsN) )
+	# print that array result (one element per line) back to the file:
+	printf '%s\n' "${newGradient[@]}" > $file
+done
