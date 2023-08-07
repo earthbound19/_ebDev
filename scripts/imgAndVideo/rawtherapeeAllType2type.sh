24a4a8603d5d629916b76f9909205e5360a8fd45
Alex Hall <earthbound19@users.noreply.github.com>
2023-08-07 07:27:34 -0600

    move $3 to $4 and add option to specify processing (pp3) file as $3 per change of script this calls

--- scripts/imgAndVideo/rawtherapeeAllType2type.sh
+++ scripts/imgAndVideo/rawtherapeeAllType2type.sh
@@ -1,11 +1,12 @@
 # DESCRIPTION
-# Calls rawtherapee2type.sh for every file of type $1 (should be a camera raw format file) in the current directory, converting to either png or tif ($2) and optionally doing the same through all subdirectories (anything for $3).
+# Calls `rawtherapee2type.sh` for every file of type $1 (should be a camera raw format file) in the current directory, converting to either png or tif ($2) and optionally doing the same through all subdirectories (anything for $3).
 
 # USAGE
 # Run with these parameters:
 # - $1 extension of source files to convert, for example cr2
 # - $2 OPTIONAL. Format (and therefore extension) to convert to. Options are png or tif. If omitted defaults to png.
-# - $3 OPTIONAL. Anything, for example the word FROBYARF, which will cause search and conversion to be done in all files of type $1 in all subdirectories under the current directory.
+# - $3 OPTIONAL. Adjustments parameter file (pp3 format) to use for processing. See parameter $3 documentation in `rawtherapee2type.sh`. If you want to use $4 (read on) but not $3, pass the word NULL for $3.
+# - $4 OPTIONAL. Anything, for example the word FROBYARF, which will cause search and conversion to be done in all files of type $1 in all subdirectories under the current directory.
 # Example that will convert all cr2 files in the current directory to tiff format files:
 #    rawtherapeeAllType2type.sh cr2
 # Example that will do the same for all cr2 files in the current directory and convert them to tif:
@@ -24,12 +25,14 @@ else
 	outputFormatParameter=png
 fi
 
+if [ "$3" ] && [ "$3" != "NULL" ]; then sidecarFileName=$3; fi
+
 # set default maxdepth parameter of this directory only; if parameter $2 passed, set it to '' (the default of any depth) :
 maxdepthParameter="-maxdepth 1"
-if [ "$3" ];then maxdepthParameter=''; fi
+if [ "$4" ];then maxdepthParameter=''; fi
 
 inputFiles=( $(find . $maxdepthParameter -type f -iname "*.$inputFileType" -printf "%P\n") )
 for inputFile in ${inputFiles[@]}
 do
-	rawtherapee2type.sh $inputFile $outputFormatParameter
+	rawtherapee2type.sh $inputFile $outputFormatParameter $sidecarFileName
 done
