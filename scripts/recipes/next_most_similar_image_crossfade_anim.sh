36a81cfe3a1829eb7261bfd2a30e4edafc34fffe
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2023-08-12 23:51:17 -0600

    - add new optional parameters for script this calls as parameters to this script (which it passes on) - improve doc

--- scripts/recipes/next_most_similar_image_crossfade_anim.sh
+++ scripts/recipes/next_most_similar_image_crossfade_anim.sh
@@ -2,10 +2,18 @@
 # Uses other scripts to create a video of crossfades through images sorted roughly by next most similar. Can be useful for exploring new abstract art ideas by crossfading similar (or very different!) images, or to explore visual ideas, or just for curiosity.
 
 # USAGE
-# Examine comments in the scripts this recipe uses to get an idea what goes on here. Run the following commands and scripts as listed in the source code, adapting for your situation. Refer to comments in code, but ffmpegCrossfadeIMGsToVideoFromFileList.sh has comments which are more complete.
+# Examine comments in the scripts this recipe uses to get an idea what goes on here. Run the following commands and scripts as listed in the source code, adapting for your situation. Refer to comments in code, but ffmpegCrossfadeIMGsToVideoFromFileList.sh has comments which are more complete. This script does pass on the folowing parameters though--because it's crazy, but I use this script from other scripts and it passes them to a script that passes them, so:
+# Run this script with these parameters:
+# - $1 OPTIONAL. Duration of crossfades between images, in decimal seconds, e.g. 2.5
+# - $2 OPTIONAL. Duration of still image pads between crossfades, in decimal seconds, e.g. 1
+# For example to set crossfades of 2.5 seconds and still images of 1 second, run:
+#    next_most_similar_image_crossfade_anim.sh 2.5 1
 
 
 # CODE
+if [ "$1" ]; then crossFadeDuration=$1; fi
+if [ "$2" ]; then padding=$2; fi
+
 pixelFormat="-pix_fmt yuv420p"
 	# All images you operate on must be pngs for this to work:
 	# To render pngs from all svgs in the current directory, run:
@@ -14,8 +22,7 @@ imgsGetSimilar.sh png
 	# After that step, if you want to insert an image to the very start of the process (e.g. to fade in from black and back to black at the end), name that image e.g. 000.png so that the following scripts will sort that first in the process:
 mkNumberedCopiesFromFileList.sh
 cd _temp_numbered
-# NOTE: you may want to adjust parameters in this next script call! :
-ffmpegCrossfadeIMGsToVideoFromFileList.sh
+ffmpegCrossfadeIMGsToVideoFromFileList.sh $crossFadeDuration $padding
 cd fadeSRCvideos
 source concatVideos.sh avi fadeSRCvideosList.txt
 convertedFileName=${concatenatedVideoFileName%.*}.mp4
