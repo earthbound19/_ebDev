3ecabd35684b46dcb740c2d475fd83c8e7d9cbec
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2023-09-04 14:40:53 -0600

    - greatly simplify parameter building by using ffprobe print format option - add option to specify any number of times to sample via $3, $4, $5..

--- scripts/imgAndVideo/ffmpegRipVideoFrameSS.sh
+++ scripts/imgAndVideo/ffmpegRipVideoFrameSS.sh
@@ -1,37 +1,41 @@
 # DESCRIPTION
-# Outputs a .jpg image frame from a given percent (time) of an input video file. Output image is _video_frame_.jpg at this writing (plan: name it after the input video).
+# Outputs a .png image frame from a given percent (time) of an input video file. Output image is named after the input video and time with a .png extension. Optionally outputs any number of additional frames from a list of percent times. See USAGE.
+
+# DEPENDENCIES
+# ffmpeg, ffprobe
 
 # USAGE
 # Run with these parameters:
-# - $1 an input video file name to grab a frame from, and
-# - $2 what percent time into the video duration to grab a frame from, expressed in decimal (and allowing high decimal precision e.g. .214 -- meaningfulness of that precision (if it is actually useful at all) may depend on the video length).
-# Example:
+# - $1 an input video file name to export a frame from, and
+# - $2 what percent time into the video duration to export a frame from, expressed in decimal (and allowing high decimal precision e.g. .214 -- meaningfulness of that precision (if it is actually useful at all) may depend on the video length).
+# - $3, $4, $5 etc. OPTIONAL. Additional percent times to export frames from, as in $2. You may provide any number of these, separated by spaces.
+# Example that will export a frame from 86 percent into the source video inputVideo.mp4:
 #    ffmpegRipVideoFrameSS.sh inputVideo.mp4 .86
-
-# KNOWN ISSUES
-# Copies the whole input video to a temp file; inefficient.
+# Example that will export frames from 10 percent, 20 percent, 86 percent, and 95 percent of the same:
+#    ffmpegRipVideoFrameSS.sh inputVideo.mp4 0.1 0.2 .86 0.95
 
 
 # CODE
-# TO DO
-# Make a temporary junction (without funky characters in the file name, so that ffprobe can work on the file) to the input file, and work from the junction?
-# Name the output .jpg image after the video file
-# Random frame selection if $2 not provided
-# Log $2 to a file for future reference
-# Kludge for titles having spaces in names:
-cp "$1" ./tmp_bbE9pWyXSVshTm.mp4
-eval "$(ffprobe -v error -of flat=s=_ -show_entries format=duration tmp_bbE9pWyXSVshTm.mp4)"
-	# Stupid data wrangling necessary on Windows (which produces windows newlines that muck up intended functionality of the Cygwin echo command:
-	echo $format_duration > tmp_eHmZQ2YtWKr8ZV7YpU3MUn3nrMV2tPT8Ge.txt
-# NOTE: THE *0.9 means 90 percent, which is what bc will multiply the total seconds of the video by, which will give us a time (in seconds) ninety percent into the video, from which a later ffmpeg command will grab a frame from:
-	echo *$2 >> tmp_eHmZQ2YtWKr8ZV7YpU3MUn3nrMV2tPT8Ge.txt
-	dos2unix tmp_eHmZQ2YtWKr8ZV7YpU3MUn3nrMV2tPT8Ge.txt
-	format_duration=$(tr -d '\n' < tmp_eHmZQ2YtWKr8ZV7YpU3MUn3nrMV2tPT8Ge.txt)
-	rm tmp_eHmZQ2YtWKr8ZV7YpU3MUn3nrMV2tPT8Ge.txt
-	# WORKING bc command example from another script:
-	# echo "scale=5; $xPix / $yPix" | bc
-selectSecond=$(echo "scale=0; $format_duration" | bc)
+if [ "$1" ]; then inputFile=$1; else printf "\nNo parameter \$1 (input file name) passed to script. Exit."; exit 1; fi
+if [ ! "$2" ]; then echo "\nNo parameter \$2 (percent of time of video to get frame from) passed to script. Exit."; exit 2; fi
 
-# re: https://stackoverflow.com/a/1198191/1397555
-ffmpeg -ss $selectSecond -i tmp_bbE9pWyXSVshTm.mp4 -crf 1 -frames:v 1 oot.jpg
-rm tmp_bbE9pWyXSVshTm.mp4
+# copy arguments to script to array:
+
+OIFS="$IFS"
+IFS=$'\n'
+arrayOfPercentTimes=("$@")
+arrayOfPercentTimes=(${arrayOfPercentTimes[@]:1})
+IFS="$OIFS"
+
+for percentTimeToExportFrameFrom in ${arrayOfPercentTimes[@]}
+do
+	# get duration of video:
+	inputMediaDuration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $inputFile)
+	selectSecond=$(echo "scale=-0; $inputMediaDuration * $percentTimeToExportFrameFrom" | bc)
+	echo "$percentTimeToExportFrameFrom percent of $inputMediaDuration is $selectSecond; exporting from at the time from source file $inputFile . . ."
+	outputFileSSstring=$(tr '.' '-' <<< $selectSecond)
+	targetFileName=${inputFile%.*}_s"$outputFileSSstring".png
+
+	# re: https://stackoverflow.com/a/1198191/1397555
+	ffmpeg -y -ss $selectSecond -i $inputFile -frames:v 1 $targetFileName
+done
