91dc91e1a9409789bc89bc87fdc452eb184f231f
earthbound19@users.noreply.github.com <earthbound19@users.noreply.github.com>
2023-09-28 01:36:00 -0600

    change comments to help future self avoid moar problem

--- scripts/imgAndVideo/color-thief-jimp-palette.js
+++ scripts/imgAndVideo/color-thief-jimp-palette.js
@@ -2,12 +2,14 @@
 // Intended for CLI use. Extracts dominant color and a palette of an arbitrary number of colors from an image, printing to stdout.
 
 // USAGE
-// Run with two parameters:
-// process.argv[2] <./path_to_image_to_process.png>
-// process.argv[3] <number of colors to extract for palette>.
-// For example, substituting /path/to/this with the actual path to this script on your system:
-//    node /path/to/this/color-thief-jimp-palette.js input.png 12
-
+// Run from the same directory with the image you wish to obtain a palette, using these parameters:
+//    process.argv[2] source image file name, e.g. _EXPORTED_2023-07-19zb_v04.png
+//    process.argv[3] integer number of colors to extract for palette
+// For example, if you have the file tst1.png in the same directory you run this script from, and you want to extract 4 colors, run:
+//    node /full_path/to/color-thief-jimp-palette.js tst1.png 4
+// NOTES
+// - it will error out if you try to extract only 1 color.
+// - it doesn't seem to return the requested number of colors consistently if you request 4 or fewer; you may get 3 or 4 colors on requesting 2, for example.
 
 // CODE
 var ColorThief = require('color-thief-jimp');
