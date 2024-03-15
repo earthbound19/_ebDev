d0ee201f6927b6a28560648ce9eaf7e5330de673
Alex Hall <earthbound19@users.noreply.github.com>
2024-03-14 21:57:20 -0600

    upd. doc re called script

--- scripts/imgAndVideo/imgs2imgsNN.sh
+++ scripts/imgAndVideo/imgs2imgsNN.sh
@@ -5,8 +5,8 @@
 # This script uses the same parameters as `img2imgNN.sh`, EXCEPT that parameter $1 is a file type instead of a specific file. All files of type $1 will be passed to imgs2imgsNN.sh:
 # - $1 source file type
 # - $2 destination format
-# - $3 scale by nearest neighbor method to pixels X
-# - $4 OPTIONAL. Force this Y-dimension, regardless of aspect. Scales by nearest neighbor method to pixels Y. ONLY USED for ppms. Ignored for all other types (aspect kept). SEE COMMENTS in i_view32.exe code lines area for options to maintain aspect and/or rotate image (wanted for my purposes at times).
+# - $3 scale by nearest neighbor method to this many pixels _in the longest dimension_ (whether that is X or Y). If the source image is the same dimension in X and Y, just use either.
+# - $4 OPTIONAL. Force this dimension _for the shorter side_, regardless of aspect. Scales by nearest neighbor method to this many pixels for the shortest edge, even if that forces a different aspect (including making that side longer). If omitted, the shortest edge is calculated automatically to maintain aspect.
 # Example command:
 #    imgs2imgsnn.sh ppm png 640
 # OR, to force a given x by y dimension for a ppm:
