1284beaa1e1b99232512da050fe0b7dd5993fb0a
Alex Hall <earthbound19@users.noreply.github.com>
2024-04-12 19:58:11 -0600

    new features / tweaks: - delete outdated (now erroneous) statement from doc - add env array named randomVerticalColorStripsOutputFileNames which may be used by a script calling with `source` to operate on the output file names; doc this - warn an exit if the source palette only has one color - don't use the same color twice in a row - change output file name format to have palette name first

--- scripts/imgAndVideo/randomVerticalColorStripes.sh
+++ scripts/imgAndVideo/randomVerticalColorStripes.sh
@@ -9,10 +9,14 @@
 # - $1 REQUIRED. The minimum number vertical color stripes to make (before* image upscale).
 # - $2 REQUIRED. The maximum number "
 # - $3 REQUIRED. How many such images to make
-# - $4 REQUIRED. The name of a file list of hex color values to randomly pick from, findable in any of the directories or sub-directories of a path searched by `findPalette.sh` (see documentation in taht script). If not provided, every stripe is a pseudo-randomly generated color (the color created from entropy at run time). Random color creation isn't recommended; using a good palette is.
+# - $4 REQUIRED. The name of a file list of hex color values to randomly pick from, findable in any of the directories or sub-directories of a path searched by `findPalette.sh` (see documentation in taht script).
 # Example that will produce minimum 3 vertical stripes, maximum 80, and 5 such images, from the palette sparkleHeartHexColors.hexplt:
 #    randomVerticalColorStripes.sh 3 80 5 sparkleHeartHexColors.hexplt
+# To make use of an array of output file names this script creates, see NOTES, just below.
 # NOTES
+# - This script creates an array of output file names which may be used by another script that calls it. The array name is randomVerticalColorStripsOutputFileNames. To get that array in memory for a script that calls it, invoke this script with `source` before it, e.g.:
+#    source randomVerticalColorStripes.sh 3 80 5 sparkleHeartHexColors.hexplt
+# -- and then you may either iterate over the array as ${randomVerticalColorStripsOutputFileNames[@]} or access its elements by index, e.g. ${randomVerticalColorStripsOutputFileNames[2]}
 # - See imgs2imgsNN.sh to resize results to an arbitrary size by nearest neighbor method (preserves hard edges).
 # - The number of and purpose of positional parameters has altered through this script's development history. If you developed a script that uses this script, and it isn't working, you may want to re-examine the parameters help above and adapt as needed. (It used to have a parameter to randomly vary min. number of columns, which is redundant. You can vary that . . . by varying the min number to begin with.)
 # - A previous version of this script had an option to randomly generate colors in sRGB color space, but that's going to give you generally not aesthetically pleasing results, so I removed the option, to require deliberate (though random pick) color choices from a palette.
@@ -45,28 +49,16 @@ then
 		# grep command filters out any comments etc. from file and retrieves only the sRGB hex values to an array:
 		hexColorsArray=( $(grep -i -o '#[0-9a-f]\{6\}' $hexColorSrcFullPath) )		# tr command removes
 		sizeOf_hexColorsArray=${#hexColorsArray[@]}
+		if [[ $sizeOf_hexColorsArray == 1 ]]; then echo "WARNING: only 1 color in source palette $hexColorSrcFullPath. Skipping (it would only result in a solid color fill). Exit."; exit 4; fi
 		sizeOf_hexColorsArray=$(( $sizeOf_hexColorsArray - 1))		# Else we get an out of range error for the zero-based index of arrays.
 	else
 		printf "\nNo parameter \$4 (name of source palette file name). See documentation in the comments of findPalette.sh for help. Exit."; exit 1
 	fi
 fi
-	# DEV SCRAPS; POSSIBLY TO DO:
-	# ? Pregenerate a long string which is so many random hex colors smushed together (without any delimiters), to pull hex colors from in chuncks of six characters for greater efficiency; re: http://stackoverflow.com/a/1405641
-	# BUT ADAPT THIS TO USE COLORS FROM THE PROVIDED HEX PALETTE.
-	# WUT FIX VAR NAMES
-	# numRandomCharsToGet=$(echo $(( arrSize * getNrandChars )))
-		# echo numRandomCharsToGet val is $numRandomCharsToGet
-	# randomCharsString=$(cat /dev/urandom | tr -cd 'a-km-np-zA-KM-NP-Z2-9' | head -c $numRandomCharsToGet)
-		# echo randomCharsString val is $randomCharsString
-	# SAME/SIMILAR (?) IN DEVELOPMENT NOTES:
-	# Initialize counter at negative the number of getNrandChars, so that the first iteration in the following loop will set it to 0, which is where we need it to start:
-	# multCounter=-$getNrandChars
-		# echo multCounter val is $multCounter
-	# for filename in ${array[@]}
-	# do
-			# multCounter=$(($multCounter + $getNrandChars))
-			# newFileBaseName=${randomCharsString:$multCounter:$getNrandChars}
 
+# create an empty array which we will populate with the generated output file names as we go:
+randomVerticalColorStripsOutputFileNames=()
+prevPick=''
 for imgNum in $( seq $howManyImages )
 do
 	howManyStripes=$(shuf -i $minColorColumnRepeat-$maxColorColumnRepeat -n 1)
@@ -75,12 +67,22 @@ do
 	for i in $( seq $howManyStripes )
 	do
 		echo Generating stripe $i of $howManyStripes for image $imgNum of $howManyImages . . .
-		repeatColumnColorCount=$(shuf -i $minColorColumnRepeat-$maxColorColumnRepeat -n 1)
+		# set number of times a color repeats (making a wider column of that color), repeatColumnColorCount :
+		repeatMaxRange=$((maxColorColumnRepeat * 50))
+		repeatColumnColorCount=$(shuf -i $minColorColumnRepeat-$repeatMaxRange -n 1)
 		# (re)initialize empty tmp constructiong string to append new color columns to:
 		tmpSTR=
 			# Pick a random color from a hex color list if such a list is specified (converting to RGB along the way); otherwise pick completely random RGB values.
 			pick=$(shuf -i 0-"$sizeOf_hexColorsArray" -n 1)
+			# if that is that same as prevPick, pick a random new color in a while loop until they are different
+			while [[ $pick == $prevPick ]]
+			do
+				# echo PICKING NEW RND
+				pick=$(shuf -i 0-"$sizeOf_hexColorsArray" -n 1)
+			done
+			prevPick=$pick
 			hex="${hexColorsArray[$pick]}"
+			# copy that to a prevHex val to compare on next loop iteration to avoid random repeat of a color:
 			# Strip the (text format required) # symbol off that. Via yet another genius breath yon: http://Unix.stackexchange.com/a/104887
 			hex=${hex//#/}
 			# Pick a number of times to repeat that chosen hex color, then write it that number of times to the temp file that will make up the eventual .ppm file: 
@@ -112,8 +114,10 @@ $count 1
 		# Format number of colors (stripes) in image to four digits. Yeah it would be wild to ever pad to that, but for file sorting reasons I want it.
 		padDigitsTo=4
 		stripesPaddedNum=$(printf "%0""$padDigitsTo""d\n" $howManyStripes)
-	ppmFileName="$imgNumPadded"_"$stripesPaddedNum"x1_stripes_"$paletteFileBaseName"_"$timestamp"
+	ppmFileName="$paletteFileBaseName"__"$imgNumPadded"_"$stripesPaddedNum"x1_stripes__"$timestamp"
 	cat ppmheader.txt ppmBody.txt > $ppmFileName.ppm
 	echo wrote new ppm file $ppmFileName.ppm
+	# add that filename to the array:
+	randomVerticalColorStripsOutputFileNames+=($ppmFileName.ppm)
 	rm ppmheader.txt ppmBody.txt
 done
