b9e5b502a45bb7dc98d1164a36ae6e5bc07245c2
Alex Hall <earthbound19@users.noreply.github.com>
2024-04-20 02:30:43 -0600

    - update to use interpolateTwoSRGBColors_coloraide.py instead of get_color_gradient_OKLAB.js - bug fix: doesn't operate on palettes that include the string *augmented* (as it was intended not to but that was broken; it's fixed)

--- scripts/imgAndVideo/augmentPalettesGrid.sh
+++ scripts/imgAndVideo/augmentPalettesGrid.sh
@@ -6,8 +6,10 @@
 # This is done by a process that uses all .hexplt format palettes in the current directory (other than any with output file names made by this script). The result is written to a .hexplt file in a grid and with markup declaring number of columns and rows. The result file is named after the containing folder you run the script from, and the columns x rows. Source palettes may have color codes arranged in any way and include comments; the only requirement is that sRGB hex color codes in the palette are prefixed with a number/hex/pound # symbol and are separated by white space.
 
 # DEPENDENCIES
-# `get_color_gradient_OKLAB.js` (and nodeJS and the packages that script requires), `getFullPathToFile.sh`
-# GNU datamash (binaries available for various platforms: https://www.gnu.org/software/datamash/) in your PATH
+# - `interpolateTwoSRGBColors_coloraide.py` and its dependencies
+# - `getFullPathToFile.sh`
+# - GNU datamash (binaries available for various platforms: https://www.gnu.org/software/datamash/) in your PATH
+# DEPRECATED DEPENDENCY that could still be used with some hacking:# `get_color_gradient_OKLAB.js` (and nodeJS and the packages that script requires)
 
 # WARNING
 # Deletes any pre-exisisting folders and files with the pattern _augmentPalettes* before doing its work.
@@ -15,14 +17,17 @@
 # USAGE
 # - copy palettes you wish to make a multi-augment grid from into their own dedicated operating folder.
 # - from that folder, run this script with these parameters:
-# - $1 how many linearly interpolated colors to insert between each color (column) in palette files.
-# - $1 how many linearly interpolated colors to insert between each row of colors, between the $1-interpolated palette files.
+# - $1 REQUIRED. How many linearly interpolated colors to insert between each color (column) in palette files.
+# - $2 REQUIRED. How many linearly interpolated colors to insert between each row of colors, between the $1-interpolated palette files.
+# - $3 OPTIONAL. An identifier string for The colorspace through which to interpolate colors. Default 'hct' if omitted. Any colorpsace supported by the coloraide library's steps (interpolation) function may be given. Notable options include 'oklab' and 'oklch'. See https://facelessuser.github.io/coloraide/colors/
 # For example, to interpolate 5 colors horizontally and vertically over all .hexplt files in the current directory, run:
 #    augmentPalettesGrid.sh 5 5
 # Or to insert 3 colors horizonatallybetween columns), and 5 colors vertically (between rows):
 #    augmentPalettesGrid.sh 3 5
 # Or to do no vertical interpolation (only use the original colors with no added colors in between), and insert 5 colors horizontally between columns:
 #    augmentPalettesGrid.sh 5 0
+# To interpolate 5 colors horizontally and vertically in oklab space instead of the default hct, run:
+#    augmentPalettesGrid.sh 5 5 oklab
 # You could pass 0 for both $1 and $2 to make a grid of all palettes, but then you would just want to use `catHexpltsGrid.sh`, which will do that more straightforwardly (and probably more efficiently).
 # NOTES
 # - you can run this in a directory where you have created palettes from it which have the regex pattern `_Augmented-.*_grid` in their file name, and it will not operate on those files (it will skip them). You can therefore reuse this script in the same directory easily, passing it different parameters each time.
@@ -40,27 +45,33 @@
 # CODE
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (how many steps to interpolate between each column (color) in the source palettes) passed to script. Exit."; exit 1; else columnsInterpolateOrigParam=$1; fi
 if [ ! "$2" ]; then printf "\nNo parameter \$2 (how many steps to interpolate between each row from one source palette to the next) passed to script. Exit."; exit 2; else rowsInterpolateOrigParam=$2; fi
+# Set default color space HCT if no parameter $3 says otherwise:
+if [ "$3" ]; then interpolationSpace=$3; else interpolationSpace='hct'; fi
 
-# Because each new interpolation iteration will start with the same color as the end color of the previous interpolation, we're going to remove the tail color of each interpolation (iteration) via the `-l 1` switch. That means $((N - 1)). ALSO, the understood literal intent of interpolation in this documentation is _how many additional colors in between_, which means (start color + inserted colors + end color), which means $((N + 2)). Summing that, it's $((N - 1 + 2)) = $((N + 3)). SO:
+# For a very long time I had a comment which I think offered an explanation with an erred or different conclusaion than the following used numbers? But, at least, the understood intent of interpolation in this documentation is _how many additional colors in between_, which means:
+#    (start color + inserted colors + end color)
+# Which has something to do with setting the following interpolation steps + 2? Maybe because we're going to generate but trim off the start and end colors of the interpolation, as they are the original colors (not the interpolated ones)? Anyway the math is:
 colsInterpolationSteps=$(($columnsInterpolateOrigParam + 2))
 rowsInterpolationSteps=$(($rowsInterpolateOrigParam + 2))
 # (This process will need to tack that last removed color back on after everything is removed.)
 
-# set local environment variable fullPathToOKLABAugmentationScript:
-fullPathToOKLABAugmentationScript=$(getFullPathToFile.sh get_color_gradient_OKLAB.js)
+# set local environment variable fullPathToInterpolationScript:
+	# DEPRECATED, as interpolateTwoSRGBColors_coloraide.py does the same thing but can do it in various color spaces:
+	# fullPathToInterpolationScript=$(getFullPathToFile.sh get_color_gradient_OKLAB.js)
+fullPathToInterpolationScript=$(getFullPathToFile.sh interpolateTwoSRGBColors_coloraide.py)
 # return with error if that's empty:
-if [[ "$fullPathToOKLABAugmentationScript" == "" ]]; then echo "ERROR: could not find script $scriptName in \$PATH. Exit."; exit 2; fi
-# echo "Found $scriptName at $fullPathToOKLABAugmentationScript -- will use that."
+if [[ "$fullPathToInterpolationScript" == "" ]]; then echo "ERROR: could not find script interpolateTwoSRGBColors_coloraide.py in \$PATH. Exit. From $0"; exit 2; fi
 
 # get directory name without path:
 currentDirNoPath=$(basename $(pwd))
 # build target file name from that:
-outputFileName="$currentDirNoPath"_augmented-"$columnsInterpolateOrigParam"x"$rowsInterpolateOrigParam"-grid.hexplt
+outputFileName="$currentDirNoPath"_Augmented-"$interpolationSpace"-"$columnsInterpolateOrigParam"x"$rowsInterpolateOrigParam"-grid.hexplt
 
 # horrifyingly large bash function; augments all .hexplt files in the current directory; REQUIRES TWO PARAMETERS (effectively $1 and $2), which are, respectively, the target file to write all augmented palette lines to, and the number of augmentation steps:
 augment_palettes () {
 	printf "" > $1
-	hexplts=($(find . -maxdepth 1 -type f -name \*.hexplt -printf "%f\n"))
+	# excluding any augmented files via ! -path, re: https://superuser.com/a/397325 -- changing to -ipath for case-insensitive search:
+	hexplts=($(find . -maxdepth 1 -type f -iname \*.hexplt ! -ipath '*augmented*' -printf "%f\n"))
 	for hexplt in ${hexplts[@]}
 	do
 	# only do things if the file name of the palette doesn't indicate that it is a grid (possibly from augmentation or concatenation) palette; to avoid broken things / unintended work, and to allow re-running this script where such a palette aleady exists, but with different parameters; print any results from grep search to null so as not to distract pointlessly:
@@ -84,12 +95,13 @@ augment_palettes () {
 				# echo "idx $counter, element \$color $color\: $thisElement, +1 element $nextElement"
 			# get the interpolated color list via call of interpolation script -- note the -l 1 parameter, which remove the last color of the augmentation! -- and put it in an array:
 			augmentedPaletteSegment=(
-			$(node $fullPathToOKLABAugmentationScript \
-			-s $thisElement \
-			-e $nextElement \
-			-n $2 \
-			-l 1 \
-			)
+				$(python $fullPathToInterpolationScript \
+				-s $thisElement \
+				-e $nextElement \
+				-n $2 \
+				-l 1 \
+				-c $interpolationSpace
+				)
 			)
 			# add those to master augmented palette:
 			for augmentedColor in ${augmentedPaletteSegment[@]}
@@ -106,6 +118,8 @@ augment_palettes () {
 		# print completed augmented palette:
 		for color in ${augmentedPalette[@]}
 		do
+			# RE: https://github.com/earthbound19/_ebDev/issues/6#issuecomment-683808878 - the doom of needing to trim the windows newline on the next line was had 2024/04/20 02:25:08 :
+			color=${color//[$'\t\r\n']}
 			printf "$color " >> $1
 		done
 		printf "\n" >> $1
