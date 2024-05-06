25ee60f1ea4b68bdd8ed39f84513bd5128c203eb
Alex Hall <earthbound19@users.noreply.github.com>
2024-05-05 21:27:04 -0600

    - major upgrade: control many options as named parameters - add -sALL parameter option to retrieve a list of all production palettes from _ebPalettes and render an image from each - bug fix: in some circumstances composite final images would not save. fixed

--- scripts/recipes/PNGofRowsOfRandomVerticalColorStripes.sh
+++ scripts/recipes/PNGofRowsOfRandomVerticalColorStripes.sh
@@ -1,64 +1,161 @@
 # DESCRIPTION
-# Uses other scripts to make many rows of various numbers of vertical color stripes from a randomly chosen palette (from _ebPalettes). Alternately can use a specified palette name ($1).
+# Uses other scripts to make many rows of various numbers of vertical color stripes from a randomly chosen palette (from _ebPalettes). Alternately can use a specified palette name ($1). It does these things admittedly relatively extremely inefficiently.
 
 # DEPENDENCIES
-# `printContentsOfRandomPalette_ls.sh` or `findPalette.sh`, `randomVerticalColorStripes.sh`, `imgs2imgsNN.sh`, `renumberFiles.sh`, and everything they may rely on.
+# `findPalette.sh`, `getRandomPaletteFileName.sh`, `printAllPaletteFileNames.sh`, `randomVerticalColorStripes.sh`, `imgs2imgsNN.sh`, `renumberFiles.sh`, everything they may rely on, and 7z CLI to archive source .ppm files.
 
 # USAGE
-# Run with these parameters:
-# - $1 OPTIONAL the name of a .hexplt file from the _ebPalettes repository (it will be located via findPalette.sh). If not provided, one is randomly chosen via printContentsOfRandomPalette_ls.sh.
-# For example, to generate an image using a randomly selected palette, run:
-#    PNGofRowsOfRandomVerticalColorStripes.sh
-# Or to generate an image from colors in the palette Clairvoyant.hexplt, run:
-#    PNGofRowsOfRandomVerticalColorStripes.sh Clairvoyant.hexplt
-# A few (IMO, possibly not humble) great palettes to try for parameter $1 are:
-#    08_Faded_Lavender_to_Faded_Yellow_Yellow_Orange_Flower.hexplt
-#    07_Dusty_Periwinkle_Blue_to_Yellow_Orange_Flower.hexplt
-#    Banana_Split.hexplt
-#    Clairvoyant.hexplt
-# TO DO
-# Parameterize min and max stripes, number of rows, and x and Y dimensions of target image.
+# Run this script with the -h or --help switch for parameters and usage examples:
+#    PNGofRowsOfRandomVerticalColorStripes.sh -h
 
+# OTHER NOTES
+# Not enough room for this in printed help anymore really, but:
+# A few palettes to try for --sourcepalettefilename that may be great are:
+   # 08_Faded_Lavender_to_Faded_Yellow_Yellow_Orange_Flower.hexplt
+   # 07_Dusty_Periwinkle_Blue_to_Yellow_Orange_Flower.hexplt
+   # Banana_Split.hexplt
+   # The_Mystic.hexplt
 
 # CODE
-if [ "$1" ]
+# TO DO
+# Make multiplier parameter for variant rows with math supporting something like the second randomVerticalColorStripes.sh call here:
+    # second ppm batch variant parameters with math supporting this:
+    # REFERENCE FOR PARAMETERS THAT WERE HARD-CODED:
+    # randomVerticalColorStripes.sh 3 22 26 $sourcePaletteFileName
+    # randomVerticalColorStripes.sh 23 111 12 $sourcePaletteFileName
+function print_halp {
+echo "
+USAGE
+Run with these parameters, all of optional:
+    [-h|--help] overrides all other script parameters to print usage help text and exit the script.
+    [-s|--sourcepalettefilename] file name of a .hexplt palette to use (from the _ebPalettes repository /palettes subfolder). If not provided, a random one will be chosen. If provided as the keyword ALL, images will be made with the provided parameters from every palette from that subfolder.
+    [-m|--minstripes] integer. Minimum random number of vertical stripes (columns) per row. If omitted a default will be used.
+    [-n|--maxstripes] integer. Maximum random number of vertical stripes (columns) per row. If omitted a default will be used.
+    [-r|--rows] integer. Number of rows. If omitted a default will be used.
+    [-x|--xdimension] integer. Number of pixels across of result PNG (x dimension). If omitted a default will be used.
+    [-y|--ydimension] integer. Number of pixels down of result PNG (y dimension). If omitted a default will be used.
+
+EXAMPLES
+To generate an image using a randomly selected palette and all other defaults, run:
+    PNGofRowsOfRandomVerticalColorStripes.sh
+Or to generate an image from colors in the palette The_Mystic.hexplt, run:
+   PNGofRowsOfRandomVerticalColorStripes.sh -sThe_Mystic.hexplt
+Note that it requires no space between the option letter and the parameter, e.g. instead of -s The_Mystic.hexplt that uses -sThe_Mystic.hexplt.
+To have minimum 4 columns and maximum 12, using The_Mystic.hexplt as a source palette, run:
+   PNGofRowsOfRandomVerticalColorStripes.sh -sThe_Mystic.hexplt -m4 -n12
+To additionally specify 20 total rows and dimensions of 640 accross and 480 down, run:
+   PNGofRowsOfRandomVerticalColorStripes.sh -sThe_Mystic.hexplt -m4 -n12 -r20 -x640 y480
+NOTES
+- Output file name format is <timestamp>_<paletteFileBaseNameNoExt>_RORVCS.png. RORVCS is both a funny word and an acronym for Rows of Random Vertical Color Stripes.
+"
+}
+
+
+# NOTES:
+# - two colons means it can take one optional parameter.
+# Also, see MORE NOTES in the case switch below.
+# Also, from a script I saw it can be useful to get the name of the script:
+PROGNAME=$(basename $0)
+# -- and then use that with the --name argument of getopts:
+#    ARGS=`getopt -q --name "$PROGNAME" --long help,output:,verbose --options ho:v -- "$@"`
+OPTS=`getopt -o has::m::n::r::x::y:: --long help,sourcepalettefilename::,minstripes::,maxstripes::,rows::,xdimension::,ydimension:: -n $PROGNAME -- "$@"`
+
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+eval set -- "$OPTS"
+
+# SET ANY DEFAULTS that would be overriden by optional arguments here:
+	minStripes=3
+	maxStripes=22
+	rows=26
+	xDimension=1920
+	yDimension=1080
+    # [-h|--help] overrides all other script parameters to print usage help text and exit the script.
+    # [-s|--sourcepalettefilename] file name of a .hexplt palette to use (from the _ebPalettes repository /palettes subfolder). If not provided, a random one will be chosen. If provided as the keyword ALL, images will be made with the provided parameters from every palette from that subfolder.
+    # [-m|--minstripes] integer. Minimum random number of vertical stripes (columns) per row. If omitted a default will be used.
+    # [-n|--maxstripes] integer. Maximum random number of vertical stripes (columns) per row. If omitted a default will be used.
+    # [-r|--rows] integer. Number of rows. If omitted a default will be used.
+    # [-x|--xdimension] integer. Number of pixels across of result PNG (x dimension). If omitted a default will be used.
+    # [-y|--ydimension] integer. Number of pixels down of result PNG (y dimension). If omitted a default will be used.
+ARG_C=default_value
+# MORE NOTES: cases that operate on 2 words, for example '-b foo', should use the shift statement twice, to remove both used words from the option list, while options that only operate on one word, for example '-a', should use the shift statement once, to remove only the one used word from the option list.
+while true; do
+  case "$1" in
+    -h | --help ) print_halp; exit 0 ;;
+    -s | --sourcepalettefilename ) if [ "$2" == "" ]; then echo "WARNING: No value or a space (resulting in empty value) after optional parameter -s | --sourcepalettefilename. Pass a value without any space after -s (for example: -sThe_Mystic.hexplt), or else don't pass -c and a default value will be used for it. Exit."; exit 4; fi; sourcePaletteFileName=$2; shift; shift ;;
+    -m | --minstripes ) if [ "$2" == "" ]; then echo "WARNING: No value or a space (resulting in empty value) after optional parameter -m | --minstripes. Pass a value without any space after -m (for example: -m8), or else don't pass -m and a default value will be used for it. Exit."; exit 4; fi; minStripes=$2; shift; shift ;;
+    -n | --maxstripes ) if [ "$2" == "" ]; then echo "WARNING: No value or a space (resulting in empty value) after optional parameter -n | --maxstripes. Pass a value without any space after -n (for example: -n42), or else don't pass -n and a default value will be used for it. Exit."; exit 4; fi; maxStripes=$2; shift; shift ;;
+    -r | --rows ) if [ "$2" == "" ]; then echo "WARNING: No value or a space (resulting in empty value) after optional parameter -r | --rows. Pass a value without any space after -r (for example: -r64), or else don't pass -r and a default value will be used for it. Exit."; exit 4; fi; rows=$2; shift; shift ;;
+    -x | --xdimension ) if [ "$2" == "" ]; then echo "WARNING: No value or a space (resulting in empty value) after optional parameter -x | --xdimension. Pass a value without any space after -x (for example: -x1080), or else don't pass -x and a default value will be used for it. Exit."; exit 4; fi; xDimension=$2; shift; shift ;;
+    -y | --ydimension ) if [ "$2" == "" ]; then echo "WARNING: No value or a space (resulting in empty value) after optional parameter -y | --ydimension. Pass a value without any space after -y (for example: -y1920), or else don't pass -y and a default value will be used for it. Exit."; exit 4; fi; yDimension=$2; shift; shift ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+# dev test prints -- comment out in production:
+# echo sourcePaletteFileName is $sourcePaletteFileName
+# echo minStripes is $minStripes
+# echo maxStripes is $maxStripes
+# echo rows is $rows
+# echo xDimension is $xDimension
+# echo xDimension is $yDimension
+
+arrayOfPaletteFileNames=()
+if [ "$sourcePaletteFileName" ] && [ "$sourcePaletteFileName" != "ALL" ]
 then
 	# Get full path to a specific palette, using RNDpaletteFileName even though it's not random, and even though this is in some sense a waste of a call -- it's just so I can only change one of these two code lines up here :)
-	RNDpaletteFileName=$(findPalette.sh $1)
-	if [ "$RNDpaletteFileName" == "" ]; then echo "ERROR: palette file is empty. Check setup or usage of either printContentsOfRandomPalette_ls.sh or findPalette.sh, whichever you're using."; exit 1; fi
-else
-	# get file name of a random palette from palette repo; printContentsOfRandomPalette_ls.sh sets the variable $RNDpaletteFileName usable in this shell if I call it via `source`:
-	source printContentsOfRandomPalette_ls.sh &>/dev/null
+	RNDpaletteFileName=$(findPalette.sh $sourcePaletteFileName)
+	arrayOfPaletteFileNames+=("$RNDpaletteFileName")
+fi
+if [ "$sourcePaletteFileName" == "ALL" ]; then arrayOfPaletteFileNames=($(printAllPaletteFileNames.sh --fullpaths)); fi
+if [ -z "$sourcePaletteFileName" ]
+then
+	# get file name of a random palette from palette repo:
+	randomPaletteFileName=$(getRandomPaletteFileName.sh)
+	arrayOfPaletteFileNames+=($randomPaletteFileName)
 fi
 
-sourcePaletteFileName="${RNDpaletteFileName##*/}"
-paletteFileBaseNameNoExt=${sourcePaletteFileName%.*}
+if [ -z ${arrayOfPaletteFileNames[0]} ];
+then
+	echo "ERROR: problem locating or using palette file. Check 1) that you passed a correct palette file name. 2) that it is a valid (non-empty) palette, or findPalette.sh, whichever you're using."
+	exit 1
+fi
 
-timestamp=$(date +"%Y_%m_%d__%H_%M_%S")
-workBaseName="$timestamp"_"$paletteFileBaseNameNoExt"
-mkdir $workBaseName
-cd $workBaseName
-destPixX=1080
-destPixY=1920
-randomVerticalColorStripes.sh 3 22 26 $sourcePaletteFileName
-randomVerticalColorStripes.sh 23 111 12 $sourcePaletteFileName
-numPPMs=$(count.sh ppm)
-verticalTilesHeight=$(($destPixY / $numPPMs))
-imgs2imgsNN.sh ppm png 1080 $verticalTilesHeight
-renumberFiles.sh -e png
-# imgList=($(printFilesTypes.sh NEWEST_FIRST png))
-mkdir pngIntermediaries
-mv *.png ./pngIntermediaries
-mkdir ppm
-mv *.ppm ./ppm/
-	# DEPRECATED option:
-	# read -p "Do things with the images in ./pngIntermediaries if you wish, for example glitch them and/or do Filter Forge filters or digital painting on them. Or do nothing with them. Then, when you're ready for them to be assembled into a 1080x1920 montage, press any key and then ENTER: " USERINPUT
-# the 38 in x38 below is the sum of the images made earlier via randomVerticalColorStripes.sh:
-cd ./pngIntermediaries
-magick montage *.png -tile 1x38 -geometry +0+0 ../_FINAL_"$workBaseName".png
-cd ..
-rm -rf pngIntermediaries
-# archive ppm sources into .7z format file, then remove archived source folder:
-7z a ppm.7z ppm
-rm -rf ppm
-cd ..
+for sourcePaletteFileName in ${arrayOfPaletteFileNames[@]}
+do
+	fileNameNoPath="${sourcePaletteFileName##*/}"
+	paletteFileBaseNameNoExt=${fileNameNoPath%.*}
+
+	timestamp=$(date +"%Y_%m_%d__%H_%M_%S")
+	workBaseName="$timestamp"_"$paletteFileBaseNameNoExt"_RORVCS
+	mkdir $workBaseName
+	cd $workBaseName
+	# even though this script call wastes a file lookup the way that script is written now:
+	randomVerticalColorStripes.sh $minStripes $maxStripes $rows $fileNameNoPath
+	numPPMs=$(count.sh ppm)
+	verticalTilesHeight=$(($yDimension / $numPPMs))
+	imgs2imgsNN.sh ppm png $xDimension $verticalTilesHeight
+	renumberFiles.sh -e png
+	# imgList=($(printFilesTypes.sh NEWEST_FIRST png))
+	mkdir pngIntermediaries
+	mv *.png ./pngIntermediaries
+	mkdir ppm
+	mv *.ppm ./ppm/
+		# DEPRECATED option:
+		# read -p "Do things with the images in ./pngIntermediaries if you wish, for example glitch them and/or do Filter Forge filters or digital painting on them. Or do nothing with them. Then, when you're ready for them to be assembled into a 1080x1920 montage, press any key and then ENTER: " USERINPUT
+	# the 38 in x38 below is the sum of the images made earlier via randomVerticalColorStripes.sh:
+	cd ./pngIntermediaries
+	# because with long names magick tripped on writing the file one directory up (failed to write the file), write it here:
+	magick montage *.png -tile 1x"$numPPMs" -geometry +0+0 _FINAL_"$workBaseName".png
+	# .. and then move it up:
+	mv _FINAL_"$workBaseName".png ..
+	cd ..
+	rm -rf pngIntermediaries
+	# archive ppm sources into .7z format file, then remove archived source folder:
+	7z a ppm.7z ppm
+	rm -rf ppm
+	cd ..
+
+	echo DONE. Results are in folder $workBaseName.
+done
