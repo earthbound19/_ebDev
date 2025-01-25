b7e2e5e001fdc3141985017cf929c6ac2aea551b
Alex Hall <earthbound19@users.noreply.github.com>
2025-01-25 01:12:38 -0700

    large language-model assisted addition of functionality to preserve comments (anything after one color and before the next, associated with the first)

--- scripts/imgAndVideo/rgbHexColorSortInOkLab.js
+++ scripts/imgAndVideo/rgbHexColorSortInOkLab.js
@@ -1,5 +1,5 @@
 // DESCRIPTION
-// Takes an input sRGB hex color list (in .hexplt format) and sorts the colors in it by next nearest perceptual distance in the okLab color space. (Tries to group colors by most similar in a list.) Does not write the result to any file; you'll need to redirect to do that (see NOTES under USAGE). NOTE THAT THIS IS BLAZING FAST compared to the rgbHex ~sort.py scripts. However, depending on the size of the source colors palette, those may be fast enough, and afford use of other color spaces. SEE ALSO `sortSRGBhexColorsColoraide.py`
+// Takes an input sRGB hex color list (in .hexplt format) and sorts the colors in it by next nearest perceptual distance in the okLab color space. (Tries to group colors by most similar in a list.) Preserves any comments (anything after one color and before the next). Does not write the result to any file; you'll need to redirect to do that (see NOTES under USAGE). NOTE THAT THIS IS BLAZING FAST compared to the rgbHex ~sort.py scripts. However, depending on the size of the source colors palette, those may be fast enough, and afford use of other color spaces. SEE ALSO `sortSRGBhexColorsColoraide.py`
 
 // DEPENDENCIES
 // nodejs with the culori library installed.
@@ -14,7 +14,7 @@
 
 
 // CODE
-// DEV NOTE: OH HOW BADLY I WANTED TO FIND A PYTHON LIBRARY that works with okLab + does distance calculations (DeltaE) between colors and adapt one of the Python color sorting scripts to do that, because OH MY UNHOLY NACHOS OF WRATH this was so, so, SOOOOO much more difficult to implement in JavaScript. But no such Python library exists, that I could find. okLab is in development for colour-science as of winter 2021..
+// DEV NOTE: OH HOW BADLY I WANTED TO FIND A PYTHON LIBRARY that works with okLab + does distance calculations (DeltaE) between colors and adapt one of the Python color sorting scripts to do that, because OH MY UNHOLY NACHOS OF WRATH this was so, so, SOOOOO much more difficult to implement in JavaScript. But no such Python library exists, that I could find. okLab is in development for colour-science as of winter 2021.. EDIT, 2025: the Python coloraide library works with oklab (okLCH), and many other color spaces. JavaScript Culori is much faster, or I would have ditched this script because maintaining and updating this JavaScript for wants is harder :)
 
 // DEPENDENCIES (INCLUDES)
 // CommonJS export of culori, re: https://culorijs.org/guides/migration/
@@ -56,23 +56,31 @@ if(typeof options.startComparisonColor !== 'undefined') {
 	}
 }
 
-// get array of sRGB hex format colors from the file:
-const regexp = /#?[a-fA-F0-9]{6}/g;
+// get array of sRGB hex values and comments from input file:
+const regexp = /(#?[a-fA-F0-9]{6})([^\n]*)/g;
 const searchResults = [...inputFileContent.matchAll(regexp)];
-// init array of colors from file + regex search result:
+// init array of colors and comments from file + regex search result:
 var comparisonColorsArray = [];
 for (const element of searchResults) {
-  comparisonColorsArray.push(element[0]);
+  comparisonColorsArray.push({ color: element[1], comment: element[2] });
 }
 
 // If there is not a -k switch instructing to keep duplicate colors, remove duplicates from original list, but maintain order (keep all unique in the same order), re: https://stackoverflow.com/a/15868720/1397555
 if(typeof options.keepDuplicateColors == 'undefined') {
-  comparisonColorsArray = [ ... new Set(comparisonColorsArray) ];
+  const uniqueColors = new Set();
+  comparisonColorsArray = comparisonColorsArray.filter(item => {
+    if (uniqueColors.has(item.color)) {
+      return false;
+    } else {
+      uniqueColors.add(item.color);
+      return true;
+    }
+  });
 }
 
 // if the value of arbitraryStartCompareColor was changed from default empty string (''), because a valid sRGB hex color value for the -s option was passed to the script, add it to the start of the list; colors will therefore be sorted by first comparing to it; will remove it afterward:
 if (arbitraryStartCompareColor != '') {
-  comparisonColorsArray.unshift(arbitraryStartCompareColor);
+  comparisonColorsArray.unshift({ color: arbitraryStartCompareColor, comment: '' });
 }
 
 const comparisonColorsArrayLength = comparisonColorsArray.length
@@ -90,35 +98,35 @@ okLCHdistance = culori.differenceEuclidean(mode = 'oklch', weights = [1, 1, 1]);
 
 // init final sort list (as empty):
 finalSortedList = [];
-// Add first item to final list, as the first item will be the first in the original list; comparisonColorsArray[0].sRGBhex and searchResults[0][0] SHOULD both be the first color in the list (although arbitraryStartCompareColor can mess with that, but the result we intend will be the same):
+// Add first item to final list, as the first item will be the first in the original list; comparisonColorsArray[0].color and searchResults[0][0] SHOULD both be the first color in the list (although arbitraryStartCompareColor can mess with that, but the result we intend will be the same):
 finalSortedList.push(comparisonColorsArray[0]);
 
 while (finalSortedList.length < comparisonColorsArrayLength) {
-  var sRGB_hex_A = comparisonColorsArray[0];
+  var sRGB_hex_A = comparisonColorsArray[0].color;
   var sRGB_hex_B;
-  var okLCHval_A = okLCHconverter(comparisonColorsArray[0]);
+  var okLCHval_A = okLCHconverter(sRGB_hex_A);
   var okLCHval_B;
   var okDist = 58848;								// way beyond any distance that will be found.
   var lowestFoundDistanceForThisPair = 58849;		// "
-  var nearestFoundColorHEX;
+  var nearestFoundColor;
   // figuring out this i = 1 (not zero) was the final thing that finished this script:
   for (var i = 1; i < comparisonColorsArray.length; i++) {
-    sRGB_hex_B = comparisonColorsArray[i]
-    okLCHval_B = okLCHconverter(comparisonColorsArray[i]);
+    sRGB_hex_B = comparisonColorsArray[i].color;
+    okLCHval_B = okLCHconverter(sRGB_hex_B);
     okDist = okLCHdistance(okLCHval_A, okLCHval_B);
     if (okDist < lowestFoundDistanceForThisPair) {
       lowestFoundDistanceForThisPair = okDist;
-      nearestFoundColorHEX = comparisonColorsArray[i];
+      nearestFoundColor = comparisonColorsArray[i];
     }
   }
-  finalSortedList.push(nearestFoundColorHEX);
-  // recreate comparisonColorsArray with sRGB_hex_A removed and nearestFoundColorHEX moved to start:
+  finalSortedList.push(nearestFoundColor);
+  // recreate comparisonColorsArray with sRGB_hex_A removed and nearestFoundColor moved to start:
   // remove sRGB_hex_A; re this horror: https://stackoverflow.com/a/20690490/1397555 :
-  comparisonColorsArray = comparisonColorsArray.filter(item => item !== sRGB_hex_A);
-  // remove nearestFoundColorHEX:
-  comparisonColorsArray = comparisonColorsArray.filter(item => item !== nearestFoundColorHEX);
-  // add nearestFoundColorHEX to start:
-  comparisonColorsArray.unshift(nearestFoundColorHEX);
+  comparisonColorsArray = comparisonColorsArray.filter(item => item.color !== sRGB_hex_A);
+  // remove nearestFoundColor:
+  comparisonColorsArray = comparisonColorsArray.filter(item => item.color !== nearestFoundColor.color);
+  // add nearestFoundColor to start:
+  comparisonColorsArray.unshift(nearestFoundColor);
 }
 
 // if the value of arbitraryStartCompareColor was changed from default empty string (''), because a valid sRGB hex color value for the -s option was passed to the script, we earlier added arbitraryStartCompareColor to the list; in that case remove it now from the final list (it will be the first item in the list):
@@ -128,29 +136,31 @@ if (arbitraryStartCompareColor != '') {
 
 // If the -k switch is used, ensure duplicates are preserved in the final sorted list
 if (typeof options.keepDuplicateColors !== 'undefined') {
-  const originalColorsArray = [...inputFileContent.matchAll(regexp)].map(match => match[0]);
+  const originalColorsArray = [...inputFileContent.matchAll(regexp)].map(match => ({ color: match[1], comment: match[2] }));
   const finalSortedListWithDuplicates = [];
   const colorCountMap = {};
 
   // Create a map to count occurrences of each color in the original list
-  originalColorsArray.forEach(color => {
-    if (colorCountMap[color]) {
-      colorCountMap[color]++;
+  originalColorsArray.forEach(item => {
+    if (colorCountMap[item.color]) {
+      colorCountMap[item.color].push(item.comment);
     } else {
-      colorCountMap[color] = 1;
+      colorCountMap[item.color] = [item.comment];
     }
   });
 
   // Add colors to the final list with duplicates preserved
-  finalSortedList.forEach(color => {
-    const count = colorCountMap[color];
-    for (let i = 0; i < count; i++) {
-      finalSortedListWithDuplicates.push(color);
+  finalSortedList.forEach(item => {
+    const comments = colorCountMap[item.color];
+    if (comments) {
+      comments.forEach(comment => {
+        finalSortedListWithDuplicates.push({ color: item.color, comment: comment });
+      });
+      delete colorCountMap[item.color]; // Remove the color from the map once processed
     }
-    delete colorCountMap[color]; // Remove the color from the map once processed
   });
 
-  finalSortedListWithDuplicates.forEach(color => console.log(color));
+  finalSortedListWithDuplicates.forEach(item => console.log(item.color + item.comment));
 } else {
-  finalSortedList.forEach(color => console.log(color));
+  finalSortedList.forEach(item => console.log(item.color + item.comment));
 }
