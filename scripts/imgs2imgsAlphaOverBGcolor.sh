14e65206f8c750b336c6c5c6ff5f7be61c3e148d
Alex Hall <earthbound19@users.noreply.github.com>
2025-04-13 22:08:44 -0600

    new

---
+++ scripts/imgs2imgsAlphaOverBGcolor.sh
@@ -0,0 +1,42 @@
+# DESCRIPTION
+# Takes all images of type $1 in the current directory and overlays them with any transparency in them (e.g. if PNG or TGA type) over background color $2, saving to a file named after the source image and indicating the background color in the file name. Destination file will be the same as the source file. Optionally does this for all files of type $1 in all subfolders also.
+
+# DEPENDENCIES
+# Graphicsmagick
+
+# USAGE
+# Run with these parameters:
+# - $1 OPTIONAL. File types to operate on. Defaults to "png" if omitted.
+# - $2 OPTIONAL. Background color to overly on in sRGB hex color code, for example ffffff for white or ff0596 for a magenta-red (rose).
+# - $2 OPTIONAL. Anything, for example the word HEIRNSH, which will cause the script to operate on all files of type $1 recursively (in subfolders). If omitted, default off (will only operate on files in the current directory).
+# For example, to override the default png with tga and keep all other defaults, run:
+#    imgs2imgsAlphaOverBGcolor.sh tga
+# Or to override the default ffffff background color with rose, run:
+#    imgs2imgsAlphaOverBGcolor.sh tga ff0596
+# To do the same but with png files (not tga) and do so in all png files in the current folder and all subfolders, run:
+#    imgs2imgsAlphaOverBGcolor.sh png ff0596 HERNSHBLAUR
+# NOTE
+# This was only tested with png images but could potentially work with all image types that support alpha which graphicsmagick can work with.
+
+
+# CODE
+if [ "$1" ]; then searchFileType=$1; else echo "No parameter \$1 (file type to work on e.g. png) passed to script; defaulting to png."; searchFileType="png"; fi
+
+if [ "$2" ]; then backgroundColor=$2; else echo "No parameter \$2 (background color to layer source images with transparency over, in sRGB hex color code format e.g. ffffff; defaulting to ffffff."; backgroundColor="ffffff"; fi
+
+echo searchFileType is $searchFileType
+echo backgroundColor is $backgroundColor
+
+# if no parameter is passed for $3, default to nothing (which searches all subdirectories):
+if [ ! "$3" ]; then subDirSearchParam="-maxdepth 1"; fi
+
+filesList=( $(find . $subDirSearchParam -type f -iname "*.$searchFileType" -printf "%P\n" ) )
+
+# iterates over files list, creating a new image for each layering it with transparency over the background color to a new file:
+for file in ${filesList[@]}
+do
+	echo overlaying source file $file with transparency on background color $backgroundColor to target file name "${file%.*}_bg_$backgroundColor.${file##*.}" . .
+	gm convert "$file" -background "#$backgroundColor" -flatten "${file%.*}_bg_$backgroundColor.${file##*.}"
+done
+
+
