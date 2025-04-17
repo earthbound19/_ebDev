ede41f975ed312a105ea86a0f624688629952c2f
Alex Hall <earthbound19@users.noreply.github.com>
2025-04-16 21:42:58 -0600

    update to instead call new single call script repeatedly.

--- scripts/imgs2imgsAlphaOverBGcolor.sh
+++ scripts/imgs2imgsAlphaOverBGcolor.sh
@@ -1,15 +1,15 @@
 # DESCRIPTION
-# Takes all images of type $1 in the current directory and overlays them with any transparency in them (e.g. if PNG or TGA type) over background color $2, saving to a file named after the source image and indicating the background color in the file name. Destination file will be the same as the source file. Optionally does this for all files of type $1 in all subfolders also.
+# Takes all images of type $1 in the current directory and overlays them with any transparency in them (e.g. if PNG or TGA type) over background color $2, saving to a file named after the source image and indicating the background color in the file name. Destination file will be the same as the source file. Optionally does this for all files of type $1 in all subfolders also. Accomplished by repeated calls to the script img2imgAlphaOverBGcolor.sh.
 
 # DEPENDENCIES
-# Graphicsmagick
+# Graphicsmagick, img2imgAlphaOverBGcolor.sh.
 
 # USAGE
 # Run with these parameters:
-# - $1 OPTIONAL. Background color to overly on in sRGB hex color code, for example ffffff for white or ff0596 for a magenta-red (rose).
+# - $1 OPTIONAL. Background color to overly on in sRGB hex color code, for example ffffff for white or ff0596 for a magenta-red (rose). If omitted, a default is used (at this writing white ffffff).
 # - $2 OPTIONAL. File types to operate on. Defaults to "png" if omitted.
 # - $3 OPTIONAL. Anything, for example the word HEIRNSH, which will cause the script to operate on all files of type $1 recursively (in subfolders). If omitted, default off (will only operate on files in the current directory).
-# For example, to override the default ffffff background color with rose ($ff0596), run:
+# For example, to override the default ffffff background color with rose ($ff0596), and operate on all files of the default type (png), run:
 #    imgs2imgsAlphaOverBGcolor.sh ff0596
 # To override the default png with tga and keep all other defaults, run:
 #    imgs2imgsAlphaOverBGcolor.sh ff0596 tga
@@ -37,8 +37,9 @@ filesList=( $(find . $subDirSearchParam -type f -iname "*.$searchFileType" -prin
 # iterates over files list, creating a new image for each layering it with transparency over the background color to a new file:
 for file in ${filesList[@]}
 do
-	echo overlaying source file $file with transparency on background color $backgroundColor to target file name "${file%.*}_bg_$backgroundColor.${file##*.}" . .
-	gm convert "$file" -background "#$backgroundColor" -flatten "${file%.*}_bg_$backgroundColor.${file##*.}"
+	echo calling script img2imgAlphaOverBGcolor.sh with positional parameters $backgroundColor $file . .
+	img2imgAlphaOverBGcolor.sh $backgroundColor $file 
+	# gm convert "$file" -background "#$backgroundColor" -flatten "${file%.*}_bg_$backgroundColor.${file##*.}"
 done
 
 
