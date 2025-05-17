28d2699106577c0cd5a8d24696498cf73e60a811
Alex Hall <earthbound19@users.noreply.github.com>
2025-05-17 17:38:12 -0600

    fill out usage example that was accidentally left as a script stub - clarify doc

--- scripts/imgAndVideo/SVGsRandomColorReplace.sh
+++ scripts/imgAndVideo/SVGsRandomColorReplace.sh
@@ -1,12 +1,12 @@
 # DESCRIPTION
-# calls SVGrandomColorReplace.sh for every .svg file in the current directory, doing random fills from .hexplt file $1, replacing color $2 with random colors from $1. (Uses a new svg file for $1 for that script, passing $1 from this as $2 and $2 from this as $3.) Note that script is named SVGrandom~ where this script is SVGsRandom~.
+# calls SVGrandomColorReplace.sh for every .svg file in the current directory, doing random fills from .hexplt file $1, replacing color $2 with random colors from $1. Uses a new svg file for $1 for that script, passing $1 from this as $2 and $2 from this as $3. Note that script is named SVGrandom~ where this script is SVGsRandom~, and this script repeatedly calls that one.
 
 # USAGE
 # Run with these parameters:
 # - $1 REQUIRED. A flat text file list of hexadecimal RGB color codes, one per line, from which to choose random colors for this fill. NOTE: each hex color in the file must be preceded by #. Note that this is $2 in the called script.
 # - $2 OPTIONAL. RGB hex color code in format f800fc (six hex digits, no starting # symbol) to search and replace with random colors from $2. If omitted, the called script uses a default (at this writing ffffff). Note that this is $3 in the called script.
-# For example:
-# scriptFileName.sh parameterOne
+# An example that will randomly replace the color 919191 with a randomly chosen color from palette bg_rnd_fills.hexplt, for every .svg file in the current directory:
+#    SVGsRandomColorReplace.sh bg_rnd_fills.hexplt 919191
 
 
 # CODE
