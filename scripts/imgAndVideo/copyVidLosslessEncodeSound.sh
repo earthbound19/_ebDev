c4eb766af74108fda78f45b76fcaa53e892d3cf5
Alex Hall <earthbound19@users.noreply.github.com>
2025-06-07 18:47:53 -0600

    update to output mkv instead of mp4; hackable back to mp4, with comments on possible preference of switches for different containers

--- scripts/imgAndVideo/copyVidLosslessEncodeSound.sh
+++ scripts/imgAndVideo/copyVidLosslessEncodeSound.sh
@@ -14,13 +14,20 @@
 # CODE
 if [ ! "$1" ]; then printf "\nNo parameter \$1 (file name of input video) passed to script. Exit."; exit 1; else inputFile=$1; fi
 
-outputFileName=${inputFile%.*}_aacSound.mp4
+	# DEPRECATED, to avoid a weird situation where .MOV format would be needed to contain an M-JPEG stream with aac sound; whereas the mkv (Matroska) container can do that without complaint; however, at this writing, powers that be streaming / social media prefer mp4:
+	# outputFileName=${inputFile%.*}_aacSound.mp4
+outputFileName=${inputFile%.*}.mkv
 if [ -e $outputFileName ]
 then
 	echo "Output file name $outputFileName already exists; will not clobber; skip. To re-render it, rename or delete that file, and run this script with the same input file again."
 else
 	echo Converting $inputFile . . .
+		# HACKING COMMENTS FOR DIFFERENT CONTAINERS:
+		# IF YOU USE A .mov container, add -movflags +faststart
+		# IF YOU WANT A LOSSLESS MP4/H264 stream (but filesizes may baloon): ffmpeg -i input.avi -c:v libx264 -crf 0 -preset ultrafast -c:a aac output.mp4
+		# MKV MATROSKA with any dubious or old / experimental codecs: ADD -strict experimental
 	ffmpeg -i $inputFile -map 0:v -vcodec copy -map 0:a -acodec aac -crf 10 $outputFileName
+
 	# copy metadata from source file to render target; the script also updates target timestamp to match metadata media creation date:
 	copyMetadataFromSourceFileToTarget.sh $inputFile $outputFileName
 fi
