67344cd130d120cbe03fa730bf022bab5984bf3b
Alex Hall <earthbound19@users.noreply.github.com>
2026-02-06 05:51:36 -0700

    trivial typo fix, add comment

--- scripts/imgAndVideo/youtube-dl.sh
+++ scripts/imgAndVideo/youtube-dl.sh
@@ -4,8 +4,9 @@
 # USAGE
 # Run this script with one parameter, which is the URL to a video to download:
 #    youtube-dl.sh URL-to-video
+# OR TRY yt-dlp (e.g. yt-dlp -x 'URL-to-media'` to extract audio), which may work better and have more features.
 
 
 # CODE
 additionalParams="--audio-quality 0"
-youtube-dl --no-playlist --skip-unavailable-fragments --no-overwrites --write-description --all-formats --youtube-skip-dash-manifest --merge-output-format mp4 --extract-audio --audio-format best  --recode-video mp4 --keep-video $additionalParams $1
+youtube-dl --no-playlist --skip-unavailable-fragments --no-overwrites --write-description --all-formats --youtube-skip-dash-manifest --merge-output-format mp4 --extract-audio --audio-format best --recode-video mp4 --keep-video $additionalParams $1
