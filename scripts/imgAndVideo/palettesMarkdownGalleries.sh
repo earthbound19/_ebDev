b5897a0124cf3dbcc3aece8154412e86173ca808
Alex Hall <earthbound19@users.noreply.github.com>
2026-02-12 21:20:53 -0700

    trivial comment typo fix

--- scripts/imgAndVideo/palettesMarkdownGalleries.sh
+++ scripts/imgAndVideo/palettesMarkdownGalleries.sh
@@ -3,8 +3,7 @@
 
 
 # CODE
-# VASTLY simplified from previous incarnations of this script, and works recursively besides
-# (those didn't) :
+# VASTLY simplified from previous incarnations of this script, and works recursively besides (those didn't) :
 find . -type d -not -path "./.git*" -exec sh -c 'cd "{}" && current_dir=$(pwd) && echo working in path $current_dir . . . && palettesMarkdownGallery.sh' \;
 
 echo "DONE running palettesMarkdownGallery.sh in every directory in this path."
