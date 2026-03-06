9b14e8fd855f26ebb033a86448fda77a6822eb14
Alex Hall <earthbound19@users.noreply.github.com>
2026-03-06 01:18:31 -0700

    add option to print only filenames in subdirectories (without paths)

--- scripts/imgAndVideo/printColorCountsAllHexplts.sh
+++ scripts/imgAndVideo/printColorCountsAllHexplts.sh
@@ -6,10 +6,13 @@
 # USAGE
 # Run without any parameters:
 # - $1 OPTIONAL. Anything, for example the word SHRIOUSLY, which will cause the script to search for .hexplt files in all subdirectories (as well as the current directory). If omitted, the script searches only the current directory.
+# - $2 OPTIONAL. Anything, for example the word SHROISULY, which will cause any subdirectory filenames to be printed without the parent path to them (file name only, no path).
 # For example, to search and print .hexplt results from only the current directory, run:
 #    printColorCountsAllHexplts.sh
 # To search and print .hexplt results from the current directory and all subdirectories, run:
 #    printColorCountsAllHexplts.sh SHRIOUSLY
+# To do the same and print only file names in subdirectories (no paths), run:
+#    printColorCountsAllHexplts.sh SHRIOUSLY SHROISULY
 
 
 # CODE
@@ -23,5 +26,10 @@ for paletteFileName in ${palettes[@]}
 do
 	colorsArray=( $(grep -i -o '#[0-9a-f]\{6\}' $paletteFileName) )
 	arrayLength=${#colorsArray[@]}
+	# strip path from filename if paramater $2 was passed:
+	if [ "$2" ]
+	then
+		paletteFileName="${paletteFileName##*/}"
+	fi
 	printf "$arrayLength,$paletteFileName\n"
 done
